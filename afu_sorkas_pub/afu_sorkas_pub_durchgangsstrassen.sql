ea0ffdd0b425d52fbb702c22f7490a493b564116
barpastu <noemi.sturm@bd.so.ch>
2017-10-19 14:33:03 +0200

    Adding afu_sorkas_pub.durchgangsstrassen

---
+++ afu_sorkas_pub/afu_sorkas_pub_durchgangsstrassen.sql
@@ -0,0 +1,22 @@
+SELECT
+	gid,
+	achseid,
+	achsname,
+	achsnummer,
+	achstyp,
+	achstypnam,
+	eigentueme,
+	enddistanz,
+	laenge,
+	modified,
+	modifiedby,
+	segmentid,
+	segmentnr,
+	startdista,
+	ogc_fid,
+	durchg,
+	the_geom
+FROM
+	sorkas.durchgangsstrassen
+WHERE
+	"archive" = 0
