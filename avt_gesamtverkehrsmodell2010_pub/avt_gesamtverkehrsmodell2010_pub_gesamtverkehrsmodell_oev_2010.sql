b3272cf7f27c3e5728ea7e632bd3a052270acd42
barpastu <noemi.sturm@bd.so.ch>
2017-10-23 12:06:37 +0200

    adding avt_gesamtverkehrsmodell2010_pub.gesamtverkehrsmodell_oev_2010

---
+++ avt_gesamtverkehrsmodell2010_pub/avt_gesamtverkehrsmodell2010_pub_gesamtverkehrsmodell_oev_2010.sql
@@ -0,0 +1,13 @@
+SELECT 
+	wkb_geometry,
+	streckennummer,
+	dtv2010_bus_modulo,
+	dtv2010_bahn_modulo,
+	asp2010_bus_modulo,
+	asp2010_bahn_modulo,
+	msp2010_bus_modulo,
+	msp2010_bahn_modulo,
+	dwv2010_bus_modulo,
+	dwv2010_bahn_modulo
+FROM 
+	oev_netz.gv2010_modulo;
