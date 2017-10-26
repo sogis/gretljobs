bb941208a706027f33476a65509be18c62181ad7
barpastu <noemi.sturm@bd.so.ch>
2017-10-26 15:15:02 +0200

    changing attribute names of primary key and geometry

--- afu_immissionskarten_pub/afu_immissionskarten_pub_luftbelastung_2010_2020.sql
+++ afu_immissionskarten_pub/afu_immissionskarten_pub_luftbelastung_2010_2020.sql
@@ -1,13 +1,13 @@
 SELECT
-	ogc_fid,
-	wkb_geometry,
-	x,
-	y,
-	ino2_2010,
-	ino2_2020,
-	ipm10_2010,
-	ipm10_2020,
-	ipm25_2010,
-	ipm25_2020
+    ogc_fid AS t_id,
+    wkb_geometry AS geometrie,
+    x,
+    y,
+    ino2_2010,
+    ino2_2020,
+    ipm10_2010,
+    ipm10_2020,
+    ipm25_2010,
+    ipm25_2020
 FROM
-	immissionskarten_luft.luftbelastung_2010_2020;
+    immissionskarten_luft.luftbelastung_2010_2020;
