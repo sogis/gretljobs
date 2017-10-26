34aa80f35a08800faef08c0f5541ff47083e9780
barpastu <noemi.sturm@bd.so.ch>
2017-10-26 10:34:47 +0200

    Changing attribute names of id and geometry

--- afu_vsb_pub/afu_vsb_pub_pruefperimeter.sql
+++ afu_vsb_pub/afu_vsb_pub_pruefperimeter.sql
@@ -1,10 +1,10 @@
 SELECT
-  flaecheid,
-  wkb_geometry,
-  belastungstyp,
-  statustyp,
-  bezeichnung,
-  anz_order
+    flaecheid AS t_id,
+    wkb_geometry AS geometrie,
+    belastungstyp,
+    statustyp,
+    bezeichnung,
+    anz_order
 FROM
-  vsb.afu_pruefperimeter_qgis_server_client_t
+    vsb.afu_pruefperimeter_qgis_server_client_t
 
