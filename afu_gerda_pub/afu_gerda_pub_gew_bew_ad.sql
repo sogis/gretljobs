ae07553d8c3a49071fb686160c84d98f52ad37ba
barpastu <noemi.sturm@bd.so.ch>
2017-10-26 14:59:58 +0200

    Changing attribute names of primary key and geometry

--- afu_gerda_pub/afu_gerda_pub_gew_bew_ad.sql
+++ afu_gerda_pub/afu_gerda_pub_gew_bew_ad.sql
@@ -1,8 +1,8 @@
 SELECT DISTINCT 
-	a.wkb_geometry, 
-	a.anlageid, 
-	s.statusid, 
-	p.pumpeart, 
+    a.wkb_geometry AS geometrie, 
+    a.anlageid, 
+    s.statusid, 
+    p.pumpeart, 
     bo.tiefebohrung AS bohrtiefe, 
     b.datum_bewilligung, 
     d.dokument, 
@@ -10,47 +10,47 @@ SELECT DISTINCT
     ((a.anlageid || ''::text) || d.dokumenteid)::integer AS anlagedokument, 
     d.doktyp
 FROM 
-	gerda.anlage_t a
-   	LEFT JOIN 
-   		gerda.erdsonden s 
-   		ON 
-   			a.anlageid = s.anlageid
-   	LEFT JOIN 
-   		gerda.pumpe p 
-   		ON 
-   			a.anlageid = p.anlageid
-   	LEFT JOIN 
-   		gerda.bohrung bo 
-   		ON 
-   			a.anlageid = bo.anlageid
-   	LEFT JOIN 
-   		gerda.bewilligung b 
-   		ON 
-   			a.anlageid = b.anlageid
-   	LEFT JOIN 
-   		gerda.geschaeft_dokumente d 
-   		ON 
-   			a.anlageid = d.anlageid
-  WHERE 
-  	s.statusid = 89 
-  	AND 
-  	a.archive = 0 
-  	AND 
-  	p.archive = 0 
-  	AND 
-  	b.archive = 0 
-  	AND 
-  	bo.archive = 0 
-  	AND 
-  	d.archive = 0
- ORDER BY 
- 	a.wkb_geometry, 
- 	a.anlageid, 
- 	s.statusid, 
- 	p.pumpeart, 
- 	b.datum_bewilligung, 
- 	d.dokument, 
- 	d.name, 
- 	((a.anlageid || ''::text) || d.dokumenteid)::integer, 
- 	d.doktyp, 
- 	bo.tiefebohrung;
+    gerda.anlage_t a
+    LEFT JOIN 
+        gerda.erdsonden s 
+        ON 
+            a.anlageid = s.anlageid
+    LEFT JOIN 
+        gerda.pumpe p 
+        ON 
+            a.anlageid = p.anlageid
+    LEFT JOIN 
+        gerda.bohrung bo 
+        ON 
+            a.anlageid = bo.anlageid
+    LEFT JOIN 
+        gerda.bewilligung b 
+        ON 
+            a.anlageid = b.anlageid
+    LEFT JOIN 
+        gerda.geschaeft_dokumente d 
+        ON 
+            a.anlageid = d.anlageid
+WHERE 
+    s.statusid = 89 
+    AND 
+    a.archive = 0 
+    AND 
+    p.archive = 0 
+    AND 
+    b.archive = 0 
+    AND 
+    bo.archive = 0 
+    AND 
+    d.archive = 0
+ORDER BY 
+    a.wkb_geometry, 
+    a.anlageid, 
+    s.statusid, 
+    p.pumpeart, 
+    b.datum_bewilligung, 
+    d.dokument, 
+    d.name, 
+    ((a.anlageid || ''::text) || d.dokumenteid)::integer, 
+    d.doktyp, 
+    bo.tiefebohrung;
