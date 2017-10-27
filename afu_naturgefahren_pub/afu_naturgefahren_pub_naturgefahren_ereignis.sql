1ba82a715c6bf55e99481fa65594f7515cad4cba
barpastu <noemi.sturm@bd.so.ch>
2017-10-27 13:40:48 +0200

    changing attribute names of primary key and geometry

--- afu_naturgefahren_pub/afu_naturgefahren_pub_naturgefahren_ereignis.sql
+++ afu_naturgefahren_pub/afu_naturgefahren_pub_naturgefahren_ereignis.sql
@@ -1,14 +1,14 @@
 SELECT
-	ogc_fid,
-	wkb_geometry,
-	datum,
-	prozess_typ,
-	storme_nr,
-	jahr,
-	gem_bfs,
-	bezirk_nr,
-	kanton_nr
+    ogc_fid AS t_id,
+    wkb_geometry AS geometrie,
+    datum,
+    prozess_typ,
+    storme_nr,
+    jahr,
+    gem_bfs,
+    bezirk_nr,
+    kanton_nr
 FROM
-	public.aww_natgef_ereignis
+    public.aww_natgef_ereignis
 WHERE
-	"archive" = 0
+    "archive" = 0
