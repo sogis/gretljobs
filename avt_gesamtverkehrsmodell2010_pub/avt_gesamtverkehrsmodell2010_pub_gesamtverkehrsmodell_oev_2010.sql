8aa4fde235d9444809084189c9b6520c1fb1ef8e
barpastu <noemi.sturm@bd.so.ch>
2017-10-27 14:46:45 +0200

    replace tabs with blank spaces

--- avt_gesamtverkehrsmodell2010_pub/avt_gesamtverkehrsmodell2010_pub_gesamtverkehrsmodell_oev_2010.sql
+++ avt_gesamtverkehrsmodell2010_pub/avt_gesamtverkehrsmodell2010_pub_gesamtverkehrsmodell_oev_2010.sql
@@ -1,13 +1,13 @@
 SELECT 
-	wkb_geometry AS geometrie,
-	streckennummer AS t_id,
-	dtv2010_bus_modulo,
-	dtv2010_bahn_modulo,
-	asp2010_bus_modulo,
-	asp2010_bahn_modulo,
-	msp2010_bus_modulo,
-	msp2010_bahn_modulo,
-	dwv2010_bus_modulo,
-	dwv2010_bahn_modulo
+    wkb_geometry AS geometrie,
+    streckennummer AS t_id,
+    dtv2010_bus_modulo,
+    dtv2010_bahn_modulo,
+    asp2010_bus_modulo,
+    asp2010_bahn_modulo,
+    msp2010_bus_modulo,
+    msp2010_bahn_modulo,
+    dwv2010_bus_modulo,
+    dwv2010_bahn_modulo
 FROM 
-	oev_netz.gv2010_modulo;
+    oev_netz.gv2010_modulo;
