f2c1b1369a6f6be5d9e19157d78be2c4cd7b9211
barpastu <noemi.sturm@bd.so.ch>
2017-10-31 09:10:33 +0100

    Adding afu_gewisso_pub.ezg_kt_so_40

---
+++ afu_gewisso_pub/afu_gewisso_pub_ezg_kt_so_40.sql
@@ -0,0 +1,32 @@
+SELECT
+    ogc_fid AS t_id,
+    wkb_geometry AS geometrie,
+    objectid,
+    ezgnr,
+    teilezgnr,
+    gwlnr,
+    measure,
+    gewissnr,
+    ch,
+    see,
+    h1,
+    h2,
+    kanal,
+    flussgb,
+    "release",
+    mutation,
+    hierarchie,
+    interns,
+    poly_id,
+    tezgnr40,
+    tezgnr150,
+    tezgnr1000,
+    shape_leng,
+    shape_area,
+    area40,
+    area_ha,
+    color
+FROM
+    gewisso.ezg_kt_so_40
+WHERE
+    "archive" = 0;
