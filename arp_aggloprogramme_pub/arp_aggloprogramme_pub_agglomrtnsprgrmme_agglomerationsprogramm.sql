693d96173e165fb11df46f8062eec80f3264dbf9
barpastu <noemi.sturm@bd.so.ch>
2017-12-01 09:02:37 +0100

    changing query of agglomrtnsprgrmme_agglomerationsprogramm to datenumbauquery, adding arp_aggloprogramme_pub.agglomrtnsprgrmme_massnahme

--- arp_aggloprogramme_pub/arp_aggloprogramme_pub_agglomrtnsprgrmme_agglomerationsprogramm.sql
+++ arp_aggloprogramme_pub/arp_aggloprogramme_pub_agglomrtnsprgrmme_agglomerationsprogramm.sql
@@ -1,12 +1,48 @@
+WITH gemeinden AS (
+    SELECT
+        ST_Multi(wkb_geometry) AS geometrie,
+        gmde AS gem_bfs,
+        name
+    FROM
+        sogis_ch_gemeinden
+    WHERE
+        archive = 0
+ 
+    UNION ALL
+ 
+    SELECT
+        ST_Multi(wkb_geometry) AS geometrie,
+        gem_bfs,
+        gmde_name AS name
+    FROM
+        geo_gemeinden
+    WHERE
+        archive = 0
+)
+
 SELECT
-    t_id,
-    aname,
-    generation,
-    gemeinde,
-    bfs_nr,
-    kanton,
-    land,
-    geometrie
+    row_number() OVER() AS t_id,    
+    aggloprogramm.aname,
+    string_agg(DISTINCT aggloprogramm.generation::text, ', ') AS generation,
+    gemeinde.aname AS gemeinde,
+    gemeinde.bfs_nr,
+    gemeinde.kanton,
+    gemeinde.land,
+    gemeinden.geometrie
+ 
 FROM
-    arp_aggloprogramme_pub.agglomrtnsprgrmme_agglomerationsprogramm
+    arp_aggloprogramme.agglomrtnsprgrmme_agglomerationsprogramm AS aggloprogramm
+    JOIN arp_aggloprogramme.agglomrtnsprgrmme_massnahme AS massnahme
+        ON aggloprogramm.t_id = massnahme.agglo_programm
+    JOIN arp_aggloprogramme.agglomrtnsprgrmme_gemeinde_massnahme AS gemeinde_massnahme
+        ON massnahme.t_id = gemeinde_massnahme.massnahme
+    JOIN arp_aggloprogramme.agglomrtnsprgrmme_gemeinde AS gemeinde
+        ON gemeinde_massnahme.gemeinde_name = gemeinde.t_id
+    JOIN gemeinden
+        ON gemeinde.bfs_nr = gemeinden.gem_bfs
+
+GROUP BY
+    aggloprogramm.aname,
+    gemeinde.t_id,
+    gemeinden.geometrie
 ;
