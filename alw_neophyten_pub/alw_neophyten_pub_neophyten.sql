ff71f8fa881fbfd7498ce68faa38d983d07bb25a
barpastu <noemi.sturm@bd.so.ch>
2018-01-17 13:32:36 +0100

    initial version with alw_neophyten_pub.neophyten

---
+++ alw_neophyten_pub/alw_neophyten_pub_neophyten.sql
@@ -0,0 +1,25 @@
+SELECT 
+    neophyten.ogc_fid AS t_id, 
+    neophyten.wkb_geometry AS geometrie, 
+    neophyten.datum_meldung, 
+    neophyten.pflart, 
+    neophyten.anzahlid, 
+    neophyten.flaecheid, 
+    neophyten.lraum, 
+    codes1.code_text AS pflanzenart, 
+    codes2.code_text AS anzahl, 
+    codes3.code_text AS flaeche, 
+    lebensraum.lraum AS lebensraum
+FROM 
+    neophyten.neophyten_t neophyten
+    LEFT JOIN neophyten.codes codes1 
+        ON neophyten.pflart = codes1.codeid
+    LEFT JOIN neophyten.codes codes2 
+        ON neophyten.anzahlid = codes2.codeid
+    LEFT JOIN neophyten.codes codes3 
+        ON neophyten.flaecheid = codes3.codeid
+    LEFT JOIN neophyten.lebensraum lebensraum 
+        ON neophyten.lraum = lebensraum.lraumid
+WHERE
+    archive = 0
+;
