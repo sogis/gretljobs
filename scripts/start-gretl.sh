1b09556f568948f8ecbb5f7901636cae967399fc
Christoph Raaflaub <raaflaub@puzzle.ch>
2018-01-19 15:05:57 +0100

    fix GRETL runtime test script

--- scripts/start-gretl.sh
+++ scripts/start-gretl.sh
@@ -15,12 +15,15 @@ while [ $# -gt 0 ]; do
   shift
 done
 
+declare gretl_cmd="gretl $task_name ${task_parameter[@]}"
+
 echo "======================================================="
 echo "Starts the GRETL runtime to execute the given GRETL job"
 echo "Docker Image: $docker_image"
 echo "task name: $task_name"
 echo "job directory: $job_directory"
 echo "task_parameter: ${task_parameter[@]}"
+echo "gretl_cmd: $gretl_cmd"
 echo "======================================================="
 
 # special run configuration for jenkins-slave based image:
@@ -38,4 +41,4 @@ docker run -i --rm \
     -v "$job_directory":/home/gradle/project \
     --user $UID \
     "$docker_image" "-c" \
-        "/usr/local/bin/run-jnlp-client > /dev/null 2>&1;cd /home/gradle/project;gretl $task_name ${task_parameter[@]}"
+        "/usr/local/bin/run-jnlp-client > /dev/null 2>&1;cd /home/gradle/project;$gretl_cmd"
