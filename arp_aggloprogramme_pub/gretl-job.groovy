a68721cd7cb68ec2b953d6f79538203f4f62658b
schmandr <andreas.schmid@bd.so.ch>
2018-03-02 15:01:37 +0100

    Set upstream repo URL

--- arp_aggloprogramme_pub/gretl-job.groovy
+++ arp_aggloprogramme_pub/gretl-job.groovy
@@ -1,5 +1,5 @@
 node ("gretl") {
-    git 'https://github.com/schmandr/gretljobs.git'
+    git 'https://github.com/sogis/gretljobs.git'
     dir(env.JOB_BASE_NAME) {
         sh 'gradle --init-script /home/gradle/init.gradle'
     }
