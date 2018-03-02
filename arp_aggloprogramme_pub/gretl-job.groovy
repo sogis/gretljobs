fb1c5126f552be9645633f237f5eb13beb4ba1d1
schmandr <andreas.schmid@bd.so.ch>
2018-03-02 14:49:02 +0100

    Generic solution for directory name

--- arp_aggloprogramme_pub/gretl-job.groovy
+++ arp_aggloprogramme_pub/gretl-job.groovy
@@ -1,6 +1,6 @@
 node ("gretl") {
     git 'https://github.com/schmandr/gretljobs.git'
-    dir('arp_aggloprogramme_pub') {
+    dir(env.JOB_BASE_NAME) {
         sh 'gradle --init-script /home/gradle/init.gradle'
     }
 }
