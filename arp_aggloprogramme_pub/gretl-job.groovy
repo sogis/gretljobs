f3b1a8d94590bc89f3f33cc69c1a24aad63b21d5
schmandr <andreas.schmid@bd.so.ch>
2018-03-08 15:33:23 +0100

    Add pipeline trigger

--- arp_aggloprogramme_pub/gretl-job.groovy
+++ arp_aggloprogramme_pub/gretl-job.groovy
@@ -1,3 +1,12 @@
+properties([
+        // keep only the last 20 builds
+        buildDiscarder(logRotator(numToKeepStr: '20')),
+        // when to run job 
+        pipelineTriggers([
+                cron('H H(3-4) * * *')
+        ])
+])
+
 node ("gretl") {
     git 'https://github.com/sogis/gretljobs.git'
     dir(env.JOB_BASE_NAME) {
