bbb9df610dff102ffb3f4d7814331da36410ef7c
schmandr <andreas.schmid@bd.so.ch>
2018-03-08 15:37:26 +0100

    Show some information about current location again

--- arp_aggloprogramme_pub/gretl-job.groovy
+++ arp_aggloprogramme_pub/gretl-job.groovy
@@ -9,7 +9,11 @@ properties([
 
 node ("gretl") {
     git 'https://github.com/sogis/gretljobs.git'
+    sh 'ls -la /home/gradle/libs'
     dir(env.JOB_BASE_NAME) {
+        // show current location and content
+        sh 'pwd && ls -la'
+        // run job
         sh 'gradle --init-script /home/gradle/init.gradle'
     }
 }
