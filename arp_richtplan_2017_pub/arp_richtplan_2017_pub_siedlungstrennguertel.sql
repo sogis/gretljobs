c084aabb4716e2c7de6fe3473efaa71a56be7bc7
barpastu <noemi.sturm@bd.so.ch>
2018-04-18 12:03:33 +0200

    adding missing attributes

--- arp_richtplan_2017_pub/arp_richtplan_2017_pub_siedlungstrennguertel.sql
+++ arp_richtplan_2017_pub/arp_richtplan_2017_pub_siedlungstrennguertel.sql
@@ -2,6 +2,12 @@ SELECT
     ogc_fid AS t_id,
     gid,
     o_art,
+    CASE 
+        WHEN o_art = 30601
+            THEN 'von kantonaler Bedeutung'
+        WHEN o_art = 30602
+            THEN 'von regionaler Bedeutung'
+    END AS o_art_txt,
     geometrie
 FROM
     arp_richtplan_2017.siedlungstrennguertel
