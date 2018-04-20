6807bb8110e7e1a9e39016e746d90d0ed56f982a
barpasen <peter.senn@bd.so.ch>
2018-04-20 09:31:57 +0200

    Auflösung der Codes nicht nötig

--- avt_oeffentlicher_verkehr_pub/avt_oeffentlicher_verkehr_pub_netz.sql
+++ avt_oeffentlicher_verkehr_pub/avt_oeffentlicher_verkehr_pub_netz.sql
@@ -1,20 +1,8 @@
 SELECT
     ogc_fid AS t_id,
     ST_Multi(wkb_geometry) AS geometrie,
-    CASE
-        WHEN typ = 1
-            THEN 'Bus'
-        WHEN typ = 2
-            THEN 'SBB'
-        WHEN typ = 3
-            THEN 'Regionalbahn'
-    END AS  typ,
-    CASE
-        WHEN herkunft = 1
-            THEN 'kopiert (Strassenetz, Achsen der Kantonsstrassen)'
-        WHEN herkunft = 2
-            THEN 'Manuell digitalisiert (z.B. von Orthofoto)'
-    END AS  herkunft
+ S  typ,
+    herkunft
 FROM
     public.avt_oev_netz
 WHERE
