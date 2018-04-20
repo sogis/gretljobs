3739a9ff07d74c3c1f468f49276b75d6c77cd4b9
barpasen <38491509+barpasen@users.noreply.github.com>
2018-04-20 15:15:33 +0200

    Avt oeffentlicher verkehr pub (#112)
    * Auflösung der Codes nicht nötig
    
    * Auflösung der Codes nicht nötig
    
    * Adding groovy-File for jenkins
    
    * gretl-job.groovy wurde irrtümlich und am falschen Ort hinzugefügt

--- avt_oeffentlicher_verkehr_pub/avt_oeffentlicher_verkehr_pub_haltestellen.sql
+++ avt_oeffentlicher_verkehr_pub/avt_oeffentlicher_verkehr_pub_haltestellen.sql
@@ -3,42 +3,10 @@ SELECT
     wkb_geometry AS geometrie,
     haltest_id,
     haltestell,
-    CASE
-        WHEN nr_tu = 1
-            THEN 'BLT (Bus + Tram)'
-        WHEN nr_tu = 2
-            THEN 'AAR (Bus)'
-        WHEN nr_tu = 3
-            THEN 'Post BE-FR-SO, Thal (Bus)'
-        WHEN nr_tu = 4
-            THEN 'BLS (Bahn)'
-        WHEN nr_tu = 5
-            THEN 'RBS (Bahn)'
-        WHEN nr_tu = 6
-            THEN 'SBB (Bahn)'
-         WHEN nr_tu = 7
-            THEN 'OeBB (Bahn)'
-        WHEN nr_tu = 9
-            THEN 'Asm (Bahn)'
-         WHEN nr_tu = 10
-            THEN 'Post NWCH (Bus)'
-        WHEN nr_tu = 11
-            THEN 'BGU (Bus)'
-        WHEN nr_tu = 12
-            THEN 'BSU (Bus)'
-        WHEN nr_tu = 13
-            THEN 'BOGG (Bus)'
-    END AS nr_tu,
+    nr_tu,
     anzahl_hs,
     didok,
-    CASE
-        WHEN verkehrsmittel = 1
-            THEN 'Bus bzw. Bushaltestelle'
-        WHEN verkehrsmittel = 2
-            THEN 'Zug bzw. Bahnhof'
-        WHEN verkehrsmittel = 3
-            THEN 'Regionalbahn bzw. Regionalbahnhof'
-    END AS verkehrsmittel
+    verkehrsmittel
 FROM
     public.avt_oev_haltestellen
 WHERE
