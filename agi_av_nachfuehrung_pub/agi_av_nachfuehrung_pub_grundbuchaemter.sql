25c4e2b489076dc5dc197f1c3b693990ac4643e2
barpastu <noemi.sturm@bd.so.ch>
2018-05-04 10:40:40 +0200

    correcting query, using tables instead of views

--- agi_av_nachfuehrung_pub/agi_av_nachfuehrung_pub_grundbuchaemter.sql
+++ agi_av_nachfuehrung_pub/agi_av_nachfuehrung_pub_grundbuchaemter.sql
@@ -1,14 +1,17 @@
 SELECT
-    amtschreiberei, 
-    amt, 
-    strasse, 
-    hausnummer, 
-    plz, 
-    ortschaft, 
-    telefon, 
-    web, 
-    email,
-    gem_bfs
-FROM
-    av_grundbuch.grundbuchkreise_grundbuchamt_v
+    kreis.gem_bfs,
+    amt.amtschreiberei, 
+    amt.amt, 
+    amt.strasse, 
+    amt.hausnummer, 
+    amt.plz, 
+    amt.ortschaft, 
+    amt.telefon, 
+    amt.web, 
+    amt.email
+FROM 
+    av_grundbuch.grundbuchkreise kreis, 
+    av_grundbuch.grundbuchamt amt
+WHERE 
+    amt.ogc_fid = kreis.grundbuchamt_id
 ;
--- agi_av_nachfuehrung_pub/agi_av_nachfuehrung_pub_nachfuehrungsgeometer.sql
+++ agi_av_nachfuehrung_pub/agi_av_nachfuehrung_pub_nachfuehrungsgeometer.sql
@@ -1,17 +1,20 @@
 SELECT 
-    name, 
-    vorname, 
-    firma, 
-    firma_zusatz, 
-    strasse, 
-    hausnr, 
-    plz, 
-    ortschaft, 
-    telefon, 
-    fax, 
-    web, 
-    email,
-    gem_bfs
+    geometer_gemeinde.gem_bfs,
+    nfgeometer.vorname, 
+    nfgeometer.name,
+    btrim(nfgeometer.firma::text) AS firma, 
+    btrim(nfgeometer.firma_zusatz::text) AS firma_zusatz, 
+    btrim(nfgeometer.strasse::text) AS strasse, 
+    btrim(nfgeometer.hausnummer::text) AS hausnr, 
+    nfgeometer.plz, 
+    btrim(nfgeometer.ortschaft::text) AS ortschaft, 
+    btrim(nfgeometer.telefon::text) AS telefon, 
+    btrim(nfgeometer.fax::text) AS fax, 
+    btrim(nfgeometer.email::text) AS email, 
+    btrim(nfgeometer.web::text) AS web
 FROM 
-    av_nfgeometer.datenabgabe_info_v
+    av_nfgeometer.nfgeometer , 
+    av_nfgeometer.geometer_gemeinde
+WHERE 
+    nfgeometer.ogc_fid = geometer_gemeinde.nfgeometer_id
 ;
