b6be7148b97fca2683a09b920dae1a8d14e58f30
Martin Schweizer <martin.schweizer@bd.so.ch>
2018-05-11 16:05:01 +0200

    chenged data-types in table awjf_wald_pub.waldge

--- awjf_wald_pub/awjf_wald_pub_waldge.sql
+++ awjf_wald_pub/awjf_wald_pub_waldge.sql
@@ -5,8 +5,19 @@ SELECT
     perimeter,
     waldge_,
     waldge_id,
-    massstab,
-    autor,
+    CASE massstab WHEN 1 THEN '<1:2500' 
+                  WHEN 2 THEN '1:2500'
+                  WHEN 3 THEN '1:5000'
+                  WHEN 4 THEN '1:10000'
+                  WHEN 5 THEN '1:25000'
+                  WHEN 6 THEN '1:50000'
+    END AS massstab,
+    CASE autor WHEN 'A' THEN 'BGU'
+               WHEN 'B' THEN 'Kaufmann'
+               WHEN 'C' THEN 'Froelicher'
+               WHEN 'D' THEN 'Borer'
+               WHEN 'W' THEN 'Waldmaske'
+    END AS autor,
     kartierung,
     ges_alt,
     ges_neu,
