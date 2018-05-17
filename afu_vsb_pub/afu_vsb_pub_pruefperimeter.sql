0e7b3ddfd06b17534b7bee7aa69962366daf1ffa
barpastu <noemi.sturm@bd.so.ch>
2018-05-17 16:11:35 +0200

    adding missing attributes

--- afu_vsb_pub/afu_vsb_pub_pruefperimeter.sql
+++ afu_vsb_pub/afu_vsb_pub_pruefperimeter.sql
@@ -196,7 +196,37 @@ SELECT
     END AS schadstoffe_3,
     statustyp,
     bezeichnung,
-    anz_order
+    anz_order,
+    'docs/ch.so.afu.luftbelastung/was_ist_zu_tun.pdf' AS was_ist_zu_tun_dokument,
+     CASE
+        WHEN belastungstyp = 1
+            THEN 'https://www.so.ch/verwaltung/bau-und-justizdepartement/amt-fuer-umwelt/boden-untergrund-geologie/boden/bodenbelastungsgebiete/'
+        WHEN 
+            belastungstyp = 2
+            OR
+            belastungstyp = 3
+            OR
+            belastungstyp = 5
+            OR
+            belastungstyp = 6
+            OR 
+            belastungstyp = 8
+            OR
+            belastungstyp = 9
+            OR 
+            belastungstyp = 10
+            OR 
+            belastungstyp = 11
+            OR
+            belastungstyp = 12
+            OR
+            belastungstyp = 220
+            OR
+            belastungstyp = 221
+            THEN 'https://www.so.ch/verwaltung/bau-und-justizdepartement/amt-fuer-umwelt/boden-untergrund-geologie/boden/pruefperimeter-bodenabtrag/'
+        WHEN belastungstyp = 7
+            THEN 'https://www.so.ch/verwaltung/bau-und-justizdepartement/amt-fuer-umwelt/boden-untergrund-geologie/altlasten-belastete-standorte/schiessanlagen/'
+    END AS weitere_infos
 FROM
     vsb.afu_pruefperimeter_qgis_server_client_t
 ;
