dbc48be9fdbd27665b4602eaa75a96f1c3a89c2f
barpasen <peter.senn@bd.so.ch>
2018-05-25 14:21:43 +0200

    convert attriubute values to text

--- avt_gesamtverkehrsmodell2010_pub/avt_gesamtverkehrsmodell2010_pub_auslastung.sql
+++ avt_gesamtverkehrsmodell2010_pub/avt_gesamtverkehrsmodell2010_pub_auslastung.sql
@@ -5,9 +5,42 @@ SELECT
     sostrid,
     wkb_geometry AS geometrie,
     so_typ,
-    ausl_kat_2010
-    ausl_kat_2020,
-    ausl_kat_2030
+    CASE
+        WHEN ausl_kat_2010 = 1
+            THEN 'flüssig'
+        WHEN ausl_kat_2010 = 2
+            THEN 'dicht'
+        WHEN ausl_kat_2010 = 3
+            THEN 'gesättigt'
+        WHEN ausl_kat_2010 = 4
+            THEN 'stop+ go'
+        WHEN ausl_kat_2010 = 5
+            THEN 'Stau'
+    END AS ausl_kat_2010,
+    CASE
+        WHEN ausl_kat_2020 = 1
+            THEN 'flüssig'
+        WHEN ausl_kat_2020 = 2
+            THEN 'dicht'
+        WHEN ausl_kat_2020 = 3
+            THEN 'gesättigt'
+        WHEN ausl_kat_2020 = 4
+            THEN 'stop+ go'
+         WHEN ausl_kat_2020 = 5
+            THEN 'Stau'
+    END AS ausl_kat_2020,
+    CASE
+        WHEN ausl_kat_2030 = 1
+            THEN 'flüssig'
+        WHEN ausl_kat_2030 = 2
+            THEN 'dicht'
+        WHEN ausl_kat_2030 = 3
+            THEN 'gesättigt'
+        WHEN ausl_kat_2030 = 4
+            THEN 'stop+ go'
+         WHEN ausl_kat_2030 = 5
+            THEN 'Stau'
+    END AS ausl_kat_2030
 FROM
     verkehrsmodell2013.auslastung_kat
 ;
