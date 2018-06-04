50076f558a6e98f0036d2aca2c5e8692bcafc100
barpastu <noemi.sturm@bd.so.ch>
2018-06-04 09:16:20 +0200

    replacing tabs with blank spaces

--- afu_gewaesserschutz_pub/afu_gewaesserschutz_pub_aww_gszustr.sql
+++ afu_gewaesserschutz_pub/afu_gewaesserschutz_pub_aww_gszustr.sql
@@ -1,17 +1,17 @@
 SELECT
     ogc_fid AS t_id,
-	wkb_geometry AS geometrie,
-	"name",
-	im_kanton,
-	typ,
-	CASE
+    wkb_geometry AS geometrie,
+    "name",
+    im_kanton,
+    typ,
+    CASE
         WHEN typ = 'Zo'
             THEN 'Zuströmbereich für Oberflächengewässer'
         WHEN typ = 'Zu'
             THEN 'Zuströmbereich für Grundwasserfassung'
     END AS typ_text
 FROM
-	public.aww_gszustr
+    public.aww_gszustr
 WHERE
-	archive = 0
+    archive = 0
 ;
