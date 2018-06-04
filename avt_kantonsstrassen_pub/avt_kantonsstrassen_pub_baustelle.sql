2e7893141d9759177a11fdcbe44682fcb9ca100e
barpasen <peter.senn@bd.so.ch>
2018-06-04 13:23:05 +0200

    Attribut-Codes aufgelöst, Dokumente als json

--- avt_kantonsstrassen_pub/avt_kantonsstrassen_pub_baustelle.sql
+++ avt_kantonsstrassen_pub/avt_kantonsstrassen_pub_baustelle.sql
@@ -1,6 +1,6 @@
 SELECT
-    t_id,
-    t_ili_tid,
+    baustelle.t_id,
+    baustelle.t_ili_tid,
     nummer,
     achsnummer,
     beschreibung,
@@ -13,10 +13,39 @@ SELECT
     beginn_jahr,
     ende_monat,
     ende_jahr,
-    status,
-    verkehrsfuehrung,
-    zustaendigkeit,
+    CASE
+        WHEN
+            status = 'im_Bau'
+            THEN ' im Bau'
+        ELSE status
+    END AS status,
+    verkehrsfuehrung.massnahme AS verkehrsfuehrung,
+    kreis||' / '||projektleiter_kuerzel  AS zustaendigkeit,
+    dokumenteliste AS dokumente,
     geometrie
 FROM
-    avt_baustellen.baustellen_baustelle
+    avt_baustellen.baustellen_baustelle AS baustelle
+LEFT JOIN avt_baustellen.baustellen_verkehrsfuehrung AS verkehrsfuehrung
+    ON verkehrsfuehrung.t_id = baustelle.verkehrsfuehrung
+LEFT JOIN avt_baustellen.baustellen_zustaendigkeit AS zustaendigkeit
+    ON zustaendigkeit.t_id = baustelle.zustaendigkeit
+LEFT JOIN (SELECT id, array_to_json(array_agg(row_to_json(dokumente)))::text AS dokumenteliste
+                   FROM
+                        (SELECT
+                             baustelle.t_id AS id,
+                             dokumententyp,
+                             dokumentname
+                        FROM
+                            avt_baustellen.baustellen_baustelle AS baustelle
+                        LEFT JOIN avt_baustellen.baustellen_dokument AS dokumente
+                            ON baustelle.t_id = dokumente.baustelle
+                        WHERE
+                            dokumententyp IS NOT NULL
+                   ) AS dokumente
+                  GROUP  BY 
+                      id,
+                     dokumententyp,
+                    dokumentname
+              ) AS dokumentliste
+    ON baustelle.t_id = dokumentliste.id
 ;
