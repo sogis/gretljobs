b8c1ceb674235f2565f3ba21eea1411dc8f0153b
Martin Schweizer <martin.schweizer@bd.so.ch>
2018-06-07 14:42:30 +0200

    Neue Schadstoffe zu ug_masch_gh_gesamt hinzugefügt

--- afu_ekat/qg_haushalte/ug_masch_gh/ug_masch_gh_eq_masch.sql
+++ afu_ekat/qg_haushalte/ug_masch_gh/ug_masch_gh_eq_masch.sql
@@ -22,7 +22,37 @@
                FROM ekat2015.emiss_quelle, ekat2015.quelle_emiss_efak
               WHERE emiss_quelle.equelle_id = 3 AND quelle_emiss_efak.s_code = 22 AND emiss_quelle.equelle_id = quelle_emiss_efak.equelle_id AND emiss_quelle.archive = 0 AND quelle_emiss_efak.archive = 0))
             ELSE 0::double precision
-        END AS emiss_nmvoc
+        END AS emiss_nmvoc, 
+        CASE
+            WHEN g.art = 0 OR g.art = 11 OR g.art = 19 THEN g.flaeche::double precision * (( SELECT quelle_emiss_efak.schast_emiss / e.anz_ha_ch::double precision * 1000::double precision * f.anz_ha_so::double precision / h.flaeche::double precision
+               FROM ekat2015.emiss_quelle, ekat2015.quelle_emiss_efak
+              WHERE emiss_quelle.equelle_id = 3 AND quelle_emiss_efak.s_code = 1 AND emiss_quelle.equelle_id = quelle_emiss_efak.equelle_id AND emiss_quelle.archive = 0 AND quelle_emiss_efak.archive = 0))
+            ELSE 0::double precision
+        END AS emiss_so2,
+        CASE
+            WHEN g.art = 0 OR g.art = 11 OR g.art = 19 THEN g.flaeche::double precision * (( SELECT quelle_emiss_efak.schast_emiss / e.anz_ha_ch::double precision * 1000::double precision * f.anz_ha_so::double precision / h.flaeche::double precision
+               FROM ekat2015.emiss_quelle, ekat2015.quelle_emiss_efak
+              WHERE emiss_quelle.equelle_id = 3 AND quelle_emiss_efak.s_code = 6 AND emiss_quelle.equelle_id = quelle_emiss_efak.equelle_id AND emiss_quelle.archive = 0 AND quelle_emiss_efak.archive = 0))
+            ELSE 0::double precision
+        END AS emiss_ch4,
+        CASE
+            WHEN g.art = 0 OR g.art = 11 OR g.art = 19 THEN g.flaeche::double precision * (( SELECT quelle_emiss_efak.schast_emiss / e.anz_ha_ch::double precision * 1000::double precision * f.anz_ha_so::double precision / h.flaeche::double precision
+               FROM ekat2015.emiss_quelle, ekat2015.quelle_emiss_efak
+              WHERE emiss_quelle.equelle_id = 3 AND quelle_emiss_efak.s_code = 7 AND emiss_quelle.equelle_id = quelle_emiss_efak.equelle_id AND emiss_quelle.archive = 0 AND quelle_emiss_efak.archive = 0))
+            ELSE 0::double precision
+        END AS emiss_pm10,
+        CASE
+            WHEN g.art = 0 OR g.art = 11 OR g.art = 19 THEN g.flaeche::double precision * (( SELECT quelle_emiss_efak.schast_emiss / e.anz_ha_ch::double precision * 1000::double precision * f.anz_ha_so::double precision / h.flaeche::double precision
+               FROM ekat2015.emiss_quelle, ekat2015.quelle_emiss_efak
+              WHERE emiss_quelle.equelle_id = 3 AND quelle_emiss_efak.s_code = 8 AND emiss_quelle.equelle_id = quelle_emiss_efak.equelle_id AND emiss_quelle.archive = 0 AND quelle_emiss_efak.archive = 0))
+            ELSE 0::double precision
+        END AS emiss_nh3,
+        CASE
+            WHEN g.art = 0 OR g.art = 11 OR g.art = 19 THEN g.flaeche::double precision * (( SELECT quelle_emiss_efak.schast_emiss / e.anz_ha_ch::double precision * 1000::double precision * f.anz_ha_so::double precision / h.flaeche::double precision
+               FROM ekat2015.emiss_quelle, ekat2015.quelle_emiss_efak
+              WHERE emiss_quelle.equelle_id = 3 AND quelle_emiss_efak.s_code = 26 AND emiss_quelle.equelle_id = quelle_emiss_efak.equelle_id AND emiss_quelle.archive = 0 AND quelle_emiss_efak.archive = 0))
+            ELSE 0::double precision
+        END AS emiss_n2o 
    FROM ekat2015.ha_raster_100 c
    LEFT JOIN ( SELECT min(intersec_bdbed_wohnzone_landw_ha_raster.art) AS art, 
             intersec_bdbed_wohnzone_landw_ha_raster.xkoord, 
--- afu_ekat/qg_haushalte/ug_masch_gh/ug_masch_gh_gesamt.sql
+++ afu_ekat/qg_haushalte/ug_masch_gh/ug_masch_gh_gesamt.sql
@@ -2,12 +2,12 @@
     coalesce(e.emiss_co,0) + coalesce(f.emiss_co,0) + coalesce(g.emiss_co,0) AS emiss_co, 
     coalesce(e.emiss_nox,0) + coalesce(f.emiss_nox,0) + coalesce(g.emiss_nox,0) AS emiss_nox, 
     coalesce(e.emiss_nmvoc,0) + coalesce(f.emiss_nmvoc,0) + coalesce(g.emiss_nmvoc,0) AS emiss_nmvoc, 
-    coalesce(f.emiss_pm10,0) + coalesce(g.emiss_pm10,0) AS emiss_pm10, 
+    coalesce(e.emiss_pm10,0) + coalesce(f.emiss_pm10,0) + coalesce(g.emiss_pm10,0) AS emiss_pm10, 
     coalesce(e.emiss_co2,0) + coalesce(f.emiss_co2,0) + coalesce(g.emiss_co2,0) AS emiss_co2, 
-    coalesce(f.emiss_nh3,0) AS emiss_nh3, 
-    coalesce(f.emiss_n2o,0) + coalesce(g.emiss_n2o,0) AS emiss_n2o, 
-    coalesce(f.emiss_ch4,0) + coalesce(g.emiss_ch4,0) AS emiss_ch4, 
-    coalesce(f.emiss_so2,0) + coalesce(g.emiss_so2,0) AS emiss_so2
+    coalesce(e.emiss_nh3,0) + coalesce(f.emiss_nh3,0) AS emiss_nh3, 
+    coalesce(e.emiss_n2o,0) + coalesce(f.emiss_n2o,0) + coalesce(g.emiss_n2o,0) AS emiss_n2o, 
+    coalesce(e.emiss_ch4,0) + coalesce(f.emiss_ch4,0) + coalesce(g.emiss_ch4,0) AS emiss_ch4, 
+    coalesce(e.emiss_so2,0) + coalesce(f.emiss_so2,0) + coalesce(g.emiss_so2,0) AS emiss_so2
    FROM ekat2015.ug_masch_gh_eq_masch e
    LEFT JOIN ekat2015.ug_masch_gh_eq_mot_hh_dies f ON e.xkoord = f.xkoord AND e.ykoord = f.ykoord
    LEFT JOIN ekat2015.ug_masch_gh_eq_mot_hh_gas g ON f.xkoord = g.xkoord AND f.ykoord = g.ykoord
