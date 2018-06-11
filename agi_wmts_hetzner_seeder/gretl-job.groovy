a90bf7ed1c091f32a6570e6ee19280be7a2d7dd5
Stefan Ziegler <edi.gonzales@gmail.com>
2018-06-11 10:41:43 +0200

    [AGI_WMTS_HETZNER] new variable for ssh key

--- agi_wmts_hetzner_seeder/gretl-job.groovy
+++ agi_wmts_hetzner_seeder/gretl-job.groovy
@@ -11,16 +11,17 @@ node("master") {
     dbUriHetznerWmts = "${env.DB_URI_HETZNER_WMTS}"
     dbCredentialNameHetznerWmts = "${DB_CREDENTIAL_HETZNER_WMTS}" // TODO: user name? ssh-key?
     hetznerWmtsServerIp = "${env.HETZNER_WMTS_SERVER_IP}"
+    hetznerWmtsServerCredential = "${env.HETZNER_WMTS_SERVER_CREDENTIAL}"
     gretljobsRepo = "${env.GRETL_JOB_REPO_URL}"
 }
 
 node ("gretl") {
     git "${gretljobsRepo}"
     dir(env.JOB_BASE_NAME) {
-        withCredentials([usernamePassword(credentialsId: "${dbCredentialNamePub}", usernameVariable: 'dbUserPub', passwordVariable: 'dbPwdPub'), usernamePassword(credentialsId: "${dbCredentialNameHetznerWmts}", usernameVariable: 'dbUserHetznerWmts', passwordVariable: 'dbPwdHetznerWmts')]) {
+        withCredentials([usernamePassword(credentialsId: "${dbCredentialNamePub}", usernameVariable: 'dbUserPub', passwordVariable: 'dbPwdPub'), usernamePassword(credentialsId: "${dbCredentialNameHetznerWmts}", usernameVariable: 'dbUserHetznerWmts', passwordVariable: 'dbPwdHetznerWmts'), sshUserPrivateKey(credentialsId: "${hetznerWmtsServerCredential}", keyFileVariable: 'sshKeyFilePathHetznerWmts')]) {
             sh "gradle --init-script /home/gradle/init.gradle \
                 -PdbUriPub='${dbUriPub}' -PdbUserPub='${dbUserPub}' -PdbPwdPub='${dbPwdPub}' \
-                -PdbUriHetznerWmts='${dbUriHetznerWmts}' -PdbUserHetznerWmts='${dbUserHetznerWmts}' -PdbPwdHetznerWmts='${dbPwdHetznerWmts}' -PhetznerWmtsServerIp='${hetznerWmtsServerIp}'"
+                -PdbUriHetznerWmts='${dbUriHetznerWmts}' -PdbUserHetznerWmts='${dbUserHetznerWmts}' -PdbPwdHetznerWmts='${dbPwdHetznerWmts}' -PhetznerWmtsServerIp='${hetznerWmtsServerIp}' -PsshKeyFilePathHetznerWmts='${sshKeyFilePathHetznerWmts}'"
         }
     }
 }
