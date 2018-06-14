8b91e8e295910caf9039f8d41c563972c668f894
barpastu <noemi.sturm@bd.so.ch>
2018-06-14 12:01:00 +0200

    integrating data conversion

--- agi_adressen_pub/agi_adressen_pub_verbindungslinie_strasse_hausnummer.sql
+++ agi_adressen_pub/agi_adressen_pub_verbindungslinie_strasse_hausnummer.sql
@@ -1,13 +1,39 @@
-SELECT
-    ogc_fid AS t_id,
-    strname,
-    hausnummer,
-    a_tid,
-    b_tid,
-    lok_tid,
-    gem_bfs,
-    the_geom AS geometrie,
-    datum
-FROM
-    av_fileverifikation.geb_shortestline_hausnummerpos
+WITH 
+    strassen AS (
+        SELECT 
+            ST_Union(geometrie) as geometrie, 
+            strassenstueck_von
+        FROM 
+            av_avdpool_ng.gebaeudeadressen_strassenstueck
+
+        GROUP BY 
+            strassenstueck_von
+    ),
+    hausnummern AS ( 
+        SELECT 
+            tid, 
+            hausnummerpos_von, 
+            pos, 
+            gem_bfs
+        FROM 
+            av_avdpool_ng.gebaeudeadressen_hausnummerpos
+    )
+
+SELECT 
+    gebaeudeadressen_lokalisationsname."text" AS strname, 
+    gebaeudeadressen_gebaeudeeingang.hausnummer, 
+    hausnummern.tid AS a_tid, 
+    gebaeudeadressen_gebaeudeeingang.tid AS b_tid, 
+    gebaeudeadressen_gebaeudeeingang.gebaeudeeingang_von AS lok_tid, 
+    ST_ShortestLine(hausnummern.pos, strassen.geometrie) AS geometrie, 
+    hausnummern.gem_bfs,
+    now() AS datum
+FROM 
+    hausnummern
+    LEFT JOIN av_avdpool_ng.gebaeudeadressen_gebaeudeeingang
+        ON gebaeudeadressen_gebaeudeeingang.tid = hausnummern.hausnummerpos_von
+    LEFT JOIN strassen
+        ON strassen.strassenstueck_von = gebaeudeadressen_gebaeudeeingang.gebaeudeeingang_von
+    LEFT JOIN av_avdpool_ng.gebaeudeadressen_lokalisationsname
+        ON gebaeudeadressen_lokalisationsname.benannte = gebaeudeadressen_gebaeudeeingang.gebaeudeeingang_von
 ;
