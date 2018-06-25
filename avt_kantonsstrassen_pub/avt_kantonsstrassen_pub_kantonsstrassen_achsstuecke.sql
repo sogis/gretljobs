285857c73fcc5bc055b1059c18b0a62e1f5492d6
barpasen <peter.senn@bd.so.ch>
2018-06-25 11:41:13 +0200

    bei allen mit max() zusammengefassten Attributen fehlte der Aliasname

--- avt_kantonsstrassen_pub/avt_kantonsstrassen_pub_kantonsstrassen_achsstuecke.sql
+++ avt_kantonsstrassen_pub/avt_kantonsstrassen_pub_kantonsstrassen_achsstuecke.sql
@@ -1,5 +1,5 @@
 SELECT
-    id AS t_id,
+    max(id) AS t_id,
     geom AS geometrie,
     "name",
     lokalisati,
@@ -11,16 +11,34 @@ SELECT
     plz_ortsch,
     sostrid,
     eid,
-    ogc_fid,
-    abschnitti,
+    max(ogc_fid) As ogc_fid,
+    max(abschnitti) AS abschnitti,
     achseid,
     achsname,
     achsnummer,
     achstyp,
     achstypnam,
-    code
+    max(code) AS code
 FROM
     strassennetz.kantonsstrassen_def
 WHERE
     archive = 0
+
+GROUP BY
+    geometrie,
+    "name",
+    lokalisati,
+    strassenst,
+    ordnung,
+    istachse,
+    gem_bfs,
+    los,
+    plz_ortsch,
+    sostrid,
+    eid,
+    achseid,
+    achsname,
+    achsnummer,
+    achstyp,
+    achstypnam
 ;
