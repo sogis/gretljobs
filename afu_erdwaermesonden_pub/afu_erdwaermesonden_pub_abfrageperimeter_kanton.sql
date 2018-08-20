9207a49a7e054fabc7a808bc11c780b3b1a76ef1
barpastu <noemi.sturm@bd.so.ch>
2018-08-20 09:05:58 +0200

    renaming attribut abfrage to html_text, adding attribut abfrage for layer legend

--- afu_erdwaermesonden_pub/afu_erdwaermesonden_pub_abfrageperimeter_kanton.sql
+++ afu_erdwaermesonden_pub/afu_erdwaermesonden_pub_abfrageperimeter_kanton.sql
@@ -7,6 +7,11 @@ SELECT
         WHEN colorid = 0 
             THEN 'Die Prüfung der Anfrage hat für den Standort $mapX$ / $mapY$ folgendes ergeben (siehe Objektblatt): '
         ELSE 'Eine Online-Abfrage ist an diesem Standort nicht möglich. Bitte setzen Sie sich mit dem zuständigen Sachbearbeiter im Amt für Umwelt in Verbindung, Kontakt siehe <a href="www.afu.so.ch/geothermie" targer="_blank">www.afu.so.ch/geothermie</a>' 
+    END AS html_text,
+    CASE
+        WHEN colorid = 0 
+            THEN 'Online-Abfrage möglich'
+        ELSE 'Keine Online-Abfrage möglich' 
     END AS abfrage
 FROM
     auszug_akt_altlasten17785.abfrageperimeter_kanton
