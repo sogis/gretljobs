59a6f628e59af3cfb8348197ec093bb03e9c59cd
barpastu <noemi.sturm@bd.so.ch>
2018-08-20 11:24:39 +0200

    changes based on review

--- afu_erdwaermesonden_pub/afu_erdwaermesonden_pub_abfrageperimeter_kanton.sql
+++ afu_erdwaermesonden_pub/afu_erdwaermesonden_pub_abfrageperimeter_kanton.sql
@@ -6,7 +6,7 @@ SELECT
     CASE
         WHEN colorid = 0 
             THEN 'Die Prüfung der Anfrage hat für den Standort $mapX$ / $mapY$ folgendes ergeben (siehe Objektblatt): '
-        ELSE 'Eine Online-Abfrage ist an diesem Standort nicht möglich. Bitte setzen Sie sich mit dem zuständigen Sachbearbeiter im Amt für Umwelt in Verbindung, Kontakt siehe <a href="www.afu.so.ch/geothermie" targer="_blank">www.afu.so.ch/geothermie</a>' 
+        ELSE 'Eine Online-Abfrage ist an diesem Standort nicht möglich. Bitte setzen Sie sich mit dem zuständigen Sachbearbeiter im Amt für Umwelt in Verbindung, Kontakt siehe <a href="https://www.afu.so.ch/geothermie" target="_blank">https://www.afu.so.ch/geothermie</a>' 
     END AS html_text,
     CASE
         WHEN colorid = 0 
