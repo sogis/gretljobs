5b8f293e2df447f39d5a96f4ea03af7d5ffe0713
barpastu <noemi.sturm@bd.so.ch>
2018-09-13 12:27:07 +0200

    moving afu_gerda_pub and afu_erdwaermesonden_pub into afu_erdwaerme_pub

---
+++ afu_erdwaerme_pub/afu_erdwaerme_pub_gew_bew_ad.sql
@@ -0,0 +1,47 @@
+SELECT DISTINCT 
+    anlage.wkb_geometry AS geometrie, 
+    anlage.anlageid, 
+    erdsonde.statusid, 
+    pumpe.pumpeart, 
+    bohrung.tiefebohrung AS bohrtiefe, 
+    bewilligung.datum_bewilligung, 
+    dokument.name, 
+    ((anlage.anlageid || ''::text) || dokument.dokumenteid)::integer AS anlagedokument, 
+    dokument.doktyp,
+    CASE
+        WHEN 
+            pumpe.pumpeart = 'eso'
+            AND
+            doktyp = 4
+                THEN 'Erdsonde'
+        WHEN
+            pumpe.pumpeart = 'eko'
+            AND
+            doktyp = 4
+                THEN 'Erdkollektor'
+    END AS objekttyp
+FROM 
+    gerda.anlage_t AS anlage
+    LEFT JOIN gerda.erdsonden AS erdsonde 
+        ON anlage.anlageid = erdsonde.anlageid
+    LEFT JOIN gerda.pumpe AS pumpe 
+        ON anlage.anlageid = pumpe.anlageid
+    LEFT JOIN gerda.bohrung AS bohrung
+        ON anlage.anlageid = bohrung.anlageid
+    LEFT JOIN gerda.bewilligung AS bewilligung 
+        ON anlage.anlageid = bewilligung.anlageid
+    LEFT JOIN gerda.geschaeft_dokumente AS dokument
+        ON anlage.anlageid = dokument.anlageid
+WHERE 
+    erdsonde.statusid = 89 
+    AND 
+    anlage.archive = 0 
+    AND 
+    pumpe.archive = 0 
+    AND 
+    bewilligung.archive = 0 
+    AND 
+    bohrung.archive = 0 
+    AND 
+    dokument.archive = 0
+;
