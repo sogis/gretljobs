ed470370d051870bbfc0e2ce48056966eb2815a2
barpasen <peter.senn@bd.so.ch>
2018-09-28 09:08:34 +0200

    Tabelle neu anlegen für Suche nach Attribut ksnr

--- avt_kantonsstrassen_pub/avt_kantonsstrassen_pub_kantonsstrassen.sql
+++ avt_kantonsstrassen_pub/avt_kantonsstrassen_pub_kantonsstrassen.sql
@@ -1,19 +1,12 @@
 SELECT
-    eid AS t_id,
     ST_Multi(ST_Linemerge(ST_Union(geometrie))) AS geometrie,
-    "name",
-    plz_ortschaft,
-    klasse,
-    kategorie,
     "KSNr" AS ksnr,
-    "Strassentyp" AS strassentyp,
-    "Strasseneigner" AS strasseneigner,
-    "OeV-Nutzung" AS oev_nutzung,
-    "ObjectID" AS objectid,
-    "AGI_Strid" AS agi_strid,
-    "AV_Seq" AS av_seq,
-    "AGr" AS agr,
-    istoffiziellebezeichnung
+    "Strassentyp" AS strassentyp
 FROM
     strassennetz.klasse_kategorie
+WHERE
+    "Strasseneigner" = 'Kanton'
+GROUP BY
+    "KSNr",
+    "Strassentyp"
 ;
