81fcc384c70b0176f6a23bb544fe951bdaf568d1
barpastu <noemi.sturm@bd.so.ch>
2018-10-23 09:40:36 +0200

    changes due to review

--- awjf_wald_pub/awjf_wald_pub_oberhoehenbonitaet.sql
+++ awjf_wald_pub/awjf_wald_pub_oberhoehenbonitaet.sql
@@ -2,7 +2,23 @@ SELECT
     ogc_fid AS t_id,
     wkb_geometry AS geometrie,
     id,
-    ohb_code
+    ohb_code,
+    CASE
+        WHEN ohb_code = 10
+            THEN '<= 10 m'
+        WHEN ohb_code = 14
+            THEN '11 - 14 m'
+        WHEN ohb_code = 18
+            THEN '15 - 18 m'
+        WHEN ohb_code = 22
+            THEN '19 - 22 m'
+        WHEN ohb_code = 26
+            THEN '23 - 26 m'
+        WHEN ohb_code = 30
+            THEN '27 - 30 m'
+        WHEN ohb_code = 100
+            THEN '> 30 m'
+    END AS oberhoehenbonitaet
 FROM 
     awjf.oberhoehenbonitaet
 WHERE 
