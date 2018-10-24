46f45ef76c4e26cb28e16811f0630d786d6c74c9
Martin Schweizer <martin.schweizer@bd.so.ch>
2018-10-24 16:01:36 +0200

    grundlegende ueberarbeitung

---
+++ afu_wasserbewirtschaftung_pub/afu_wasserbewirtschaftung_grundwasserwaermepumpen_entnahmeschacht_pub.sql
@@ -0,0 +1,40 @@
+SELECT 
+    CASE 
+        WHEN (objekt.aufnahmedatum < (SELECT date('now') - interval '5 years') AND grundwasserwaerme.zustand = 4) 
+            THEN 'alte_voranfrage' 
+        WHEN (objekt.aufnahmedatum >= (SELECT date('now') - interval '5 years') AND grundwasserwaerme.zustand = 4) 
+            THEN 'neue_voranfrage' 
+        WHEN (grundwasserwaerme.schachttyp != 2 AND grundwasserwaerme.zustand != 4) 
+            THEN 'Grundwasserwärmepumpe bewilligt'
+        ELSE 'unbekannter_Verfahrensstand'
+        END AS verfahrensstand, 
+        grundwasserwaerme.bezeichnung AS objektname, 
+        grundwasserwaerme.vegas_id AS objektnummer, 
+        grundwasserwaerme.beschreibung AS technische_angabe, 
+        grundwasserwaerme.bemerkung AS bemerkung, 
+        dokumente.dokumente, 
+        grundwasserwaerme.wkb_geometry AS geometrie
+FROM 
+    vegas.obj_grundwasserwaerme grundwasserwaerme,
+    vegas.obj_objekt objekt
+LEFT JOIN 
+    (SELECT 
+         array_agg(x.bezeichnung) AS dokumente, 
+         y.vegas_id
+     FROM 
+         vegas.adm_dokument x, 
+         vegas.adm_objekt_dokument y 
+     WHERE x.dokument_id = y.dokument_id
+     GROUP BY y.vegas_id) dokumente ON objekt.vegas_id = dokumente.vegas_id
+WHERE 
+    grundwasserwaerme.mobj_id = objekt.mobj_id
+    AND 
+    objekt.objekttyp_id = 18
+    AND 
+    -- Da der Verfahrensstand zwingend gegeben sein muss, muss sichergestellt werden, dass dieser keinen NULL-Wert enthält! 
+    ((grundwasserwaerme.zustand = 4) OR (grundwasserwaerme.schachttyp != 2))
+    AND 
+    objekt."archive" = 0 
+    AND 
+    grundwasserwaerme."archive" = 0
+;
