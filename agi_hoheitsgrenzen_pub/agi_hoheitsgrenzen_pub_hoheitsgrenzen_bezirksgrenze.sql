a4b0179e8b5d0c1968b9bdcbed5ed606d8f7f05e
barpastu <noemi.sturm@bd.so.ch>
2018-10-24 12:11:07 +0200

    improve query due to review

--- agi_hoheitsgrenzen_pub/agi_hoheitsgrenzen_pub_hoheitsgrenzen_bezirksgrenze.sql
+++ agi_hoheitsgrenzen_pub/agi_hoheitsgrenzen_pub_hoheitsgrenzen_bezirksgrenze.sql
@@ -1,14 +1,14 @@
-SELECT 
+SELECT
     hoheitsgrenzen_gemeindegrenze.bezirksname,
-    bezirksnummer,
+    hoheitsgrenzen_bezirk.bezirksnummer,
     hoheitsgrenzen_gemeindegrenze.kantonsname,
-    ST_Multi(ST_Union(geometrie)) AS geometrie
+    ST_Multi(ST_Union(hoheitsgrenzen_gemeindegrenze.geometrie)) AS geometrie
 FROM
     agi_hoheitsgrenzen_pub.hoheitsgrenzen_gemeindegrenze
     LEFT JOIN agi_hoheitsgrenzen.hoheitsgrenzen_bezirk
-        ON hoheitsgrenzen_bezirk.bezirksname=hoheitsgrenzen_gemeindegrenze.bezirksname    
+        ON hoheitsgrenzen_bezirk.bezirksname=hoheitsgrenzen_gemeindegrenze.bezirksname
 GROUP BY
-    hoheitsgrenzen_gemeindegrenze.bezirksname, 
-    bezirksnummer, 
+    hoheitsgrenzen_gemeindegrenze.bezirksname,
+    hoheitsgrenzen_bezirk.bezirksnummer,
     hoheitsgrenzen_gemeindegrenze.kantonsname
 ;
