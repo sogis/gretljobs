815774dbd977762719ee2023f2c0c9bbbccb2e07
barpastu <noemi.sturm@bd.so.ch>
2018-10-25 16:00:53 +0200

    merge agi_hoheitsgrenzen_pub and agi_hoheitsgrenzen_pub_sogis

---
+++ agi_hoheitsgrenzen_pub/agi_hoheitsgrenzen_pub_pub_hoheitsgrenzen_kantonsgrenze.sql
@@ -0,0 +1,10 @@
+SELECT
+    t_id,
+    t_ili_tid,
+    kantonsname,
+    kantonskuerzel,
+    kantonsnummer,
+    geometrie
+FROM
+    agi_hoheitsgrenzen_pub.hoheitsgrenzen_kantonsgrenze
+;
---
+++ agi_hoheitsgrenzen_pub/agi_hoheitsgrenzen_pub_pub_hoheitsgrenzen_kantonsname_a3.sql
@@ -0,0 +1,13 @@
+SELECT
+    t_id,
+    t_ili_tid,
+    kantonsnummer,
+    kantonsname,
+    ori,
+    hali,
+    vali,
+    beschriftungstext,
+    pos
+FROM
+    agi_hoheitsgrenzen_pub.hoheitsgrenzen_kantonsname_a3
+;
