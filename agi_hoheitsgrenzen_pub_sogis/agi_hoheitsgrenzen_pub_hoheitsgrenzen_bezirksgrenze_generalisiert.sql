5ed3ac38038846d8f8870bbbfd254ee30c93a62d
barpastu <noemi.sturm@bd.so.ch>
2018-10-25 14:10:24 +0200

    adding missing files

---
+++ agi_hoheitsgrenzen_pub_sogis/agi_hoheitsgrenzen_pub_hoheitsgrenzen_bezirksgrenze_generalisiert.sql
@@ -0,0 +1,23 @@
+DELETE FROM agi_hoheitsgrenzen_pub.hoheitsgrenzen_bezirksgrenze_generalisiert
+;
+
+INSERT INTO agi_hoheitsgrenzen_pub.hoheitsgrenzen_bezirksgrenze_generalisiert (
+    bezirksname,
+    bezirksnummer,
+    kantonsname, 
+    geometrie
+)
+SELECT 
+    hoheitsgrenzen_gemeindegrenze_generalisiert.bezirksname,
+    hoheitsgrenzen_bezirk.bezirksnummer,
+    hoheitsgrenzen_gemeindegrenze_generalisiert.kantonsname,
+    ST_Multi(ST_Union(hoheitsgrenzen_gemeindegrenze_generalisiert.geometrie))
+FROM
+    agi_hoheitsgrenzen_pub.hoheitsgrenzen_gemeindegrenze_generalisiert
+    LEFT JOIN agi_hoheitsgrenzen.hoheitsgrenzen_bezirk
+        ON hoheitsgrenzen_bezirk.bezirksname = hoheitsgrenzen_gemeindegrenze_generalisiert.bezirksname
+GROUP BY
+    hoheitsgrenzen_gemeindegrenze_generalisiert.bezirksname,
+    hoheitsgrenzen_bezirk.bezirksnummer,
+    hoheitsgrenzen_gemeindegrenze_generalisiert.kantonsname
+;
---
+++ agi_hoheitsgrenzen_pub_sogis/agi_hoheitsgrenzen_pub_hoheitsgrenzen_gemeindegrenze_generalisiert.sql
@@ -0,0 +1,72 @@
+DELETE FROM agi_hoheitsgrenzen_pub.hoheitsgrenzen_gemeindegrenze_generalisiert
+;
+
+WITH
+    polygons AS (
+        SELECT
+            bfs_gemeindenummer,
+            (ST_Dump(geometrie)).geom
+        FROM
+            agi_hoheitsgrenzen_pub.hoheitsgrenzen_gemeindegrenze
+    ),
+    rings AS (
+        SELECT
+            ST_ExteriorRing((ST_DumpRings(geom)).geom) as geom
+        FROM 
+            polygons
+    ),
+    simplerings AS (
+        SELECT
+            ST_SimplifyPreserveTopology(ST_LineMerge(St_Union(geom)),50) as geom
+        FROM
+            rings
+    ),
+    simplelines AS (
+        SELECT
+            (ST_Dump(geom)).geom
+        FROM 
+            simplerings
+    ),
+    simplepolys AS (
+        SELECT 
+            (ST_Dump(ST_Polygonize(DISTINCT geom))).geom as geom
+        FROM 
+            simplelines
+    ),
+    simplegemeinde AS (
+        SELECT 
+            hoheitsgrenzen_gemeindegrenze.gemeindename,
+            hoheitsgrenzen_gemeindegrenze.bfs_gemeindenummer,
+            hoheitsgrenzen_gemeindegrenze.bezirksname,
+            hoheitsgrenzen_gemeindegrenze.kantonsname,
+            simplepolys.geom
+        FROM
+            agi_hoheitsgrenzen_pub.hoheitsgrenzen_gemeindegrenze, 
+            simplepolys
+        WHERE
+            ST_Intersects(hoheitsgrenzen_gemeindegrenze.geometrie, simplepolys.geom)
+            AND
+            ST_Area(ST_Intersection(simplepolys.geom, hoheitsgrenzen_gemeindegrenze.geometrie))/ST_Area(simplepolys.geom) > 0.5)
+
+INSERT INTO agi_hoheitsgrenzen_pub.hoheitsgrenzen_gemeindegrenze_generalisiert(
+    gemeindename,
+    bfs_gemeindenummer,
+    bezirksname,
+    kantonsname,
+    geometrie
+)
+
+SELECT
+    gemeindename,
+    bfs_gemeindenummer,
+    bezirksname,
+    kantonsname,
+    ST_Collect(geom) AS geometrie
+FROM 
+    simplegemeinde
+GROUP BY
+    bfs_gemeindenummer,
+    gemeindename, 
+    bezirksname,
+    kantonsname
+;
