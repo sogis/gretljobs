dba21418680f79c21c3cebd8d3d8f7388f632470
barpastu <noemi.sturm@bd.so.ch>
2018-11-09 07:30:52 +0100

    renaming db parameters

--- arp_veriso_nplso_grundlagendaten/gretl-job.groovy
+++ arp_veriso_nplso_grundlagendaten/gretl-job.groovy
@@ -1,24 +1,24 @@
 def dbUriSogis = ''
 def dbCredentialNameSogis = ''
-def dbUriNplso = ''
-def dbCredentialNameNplso = ''
+def dbUriVerisoNplso = ''
+def dbCredentialNameVerisoNplso = ''
 def gretljobsRepo = ''
 
 node("master") {
     dbUriSogis = "${env.DB_URI_SOGIS}"
     dbCredentialNameSogis = "${DB_CREDENTIAL_GRETL}"
-    dbUriNplso = "${env.DB_URI_NPLSO}"
-    dbCredentialNameNplso = "${DB_CREDENTIAL_GRETL}"
+    dbUriVerisoNplso = "${env.DB_URI_VERISO_NPLSO}"
+    dbCredentialNameVerisoNplso = "${DB_CREDENTIAL_GRETL}"
     gretljobsRepo = "${env.GRETL_JOB_REPO_URL}"
 }
 
 node ("gretl") {
     git "${gretljobsRepo}"
     dir(env.JOB_BASE_NAME) {
-        withCredentials([usernamePassword(credentialsId: "${dbCredentialNameSogis}", usernameVariable: 'dbUserSogis', passwordVariable: 'dbPwdSogis'), usernamePassword(credentialsId: "${dbCredentialNameNplso}", usernameVariable: 'dbUserNplso', passwordVariable: 'dbPwdNplso')]) {
+        withCredentials([usernamePassword(credentialsId: "${dbCredentialNameSogis}", usernameVariable: 'dbUserSogis', passwordVariable: 'dbPwdSogis'), usernamePassword(credentialsId: "${dbCredentialNameVerisoNplso}", usernameVariable: 'dbUserVerisoNplso', passwordVariable: 'dbPwdVerisoNplso')]) {
             sh "gradle --init-script /home/gradle/init.gradle \
                 -PdbUriSogis='${dbUriSogis}' -PdbUserSogis='${dbUserSogis}' -PdbPwdSogis='${dbPwdSogis}' \
-                -PdbUriNplso='${dbUriNplso}' -PdbUserNplso='${dbUserNplso}' -PdbPwdNplso='${dbPwdNplso}'"
+                -PdbUriVerisoNplso='${dbUriVerisoNplso}' -PdbUserVerisoNplso='${dbUserVerisoNplso}' -PdbPwdVerisoNplso='${dbPwdVerisoNplso}'"
         }
     }
 }
