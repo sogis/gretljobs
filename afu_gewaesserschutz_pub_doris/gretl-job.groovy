2e582fc733ae0815fc3ee8192660ce9f01f829bb
barpastu <noemi.sturm@bd.so.ch>
2018-12-10 10:43:29 +0100

    splitting gretljob afu_gewaesserschutz_pub to afu_gewasserschutz_pub_doris and afu_gewaesserschutz_pub_claude

---
+++ afu_gewaesserschutz_pub_doris/gretl-job.groovy
@@ -0,0 +1,26 @@
+def dbUriSogis = ''
+def dbCredentialNameSogis = ''
+def dbUriPub = ''
+def dbCredentialNamePub = ''
+def gretljobsRepo = ''
+
+node("master") {
+    dbUriSogis = "${env.DB_URI_SOGIS}"
+    dbCredentialNameSogis = "${DB_CREDENTIAL_GRETL}"
+    dbUriPub = "${env.DB_URI_PUB}"
+    dbCredentialNamePub = "${DB_CREDENTIAL_GRETL}"
+    gretljobsRepo = "${env.GRETL_JOB_REPO_URL}"
+}
+
+node ("gretl") {
+    gitBranch = "${params.BRANCH ?: 'master'}"
+    git url: "${gretljobsRepo}", branch: gitBranch
+    dir(env.JOB_BASE_NAME) {
+        withCredentials([usernamePassword(credentialsId: "${dbCredentialNameSogis}", usernameVariable: 'dbUserSogis', passwordVariable: 'dbPwdSogis'), usernamePassword(credentialsId: "${dbCredentialNamePub}", usernameVariable: 'dbUserPub', passwordVariable: 'dbPwdPub')]) {
+            sh "gradle --init-script /home/gradle/init.gradle \
+                -PdbUriSogis='${dbUriSogis}' -PdbUserSogis='${dbUserSogis}' -PdbPwdSogis='${dbPwdSogis}' \
+                -PdbUriPub='${dbUriPub}' -PdbUserPub='${dbUserPub}' -PdbPwdPub='${dbPwdPub}'"
+        }
+    }
+}
+
