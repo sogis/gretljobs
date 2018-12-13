0228abee2487849b55a2a99acca71b5c307a8a7a
schmandr <andreas.schmid@bd.so.ch>
2018-12-13 14:22:59 +0100

    Accept any gradle option

--- scripts/start-gretl.sh
+++ scripts/start-gretl.sh
@@ -1,16 +1,23 @@
 #!/bin/bash
 
 # use like this:
-# start-gretl.sh --docker_image sogis/gretl-runtime:14 --job_directory /home/gretl --task_name gradleTaskName -Pparam1=1 -Pparam2=2
+# start-gretl.sh --docker-image sogis/gretl-runtime:latest --job-directory ~/gretljobs/jobname [taskName...] [--option-name...]
+# [--option-name...] takes any Gradle option, including project properties
+# (e.g. -Pmyprop=myvalue) and system properties (e.g. -Dmyprop=myvalue).
+# See https://docs.gradle.org/current/userguide/command_line_interface.html
+# or the output of gradle -h for available options.
 
-task_parameter=()
+gradle_options=()
 
 while [ $# -gt 0 ]; do
-    if [[ $1 == *"--"* ]]; then
-        v="${1/--/}"
-        declare $v="$2"
-   elif [[ "$1" =~ ^"-P" ]]; then
-        task_parameter+=($1)
+    if [[ $1 == "--docker-image" ]]; then
+        declare docker_image="$2"
+        shift
+    elif [[ $1 == "--job-directory" ]]; then
+        declare job_directory="$2"
+        shift
+    else
+        gradle_options+=($1)
    fi
   shift
 done
@@ -30,15 +37,15 @@ db_parameter=(
 -PdbPwdAltlast4web=\'$DB_PWD_ALTLAST4WEB\' \
 )
 
-declare gretl_cmd="gretl $task_name ${task_parameter[@]} -PgretlShare=/tmp/gretl-share ${db_parameter[@]}"
+declare gretl_cmd="gretl ${gradle_options[@]} -PgretlShare=/tmp/gretl-share ${db_parameter[@]}"
 
 echo "======================================================="
 echo "Starts the GRETL runtime to execute the given GRETL job"
 echo "Docker Image: $docker_image"
-echo "task name: $task_name"
 echo "job directory: $job_directory"
-echo "task_parameter: ${task_parameter[@]}"
-#echo "gretl_cmd: $gretl_cmd"
+echo "Gradle options: ${gradle_options[@]}"
+# The following line is commented out because it would display database credentials
+# echo "gretl_cmd: $gretl_cmd"
 echo "======================================================="
 
 # special run configuration for jenkins-slave based image:
