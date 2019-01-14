4aa7eff02e955c1c00d6d3629e03dff27a0c56ff
barpastu <noemi.sturm@bd.so.ch>
2019-01-14 14:11:17 +0100

    send mail on failed job

--- agi_av_export_ai/gretl-job.groovy
+++ agi_av_export_ai/gretl-job.groovy
@@ -13,10 +13,23 @@ node("master") {
 }
 
 node ("gretl") {
-    git "${gretljobsRepo}"
-    dir(env.JOB_BASE_NAME) {
-        withCredentials([usernamePassword(credentialsId: "${dbCredentialNameSogis}", usernameVariable: 'dbUserSogis', passwordVariable: 'dbPwdSogis'), usernamePassword(credentialsId: "${aiCredentialName}", usernameVariable: 'aiUser', passwordVariable: 'aiPwd')]) {
-            sh "gradle --init-script /home/gradle/init.gradle -PdbUriSogis='${dbUriSogis}' -PdbUserSogis='${dbUserSogis}' -PdbPwdSogis='${dbPwdSogis}' -PaiServer='${aiServer}' -PaiUser='${aiUser}' -PaiPwd='${aiPwd}'"
+    try {
+        gitBranch = "${params.BRANCH ?: 'master'}"
+        git url: "${gretljobsRepo}", branch: gitBranch
+        dir(env.JOB_BASE_NAME) {
+            withCredentials([usernamePassword(credentialsId: "${dbCredentialNameSogis}", usernameVariable: 'dbUserSogis', passwordVariable: 'dbPwdSogis'), usernamePassword(credentialsId: "${aiCredentialName}", usernameVariable: 'aiUser', passwordVariable: 'aiPwd')]) {
+                sh "gradle --init-script /home/gradle/init.gradle -PdbUriSogis='${dbUriSogis}' -PdbUserSogis='${dbUserSogis}' -PdbPwdSogis='${dbPwdSogis}' -PaiServer='${aiServer}' -PaiUser='${aiUser}' -PaiPwd='${aiPwd}'"
+            }
         }
+    }
+    catch (e) {
+        echo 'Job failed'
+        emailext (
+                to: '${DEFAULT_RECIPIENTS}',
+                recipientProviders: [requestor()],
+                subject: "FAILED: Job ${env.JOB_NAME} ${env.BUILD_DISPLAY_NAME}",
+                body: "FAILED: Job ${env.JOB_NAME} ${env.BUILD_DISPLAY_NAME}. Check console output at ${env.BUILD_URL}"
+        )
+        throw e
     }
 }
--- arp_veriso_nplso_grundlagendaten/gretl-job.groovy
+++ arp_veriso_nplso_grundlagendaten/gretl-job.groovy
@@ -13,14 +13,26 @@ node("master") {
 }
 
 node ("gretl") {
-    gitBranch = "${params.BRANCH ?: 'master'}"
-    git url: "${gretljobsRepo}", branch: gitBranch
-    dir(env.JOB_BASE_NAME) {
-        withCredentials([usernamePassword(credentialsId: "${dbCredentialNameSogis}", usernameVariable: 'dbUserSogis', passwordVariable: 'dbPwdSogis'), usernamePassword(credentialsId: "${dbCredentialNameVerisoNplso}", usernameVariable: 'dbUserVerisoNplso', passwordVariable: 'dbPwdVerisoNplso')]) {
-            sh "gradle --init-script /home/gradle/init.gradle \
+    try {
+        gitBranch = "${params.BRANCH ?: 'master'}"
+        git url: "${gretljobsRepo}", branch: gitBranch
+        dir(env.JOB_BASE_NAME) {
+            withCredentials([usernamePassword(credentialsId: "${dbCredentialNameSogis}", usernameVariable: 'dbUserSogis', passwordVariable: 'dbPwdSogis'), usernamePassword(credentialsId: "${dbCredentialNameVerisoNplso}", usernameVariable: 'dbUserVerisoNplso', passwordVariable: 'dbPwdVerisoNplso')]) {
+                sh "gradle --init-script /home/gradle/init.gradle \
                 -PdbUriSogis='${dbUriSogis}' -PdbUserSogis='${dbUserSogis}' -PdbPwdSogis='${dbPwdSogis}' \
                 -PdbUriVerisoNplso='${dbUriVerisoNplso}' -PdbUserVerisoNplso='${dbUserVerisoNplso}' -PdbPwdVerisoNplso='${dbPwdVerisoNplso}'"
+            }
         }
+    }
+    catch (e) {
+        echo 'Job failed'
+        emailext (
+                to: '${DEFAULT_RECIPIENTS}',
+                recipientProviders: [requestor()],
+                subject: "FAILED: Job ${env.JOB_NAME} ${env.BUILD_DISPLAY_NAME}",
+                body: "FAILED: Job ${env.JOB_NAME} ${env.BUILD_DISPLAY_NAME}. Check console output at ${env.BUILD_URL}"
+        )
+        throw e
     }
 }
 
