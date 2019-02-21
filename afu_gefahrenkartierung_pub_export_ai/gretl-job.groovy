87593499a7fdefb13cd761a5f48e597c4d1c8b9f
Andreas Schmid <andreas.schmid@bd.so.ch>
2019-02-21 10:53:54 +0100

    Fix DB credential variables (#337)
    * Fix DB credential variables
    
    * Add job.properties

--- afu_gefahrenkartierung_pub_export_ai/gretl-job.groovy
+++ afu_gefahrenkartierung_pub_export_ai/gretl-job.groovy
@@ -1,14 +1,20 @@
 def dbUriSogis = ''
 def dbCredentialNameSogis = ''
+def dbUriEdit = ''
+def dbCredentialNameEdit = ''
+def dbUriPub = ''
+def dbCredentialNamePub = ''
 def aiServer = ''
 def aiCredentialName = ''
 def gretljobsRepo = ''
 
 node("master") {
-    dbUriPub = "${env.DB_URI_PUB}"
-    dbCredentialNamePub = "${DB_CREDENTIAL_GRETL}"
+    dbUriSogis = "${env.DB_URI_SOGIS}"
+    dbCredentialNameSogis = "${DB_CREDENTIAL_GRETL}"
     dbUriEdit = "${env.DB_URI_EDIT}"
     dbCredentialNameEdit = "${DB_CREDENTIAL_GRETL}"
+    dbUriPub = "${env.DB_URI_PUB}"
+    dbCredentialNamePub = "${DB_CREDENTIAL_GRETL}"
     aiServer = "${env.AI_SERVER}"
     aiCredentialName = "${AI_CREDENTIAL}"
     gretljobsRepo = "${env.GRETL_JOB_REPO_URL}"
@@ -19,10 +25,17 @@ node ("gretl") {
         gitBranch = "${params.BRANCH ?: 'master'}"
         git url: "${gretljobsRepo}", branch: gitBranch
         dir(env.JOB_BASE_NAME) {
-            withCredentials([usernamePassword(credentialsId: "${dbCredentialNameSogis}", usernameVariable: 'dbUserSogis', passwordVariable: 'dbPwdSogis'), usernamePassword(credentialsId: "${aiCredentialName}", usernameVariable: 'aiUser', passwordVariable: 'aiPwd')]) {
+            credentials = [
+                usernamePassword(credentialsId: "${dbCredentialNameSogis}", usernameVariable: 'dbUserSogis', passwordVariable: 'dbPwdSogis'),
+                usernamePassword(credentialsId: "${dbCredentialNameEdit}", usernameVariable: 'dbUserEdit', passwordVariable: 'dbPwdEdit'),
+                usernamePassword(credentialsId: "${dbCredentialNamePub}", usernameVariable: 'dbUserPub', passwordVariable: 'dbPwdPub'),
+                usernamePassword(credentialsId: "${aiCredentialName}", usernameVariable: 'aiUser', passwordVariable: 'aiPwd')
+            ]
+            withCredentials(credentials) {
                 sh "gradle --init-script /home/gradle/init.gradle \
-                -PdbUriPub='${dbUriPub}' -PdbUserPub='${dbUserPub}' -PdbPwdPub='${dbPwdPub}' \
+                -PdbUriSogis='${dbUriSogis}' -PdbUserSogis='${dbUserSogis}' -PdbPwdSogis='${dbPwdSogis}' \
                 -PdbUriEdit='${dbUriEdit}' -PdbUserEdit='${dbUserEdit}' -PdbPwdEdit='${dbPwdEdit}' \
+                -PdbUriPub='${dbUriPub}' -PdbUserPub='${dbUserPub}' -PdbPwdPub='${dbPwdPub}' \
                 -PaiServer='${aiServer}' -PaiUser='${aiUser}' -PaiPwd='${aiPwd}'"
             }
         }
