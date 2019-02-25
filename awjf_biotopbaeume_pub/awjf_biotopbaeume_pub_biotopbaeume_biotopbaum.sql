b8f309dee8061c94d3c591026557dcabd56651e4
barpasen <peter.senn@bd.so.ch>
2019-02-25 16:20:46 +0100

    Nochmal Test mit Leerschlag nach veränderten Zeilen

--- awjf_biotopbaeume_pub/awjf_biotopbaeume_pub_biotopbaeume_biotopbaum.sql
+++ awjf_biotopbaeume_pub/awjf_biotopbaeume_pub_biotopbaeume_biotopbaum.sql
@@ -29,10 +29,10 @@ SELECT
             THEN 'Schneeballblättriger Ahorn'
         WHEN baumart = 'Uebrige_Laubbaeume'
             THEN 'Übrige Laubbäume'
-        ELSE baumart
-    END AS baumart,
-    bhd,
-    baumhoehe,
+        ELSE baumart 
+    END AS baumart, 
+    bhd, 
+    baumhoehe, 
     CASE
         WHEN merkmal_1 = 'm1_Stammdurchmesser_70'
             THEN 'Stammdurchmesser ≥ 70cm'
@@ -236,7 +236,7 @@ GROUP BY
     waldeigentuemer_code,
     baumart,
     bhd,
-    baumhoehe,
+    baumhoehe, 
     merkmal_1,
     beschreibung_merkmal_1,
     merkmal_2,
