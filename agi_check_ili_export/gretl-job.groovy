b8fc890f83f37dade0249284177f7eb515e77554
barpastu <noemi.sturm@bd.so.ch>
2019-03-04 10:49:48 +0100

    adding some missing models and some new models; add a log file check

--- agi_check_ili_export/gretl-job.groovy
+++ agi_check_ili_export/gretl-job.groovy
@@ -2,6 +2,8 @@ def dbUriSogis = ''
 def dbCredentialNameSogis = ''
 def dbUriPub = ''
 def dbCredentialNamePub = ''
+def dbUriEdit = ''
+def dbCredentialNameEdit = ''
 def gretljobsRepo = ''
 
 node("master") {
@@ -9,6 +11,8 @@ node("master") {
     dbCredentialNameSogis = "${DB_CREDENTIAL_GRETL}"
     dbUriPub = "${env.DB_URI_PUB}"
     dbCredentialNamePub = "${DB_CREDENTIAL_GRETL}"
+    dbUriEdit = "${env.DB_URI_EDIT}"
+    dbCredentialNameEdit = "${DB_CREDENTIAL_GRETL}"
     gretljobsRepo = "${env.GRETL_JOB_REPO_URL}"
 }
 
@@ -17,20 +21,21 @@ node ("gretl") {
         gitBranch = "${params.BRANCH ?: 'master'}"
         git url: "${gretljobsRepo}", branch: gitBranch
         dir(env.JOB_BASE_NAME) {
-            withCredentials([usernamePassword(credentialsId: "${dbCredentialNameSogis}", usernameVariable: 'dbUserSogis', passwordVariable: 'dbPwdSogis'), usernamePassword(credentialsId: "${dbCredentialNamePub}", usernameVariable: 'dbUserPub', passwordVariable: 'dbPwdPub')]) {
+            withCredentials([usernamePassword(credentialsId: "${dbCredentialNameSogis}", usernameVariable: 'dbUserSogis', passwordVariable: 'dbPwdSogis'), usernamePassword(credentialsId: "${dbCredentialNamePub}", usernameVariable: 'dbUserPub', passwordVariable: 'dbPwdPub'), usernamePassword(credentialsId: "${dbCredentialNameEdit}", usernameVariable: 'dbUserEdit', passwordVariable: 'dbPwdEdit')]) {
                 sh "gradle --init-script /home/gradle/init.gradle \
-                    -PdbUriSogis='${dbUriSogis}' -PdbUserSogis='${dbUserSogis}' -PdbPwdSogis='${dbPwdSogis}' \
-                    -PdbUriPub='${dbUriPub}' -PdbUserPub='${dbUserPub}' -PdbPwdPub='${dbPwdPub}'"
+                -PdbUriSogis='${dbUriSogis}' -PdbUserSogis='${dbUserSogis}' -PdbPwdSogis='${dbPwdSogis}' \
+                -PdbUriEdit='${dbUriEdit}' -PdbUserEdit='${dbUserEdit}' -PdbPwdEdit='${dbPwdEdit}' \
+                -PdbUriPub='${dbUriPub}' -PdbUserPub='${dbUserPub}' -PdbPwdPub='${dbPwdPub}'"
             }
         }
     }
     catch (e) {
         echo 'Job failed'
         emailext (
-            to: '${DEFAULT_RECIPIENTS}',
-            recipientProviders: [requestor()],
-            subject: "FAILED: Job ${env.JOB_NAME} ${env.BUILD_DISPLAY_NAME}",
-            body: "FAILED: Job ${env.JOB_NAME} ${env.BUILD_DISPLAY_NAME}. Check console output at ${env.BUILD_URL}"
+                to: '${DEFAULT_RECIPIENTS}',
+                recipientProviders: [requestor()],
+                subject: "FAILED: Job ${env.JOB_NAME} ${env.BUILD_DISPLAY_NAME}",
+                body: "FAILED: Job ${env.JOB_NAME} ${env.BUILD_DISPLAY_NAME}. Check console output at ${env.BUILD_URL}"
         )
         throw e
     }
