32b80ee11f44e8a7c30343e4012abf4d88fbe0f5
uncoyote <jeker.git@outlook.de>
2019-04-02 16:10:58 +0200

    merged attributes, removed sogis_db transfer

--- afu_altlasten_pub/afu_altlasten_pub_belastete_standorte_altlast4web.sql
+++ afu_altlasten_pub/afu_altlasten_pub_belastete_standorte_altlast4web.sql
@@ -3,11 +3,10 @@ SELECT
     wkb_geometry AS geometrie,
     bezeichnung,
     vflz_combined_id_kt,
-    c_vflz_bearbstand,
+    CONCAT(c_vflz_unterstand, ', ' || c_vflz_bearbstand) AS c_vflz_bearbstand,
     c_bere_res_abwbewe,
     c_bere_res_abwbewe_txt,
-    c_vflz_unterstand,
-    c_vflz_vftyp 
+    c_vflz_vftyp
 FROM
     sogis_export.altlasten_belastete_standorte_somap
 ;
