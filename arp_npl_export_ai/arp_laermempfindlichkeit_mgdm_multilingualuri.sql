b45d96a8c1a24789b847904becaaf1a334d7a5d7
Stefan Ziegler <edi.gonzales@gmail.com>
2019-06-07 08:18:26 +0200

    [arp_npl_export_ai] completely rewritten sql stuff since it was broken by design

--- arp_npl_export_ai/arp_laermempfindlichkeit_mgdm_multilingualuri.sql
+++
@@ -1,9 +0,0 @@
-INSERT INTO arp_laermempfindlichkeit_mgdm.multilingualuri(
-    rechtsvrschrftn_dkment_textimweb
-)
-
-SELECT
-    t_id
-FROM
-    arp_laermempfindlichkeit_mgdm.rechtsvorschrften_dokument
-;
--- arp_npl_export_ai/arp_npl_mgdm_hauptnutzung_ch_hauptnutzung_ch.sql
+++
@@ -1,8 +0,0 @@
-SELECT
-    t_id,
-    t_ili_tid,
-    code,
-    bezeichnung
-FROM
-    arp_npl_hauptnutzung_mgdm.hauptnutzung_ch_hauptnutzung_ch
-;
