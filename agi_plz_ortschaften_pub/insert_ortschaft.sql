95bde81098c96a64302c8d25419567d3504ea090
bjsvwcur <38523407+bjsvwcur@users.noreply.github.com>
2019-06-18 09:45:02 +0200

    [agi_plz_ortschaften_pub] change procedure to Edit DB (#402)
    * [agi_plz_ortschaften_pub] GRETL-Job Erweitert auf Edit-DB
    
    * [agi_plz_ortschaften_pub] GRETL-Job Erweitert auf Edit-DB
    
    * [agi_plz_ortschaften_pub] change procedure to Edit DB
    
    * Update build.gradle
    
    * Update insert_postleitzahl.sql
    
    * Update build.gradle
    
    * Update build.gradle
    
    * Update build.gradle
    
    * Update build.gradle
    
    * Update build.gradle
    
    * Update build.gradle
    
    * Update build.gradle
    
    * Update build.gradle
    
    * Update build.gradle
    
    * Update build.gradle
    
    * Update build.gradle

---
+++ agi_plz_ortschaften_pub/insert_ortschaft.sql
@@ -0,0 +1,16 @@
+INSERT INTO agi_plz_ortschaften_pub.plzortschaften_ortschaft(ortschaftsname, status, geometrie)
+(
+SELECT 
+	plzortschaft_ortschaftsname.atext AS ortschaftsname, 
+    plzortschaft_ortschaft.status, 
+    ST_Multi(ST_Union(plzortschaft_ortschaft.flaeche)) AS geometrie
+FROM 
+    agi_plz_ortschaften.plzortschaft_ortschaftsname
+LEFT JOIN 
+    agi_plz_ortschaften.plzortschaft_ortschaft
+    ON plzortschaft_ortschaftsname.ortschaftsname_von = plzortschaft_ortschaft.t_id
+GROUP BY
+    plzortschaft_ortschaftsname.atext,
+    plzortschaft_ortschaft.status
+)
+;
--- agi_plz_ortschaften_pub/plzortschaften_postleitzahl.sql
+++ agi_plz_ortschaften_pub/plzortschaften_postleitzahl.sql
@@ -1,12 +1,8 @@
 SELECT 
-    plzortschaft_plz6.plz, 
-    plzortschaft_plz6.zusatzziffern,
-    plzortschaft_plz6.status,
-    ST_Multi(ST_Union(plzortschaft_plz6.flaeche)) AS geometrie
-FROM 
-    agi_plz_ortschaften.plzortschaft_plz6
-GROUP BY 
     plz, 
-    zusatzziffern, 
-    status
+    zusatzziffern,
+    status,
+    geometrie
+FROM 
+    agi_plz_ortschaften_pub.plzortschaften_postleitzahl
 ;
