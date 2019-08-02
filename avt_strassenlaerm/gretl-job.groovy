0539bdda64e75f5196beb1bdc3f1d612afb95158
Stefan Ziegler <edi.gonzales@gmail.com>
2019-08-02 16:52:17 +0200

    add avt_strassenlaerm job

---
+++ avt_strassenlaerm/gretl-job.groovy
@@ -0,0 +1,38 @@
+def dbUriEdit = ''
+def dbCredentialNameEdit = ''
+def dbUriPub = ''
+def dbCredentialNamePub = ''
+def gretljobsRepo = ''
+
+node("master") {
+    dbUriEdit = "${env.DB_URI_EDIT}"
+    dbCredentialNameEdit = "${DB_CREDENTIAL_GRETL}"
+    dbUriPub = "${env.DB_URI_PUB}"
+    dbCredentialNamePub = "${DB_CREDENTIAL_GRETL}"
+    gretljobsRepo = "${env.GRETL_JOB_REPO_URL}"
+}
+
+node ("gretl") {
+    try {
+        gitBranch = "${params.BRANCH ?: 'master'}"
+        git url: "${gretljobsRepo}", branch: gitBranch
+        dir(env.JOB_BASE_NAME) {
+            withCredentials(usernamePassword(credentialsId: "${dbCredentialNameEdit}", usernameVariable: 'dbUserEdit', passwordVariable: 'dbPwdEdit'), usernamePassword(credentialsId: "${dbCredentialNamePub}", usernameVariable: 'dbUserPub', passwordVariable: 'dbPwdPub')]) {
+                sh "gradle --init-script /home/gradle/init.gradle \
+                -PdbUriEdit='${dbUriEdit}' -PdbUserEdit='${dbUserEdit}' -PdbPwdEdit='${dbPwdEdit}' \
+                -PdbUriPub='${dbUriPub}' -PdbUserPub='${dbUserPub}' -PdbPwdPub='${dbPwdPub}'"
+            }
+        }
+    }
+    catch (e) {
+        echo 'Job failed'
+        emailext (
+                to: '${DEFAULT_RECIPIENTS}',
+                recipientProviders: [requestor()],
+                subject: "FAILED: Job ${env.JOB_NAME} ${env.BUILD_DISPLAY_NAME}",
+                body: "FAILED: Job ${env.JOB_NAME} ${env.BUILD_DISPLAY_NAME}. Check console output at ${env.BUILD_URL}"
+        )
+        throw e
+    }
+}
+
