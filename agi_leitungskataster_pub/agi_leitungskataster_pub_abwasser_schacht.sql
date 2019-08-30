c27863ee7973e3e139447cdece052abd7c328c51
bjsvwcur <38523407+bjsvwcur@users.noreply.github.com>
2019-08-30 13:49:35 +0200

    Update agi_leitungskataster_pub_abwasser_schacht.sql

--- agi_leitungskataster_pub/agi_leitungskataster_pub_abwasser_schacht.sql
+++ agi_leitungskataster_pub/agi_leitungskataster_pub_abwasser_schacht.sql
@@ -1,6 +1,5 @@
 SELECT 
-    row_number() OVER ()::integer AS ogc_fid, 
-    bauwerk.t_id AS t_id, 
+    row_number() OVER ()::integer AS t_id, 
     knoten.lage AS geometrie, 
     bauwerk.baujahr, 
     bauwerk.bezeichnung, 
