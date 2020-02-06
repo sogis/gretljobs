9fef91c4eca3711963e4f242803f0f1c870d826f
uncoyote <jeker.git@outlook.de>
2020-02-06 14:31:38 +0100

    [afu_gewaesserschutz_pub] Correct gszustr query

--- afu_gewaesserschutz_pub/sogis/aww_gszustr.sql
+++ afu_gewaesserschutz_pub/sogis/aww_gszustr.sql
@@ -1,12 +1,17 @@
-SELECT 
-	t_id, 
-	geometrie, 
-	name, 
-	im_kanton, 
-	typ, 
-	typ_text
-FROM	
-	public.aww_gszustr
+SELECT
+    ogc_fid AS t_id,
+    wkb_geometry AS geometrie,
+    "name",
+    im_kanton,
+    typ,
+    CASE
+        WHEN typ = 'Zo'
+            THEN 'Zo: Zuströmbereich für Oberflächengewässer'
+        WHEN typ = 'Zu'
+            THEN 'Zu: Zuströmbereich für Grundwasserfassung'
+    END AS typ_text
+FROM
+    public.aww_gszustr
 WHERE
-	archive = 0	AND im_kanton = 0
+    archive = 0 AND im_kanton = 0
 ;
