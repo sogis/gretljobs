019a2a8cc8e7258e022ffbb158859873b86624a5
uncoyote <jeker.git@outlook.de>
2020-02-10 10:35:22 +0100

    afu_erdwaermesonden_pub: 'Angepasst auf left outer, um Anlagen ohne Bohrung auch zu publizieren'

--- afu_erdwaermesonden_pub/bohrungen.sql
+++ afu_erdwaermesonden_pub/bohrungen.sql
@@ -1,30 +1,29 @@
 SELECT
-	  erdwaermesonden_bohrung.t_id,
-      erdwaermesonden_bohrung.anlage_uuid,
-      bohrung_nummer AS nummer,
-      status_code AS anlage_status_code,
-      status_text AS anlage_status_text,
-      bezeichnung AS anlage_bezeichnung,
-      anzahl_bohrloecher AS anlage_anzahl_bohrloecher,
-      grundbuchnummer_text AS anlage_grundbuchnummer_text,
-      datum_bewilligung AS anlage_datum_bewilligung,
-      gemeindename AS anlage_gemeindename,
-      sondenmeter_summe AS anlage_sondenmeter_summe,
-      leistung AS anlage_leistung,
-      CASE
-          WHEN 'ja' = LOWER(waermeeintrag) THEN TRUE
-          ELSE FALSE
-      END AS anlage_waermeeintrag,
-      waermequelle_code,
-      waermequelle_text,
-      bohrtiefe,
-      vorkommnisse,
-      bohrprofil_link,
-      bohrprofil_vorhanden,
-      erdwaermesonden_bohrung.geometrie
+	erdwaermesonden_anlage.anlage_uuid,
+	bohrung_nummer AS nummer,
+	status_code AS anlage_status_code,
+	status_text AS anlage_status_text,
+	bezeichnung AS anlage_bezeichnung,
+	anzahl_bohrloecher AS anlage_anzahl_bohrloecher,
+	grundbuchnummer_text AS anlage_grundbuchnummer_text,
+	datum_bewilligung AS anlage_datum_bewilligung,
+	gemeindename AS anlage_gemeindename,
+	sondenmeter_summe AS anlage_sondenmeter_summe,
+	leistung AS anlage_leistung,
+	CASE
+	WHEN 'ja' = LOWER(waermeeintrag) THEN TRUE
+	ELSE FALSE
+	END AS anlage_waermeeintrag,
+	waermequelle_code,
+	waermequelle_text,
+	bohrtiefe,
+	vorkommnisse,
+	bohrprofil_link,
+	COALESCE(FALSE, bohrprofil_vorhanden) AS bohrprofil_vorhanden,
+	erdwaermesonden_bohrung.geometrie
 FROM
-	afu_erdwaermesonden.erdwaermesonden_bohrung
-	INNER JOIN afu_erdwaermesonden.erdwaermesonden_anlage
-		ON erdwaermesonden_bohrung.anlage_uuid = erdwaermesonden_anlage.anlage_uuid
+	afu_erdwaermesonden.erdwaermesonden_anlage
+	LEFT JOIN afu_erdwaermesonden.erdwaermesonden_bohrung
+		ON erdwaermesonden_anlage.anlage_uuid = erdwaermesonden_bohrung.anlage_uuid
 WHERE 
 	geschaeftsstatus_code = 'ComEwsStatusVgr-abg'
