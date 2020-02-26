cef309acec2a1704df12eb9aa6f13cab3e789b5f
bjsvwcur <sandra.curiger@bd.so.ch>
2020-02-26 12:14:15 +0100

    [arp_mehrjahresprogramm_edit] Korrektur SQL-Files

--- arp_mehrjahresprogramm_edit/mjpnatur_flaechen.sql
+++ arp_mehrjahresprogramm_edit/mjpnatur_flaechen.sql
@@ -1,5 +1,5 @@
 SELECT 
-    fla_geom_t.wkb_geometry AS geometrie,
+    ST_Multi(fla_geom_t.wkb_geometry) AS geometrie,
     vereinbarung.vbnr AS vereinbarungsnummer
 FROM mjpnatur.flaechen_geom_t AS fla_geom_t
     LEFT JOIN mjpnatur.flaechen AS fla_attr
