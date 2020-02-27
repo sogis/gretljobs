54e94834b74e901ec49a3da751360a87e376cc00
bjsvwcur <sandra.curiger@bd.so.ch>
2020-02-27 11:01:18 +0100

    [arp_nutzungsvereinbarung_pub] Daten neu in edit-DB

--- arp_nutzungsvereinbarung_pub/arp_nutzungsvereinbarung_pub_nutzungsvrnbrngen_nutzungsvreinbarungen.sql
+++ arp_nutzungsvereinbarung_pub/arp_nutzungsvereinbarung_pub_nutzungsvrnbrngen_nutzungsvreinbarungen.sql
@@ -1,41 +1,39 @@
 WITH
     nutzungsvereinbarung AS (
-        SELECT 
+        SELECT
             nutzungsvereinbarungen.t_ili_tid,
             nutzungsvereinbarungen.nummer,
             nutzungsvereinbarungen.vertrag,
             nutzungsvereinbarungen.datum,
             nutzungsvereinbarungen.flaechenart,
-            personen.vorname || ' ' || personen.name || ', ' || personen.ort AS bewirtschafter,
+            mjp_person.vorname || ' ' || mjp_person.aname || ', ' || mjp_person.ortschaft AS bewirtschafter,
             projekte.aname AS projekt,
             nutzungsvereinbarungen.geometrie
         FROM
             arp_nutzungsvereinbarung.nutzungsvrnbrngen_nutzungsvereinbarungen AS nutzungsvereinbarungen
             LEFT JOIN arp_nutzungsvereinbarung.nutzungsvrnbrngen_projekte AS projekte
                 ON projekte.t_id = nutzungsvereinbarungen.projekt_vereinbarung
-            LEFT JOIN mjpnatur.personen
-                ON personen.persid = nutzungsvereinbarungen.bewirtschafter_persid
-        WHERE
-            personen.archive = 0
+            LEFT JOIN arp_mehrjahresprogramm.mehrjahresprgramm_person AS mjp_person
+                ON mjp_person.personenid = nutzungsvereinbarungen.bewirtschafter_persid   
     ),
     grundstueck AS (
         SELECT
             grundstueck.nummer,
             liegenschaft.geometrie,
-            gemeinde.name
+            gemeinde.aname AS name
         FROM
-            av_avdpool_ng.liegenschaften_liegenschaft AS liegenschaft
-            JOIN av_avdpool_ng.liegenschaften_grundstueck AS grundstueck
-                ON liegenschaft.liegenschaft_von = grundstueck.tid
-            LEFT JOIN av_avdpool_ng.gemeindegrenzen_gemeinde AS gemeinde
-                ON gemeinde.gem_bfs = grundstueck.gem_bfs
+            agi_dm01avso24.liegenschaften_liegenschaft AS liegenschaft
+            JOIN agi_dm01avso24.liegenschaften_grundstueck AS grundstueck
+                ON liegenschaft.liegenschaft_von = grundstueck.t_id
+            LEFT JOIN agi_dm01avso24.gemeindegrenzen_gemeinde AS gemeinde
+                ON gemeinde.t_datasetname = grundstueck.t_datasetname
     ),
     flurname AS (
         SELECT
             flurname.geometrie,
-            flurname.name
+            flurname.aname AS name
         FROM
-            av_avdpool_ng.nomenklatur_flurname AS flurname
+            agi_dm01avso24.nomenklatur_flurname AS flurname
   )
 
 SELECT
@@ -46,7 +44,7 @@ SELECT
     nutzungsvereinbarung.flaechenart,
     nutzungsvereinbarung.bewirtschafter AS bewirtschafter_name,
     nutzungsvereinbarung.projekt AS projekte,
-    string_agg(DISTINCT grundstueck.name, ', ' ORDER BY grundstueck.name) AS gemeinde,
+    string_agg(DISTINCT grundstueck.name , ', ' ORDER BY grundstueck.name) AS gemeinde,
     string_agg(DISTINCT grundstueck.nummer, ', ' ORDER BY grundstueck.nummer) AS grundstueck_nr,
     string_agg(DISTINCT flurname.name, ', ' ORDER BY flurname.name) AS flurname,
     nutzungsvereinbarung.geometrie
