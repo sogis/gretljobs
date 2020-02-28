77ff1d55054b117f41a6c6955c80fd1bb405c7b5
bjsvwcur <sandra.curiger@bd.so.ch>
2020-02-28 13:52:47 +0100

    [arp_mehrjahresprogramm_edit] persid hinzugefügt

--- arp_mehrjahresprogramm_edit/mjpnatur_flaechen.sql
+++ arp_mehrjahresprogramm_edit/mjpnatur_flaechen.sql
@@ -1,6 +1,7 @@
 SELECT 
     ST_Multi(fla_geom_t.wkb_geometry) AS geometrie,
-    vereinbarung.vbnr AS vereinbarungsnummer
+    vereinbarung.vbnr AS vereinbarungsnummer,
+    vereinbarung.persid AS person_flaeche
 FROM mjpnatur.flaechen_geom_t AS fla_geom_t
     LEFT JOIN mjpnatur.flaechen AS fla_attr
         ON fla_attr.vereinbarungid = fla_geom_t.polyid
