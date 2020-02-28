6f138ce513a8ec73293684cddb17f084f49c0213
bjsvwcur <38523407+bjsvwcur@users.noreply.github.com>
2020-02-28 15:24:57 +0100

    Update mjpnatur_flaechen.sql

--- arp_mehrjahresprogramm_edit/mjpnatur_flaechen.sql
+++ arp_mehrjahresprogramm_edit/mjpnatur_flaechen.sql
@@ -1,10 +1,10 @@
 SELECT 
     ST_Multi(fla_geom_t.wkb_geometry) AS geometrie,
     vereinbarung.vbnr AS vereinbarungsnummer,
-    vereinbarung.persid AS person_flaeche
+    vereinbarung.persid AS PersonenID
 FROM mjpnatur.flaechen_geom_t AS fla_geom_t
     LEFT JOIN mjpnatur.flaechen AS fla_attr
         ON fla_attr.vereinbarungid = fla_geom_t.polyid
     LEFT JOIN mjpnatur.vereinbarung AS vereinbarung
         ON fla_attr.vereinbarungid = vereinbarung.vereinbarungsid
-WHERE vereinbarung.archive = 0 AND vereinbarung.persid IS NOT NULL
+WHERE vereinbarung.archive = 0
