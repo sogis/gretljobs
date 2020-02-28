51e9dd2aa3dbdd4067817d9bc6ab03ceebcfff4a
bjsvwcur <38523407+bjsvwcur@users.noreply.github.com>
2020-02-28 14:40:11 +0100

    Update mjpnatur_flaechen.sql

--- arp_mehrjahresprogramm_edit/mjpnatur_flaechen.sql
+++ arp_mehrjahresprogramm_edit/mjpnatur_flaechen.sql
@@ -7,4 +7,4 @@ FROM mjpnatur.flaechen_geom_t AS fla_geom_t
         ON fla_attr.vereinbarungid = fla_geom_t.polyid
     LEFT JOIN mjpnatur.vereinbarung AS vereinbarung
         ON fla_attr.vereinbarungid = vereinbarung.vereinbarungsid
-WHERE fla_geom_t.archive = 0 AND vereinbarung.persid IS NOT NULL
+WHERE vereinbarung.archive = 0 AND vereinbarung.persid IS NOT NULL
