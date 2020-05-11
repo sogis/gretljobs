cd7c6f37c12b8055132f72c7b92f8abced840a75
andreasneumann <a.neumann@carto.net>
2020-05-11 22:04:14 +0200

    improving hoheitsgrenzpunkte

--- agi_mopublic_pub/agi_mopublic_pub_mopublic_hoheitsgrenzpunkt.sql
+++ agi_mopublic_pub/agi_mopublic_pub_mopublic_hoheitsgrenzpunkt.sql
@@ -1,3 +1,12 @@
+WITH aimport AS
+(
+	SELECT
+		max(importdate) AS importdate, dataset
+	FROM
+		agi_dm01avso24.t_ili2db_import
+	GROUP BY
+		dataset 
+)
 SELECT 
     hoheitsgrenzpunkt.identifikator AS nummer,
     hoheitsgrenzpunkt.punktzeichen AS punktzeichen_txt,
@@ -42,14 +51,6 @@ FROM
         ON hoheitsgrenzpunkt.entstehung = nachfuehrung.t_id
     LEFT JOIN agi_dm01avso24.t_ili2db_basket AS basket
         ON hoheitsgrenzpunkt.t_basket = basket.t_id    
-    LEFT JOIN 
-    (
-        SELECT
-            max(importdate) AS importdate, dataset
-        FROM
-            agi_dm01avso24.t_ili2db_import
-        GROUP BY
-            dataset 
-    ) AS aimport
+    LEFT JOIN aimport
         ON basket.dataset = aimport.dataset    
 ;
