2a1e794f5b0d5ff20a4a4735dfe7e6a686ba17be
andreasneumann <a.neumann@carto.net>
2020-05-11 17:45:21 +0200

    improving bodenbedeckung

--- agi_mopublic_pub/agi_mopublic_pub_mopublic_bodenbedeckung_proj.sql
+++ agi_mopublic_pub/agi_mopublic_pub_mopublic_bodenbedeckung_proj.sql
@@ -1,3 +1,12 @@
+WITH aimport AS
+(
+	SELECT
+		max(importdate) AS importdate, dataset
+	FROM
+		agi_dm01avso24.t_ili2db_import
+	GROUP BY
+		dataset 
+)
 SELECT
     CASE 
         WHEN split_part(bodenbedeckung.art,'.', array_upper(string_to_array(bodenbedeckung.art, '.'), 1))='fliessendes' 
@@ -19,14 +28,6 @@ FROM
         ON bodenbedeckung.entstehung = nachfuehrung.t_id
     LEFT JOIN agi_dm01avso24.t_ili2db_basket AS basket
     	ON bodenbedeckung.t_basket = basket.t_id
-    LEFT JOIN 
-    (
-    	SELECT
-			max(importdate) AS importdate, dataset
-		FROM
-			agi_dm01avso24.t_ili2db_import
-		GROUP BY
-			dataset 
-    ) AS  aimport
-    	ON basket.dataset = aimport.dataset
+    LEFT JOIN aimport
+        ON basket.dataset = aimport.dataset    
 ; 
