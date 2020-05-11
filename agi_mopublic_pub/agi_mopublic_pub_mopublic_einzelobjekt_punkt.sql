75274640f9092390adc8cf0955e29ea36444f171
andreasneumann <a.neumann@carto.net>
2020-05-11 17:54:03 +0200

    improving einzelobjekte

--- agi_mopublic_pub/agi_mopublic_pub_mopublic_einzelobjekt_punkt.sql
+++ agi_mopublic_pub/agi_mopublic_pub_mopublic_einzelobjekt_punkt.sql
@@ -1,3 +1,12 @@
+WITH aimport AS
+(
+	SELECT
+		max(importdate) AS importdate, dataset
+	FROM
+		agi_dm01avso24.t_ili2db_import
+	GROUP BY
+		dataset 
+)
 SELECT
     split_part(einzelobjekt.art,'.',array_upper(string_to_array(einzelobjekt.art,'.'), 1)) AS art_txt,
     CASE 
@@ -17,14 +26,6 @@ FROM
         ON nachfuehrung.t_id = einzelobjekt.entstehung
     LEFT JOIN agi_dm01avso24.t_ili2db_basket AS basket
     	ON einzelobjekt.t_basket = basket.t_id
-    LEFT JOIN 
-    (
-    	SELECT
-			max(importdate) AS importdate, dataset
-		FROM
-			agi_dm01avso24.t_ili2db_import
-		GROUP BY
-			dataset 
-    ) AS  aimport
-    	ON basket.dataset = aimport.dataset
+    LEFT JOIN aimport
+        ON basket.dataset = aimport.dataset    
 ;
