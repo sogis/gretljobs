50b6f8a0b7b3452f997cda5746064f5f6d266e0f
uncoyote <jeker.git@outlook.de>
2020-05-27 09:29:50 +0200

    ada_denkmalschutz_pub: job created

---
+++ ada_denkmalschutz_pub/denkmal_polygon.sql
@@ -0,0 +1,33 @@
+WITH 
+
+mpoly AS (
+	SELECT 
+		denkmal_id,
+		st_multi(st_union(apolygon)) AS mpoly
+	FROM 
+		ada_denkmalschutz.gis_geometrie
+	WHERE 
+		apolygon IS NOT NULL 
+	GROUP BY 
+		denkmal_id
+)
+
+SELECT 
+	id, 
+	objektname, 
+	gemeindename, 
+	gemeindeteil, 
+	adr_strasse, 
+	adr_hausnummer, 
+	objektart_code, 
+	objektart_text, 
+	schutzstufe_code, 
+	schutzstufe_text, 
+	schutzdurchgemeinde,
+	mpoly 
+FROM 
+	ada_denkmalschutz.fachapplikation_denkmal
+JOIN
+	mpoly 
+		ON fachapplikation_denkmal.id = mpoly.denkmal_id
+;
---
+++ ada_denkmalschutz_pub/denkmal_punkt.sql
@@ -0,0 +1,33 @@
+WITH 
+
+mpoint AS (
+	SELECT 
+		denkmal_id,
+		st_multi(st_union(punkt)) AS mpunkt
+	FROM 
+		ada_denkmalschutz.gis_geometrie
+	WHERE 
+		punkt IS NOT NULL 
+	GROUP BY 
+		denkmal_id
+)
+
+SELECT 
+	id, 
+	objektname, 
+	gemeindename, 
+	gemeindeteil, 
+	adr_strasse, 
+	adr_hausnummer, 
+	objektart_code, 
+	objektart_text, 
+	schutzstufe_code, 
+	schutzstufe_text, 
+	schutzdurchgemeinde,
+	mpunkt
+FROM 
+	ada_denkmalschutz.fachapplikation_denkmal
+JOIN
+	mpoint 
+		ON fachapplikation_denkmal.id = mpoint.denkmal_id
+;
