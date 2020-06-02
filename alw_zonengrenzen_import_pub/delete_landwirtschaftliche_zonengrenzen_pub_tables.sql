cbdf88728513e3f993e878aa9b9b86d3fb072f1a
barpasen <peter.senn@bd.so.ch>
2020-06-02 11:42:58 +0200

    Überarbeitete Version

---
+++ alw_zonengrenzen_import_pub/delete_landwirtschaftliche_zonengrenzen_pub_tables.sql
@@ -0,0 +1,24 @@
+DELETE FROM
+    alw_zonengrenzen_pub.catlgbjttrs_v1catalogues_item
+;
+DELETE FROM 
+    alw_zonengrenzen_pub.gueltigkeitsdauer_bezugsjahr
+;
+DELETE FROM 
+    alw_zonengrenzen_pub.languagecode_iso639_1
+;
+DELETE FROM
+    alw_zonengrenzen_pub.localisedtext
+;
+DELETE FROM 
+    alw_zonengrenzen_pub.lz_kataloge_lz_katalog_typ
+;
+DELETE FROM 
+    alw_zonengrenzen_pub.lz_kataloge_lz_katalog_typref
+;
+DELETE FROM 
+    alw_zonengrenzen_pub.multilingualtext 
+;
+DELETE FROM 
+    alw_zonengrenzen_pub.zonengrenzen_lz_flaeche
+;
