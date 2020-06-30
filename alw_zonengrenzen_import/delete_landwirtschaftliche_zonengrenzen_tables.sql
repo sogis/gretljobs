ef476112e9f0e402d09240b3d8ac0f61e6520c64
barpasen <peter.senn@bd.so.ch>
2020-06-30 14:04:59 +0200

    Zonengrenzen neu in Edit-DB

---
+++ alw_zonengrenzen_import/delete_landwirtschaftliche_zonengrenzen_tables.sql
@@ -0,0 +1,24 @@
+DELETE FROM
+    alw_zonengrenzen.catlgbjttrs_v1catalogues_item
+;
+DELETE FROM 
+    alw_zonengrenzen.gueltigkeitsdauer_bezugsjahr
+;
+DELETE FROM 
+    alw_zonengrenzen.languagecode_iso639_1
+;
+DELETE FROM
+    alw_zonengrenzen.localisedtext
+;
+DELETE FROM 
+    alw_zonengrenzen.lz_kataloge_lz_katalog_typ
+;
+DELETE FROM 
+    alw_zonengrenzen.lz_kataloge_lz_katalog_typref
+;
+DELETE FROM 
+    alw_zonengrenzen.multilingualtext 
+;
+DELETE FROM 
+    alw_zonengrenzen.zonengrenzen_lz_flaeche
+;
