1bd1b246b3120f99a757e34a0a452c926b5c1da0
bjsvwcur <38523407+bjsvwcur@users.noreply.github.com>
2020-08-18 16:55:06 +0200

    Update arp_agglomerationsprogramme_pub_agglomrtnsprgrmme_massnahme_flaeche.sql

--- arp_agglomerationsprogramme_pub/arp_agglomerationsprogramme_pub_agglomrtnsprgrmme_massnahme_flaeche.sql
+++ arp_agglomerationsprogramme_pub/arp_agglomerationsprogramme_pub_agglomrtnsprgrmme_massnahme_flaeche.sql
@@ -1,6 +1,6 @@
 SELECT
     massnahme.t_id,
-    aggloprogramm.aname AS aggloprogramm,
+    aggloprogramm.aname AS agglomerationsprogramm,
     aggloprogramm.generation,
     aggloprogramm.agglo_nu AS agglomerationsprogramm_nr,
     aggloprogramm.url AS agglomerationsprogramm_url,
