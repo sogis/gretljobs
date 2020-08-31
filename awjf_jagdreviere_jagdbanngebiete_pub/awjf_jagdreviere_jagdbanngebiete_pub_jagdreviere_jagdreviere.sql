33da34acd454b705831939a28eec97569c61d0f2
bjsvwcur <sandra.curiger@bd.so.ch>
2020-08-31 16:33:17 +0200

    [awjf_jagdreviere_jagdbanngebiete_pub] Neuer Gretljob

---
+++ awjf_jagdreviere_jagdbanngebiete_pub/awjf_jagdreviere_jagdbanngebiete_pub_jagdreviere_jagdreviere.sql
@@ -0,0 +1,29 @@
+WITH gemeinden AS (
+    SELECT
+        jagdreviere_jagdreviere.t_id,
+        string_agg(DISTINCT hoheitsgrenzen_gemeindegrenze.gemeindename, ', ' ORDER BY hoheitsgrenzen_gemeindegrenze.gemeindename ASC) AS gemeinden
+    FROM
+        agi_hoheitsgrenzen_pub.hoheitsgrenzen_gemeindegrenze,
+        awjf_jagdreviere_jagdbanngebiete.jagdreviere_jagdreviere
+    WHERE
+        ST_DWithin(jagdreviere_jagdreviere.geometrie, hoheitsgrenzen_gemeindegrenze.geometrie, 0)
+    GROUP BY
+       jagdreviere_jagdreviere.t_id
+)
+
+SELECT
+    jagdreviere.nummer,
+	jagdreviere.aname,
+	jagdreviere.pacht,
+	jagdreviere.geometrie,
+	round(st_perimeter(jagdreviere.geometrie))::integer AS umfang,
+	round(st_area(jagdreviere.geometrie))::integer AS flache,
+	hegering.aname AS hegering,
+	gemeinden.gemeinden
+	
+FROM
+    awjf_jagdreviere_jagdbanngebiete.jagdreviere_jagdreviere jagdreviere
+    LEFT JOIN awjf_jagdreviere_jagdbanngebiete.jagdreviere_hegering hegering
+        ON jagdreviere.hegering_jagdrevier = hegering.t_id
+	LEFT JOIN gemeinden
+        ON gemeinden.t_id = jagdreviere.t_id
