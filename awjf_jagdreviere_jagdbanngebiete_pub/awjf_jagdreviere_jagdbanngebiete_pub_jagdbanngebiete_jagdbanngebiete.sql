2e653accc02272185c055961965ea71de1bf3dc5
bjsvwcur <sandra.curiger@bd.so.ch>
2020-08-31 16:41:52 +0200

    [awjf_jagdreviere_jagdbanngebiete_pub] Korrektur Attributname flaeche

--- awjf_jagdreviere_jagdbanngebiete_pub/awjf_jagdreviere_jagdbanngebiete_pub_jagdbanngebiete_jagdbanngebiete.sql
+++ awjf_jagdreviere_jagdbanngebiete_pub/awjf_jagdreviere_jagdbanngebiete_pub_jagdbanngebiete_jagdbanngebiete.sql
@@ -3,7 +3,7 @@ SELECT
     jagdbanngebiete.aname,
     jagdbanngebiete.geometrie,
     art.description AS art,
-    round(st_area(jagdbanngebiete.geometrie))::integer AS flache,
+    round(st_area(jagdbanngebiete.geometrie))::integer AS flaeche,
     round(st_perimeter(jagdbanngebiete.geometrie))::integer AS umfang
 FROM
     awjf_jagdreviere_jagdbanngebiete.jagdbanngebiete_jagdbanngebiete jagdbanngebiete
