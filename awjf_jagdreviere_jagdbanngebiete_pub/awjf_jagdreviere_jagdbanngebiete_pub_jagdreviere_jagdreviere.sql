bafdf345b609307350f88a1f43c78d9c40db1c03
bjsvwcur <38523407+bjsvwcur@users.noreply.github.com>
2020-08-31 16:37:49 +0200

    Update awjf_jagdreviere_jagdbanngebiete_pub_jagdreviere_jagdreviere.sql

--- awjf_jagdreviere_jagdbanngebiete_pub/awjf_jagdreviere_jagdbanngebiete_pub_jagdreviere_jagdreviere.sql
+++ awjf_jagdreviere_jagdbanngebiete_pub/awjf_jagdreviere_jagdbanngebiete_pub_jagdreviere_jagdreviere.sql
@@ -1,14 +1,14 @@
 WITH gemeinden AS (
-    SELECT
-        jagdreviere_jagdreviere.t_id,
-        string_agg(DISTINCT hoheitsgrenzen_gemeindegrenze.gemeindename, ', ' ORDER BY hoheitsgrenzen_gemeindegrenze.gemeindename ASC) AS gemeinden
-    FROM
-        agi_hoheitsgrenzen_pub.hoheitsgrenzen_gemeindegrenze,
-        awjf_jagdreviere_jagdbanngebiete.jagdreviere_jagdreviere
-    WHERE
-        ST_DWithin(jagdreviere_jagdreviere.geometrie, hoheitsgrenzen_gemeindegrenze.geometrie, 0)
-    GROUP BY
-       jagdreviere_jagdreviere.t_id
+SELECT
+    jagdreviere_jagdreviere.t_id,
+    string_agg(DISTINCT hoheitsgrenzen_gemeindegrenze.gemeindename, ', ' ORDER BY hoheitsgrenzen_gemeindegrenze.gemeindename ASC) AS gemeinden
+FROM
+    agi_hoheitsgrenzen_pub.hoheitsgrenzen_gemeindegrenze,
+    awjf_jagdreviere_jagdbanngebiete.jagdreviere_jagdreviere
+WHERE
+    ST_DWithin(jagdreviere_jagdreviere.geometrie, hoheitsgrenzen_gemeindegrenze.geometrie, 0)
+GROUP BY
+   jagdreviere_jagdreviere.t_id
 )
 
 SELECT
