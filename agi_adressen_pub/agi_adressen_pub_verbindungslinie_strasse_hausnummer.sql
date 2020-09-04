5ea8ee72ad4bec0e8879b40c9a124ea8ee13b008
Andreas Schmid <andreas.schmid@bd.so.ch>
2020-09-04 11:26:12 +0200

    [agi_adressen_pub] Unnötige Leerzeichen an Zeilenenden gelöscht

--- agi_adressen_pub/agi_adressen_pub_verbindungslinie_strasse_hausnummer.sql
+++ agi_adressen_pub/agi_adressen_pub_verbindungslinie_strasse_hausnummer.sql
@@ -1,33 +1,33 @@
-WITH 
+WITH
     strassen AS (
-        SELECT 
-            ST_Union(geometrie) as geometrie, 
+        SELECT
+            ST_Union(geometrie) as geometrie,
             strassenstueck_von
-        FROM 
+        FROM
             agi_dm01avso24.gebaeudeadressen_strassenstueck
-        GROUP BY 
+        GROUP BY
             strassenstueck_von
     ),
-    hausnummern AS ( 
-        SELECT 
+    hausnummern AS (
+        SELECT
             t_id,
-            hausnummerpos_von, 
-            pos, 
+            hausnummerpos_von,
+            pos,
             CAST(t_datasetname AS INT) AS gem_bfs
-        FROM 
+        FROM
             agi_dm01avso24.gebaeudeadressen_hausnummerpos
     )
 
-SELECT 
+SELECT
     gebaeudeadressen_lokalisationsname.atext AS strname,
-    gebaeudeadressen_gebaeudeeingang.hausnummer, 
+    gebaeudeadressen_gebaeudeeingang.hausnummer,
     hausnummern.t_id AS hausnummerpos_tid,
     gebaeudeadressen_gebaeudeeingang.t_id AS gebaeudeeingang_tid,
-    gebaeudeadressen_gebaeudeeingang.gebaeudeeingang_von AS lok_tid, 
-    ST_ShortestLine(hausnummern.pos, strassen.geometrie) AS geometrie, 
+    gebaeudeadressen_gebaeudeeingang.gebaeudeeingang_von AS lok_tid,
+    ST_ShortestLine(hausnummern.pos, strassen.geometrie) AS geometrie,
     hausnummern.gem_bfs,
     now() AS datum
-FROM 
+FROM
     hausnummern
     LEFT JOIN agi_dm01avso24.gebaeudeadressen_gebaeudeeingang
         ON gebaeudeadressen_gebaeudeeingang.t_id = hausnummern.hausnummerpos_von
