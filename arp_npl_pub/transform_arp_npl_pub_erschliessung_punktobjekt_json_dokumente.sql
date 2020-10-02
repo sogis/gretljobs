51c1ac9eb5d4e76c401aec2a3599368104429b06
bjsvwcur <sandra.curiger@bd.so.ch>
2020-10-02 10:04:56 +0200

    [arp_npl_pub] JSON-Bugfix im NPL-Datenumbau

--- arp_npl_pub/transform_arp_npl_pub_erschliessung_punktobjekt_json_dokumente.sql
+++ arp_npl_pub/transform_arp_npl_pub_erschliessung_punktobjekt_json_dokumente.sql
@@ -72,10 +72,20 @@ json_documents_all AS
   FROM
   (
     SELECT
-      t_id, dokumentid, titel, offiziellertitel AS offizieller_titel, abkuerzung,
-      offiziellenr AS offizielle_nr, kanton, gemeinde, publiziertab AS publiziert_ab, rechtsstatus,
-      ('https://geo.so.ch/docs/ch.so.arp.zonenplaene/Zonenplaene_pdf/'||"textimweb")::text AS textimweb,
-      bemerkungen, rechtsvorschrift
+      t_id, 
+      dokumentid AS "Dokument_ID", 
+      titel AS "Titel", 
+      offiziellertitel AS "Offizieller_Titel", 
+      abkuerzung AS "Abkuerzung",
+      offiziellenr AS "Offizielle_Nr", 
+      kanton AS "Kanton", 
+      gemeinde AS "Gemeinde", 
+      publiziertab AS "publiziert_ab", 
+      rechtsstatus AS "Rechtsstatus",
+      ('https://geo.so.ch/docs/ch.so.arp.zonenplaene/Zonenplaene_pdf/'||"textimweb")::text AS "Text_im_Web",
+      bemerkungen AS "Bemerkungen", 
+      rechtsvorschrift AS "Rechtsvorschrift", 
+      'SO_Nutzungsplanung_Publikation_20190909.Nutzungsplanung.Dokument' AS "@type"
     FROM
       arp_npl.rechtsvorschrften_dokument
   ) AS t
