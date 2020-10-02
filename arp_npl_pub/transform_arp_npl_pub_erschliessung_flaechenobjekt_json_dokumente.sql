3d56d5ad2ff2543cfa0509d59da2847fb9c4afe8
bjsvwcur <38523407+bjsvwcur@users.noreply.github.com>
2020-10-02 14:44:47 +0200

    Update transform_arp_npl_pub_erschliessung_flaechenobjekt_json_dokumente.sql

--- arp_npl_pub/transform_arp_npl_pub_erschliessung_flaechenobjekt_json_dokumente.sql
+++ arp_npl_pub/transform_arp_npl_pub_erschliessung_flaechenobjekt_json_dokumente.sql
@@ -92,7 +92,7 @@ json_documents_all AS
                   ) docs
               ))
          )
-     ) AS dokumente,
+     ) AS json_dokument,
      t_id
   FROM
       arp_npl.rechtsvorschrften_dokument
