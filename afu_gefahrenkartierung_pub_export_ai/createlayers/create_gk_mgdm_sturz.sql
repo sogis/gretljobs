de2c5714ebf08006f528fe7a908d2cad68449396
dva999 <49029487+dva999@users.noreply.github.com>
2020-10-06 14:51:00 +0200

    update bemerkung as null

--- afu_gefahrenkartierung_pub_export_ai/createlayers/create_gk_mgdm_sturz.sql
+++ afu_gefahrenkartierung_pub_export_ai/createlayers/create_gk_mgdm_sturz.sql
@@ -241,7 +241,7 @@ SELECT -- Union, damit allfällige Ueberlappungen innerhalb der "keine"-Flächen
     ST_MakeValid(ST_SnapToGrid((ST_Dump(ST_Union(res_poly))).geom, 0.001)) AS geometrie,
     'Sturz' AS prozessa,
     'keine' AS gef_stufe,
-    '' AS bemerkung
+    null AS bemerkung
 FROM keine_diff
 GROUP BY t_id
 UNION ALL
@@ -250,7 +250,7 @@ SELECT -- Union, damit allfällige Ueberlappungen innerhalb der "vorhanden"-Flä
     (ST_Dump(ST_MakeValid(ST_SnapToGrid((ST_Dump(ST_Union(res_poly))).geom, 0.001)))).geom AS geometrie,
     'Sturz' AS prozessa,
     'vorhanden' AS gef_stufe,
-    '' AS bemerkung
+    null AS bemerkung
 FROM vorhanden_diff
 GROUP BY t_id
 UNION ALL
@@ -259,7 +259,7 @@ SELECT -- Union, damit allfällige Ueberlappungen innerhalb der "gering"-Fläche
     (ST_Dump(ST_MakeValid(ST_SnapToGrid((ST_Dump(ST_Union(res_poly))).geom, 0.001)))).geom AS geometrie,
     'Sturz' AS prozessa,
     'gering' AS gef_stufe,
-    '' AS bemerkung    
+    null AS bemerkung    
 FROM gering_diff
 GROUP BY t_id
 UNION ALL
@@ -268,7 +268,7 @@ SELECT -- Union, damit allfällige Ueberlappungen innerhalb der "mittel"-Fläche
     (ST_Dump(ST_MakeValid(ST_SnapToGrid((ST_Dump(ST_Union(res_poly))).geom, 0.001)))).geom AS geometrie,
     'Sturz' AS prozessa,
     'mittel' AS gef_stufe,
-    '' AS bemerkung
+    null AS bemerkung
 FROM mittel_diff
 GROUP BY t_id
 UNION ALL
@@ -277,7 +277,7 @@ SELECT -- Union, damit allfällige Ueberlappungen innerhalb der "erheblich"-Flä
     (ST_Dump(ST_MakeValid(ST_SnapToGrid((ST_Dump(ST_Union(geometry))).geom, 0.001)))).geom AS geometrie,
     'Sturz' AS prozessa,
     'erheblich' AS gef_stufe,
-    '' AS bemerkung    
+    null AS bemerkung    
 FROM stufe_erheblich
 GROUP BY t_id
 ),
