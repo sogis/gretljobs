843b5dbef911765f2e663a765beeb04252201aae
bjsvwcur <sandra.curiger@bd.so.ch>
2020-11-09 16:53:36 +0100

    [agi_plz_ortschaften_pub] Update ili2pg4.3.1

---
+++ agi_plz_ortschaften_pub/plzortschaft_plz6.sql
@@ -0,0 +1,12 @@
+SELECT
+    t_id,
+    astatus AS status,
+    inaenderung,
+    plz,
+    zusatzziffern,
+    entstehung,
+    plz6_von,
+    flaeche
+FROM 
+    agi_plz_ortschaften.plzortschaft_plz6
+;
