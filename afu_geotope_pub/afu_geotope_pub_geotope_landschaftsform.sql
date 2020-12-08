8401e7bd3be63ad8550ddd7100706f387b5a89f0
bjsvwcur <38523407+bjsvwcur@users.noreply.github.com>
2020-12-08 14:33:25 +0100

    Update afu_geotope_pub_geotope_landschaftsform.sql

--- afu_geotope_pub/afu_geotope_pub_geotope_landschaftsform.sql
+++ afu_geotope_pub/afu_geotope_pub_geotope_landschaftsform.sql
@@ -80,7 +80,7 @@ WITH fachbereich AS (
             ON plzortschaft_ortschaftsname.ortschaftsname_von = plzortschaft_ortschaft.t_id
     GROUP BY
         plzortschaft_ortschaftsname.atext,
-        plzortschaft_ortschaft.status
+        plzortschaft_ortschaft.astatus
 ), ortschaften AS (
     SELECT
         geotope_landschaftsform.t_id,
