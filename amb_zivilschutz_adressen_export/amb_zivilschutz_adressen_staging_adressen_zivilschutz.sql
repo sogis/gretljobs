3bf61837a55bf16a280328f8f80fdec5cb039641
bjsvwcur <38523407+bjsvwcur@users.noreply.github.com>
2021-01-14 17:00:49 +0100

    [amb_zivilschutz_adressen] Add EO_Flaechen und Proj BB Adressen

--- amb_zivilschutz_adressen_export/amb_zivilschutz_adressen_staging_adressen_zivilschutz.sql
+++ amb_zivilschutz_adressen_export/amb_zivilschutz_adressen_staging_adressen_zivilschutz.sql
@@ -31,8 +31,68 @@ adressenZusammenfuehren AS (
     AND
         b.art_txt = 'Gebaeude'
 UNION ALL
+        SELECT
+        DISTINCT ON (a.strassenname, a.nummer, a.ortschaft) -- in Tabelle adressen.adressen gibt es doppelte Adresse wegen EO.Flaechenelement
+        a.t_id,
+        a.strassenname AS lokalisationsname,
+        a.nummer as hausnummer,
+        a.plz4 AS plz,
+        a.ortschaft,
+        g.gemeindename AS gemeinde,
+        a.egid AS gwr_egid,
+        a.edid AS gwr_edid,
+        ST_X(a.geometrie) AS koord_ost,
+        st_y(a.geometrie) AS koord_nord,
+        a.astatus,
+        b.geometrie AS gwr_egid_geom,
+        a.geometrie AS gwr_edid_geom,
+        a.bfsnr
+    FROM
+        agi_swisstopo_gebaeudeadressen_pub.gebaeudeadressen_adresse AS a
+    LEFT JOIN agi_mopublic_pub.mopublic_bodenbedeckung_proj AS b
+        ON 
+        a.geometrie && b.geometrie
+        AND
+        st_distance(a.geometrie, b.geometrie) = 0
+    LEFT JOIN agi_mopublic_pub.mopublic_gemeindegrenze AS g
+        ON a.bfsnr::int = g.bfs_nr
+    WHERE
+        a.nummer IS NOT NULL -- nur die mit Hausnummern
+    AND
+        b.art_txt = 'Gebaeude'
+UNION ALL
+        SELECT
+        DISTINCT ON (a.strassenname, a.nummer, a.ortschaft) -- in Tabelle adressen.adressen gibt es doppelte Adresse wegen EO.Flaechenelement
+        a.t_id,
+        a.strassenname AS lokalisationsname,
+        a.nummer as hausnummer,
+        a.plz4 AS plz,
+        a.ortschaft,
+        g.gemeindename AS gemeinde,
+        a.egid AS gwr_egid,
+        a.edid AS gwr_edid,
+        ST_X(a.geometrie) AS koord_ost,
+        st_y(a.geometrie) AS koord_nord,
+        a.astatus,
+        b.geometrie AS gwr_egid_geom,
+        a.geometrie AS gwr_edid_geom,
+        a.bfsnr
+    FROM
+        agi_swisstopo_gebaeudeadressen_pub.gebaeudeadressen_adresse AS a
+    LEFT JOIN agi_mopublic_pub.mopublic_einzelobjekt_flaeche AS b
+        ON 
+        a.geometrie && b.geometrie
+        AND
+        st_distance(a.geometrie, b.geometrie) = 0
+    LEFT JOIN agi_mopublic_pub.mopublic_gemeindegrenze AS g
+        ON a.bfsnr::int = g.bfs_nr
+    WHERE
+        a.nummer IS NOT NULL -- nur die mit Hausnummern
+    AND
+        b.art_txt = 'Unterstand' OR b.art_txt = 'unterirdisches_Gebaeude'
+UNION ALL
     SELECT
-        DISTINCT ON (a.strassenname, a.hausnummer, a.ortschaft) -- in Tabelle adressen.adressen gibt es doppelte Adresse wegen EO.Flaechenelement
+        DISTINCT ON (a.strassenname, a.hausnummer, a.ortschaft) -- Adressen BB
         a.t_id,
         a.strassenname AS lokalisationsname,
         a.hausnummer,
@@ -60,6 +120,66 @@ UNION ALL
         a.hausnummer IS NOT NULL -- nur die mit Hausnummern
     AND
         b.art_txt = 'Gebaeude' AND a.egid IS NULL
+UNION ALL
+    SELECT
+        DISTINCT ON (a.strassenname, a.hausnummer, a.ortschaft) -- Adressen Pro BB
+        a.t_id,
+        a.strassenname AS lokalisationsname,
+        a.hausnummer,
+        a.plz,
+        a.ortschaft,
+        g.gemeindename AS gemeinde,
+        a.egid AS gwr_egid,
+        a.edid AS gwr_edid,
+        ST_X(a.lage) AS koord_ost,
+        st_y(a.lage) AS koord_nord,
+        a.astatus,
+        b.geometrie AS gwr_egid_geom,
+        a.lage AS gwr_edid_geom,
+        CAST(a.bfs_nr AS varchar(20)) AS bfs_nr
+    FROM
+        agi_mopublic_pub.mopublic_gebaeudeadresse AS a
+    LEFT JOIN agi_mopublic_pub.mopublic_bodenbedeckung_proj AS b
+        ON 
+        a.lage && b.geometrie
+        AND
+        st_distance(a.lage, b.geometrie) = 0
+    LEFT JOIN agi_mopublic_pub.mopublic_gemeindegrenze AS g
+        ON a.bfs_nr::int = g.bfs_nr
+    WHERE
+        a.hausnummer IS NOT NULL -- nur die mit Hausnummern
+    AND
+        b.art_txt = 'Gebaeude' AND a.egid IS NULL
+UNION ALL
+    SELECT
+        DISTINCT ON (a.strassenname, a.hausnummer, a.ortschaft) --Adresse EO.Flaechenelement
+        a.t_id,
+        a.strassenname AS lokalisationsname,
+        a.hausnummer,
+        a.plz,
+        a.ortschaft,
+        g.gemeindename AS gemeinde,
+        a.egid AS gwr_egid,
+        a.edid AS gwr_edid,
+        ST_X(a.lage) AS koord_ost,
+        st_y(a.lage) AS koord_nord,
+        a.astatus,
+        b.geometrie AS gwr_egid_geom,
+        a.lage AS gwr_edid_geom,
+        CAST(a.bfs_nr AS varchar(20)) AS bfs_nr
+    FROM
+        agi_mopublic_pub.mopublic_gebaeudeadresse AS a
+    LEFT JOIN agi_mopublic_pub.mopublic_einzelobjekt_flaeche AS b
+        ON 
+        a.lage && b.geometrie
+        AND
+        st_distance(a.lage, b.geometrie) = 0
+    LEFT JOIN agi_mopublic_pub.mopublic_gemeindegrenze AS g
+        ON a.bfs_nr::int = g.bfs_nr
+    WHERE
+        a.hausnummer IS NOT NULL -- nur die mit Hausnummern
+    AND
+        (b.art_txt = 'Unterstand' OR b.art_txt = 'unterirdisches_Gebaeude') AND a.egid IS NULL
 ),
 adressen AS (
     SELECT
