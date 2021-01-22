572504db0aa1b072bd86f65ea2059decab432dac
EchoCharlie <martin.schweizer@bd.so.ch>
2021-01-22 11:43:17 +0100

    [amb_notfalltreffpunkte_pub] amb_notfalltreffpunkte migriert von sogis-db auf edit-db

--- amb_notfalltreffpunkte_pub/amb_notfalltreffpunkte_pub.sql
+++ amb_notfalltreffpunkte_pub/amb_notfalltreffpunkte_pub.sql
@@ -6,22 +6,46 @@ SELECT
     a.rfs, 
     a.einwohner,
     a.art,
-    coalesce(b.str_name,'unbekannt') AS strasse, 
+    coalesce(b.strasse,'unbekannt') AS strasse, 
     coalesce(b.hausnummer,'unbekannt') AS hausnummer, 
-    coalesce(b.plz4, 0) AS plz, 
-    coalesce(b.ortschaft, 'unbekannt') AS ort, 
+    coalesce(postleitzahl.plz, 0) AS plz, 
+    coalesce(ortschaft.ortschaftsname, 'unbekannt') AS ort, 
     a.url, 
     a.geometrie
 FROM 
     amb_notfalltreffpunkte.notfalltreffpnkte_treffpunkt a 
     LEFT JOIN (
-        SELECT DISTINCT ON (gwr_egid_geom) 
-            str_name, 
-            hausnummer, 
-            plz4, 
-            ortschaft, 
-            gwr_egid_geom 
-        FROM adressen.adressen 
-        WHERE ARCHIVE = 0
+        SELECT 
+            bodenflaeche.geometrie, 
+	        adresse.hausnummer, 
+	        strasse.atext AS strasse 
+        FROM 
+            agi_dm01avso24.bodenbedeckung_boflaeche bodenflaeche 
+        LEFT JOIN 
+            agi_dm01avso24.gebaeudeadressen_gebaeudeeingang adresse 
+	        ON 
+	        st_dwithin(adresse.lage,bodenflaeche.geometrie,0)
+        LEFT JOIN 
+            agi_dm01avso24.gebaeudeadressen_lokalisation lokalisation 
+	        ON 
+	        lokalisation.t_id = adresse.gebaeudeeingang_von
+        LEFT JOIN 
+            agi_dm01avso24.gebaeudeadressen_lokalisationsname strasse 
+	        ON 
+	        strasse.benannte = lokalisation.t_id
+        WHERE 
+            strasse.atext IS NOT NULL
+		    AND 
+		    adresse.hausnummer IS NOT NULL
     ) b 
-        ON st_dwithin(a.geometrie,b.gwr_egid_geom,0);
+	    ON 
+	    st_dwithin(a.geometrie,b.geometrie,0)
+	LEFT JOIN 
+        agi_plz_ortschaften_pub.plzortschaften_postleitzahl postleitzahl 
+	    ON 
+	    st_dwithin(a.geometrie,postleitzahl.geometrie,0)
+    LEFT JOIN 
+        agi_plz_ortschaften_pub.plzortschaften_ortschaft ortschaft 
+	    ON 
+	    st_dwithin(a.geometrie,ortschaft.geometrie,0)
+	;
