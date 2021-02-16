d3e968418eb354f207a0d8a12e9fb94f373c7134
Andreas Neumann <a.neumann@carto.net>
2021-02-16 11:40:10 +0100

    [alw_strukturverbesserungen_suissemelio] initial checkin

---
+++ alw_strukturverbesserungen_suissemelio/projekt.sql
@@ -0,0 +1,10 @@
+SELECT
+    t_id,
+    geometrie,
+    projekttyp AS projekttypen,
+    geschaeftsnummer,
+    kantonsnummer
+  FROM alw_strukturverbesserungen.raeumlicheelemnte_projekt
+    WHERE
+      projekttyp NOT IN ('Tiefbau','Hochbau','Weitere_ALW_Projekte')
+;
