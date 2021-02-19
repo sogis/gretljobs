ac779dfab69fae7177f5a2fdcb155f7acaad75fe
Andreas Neumann <a.neumann@carto.net>
2021-02-19 09:03:38 +0100

    [alw_strukturverbesserungen_suissemelio] Beizugsgebiet u Projekt: Filtern nach Geschäftsnr

--- alw_strukturverbesserungen_suissemelio/beizugsgebiet.sql
+++ alw_strukturverbesserungen_suissemelio/beizugsgebiet.sql
@@ -8,5 +8,6 @@ SELECT
   FROM alw_strukturverbesserungen.raeumlicheelemnte_beizugsgebiet bg
     LEFT JOIN alw_strukturverbesserungen.raeumlicheelemnte_beizugsgebiet_projekt zt ON bg.t_id = zt.beizugsgebiet
     LEFT JOIN alw_strukturverbesserungen.raeumlicheelemnte_projekt prj ON prj.t_id = zt.projekt
+  WHERE prj.geschaeftsnummer IS NOT NULL
   GROUP BY bg.t_id, bg.geometrie, bg.datum_nachfuehrung, bg.typ
 ;
--- alw_strukturverbesserungen_suissemelio/projekt.sql
+++ alw_strukturverbesserungen_suissemelio/projekt.sql
@@ -7,4 +7,5 @@ SELECT
   FROM alw_strukturverbesserungen.raeumlicheelemnte_projekt
     WHERE
       projekttyp NOT IN ('Tiefbau','Hochbau','Weitere_ALW_Projekte')
+      AND geschaeftsnummer IS NOT NULL
 ;
