03291c3741c9b65b9bf983159f894dc27de78e2b
Andreas Neumann <a.neumann@carto.net>
2021-05-03 14:03:30 +0200

    [alw_strukturverbesserungen_suissemelio] Beizugsgebietsflaechen - make sure strings get trimmed if too long

--- alw_strukturverbesserungen_suissemelio/beizugsgebiet.sql
+++ alw_strukturverbesserungen_suissemelio/beizugsgebiet.sql
@@ -3,8 +3,8 @@ SELECT
     (ST_Dump(bg.geometrie)).geom AS geometrie,
     bg.datum_nachfuehrung,
     bg.typ,
-    string_agg(prj.geschaeftsnummer,', ') AS geschaeftsnummer,
-    string_agg(prj.kantonsnummer,', ') AS kantonsnummer
+    Left(string_agg(prj.geschaeftsnummer,', '),10) AS geschaeftsnummer,
+    Left(string_agg(prj.kantonsnummer,', '), 40) AS kantonsnummer
   FROM alw_strukturverbesserungen.raeumlicheelemnte_beizugsgebiet bg
     LEFT JOIN alw_strukturverbesserungen.raeumlicheelemnte_beizugsgebiet_projekt zt ON bg.t_id = zt.beizugsgebiet
     LEFT JOIN alw_strukturverbesserungen.raeumlicheelemnte_projekt prj ON prj.t_id = zt.projekt
