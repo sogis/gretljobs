c7bf30006a1b1384821523ead18021cc998babb0
bjsvwcur <38523407+bjsvwcur@users.noreply.github.com>
2021-05-31 09:30:35 +0200

    Update agi_swissboundaries3d_pub_edit_swissboundaries3d_kanton.sql

--- agi_ch_gemeinden/agi_swissboundaries3d_pub_edit_swissboundaries3d_kanton.sql
+++ agi_ch_gemeinden/agi_swissboundaries3d_pub_edit_swissboundaries3d_kanton.sql
@@ -1,16 +1,16 @@
 --Kanton
 WITH geometrie AS (
     SELECT
-        tlm_grenzen_tlm_kantonsgebiet.kantonsnummer,
-        tlm_grenzen_tlm_kantonsgebiet.aname AS kantonsname,
+        tlm_kantonsgebiet.kantonsnummer,
+        tlm_kantonsgebiet.aname AS kantonsname,
         land.aname AS land,
-        ST_Force_2D(ST_Collect(tlm_grenzen_tlm_kantonsgebiet.shape)) AS geometrie
+        ST_Force_2D(ST_Collect(tlm_kantonsgebiet.shape)) AS geometrie
     FROM
-        agi_swissboundaries3d.tlm_grenzen_tlm_kantonsgebiet
+        agi_swissboundaries3d.tlm_kantonsgebiet
         LEFT JOIN
-            agi_swissboundaries3d.tlm_grenzen_tlm_landesgebiet land
+            agi_swissboundaries3d.tlm_landesgebiet land
             ON
-                land.icc = tlm_grenzen_tlm_kantonsgebiet.icc
+                land.icc = tlm_kantonsgebiet.icc
     WHERE
         land.land_teil = 0
         OR
@@ -31,7 +31,7 @@ SELECT
 FROM
     geometrie
     LEFT JOIN 
-        agi_swissboundaries3d.tlm_grenzen_tlm_kantonsgebiet kanton
+        agi_swissboundaries3d.tlm_kantonsgebiet kanton
         ON
             kanton.kantonsnummer = geometrie.kantonsnummer
 WHERE
