b343074567f45baca51ae1f89b79095adf950673
barpasen <peter.senn@bd.so.ch>
2021-07-05 14:36:26 +0200

    [awa_stromversorgungssicherheit_netzgebiete_pub] new model

--- awa_stromversorgungssicherheit_netzgebiete_pub/awa_stromversorgungssicherheit_pub_ebene_5.sql
+++ awa_stromversorgungssicherheit_netzgebiete_pub/awa_stromversorgungssicherheit_pub_ebene_5.sql
@@ -1,8 +1,8 @@
 SELECT 
-    ruledarea_level5.aname AS "name",
+    ruledarea_level5.aname,
     ageometry AS geometrie,
     canton AS kanton,
-    organisation.aname AS "operator" 
+    organisation.aname AS betreiber 
 FROM
     awa_stromversorgungssicherheit.supplyscy_rldreas_ruledarea_level5 AS ruledarea_level5
     LEFT JOIN awa_stromversorgungssicherheit.supplyscy_rldreas_organisation AS organisation
