c1a8cbeaa7b152367403ad9b83ce12f1c7d22ec5
barpasen <peter.senn@bd.so.ch>
2021-09-28 13:12:09 +0200

    [alw_tiergesundheit_massnahmen_pub] initial import

---
+++ alw_tiergesundheit_massnahmen_pub/alw_tiergesundheit_bienensperrgebiet.sql
@@ -0,0 +1,11 @@
+SELECT
+    geometrie,
+    jahr,
+    typ_sperre,
+    verwendungszweck,
+    datum_installation,
+    aktiv,
+    datum_inaktiv
+FROM
+    alw_tiergesundheit_massnahmen.massnhmnrgsndheit_bienensperrgebiet
+;
