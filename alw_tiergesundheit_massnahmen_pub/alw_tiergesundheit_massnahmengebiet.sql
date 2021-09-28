3ea13ceaf867919de993bf68ea654f9751d9f08d
barpasen <peter.senn@bd.so.ch>
2021-09-28 13:12:09 +0200

    [alw_tiergesundheit_massnahmen_pub] initial import

---
+++ alw_tiergesundheit_massnahmen_pub/alw_tiergesundheit_massnahmengebiet.sql
@@ -0,0 +1,11 @@
+SELECT
+    geometrie,
+    jahr,
+    seuchenart,
+    verwendungszweck,
+    datum_installation,
+    aktiv,
+    datum_inaktiv
+FROM
+    alw_tiergesundheit_massnahmen.massnhmnrgsndheit_tiergesundheit_massnahmengebiet
+;
