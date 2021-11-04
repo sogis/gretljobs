4169e3162265d264e651765002252a9018152c4d
barpasen <peter.senn@bd.so.ch>
2021-11-04 11:04:40 +0100

    initial version

---
+++ alw_gewaesserraum_v1_pub/alw_gewaesserraum_v1.sql
@@ -0,0 +1,12 @@
+SELECT
+    geometrie,
+    rechtsstatus,
+    fff_massgebend,
+    bfs_nr,
+    herkunft,
+    rechtskraftdatum,
+    bemerkung,
+    erstellungsdatum
+FROM
+    alw_gewaesserraum_v1.gewaesserraum
+;
