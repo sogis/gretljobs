44730f4194051ac7d1ede41aa2fc55792b9a1dcb
barpasen <peter.senn@bd.so.ch>
2021-11-10 12:21:09 +0100

    [afu_geologie_pub] migrated from sogis-DB

--- afu_geologie_pub/afu_geologie_pub_karst.sql
+++
@@ -1,9 +0,0 @@
-SELECT
-    ogc_fid AS t_id,
-    wkb_geometry AS geometrie,
-    symbol
-FROM
-    geologie.karst
-WHERE
-    archive = 0
-;
---
+++ afu_geologie_pub/tektonische_strukturen_pub.sql
@@ -0,0 +1,8 @@
+SELECT
+    ST_MakeValid(ST_RemoveRepeatedPoints(ST_SnapToGrid(geometrie, 0.0001))) AS geometrie, 
+    typ
+FROM 
+    afu_geologie_v1.geologie_tektonische_strukturen AS tektonische_strukturen
+    LEFT JOIN afu_geologie_v1.geologie_tektonische_strukturen_typ AS typ
+    ON tektonische_strukturen.typ = typ.ilicode
+;
