7dfce73941992887b9a9919c8fb28f4de586eaae
EchoCharlie <martin.schweizer@bd.so.ch>
2021-11-17 14:08:36 +0100

    [alw_fruchtfolgeflaechen]changed task_names dublicated

--- alw_fruchtfolgeflaechen/02_Vorberechnung/make_table_fff_maske_bodenbedeckung.sql
+++ alw_fruchtfolgeflaechen/02_Vorberechnung/make_table_fff_maske_bodenbedeckung.sql
@@ -1,16 +1,7 @@
--- alw_fruchtfolgeflaechen.fff_maske_bodenbedeckung definition
-
--- Drop table
-
--- DROP TABLE alw_fruchtfolgeflaechen.fff_maske_bodenbedeckung;
+DROP TABLE if exists alw_fruchtfolgeflaechen.fff_maske_bodenbedeckung;
 
 CREATE TABLE alw_fruchtfolgeflaechen.fff_maske_bodenbedeckung (
 	geometrie geometry NULL,
 	bfs_nr int4 NULL,
 	anrechenbar int4 NULL
 );
-
--- Permissions
-
-ALTER TABLE alw_fruchtfolgeflaechen.fff_maske_bodenbedeckung OWNER TO postgres;
-GRANT ALL ON TABLE alw_fruchtfolgeflaechen.fff_maske_bodenbedeckung TO postgres;
