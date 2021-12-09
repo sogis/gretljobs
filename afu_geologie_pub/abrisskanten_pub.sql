217ee7b102481abfa36673403e88b4a92dc0ead6
barpasen <peter.senn@bd.so.ch>
2021-12-09 15:10:00 +0100

    [afu_geologie] txt_attrs added

--- afu_geologie_pub/abrisskanten_pub.sql
+++ afu_geologie_pub/abrisskanten_pub.sql
@@ -1,8 +1,9 @@
 SELECT
     geometrie,     
-    typ
+    typ,
+    code.dispname AS typ_txt
 FROM 
-    afu_geologie_v1.geologie_abrisskanten
+    afu_geologie_v1.geologie_abrisskanten AS abrisskanten
+    LEFT JOIN afu_geologie_v1.geologie_abrisskanten_typ AS code
+    ON abrisskanten.typ = code.ilicode
 ;
-        
-                 
--- afu_geologie_pub/tektonische_strukturen_pub.sql
+++ afu_geologie_pub/tektonische_strukturen_pub.sql
@@ -1,6 +1,7 @@
 SELECT
     ST_MakeValid(ST_RemoveRepeatedPoints(ST_SnapToGrid(geometrie, 0.0001))) AS geometrie, 
-    typ
+    typ,
+    typ.dispname AS typ_txt
 FROM 
     afu_geologie_v1.geologie_tektonische_strukturen AS tektonische_strukturen
     LEFT JOIN afu_geologie_v1.geologie_tektonische_strukturen_typ AS typ
