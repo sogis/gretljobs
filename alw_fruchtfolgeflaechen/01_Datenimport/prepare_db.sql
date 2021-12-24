8e1da14a21efa13cf29586b5c6c30e6a9478e314
EchoCharlie <martin.schweizer@bd.so.ch>
2021-12-24 13:22:38 +0100

    [alw_fruchtfolgeflaechen]Bugfix

--- alw_fruchtfolgeflaechen/01_Datenimport/prepare_db.sql
+++ alw_fruchtfolgeflaechen/01_Datenimport/prepare_db.sql
@@ -1,1 +1,4 @@
-CREATE EXTENSION "uuid-ossp";
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
+--Klimaeignung kann nicht importiert werden, wenn da noch was ist. 
+drop schema if exists klimaeignung cascade;
