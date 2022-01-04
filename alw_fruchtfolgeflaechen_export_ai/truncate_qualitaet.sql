cfc899cd2960b35441c01bbc425e6a37a9c3e64a
EchoCharlie <martin.schweizer@bd.so.ch>
2022-01-04 11:32:32 +0100

    [alw_fruchtfolgeflaechen_export_ai]Bugfix

--- alw_fruchtfolgeflaechen_export_ai/truncate_qualitaet.sql
+++ alw_fruchtfolgeflaechen_export_ai/truncate_qualitaet.sql
@@ -1,1 +1,2 @@
-TRUNCATE alw_fruchtfolgeflaechen_mgdm_v1.qualitaet_kantonal CASCADE;
+delete from alw_fruchtfolgeflaechen_mgdm_v1.fruchtfolgeflaeche;
+delete from alw_fruchtfolgeflaechen_mgdm_v1.qualitaet_kantonal; 
