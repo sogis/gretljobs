c3b7ae86f543dfde34d023a8826719bdecd3c86c
barpasen <peter.senn@bd.so.ch>
2022-01-05 13:08:49 +0100

    [awjf_wald_oberhoehenbonitaet_pub] changed oberhoehenbonitaets_code to Enumeration

--- awjf_wald_oberhoehenbonitaet_pub/awjf_wald_oberhoehenbonitaet_pub.sql
+++ awjf_wald_oberhoehenbonitaet_pub/awjf_wald_oberhoehenbonitaet_pub.sql
@@ -1,6 +1,9 @@
 SELECT
     oberhoehenbonitaets_code,
+    oberhoehenbonitaets_code.dispname AS oberhoehenbonitaets_code_txt,
     geometrie
 FROM
     awjf_wald_oberhoehenbonitaet_v1.oberhoehenbonitaet
+    LEFT JOIN awjf_wald_oberhoehenbonitaet_v1.oberhoehenbonitaet_oberhoehenbonitaets_code AS oberhoehenbonitaets_code
+    ON oberhoehenbonitaets_code = oberhoehenbonitaets_code.ilicode
 ;
