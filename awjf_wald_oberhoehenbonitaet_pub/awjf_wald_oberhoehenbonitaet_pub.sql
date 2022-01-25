817e68ac45e17af9429b5c4c8246665b9f48b39f
barpasen <peter.senn@bd.so.ch>
2022-01-25 10:39:12 +0100

    [awjf_wald_oberhoehenbonitaet_pub] now in job awjf_wald_pub

--- awjf_wald_oberhoehenbonitaet_pub/awjf_wald_oberhoehenbonitaet_pub.sql
+++
@@ -1,9 +0,0 @@
-SELECT
-    oberhoehenbonitaets_code,
-    oberhoehenbonitaets_code.dispname AS oberhoehenbonitaets_code_txt,
-    geometrie
-FROM
-    awjf_wald_oberhoehenbonitaet_v1.oberhoehenbonitaet
-    LEFT JOIN awjf_wald_oberhoehenbonitaet_v1.oberhoehenbonitaet_oberhoehenbonitaets_code AS oberhoehenbonitaets_code
-    ON oberhoehenbonitaets_code = oberhoehenbonitaets_code.ilicode
-;
