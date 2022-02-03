47840fb5afa30e2c9bc7709f462003fb465843ce
Andreas Neumann <a.neumann@carto.net>
2022-02-03 19:25:35 +0100

    [arp_auswertung_nutzungsplanung_pub] Initial check-in

---
+++ arp_auswertung_nutzungsplanung_pub/bauzonenstatistik_bebauungsstand_mit_zonen_und_lsgrenzen.sql
@@ -0,0 +1,218 @@
+DELETE FROM ${BZ_SchemaName}.bauzonenstatistik_bebauungsstand_mit_zonen_und_lsgrenzen
+  WHERE bfs_nr = ${gem_bfs}
+;
+INSERT
+  INTO ${BZ_SchemaName}.bauzonenstatistik_bebauungsstand_mit_zonen_und_lsgrenzen
+   (grundnutzung_typ_kt,bebauungsstand,bfs_nr,gemeindename,flaeche,geometrie)
+   
+-- hier wird Gemeinde selektiert
+WITH
+bfsnr AS (
+	SELECT
+	  ${gem_bfs} AS nr
+     --Himmelried: 2618, Büsserach: 2614, Oensingen: 2407
+),
+-- Gemeindegeometrie
+gem AS (
+  SELECT
+    geometrie,
+    gemeindename,
+    bfs_gemeindenummer
+  FROM
+    agi_hoheitsgrenzen_pub.hoheitsgrenzen_gemeindegrenze
+   WHERE bfs_gemeindenummer = (SELECT nr FROM bfsnr)
+),
+-- Gebäude inkl. projektierten Gebäuden
+geb AS (
+  SELECT
+    --positive buffer of 12m and then negative buffer of 6m
+    --special buffer parameters 
+    ST_Buffer(ST_Buffer(geometrie,12,'endcap=flat join=miter'),-6,'endcap=flat join=miter') AS geometrie
+  FROM
+    agi_mopublic_pub.mopublic_bodenbedeckung
+   WHERE
+    art_txt = 'Gebaeude'
+    AND bfs_nr = (SELECT nr FROM bfsnr)
+    AND ST_Area(geometrie) >= 25 --kleinere Gebäude werden ignoriert (gelten als abbrechbar)
+ UNION ALL
+  SELECT
+    --positive buffer of 12m and then negative buffer of 6m
+    --special buffer parameters 
+    ST_Buffer(ST_Buffer(geometrie,12,'endcap=flat join=miter'),-6,'endcap=flat join=miter') AS geometrie
+  FROM
+    agi_mopublic_pub.mopublic_bodenbedeckung_proj
+   WHERE
+    art_txt = 'Gebaeude'
+    AND bfs_nr = (SELECT nr FROM bfsnr)
+    AND ST_Area(geometrie) >= 25 --kleinere Gebäude werden ignoriert (gelten als abbrechbar)
+),
+-- Einzelbojekte
+eob AS (
+  SELECT
+   ST_Buffer(geometrie,1,'endcap=flat join=miter') AS geometrie
+  FROM agi_mopublic_pub.mopublic_einzelobjekt_flaeche
+   WHERE
+    bfs_nr = (SELECT nr FROM bfsnr)
+    AND art_txt IN ('Unterstand','unterirdisches_Gebaeude','uebriger_Gebaeudeteil','Reservoir','Aussichtsturm','Bahnsteig')
+    AND ST_Area(geometrie) >= 25
+),
+-- Liegenschaften
+gr AS (
+  SELECT
+   t_id,
+   geometrie,
+   ST_Buffer(ST_ExteriorRing(geometrie),4,'endcap=flat join=miter') AS geom_buff,
+   nummer,
+   egrid
+  FROM agi_mopublic_pub.mopublic_grundstueck mg 
+   WHERE
+    art_txt = 'Liegenschaft'
+    AND bfs_nr = (SELECT nr FROM bfsnr)
+),
+-- Nutzungszonen
+nutzzon AS (
+  SELECT
+    t_id,
+    geometrie,
+    ST_Buffer(ST_Exteriorring(geometrie),4,'endcap=flat join=miter') AS geom_buff,
+    typ_kt,
+    bfs_nr
+  FROM arp_npl_pub.nutzungsplanung_grundnutzung
+    WHERE bfs_nr = (SELECT nr FROM bfsnr)
+    AND typ_code_kommunal < 2000
+    AND typ_kt NOT IN ('N160_Gruen_und_Freihaltezone_innerhalb_Bauzone','N161_kommunale_Uferschutzzone_innerhalb_Bauzone','N169_weitere_eingeschraenkte_Bauzonen','N180_Verkehrszone_Stras-se','N181_Verkehrszone_Bahnareal','N182_Verkehrszone_Flugplatzareal','N189_weitere_Verkehrszonen')
+),
+-- Dissolve (GIS Union) aller Nutzungszonen in Siedlungsflächen
+nutzzon_dissolved_tmp AS (
+  SELECT
+    ST_Union(geometrie,0.001) AS geometrie
+  FROM nutzzon
+),
+-- clean up narrow holes (data errors)
+-- first step: dump geometries and rings
+nutzzon_rings_tmp AS (
+  SELECT
+    (ST_DumpRings((st_dump(geometrie)).geom)).geom AS geometrie
+  FROM nutzzon_dissolved_tmp
+),
+-- second intermediate step for cleaning: calculate inscribed circle
+nutzzon_rings_tmp2 AS (
+  SELECT
+   geometrie,
+   (ST_MaximumInscribedCircle(geometrie)).radius AS radius
+  FROM nutzzon_rings_tmp
+),
+-- third and final step for cleaning: filter away small rings and rebuild polygons
+-- also rejoin original attributes
+nutzzon_dissolved AS (
+  SELECT
+   ST_BuildArea(ST_Collect(geometrie)) AS geometrie
+  FROM nutzzon_rings_tmp2
+   WHERE
+     radius > 0.4 -- very narrow polygons with only 80cm width are filtered away
+),
+-- Verkehrsflächen
+verkfl AS (
+  SELECT
+   ST_Buffer(geometrie,4,'endcap=flat join=miter') AS geometrie
+  FROM agi_mopublic_pub.mopublic_bodenbedeckung mb 
+   WHERE
+    bfs_nr = (SELECT nr FROM bfsnr)
+    AND art_txt = 'Strasse_Weg'
+),
+--Merge (SQL Union) aller nicht bebaubaren Flächen and clip it (ST_Intersection) to Nutzunszonen Siedlungsgebiet
+--TODO: test if intersection would be faster if done after the  ST_Union() step below
+nicht_bebaubar_tmp AS (
+  SELECT ST_Intersection(geb.geometrie,nd.geometrie,0.001) AS geometrie FROM geb, nutzzon_dissolved nd WHERE ST_Intersects(geb.geometrie,nd.geometrie)
+  UNION ALL
+  SELECT ST_Intersection(eob.geometrie,nd.geometrie,0.001) AS geometrie FROM eob, nutzzon_dissolved nd WHERE ST_Intersects(eob.geometrie,nd.geometrie)
+  UNION ALL
+  SELECT ST_Intersection(verkfl.geometrie,nd.geometrie,0.001) AS geometrie FROM verkfl, nutzzon_dissolved nd WHERE ST_Intersects(verkfl.geometrie,nd.geometrie)
+  UNION ALL
+  SELECT ST_Intersection(gr.geom_buff,nd.geometrie,0.001) AS geometrie FROM gr, nutzzon_dissolved nd WHERE ST_Intersects(gr.geometrie,nd.geometrie)
+  UNION ALL
+  SELECT geom_buff AS geometrie FROM nutzzon
+),
+-- Dissolve (GIS Union) aller nicht bebaubaren Flächen
+nicht_bebaubar AS (
+  SELECT ST_CollectionExtract(ST_Union(geometrie,0.001),3) AS geometrie FROM nicht_bebaubar_tmp
+),
+-- Calculate Difference between Nutzungszonen Siedlungsgebiet and bebaubaren Flächen, Dump them to individual polygons
+bebaubar_tmp AS (
+  SELECT (ST_Dump(ST_Difference(nd.geometrie,nbb.geometrie,0.001))).geom AS geometrie FROM nutzzon_dissolved nd, nicht_bebaubar nbb
+),
+-- filter away small parts of polygons < 180m2, negative buffer followed by positive buffer to get rid of very narrow polygon passages and spikes
+double_buff AS (
+  SELECT
+     (ST_Dump(
+      ST_Buffer(
+        ST_Buffer(
+          geometrie,
+          -3,
+          'endcap=flat join=miter mitre_limit=2.0'),
+        3,
+        'endcap=flat join=miter mitre_limit=2.0')
+      )
+    ).geom
+    AS geometrie
+  FROM
+    bebaubar_tmp
+   WHERE
+     ST_Area(geometrie) >= 180
+),
+-- final version of unbebaute Flächen
+-- filter small polygons away and calculate pole of inaccessibility with ST_MaximumInscribedCircle
+unbebaut_final AS (
+  SELECT
+    (ST_MaximumInscribedCircle(geometrie)).radius AS radius,
+    geometrie
+  FROM double_buff
+   WHERE
+    ST_Area(geometrie) >= 180
+),
+-- ST_Union of unbebaut
+unbebaut_dissolved AS (
+  SELECT
+    ST_Union(geometrie,0.001) AS geometrie
+  FROM unbebaut_final
+   WHERE
+    radius > 5
+)
+,
+-- final version of bebaute Flächen mit ST_Difference
+bebaut_final_tmp AS (
+  SELECT
+    (ST_Dump(ST_Difference(nd.geometrie,unbb.geometrie,0.001))).geom AS geometrie
+  FROM nutzzon_dissolved nd, unbebaut_dissolved unbb
+),
+-- intersection with undissolved Nutzungszonen to get polygons that honor the Nutzungszonen
+bebaut_final AS (
+  SELECT ST_Intersection(bft.geometrie,nz.geometrie,0.001) AS geometrie
+    FROM bebaut_final_tmp bft, nutzzon nz
+    WHERE ST_Intersects(nz.geometrie,bft.geometrie)
+),
+-- union of bebaut_final and unbebaut_dissolved
+gesamt_final AS (
+  SELECT 'bebaut' AS bebauungsstand, ST_Multi((ST_Dump(bb.geometrie)).geom) AS geometrie FROM bebaut_final bb
+    UNION ALL
+  SELECT 'unbebaut' AS bebauungsstand, ST_Multi((ST_Dump(ubb.geometrie)).geom) AS geometrie FROM unbebaut_dissolved ubb
+)
+-- areas (m2) can only be calculated at the very end after the ST_Dump()
+-- also joining Nutzungszonen and Gemeinden
+SELECT 
+  nz.typ_kt AS grundnutzung_typ_kt,
+  f.bebauungsstand,
+  nz.bfs_nr,
+  gem.gemeindename,
+  Round(ST_Area(f.geometrie)) AS flaeche,
+  f.geometrie
+FROM gesamt_final f
+  LEFT JOIN nutzzon nz ON ST_Intersects(ST_PointOnSurface(f.geometrie),nz.geometrie)
+  LEFT JOIN gem ON gem.bfs_gemeindenummer = nz.bfs_nr
+ WHERE
+   f.geometrie IS NOT NULL
+   AND ST_IsValid(f.geometrie)
+   AND ST_GeometryType(f.geometrie) = 'ST_MultiPolygon'
+   AND Round(ST_Area(f.geometrie)) > 0
+ ORDER BY nz.bfs_nr, nz.typ_kt, f.bebauungsstand
+;
---
+++ arp_auswertung_nutzungsplanung_pub/bauzonenstatistik_gines.sql
@@ -0,0 +1,111 @@
+-- Zusammenzug bebaute und unbebaute Parzellen nach Gemeinde, Zonentyp und bebauungsstatus
+-- abgeleitet von Tabelle ${BZ_SchemaName}.bauzonenstatistik_bebauungsstand_pro_gemeinde
+
+DELETE FROM ${BZ_SchemaName}.bauzonenstatistik_gines;
+
+INSERT INTO
+  ${BZ_SchemaName}.bauzonenstatistik_gines 
+    (bfs_gemeindenummer, gemeindename, n110_wohnzone_1_g_bebaut, n110_wohnzone_1_g_unbebaut, n111_wohnzone_2_g_bebaut, n111_wohnzone_2_g_unbebaut,
+     n112_wohnzone_3_g_bebaut, n112_wohnzone_3_g_unbebaut, n113_wohnzone_4_g_bebaut, n113_wohnzone_4_g_unbebaut, n114_wohnzone_5_g_bebaut, n114_wohnzone_5_g_unbebaut, n115_wohnzone_6_g_bebaut,
+     n115_wohnzone_6_g_unbebaut, n116_wohnzone_7_g_und_groesser_bebaut, n116_wohnzone_7_g_und_groesser_unbebaut, n117_zone_fuer_terrassenhaeuser_terrassensiedlung_bebaut,
+     n117_zone_fuer_terrassenhaeuser_terrassensiedlung_unbebaut, n120_gewerbezone_ohne_wohnen_bebaut, n120_gewerbezone_ohne_wohnen_unbebaut, n121_industriezone_bebaut,
+     n121_industriezone_unbebaut, n122_arbeitszone_bebaut, n122_arbeitszone_unbebaut, n130_gewerbezone_mit_wohnen_mischzone_bebaut,
+     n130_gewerbezone_mit_wohnen_mischzone_unbebaut, n131_gewerbezone_mit_wohnen_mischzone_2_g_bebaut, n131_gewerbezone_mit_wohnen_mischzone_2_g_unbebaut,
+     n132_gewerbezone_mit_wohnen_mischzone_3_g_bebaut, n132_gewerbezone_mit_wohnen_mischzone_3_g_unbebaut, n133_gewerbezone_mit_wohnen_mischzone_4_g_und_groesser_bebaut,
+     n133_gewerbezone_mit_wohnen_mischzone_4_g_und_groesser_unbebaut, n134_zone_fuer_publikumsintensive_anlagen_bebaut, n134_zone_fuer_publikumsintensive_anlagen_unbebaut,
+     n140_kernzone_bebaut, n140_kernzone_unbebaut, n141_zentrumszone_bebaut, n141_zentrumszone_unbebaut, n142_erhaltungszone_bebaut, n142_erhaltungszone_unbebaut,
+     n150_zone_fuer_oeffentliche_bauten_bebaut, n150_zone_fuer_oeffentliche_bauten_unbebaut, n151_zone_fuer_oeffentliche_anlagen_bebaut,
+     n151_zone_fuer_oeffentliche_anlagen_unbebaut, n162_landwirtschaftliche_kernzone_bebaut, n162_landwirtschaftliche_kernzone_unbebaut, n163_weilerzone_bebaut,
+     n163_weilerzone_unbebaut, n170_zone_fuer_freizeit_und_erholung_bebaut, n170_zone_fuer_freizeit_und_erholung_unbebaut, n180_verkehrszone_strasse_bebaut,
+     n180_verkehrszone_strasse_unbebaut, n190_spezialzone_bebaut, n190_spezialzone_unbebaut, n430_reservezone_wohnzone_mischzone_kernzone_zentrumszone,
+     n431_reservezone_arbeiten, n432_reservezone_oe_ba, n439_reservezone, n320_gewaesser
+    )
+
+WITH 
+gg AS (
+ SELECT
+    gg.bfs_gemeindenummer,
+    gg.gemeindename,
+    COALESCE(Round((SUM(ST_Area(grunutz.geometrie)) FILTER (WHERE grunutz.typ_kt='N430_Reservezone_Wohnzone_Mischzone_Kernzone_Zentrumszone'))::numeric / 10000,2),0) AS n430_reservezone_wohnzone_mischzone_kernzone_zentrumszone,
+    COALESCE(Round((SUM(ST_Area(grunutz.geometrie)) FILTER (WHERE grunutz.typ_kt='N431_Reservezone_Arbeiten'))::numeric / 10000,2),0) AS n431_reservezone_arbeiten,
+    COALESCE(Round((SUM(ST_Area(grunutz.geometrie)) FILTER (WHERE grunutz.typ_kt='N432_Reservezone_OeBA'))::numeric / 10000,2),0) AS n432_reservezone_oe_ba,
+    COALESCE(Round((SUM(ST_Area(grunutz.geometrie)) FILTER (WHERE grunutz.typ_kt='N439_Reservezone'))::numeric / 10000,2),0) AS n439_reservezone,
+    COALESCE(Round((SUM(ST_Area(grunutz.geometrie)) FILTER (WHERE grunutz.typ_kt='N320_Gewaesser'))::numeric / 10000,2),0) AS n320_gewaesser
+  FROM agi_hoheitsgrenzen_pub.hoheitsgrenzen_gemeindegrenze gg
+   LEFT JOIN arp_npl_pub.nutzungsplanung_grundnutzung grunutz
+    ON grunutz.bfs_nr = gg.bfs_gemeindenummer
+       -- Gewaesser und Reservezonen
+      AND grunutz.typ_kt IN ('N320_Gewaesser','N430_Reservezone_Wohnzone_Mischzone_Kernzone_Zentrumszone','N431_Reservezone_Arbeiten','N432_Reservezone_OeBA','N439_Reservezone') 
+     --WHERE gg.bfs_gemeindenummer < 2410
+      GROUP BY gg.bfs_gemeindenummer, gg.gemeindename
+      ORDER BY gg.bfs_gemeindenummer ASC
+)
+-- finales Resultat, einzelne Zonen in Spalten, Gemeinden in Zeilen
+SELECT 
+  gg.bfs_gemeindenummer,
+  gg.gemeindename,
+  COALESCE(Round((SUM(ls.flaeche_beschnitten) FILTER (WHERE ls.grundnutzung_typ_kt = 'N110_Wohnzone_1_G' AND ls.bebauungsstand IN ('bebaut','teilweise_bebaut')))::numeric / 10000,2),0) AS n110_wohnzone_1_g_bebaut,
+  COALESCE(Round((SUM(ls.flaeche_beschnitten) FILTER (WHERE ls.grundnutzung_typ_kt = 'N110_Wohnzone_1_G' AND ls.bebauungsstand = 'unbebaut'))::numeric / 10000,2),0) AS n110_wohnzone_1_g_unbebaut,
+  COALESCE(Round((SUM(ls.flaeche_beschnitten) FILTER (WHERE ls.grundnutzung_typ_kt = 'N111_Wohnzone_2_G' AND ls.bebauungsstand IN ('bebaut','teilweise_bebaut')))::numeric / 10000,2),0) AS n111_wohnzone_2_g_bebaut,
+  COALESCE(Round((SUM(ls.flaeche_beschnitten) FILTER (WHERE ls.grundnutzung_typ_kt = 'N111_Wohnzone_2_G' AND ls.bebauungsstand = 'unbebaut'))::numeric / 10000,2),0) AS n111_wohnzone_2_g_unbebaut,
+  COALESCE(Round((SUM(ls.flaeche_beschnitten) FILTER (WHERE ls.grundnutzung_typ_kt = 'N112_Wohnzone_3_G' AND ls.bebauungsstand IN ('bebaut','teilweise_bebaut')))::numeric / 10000,2),0) AS n112_wohnzone_3_g_bebaut,
+  COALESCE(Round((SUM(ls.flaeche_beschnitten) FILTER (WHERE ls.grundnutzung_typ_kt = 'N112_Wohnzone_3_G' AND ls.bebauungsstand = 'unbebaut'))::numeric / 10000,2),0) AS n112_wohnzone_3_g_unbebaut,
+  COALESCE(Round((SUM(ls.flaeche_beschnitten) FILTER (WHERE ls.grundnutzung_typ_kt = 'N113_Wohnzone_4_G' AND ls.bebauungsstand IN ('bebaut','teilweise_bebaut')))::numeric / 10000,2),0) AS n113_wohnzone_4_g_bebaut,
+  COALESCE(Round((SUM(ls.flaeche_beschnitten) FILTER (WHERE ls.grundnutzung_typ_kt = 'N113_Wohnzone_4_G' AND ls.bebauungsstand = 'unbebaut'))::numeric / 10000,2),0) AS n113_wohnzone_4_g_unbebaut,
+  COALESCE(Round((SUM(ls.flaeche_beschnitten) FILTER (WHERE ls.grundnutzung_typ_kt = 'N114_Wohnzone_5_G' AND ls.bebauungsstand IN ('bebaut','teilweise_bebaut')))::numeric / 10000,2),0) AS n114_wohnzone_5_g_bebaut,
+  COALESCE(Round((SUM(ls.flaeche_beschnitten) FILTER (WHERE ls.grundnutzung_typ_kt = 'N114_Wohnzone_5_G' AND ls.bebauungsstand = 'unbebaut'))::numeric / 10000,2),0) AS n114_wohnzone_5_g_unbebaut,
+  COALESCE(Round((SUM(ls.flaeche_beschnitten) FILTER (WHERE ls.grundnutzung_typ_kt = 'N115_Wohnzone_6_G' AND ls.bebauungsstand IN ('bebaut','teilweise_bebaut')))::numeric / 10000,2),0) AS n115_wohnzone_6_g_bebaut,
+  COALESCE(Round((SUM(ls.flaeche_beschnitten) FILTER (WHERE ls.grundnutzung_typ_kt = 'N115_Wohnzone_6_G' AND ls.bebauungsstand = 'unbebaut'))::numeric / 10000,2),0) AS n115_wohnzone_6_g_unbebaut,
+  COALESCE(Round((SUM(ls.flaeche_beschnitten) FILTER (WHERE ls.grundnutzung_typ_kt = 'N116_Wohnzone_7_G_und_groesser' AND ls.bebauungsstand IN ('bebaut','teilweise_bebaut')))::numeric / 10000,2),0) AS n116_wohnzone_7_g_und_groesser_bebaut,
+  COALESCE(Round((SUM(ls.flaeche_beschnitten) FILTER (WHERE ls.grundnutzung_typ_kt = 'N116_Wohnzone_7_G_und_groesser' AND ls.bebauungsstand = 'unbebaut'))::numeric / 10000,2),0) AS n116_wohnzone_7_g_und_groesser_unbebaut,
+  COALESCE(Round((SUM(ls.flaeche_beschnitten) FILTER (WHERE ls.grundnutzung_typ_kt = 'N117_Zone_fuer_Terrassenhaeuser_Terrassensiedlung' AND ls.bebauungsstand IN ('bebaut','teilweise_bebaut')))::numeric / 10000,2),0) AS n117_zone_fuer_terrassenhaeuser_terrassensiedlung_bebaut,
+  COALESCE(Round((SUM(ls.flaeche_beschnitten) FILTER (WHERE ls.grundnutzung_typ_kt = 'N117_Zone_fuer_Terrassenhaeuser_Terrassensiedlung' AND ls.bebauungsstand = 'unbebaut'))::numeric / 10000,2),0) AS n117_zone_fuer_terrassenhaeuser_terrassensiedlung_unbebaut,
+  COALESCE(Round((SUM(ls.flaeche_beschnitten) FILTER (WHERE ls.grundnutzung_typ_kt = 'N120_Gewerbezone_ohne_Wohnen' AND ls.bebauungsstand IN ('bebaut','teilweise_bebaut')))::numeric / 10000,2),0) AS n120_gewerbezone_ohne_wohnen_bebaut,
+  COALESCE(Round((SUM(ls.flaeche_beschnitten) FILTER (WHERE ls.grundnutzung_typ_kt = 'N120_Gewerbezone_ohne_Wohnen' AND ls.bebauungsstand = 'unbebaut'))::numeric / 10000,2),0) AS n120_gewerbezone_ohne_wohnen_unbebaut,
+  COALESCE(Round((SUM(ls.flaeche_beschnitten) FILTER (WHERE ls.grundnutzung_typ_kt = 'N121_Industriezone' AND ls.bebauungsstand IN ('bebaut','teilweise_bebaut')))::numeric / 10000,2),0) AS n121_industriezone_bebaut,
+  COALESCE(Round((SUM(ls.flaeche_beschnitten) FILTER (WHERE ls.grundnutzung_typ_kt = 'N121_Industriezone' AND ls.bebauungsstand = 'unbebaut'))::numeric / 10000,2),0) AS n121_industriezone_unbebaut,
+  COALESCE(Round((SUM(ls.flaeche_beschnitten) FILTER (WHERE ls.grundnutzung_typ_kt = 'N122_Arbeitszone' AND ls.bebauungsstand IN ('bebaut','teilweise_bebaut')))::numeric / 10000,2),0) AS n122_arbeitszone_bebaut,
+  COALESCE(Round((SUM(ls.flaeche_beschnitten) FILTER (WHERE ls.grundnutzung_typ_kt = 'N122_Arbeitszone' AND ls.bebauungsstand = 'unbebaut'))::numeric / 10000,2),0) AS n122_arbeitszone_unbebaut,
+  COALESCE(Round((SUM(ls.flaeche_beschnitten) FILTER (WHERE ls.grundnutzung_typ_kt = 'N130_Gewerbezone_mit_Wohnen_Mischzone' AND ls.bebauungsstand IN ('bebaut','teilweise_bebaut')))::numeric / 10000,2),0) AS n130_gewerbezone_mit_wohnen_mischzone_bebaut,
+  COALESCE(Round((SUM(ls.flaeche_beschnitten) FILTER (WHERE ls.grundnutzung_typ_kt = 'N130_Gewerbezone_mit_Wohnen_Mischzone' AND ls.bebauungsstand = 'unbebaut'))::numeric / 10000,2),0) AS n130_gewerbezone_mit_wohnen_mischzone_unbebaut,
+  COALESCE(Round((SUM(ls.flaeche_beschnitten) FILTER (WHERE ls.grundnutzung_typ_kt = 'N131_Gewerbezone_mit_Wohnen_Mischzone_2_G' AND ls.bebauungsstand IN ('bebaut','teilweise_bebaut')))::numeric / 10000,2),0) AS n131_gewerbezone_mit_wohnen_mischzone_2_g_bebaut,
+  COALESCE(Round((SUM(ls.flaeche_beschnitten) FILTER (WHERE ls.grundnutzung_typ_kt = 'N131_Gewerbezone_mit_Wohnen_Mischzone_2_G' AND ls.bebauungsstand = 'unbebaut'))::numeric / 10000,2),0) AS n131_gewerbezone_mit_wohnen_mischzone_2_g_unbebaut,
+  COALESCE(Round((SUM(ls.flaeche_beschnitten) FILTER (WHERE ls.grundnutzung_typ_kt = 'N132_Gewerbezone_mit_Wohnen_Mischzone_3_G' AND ls.bebauungsstand IN ('bebaut','teilweise_bebaut')))::numeric / 10000,2),0) AS n132_gewerbezone_mit_wohnen_mischzone_3_g_bebaut,
+  COALESCE(Round((SUM(ls.flaeche_beschnitten) FILTER (WHERE ls.grundnutzung_typ_kt = 'N132_Gewerbezone_mit_Wohnen_Mischzone_3_G' AND ls.bebauungsstand = 'unbebaut'))::numeric / 10000,2),0) AS n132_gewerbezone_mit_wohnen_mischzone_3_g_unbebaut,
+  COALESCE(Round((SUM(ls.flaeche_beschnitten) FILTER (WHERE ls.grundnutzung_typ_kt = 'N133_Gewerbezone_mit_Wohnen_Mischzone_4_G_und_groesser' AND ls.bebauungsstand IN ('bebaut','teilweise_bebaut')))::numeric / 10000,2),0) AS n133_gewerbezone_mit_wohnen_mischzone_4_g_und_groesser_bebaut,
+  COALESCE(Round((SUM(ls.flaeche_beschnitten) FILTER (WHERE ls.grundnutzung_typ_kt = 'N133_Gewerbezone_mit_Wohnen_Mischzone_4_G_und_groesser' AND ls.bebauungsstand = 'unbebaut'))::numeric / 10000,2),0) AS n133_gewerbezone_mit_wohnen_mischzone_4_g_und_groesser_unbebaut,
+  COALESCE(Round((SUM(ls.flaeche_beschnitten) FILTER (WHERE ls.grundnutzung_typ_kt = 'N134_Zone_fuer_publikumsintensive_Anlagen' AND ls.bebauungsstand IN ('bebaut','teilweise_bebaut')))::numeric / 10000,2),0) AS n134_zone_fuer_publikumsintensive_anlagen_bebaut,
+  COALESCE(Round((SUM(ls.flaeche_beschnitten) FILTER (WHERE ls.grundnutzung_typ_kt = 'N134_Zone_fuer_publikumsintensive_Anlagen' AND ls.bebauungsstand = 'unbebaut'))::numeric / 10000,2),0) AS n134_zone_fuer_publikumsintensive_anlagen_unbebaut,
+  COALESCE(Round((SUM(ls.flaeche_beschnitten) FILTER (WHERE ls.grundnutzung_typ_kt = 'N140_Kernzone' AND ls.bebauungsstand IN ('bebaut','teilweise_bebaut')))::numeric / 10000,2),0) AS n140_kernzone_bebaut,
+  COALESCE(Round((SUM(ls.flaeche_beschnitten) FILTER (WHERE ls.grundnutzung_typ_kt = 'N140_Kernzone' AND ls.bebauungsstand = 'unbebaut'))::numeric / 10000,2),0) AS n140_kernzone_unbebaut,
+  COALESCE(Round((SUM(ls.flaeche_beschnitten) FILTER (WHERE ls.grundnutzung_typ_kt = 'N141_Zentrumszone' AND ls.bebauungsstand IN ('bebaut','teilweise_bebaut')))::numeric / 10000,2),0) AS n141_zentrumszone_bebaut,
+  COALESCE(Round((SUM(ls.flaeche_beschnitten) FILTER (WHERE ls.grundnutzung_typ_kt = 'N141_Zentrumszone' AND ls.bebauungsstand = 'unbebaut'))::numeric / 10000,2),0) AS n141_zentrumszone_unbebaut,
+  COALESCE(Round((SUM(ls.flaeche_beschnitten) FILTER (WHERE ls.grundnutzung_typ_kt = 'N142_Erhaltungszone' AND ls.bebauungsstand IN ('bebaut','teilweise_bebaut')))::numeric / 10000,2),0) AS n142_erhaltungszone_bebaut,
+  COALESCE(Round((SUM(ls.flaeche_beschnitten) FILTER (WHERE ls.grundnutzung_typ_kt = 'N142_Erhaltungszone' AND ls.bebauungsstand = 'unbebaut'))::numeric / 10000,2),0) AS n142_erhaltungszone_unbebaut,
+  COALESCE(Round((SUM(ls.flaeche_beschnitten) FILTER (WHERE ls.grundnutzung_typ_kt = 'N150_Zone_fuer_oeffentliche_Bauten' AND ls.bebauungsstand IN ('bebaut','teilweise_bebaut')))::numeric / 10000,2),0) AS n150_zone_fuer_oeffentliche_bauten_bebaut,
+  COALESCE(Round((SUM(ls.flaeche_beschnitten) FILTER (WHERE ls.grundnutzung_typ_kt = 'N150_Zone_fuer_oeffentliche_Bauten' AND ls.bebauungsstand = 'unbebaut'))::numeric / 10000,2),0) AS n150_zone_fuer_oeffentliche_bauten_unbebaut,
+  COALESCE(Round((SUM(ls.flaeche_beschnitten) FILTER (WHERE ls.grundnutzung_typ_kt = 'N151_Zone_fuer_oeffentliche_Anlagen' AND ls.bebauungsstand IN ('bebaut','teilweise_bebaut')))::numeric / 10000,2),0) AS n151_zone_fuer_oeffentliche_anlagen_bebaut,
+  COALESCE(Round((SUM(ls.flaeche_beschnitten) FILTER (WHERE ls.grundnutzung_typ_kt = 'N151_Zone_fuer_oeffentliche_Anlagen' AND ls.bebauungsstand = 'unbebaut'))::numeric / 10000,2),0) AS n151_zone_fuer_oeffentliche_anlagen_unbebaut,
+  COALESCE(Round((SUM(ls.flaeche_beschnitten) FILTER (WHERE ls.grundnutzung_typ_kt = 'N162_Landwirtschaftliche_Kernzone' AND ls.bebauungsstand IN ('bebaut','teilweise_bebaut')))::numeric / 10000,2),0) AS n162_landwirtschaftliche_kernzone_bebaut,
+  COALESCE(Round((SUM(ls.flaeche_beschnitten) FILTER (WHERE ls.grundnutzung_typ_kt = 'N162_Landwirtschaftliche_Kernzone' AND ls.bebauungsstand = 'unbebaut'))::numeric / 10000,2),0) AS n162_landwirtschaftliche_kernzone_unbebaut,
+  COALESCE(Round((SUM(ls.flaeche_beschnitten) FILTER (WHERE ls.grundnutzung_typ_kt = 'N163_Weilerzone' AND ls.bebauungsstand IN ('bebaut','teilweise_bebaut')))::numeric / 10000,2),0) AS n163_weilerzone_bebaut,
+  COALESCE(Round((SUM(ls.flaeche_beschnitten) FILTER (WHERE ls.grundnutzung_typ_kt = 'N163_Weilerzone' AND ls.bebauungsstand = 'unbebaut'))::numeric / 10000,2),0) AS n163_weilerzone_unbebaut,
+  COALESCE(Round((SUM(ls.flaeche_beschnitten) FILTER (WHERE ls.grundnutzung_typ_kt = 'N170_Zone_fuer_Freizeit_und_Erholung' AND ls.bebauungsstand IN ('bebaut','teilweise_bebaut')))::numeric / 10000,2),0) AS n170_zone_fuer_freizeit_und_erholung_bebaut,
+  COALESCE(Round((SUM(ls.flaeche_beschnitten) FILTER (WHERE ls.grundnutzung_typ_kt = 'N170_Zone_fuer_Freizeit_und_Erholung' AND ls.bebauungsstand = 'unbebaut'))::numeric / 10000,2),0) AS n170_zone_fuer_freizeit_und_erholung_unbebaut,
+  COALESCE(Round((SUM(ls.flaeche_beschnitten) FILTER (WHERE ls.grundnutzung_typ_kt = 'N180_Verkehrszone_Strasse' AND ls.bebauungsstand IN ('bebaut','teilweise_bebaut')))::numeric / 10000,2),0) AS n180_verkehrszone_strasse_bebaut,
+  COALESCE(Round((SUM(ls.flaeche_beschnitten) FILTER (WHERE ls.grundnutzung_typ_kt = 'N180_Verkehrszone_Strasse' AND ls.bebauungsstand = 'unbebaut'))::numeric / 10000,2),0) AS n180_verkehrszone_strasse_unbebaut,
+  COALESCE(Round((SUM(ls.flaeche_beschnitten) FILTER (WHERE ls.grundnutzung_typ_kt = 'N190_Spezialzone' AND ls.bebauungsstand IN ('bebaut','teilweise_bebaut')))::numeric / 10000,2),0) AS n190_spezialzone_bebaut,
+  COALESCE(Round((SUM(ls.flaeche_beschnitten) FILTER (WHERE ls.grundnutzung_typ_kt = 'N190_Spezialzone' AND ls.bebauungsstand = 'unbebaut'))::numeric / 10000,2),0) AS n190_spezialzone_unbebaut,
+  gg.n430_reservezone_wohnzone_mischzone_kernzone_zentrumszone,
+  gg.n431_reservezone_arbeiten,
+  gg.n432_reservezone_oe_ba,
+  gg.n439_reservezone,
+  gg.n320_gewaesser
+FROM
+  gg
+  LEFT JOIN ${BZ_SchemaName}.bauzonenstatistik_liegenschaft_nach_bebauungsstand ls
+    ON ls.bfs_nr = gg.bfs_gemeindenummer
+  GROUP BY gg.bfs_gemeindenummer, gg.gemeindename, gg.n430_reservezone_wohnzone_mischzone_kernzone_zentrumszone, gg.n431_reservezone_arbeiten,
+    gg.n432_reservezone_oe_ba, gg.n439_reservezone, gg.n320_gewaesser
+  ORDER BY gg.bfs_gemeindenummer 
+ ;
