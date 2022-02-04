e16b1a324d1054bc4bdd9bc96675ef04ac75ed72
EchoCharlie <martin.schweizer@bd.so.ch>
2022-02-04 09:29:54 +0100

    [alw_fruchtfolgeflaechen]Bugfix in fff_statistik.sql

--- alw_fruchtfolgeflaechen/04_Finish/fff_statistik.sql
+++ alw_fruchtfolgeflaechen/04_Finish/fff_statistik.sql
@@ -3,13 +3,13 @@ SELECT
     gemeinden.bfs_nr AS bfs_nummer, 
     fff.anrechenbar,
     fff.spezialfall,
-	round(sum(STarea(STintersection(fff.geometrie, gemeinden.geometrie)))::NUMERIC, 2)/ 100 AS flaeche,
-	round(sum(STarea(STintersection(fff.geometrie, gemeinden.geometrie)))::NUMERIC, 2)/ 100 * anrechenbar AS flaeche_anrechenbar
+	round(sum(ST_area(ST_intersection(fff.geometrie, gemeinden.geometrie)))::NUMERIC, 2)/ 100 AS flaeche,
+	round(sum(ST_area(ST_intersection(fff.geometrie, gemeinden.geometrie)))::NUMERIC, 2)/ 100 * anrechenbar AS flaeche_anrechenbar
 FROM
 	alw_fruchtfolgeflaechen.fruchtfolgeflaeche_clean fff,
 	agi_mopublic_pub.mopublic_gemeindegrenze gemeinden
 WHERE
-	STintersects(fff.geometrie,
+	ST_intersects(fff.geometrie,
 	gemeinden.geometrie)
 GROUP BY
 	fff.spezialfall,
