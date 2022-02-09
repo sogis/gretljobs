4f51502520b5a18dc6a0969e7083a04c3b53e942
barpasen <peter.senn@bd.so.ch>
2022-02-09 15:37:43 +0100

    [afu_oekomorphologie_import] initial version

---
+++ afu_oekomorphologie_import/delete_from_oekomorphologie_tables.sql
@@ -0,0 +1,9 @@
+DELETE FROM
+    afu_fliessgewaesser_staging_v1.oekomorphcsv
+;
+DELETE FROM 
+    afu_fliessgewaesser_staging_v1.absturzcsv
+;
+DELETE FROM 
+    afu_fliessgewaesser_staging_v1.bauwerkcsv
+;
