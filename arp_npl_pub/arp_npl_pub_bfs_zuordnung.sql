2903fc0d73a2f70424cc1c25615feeb833bc6446
Andreas Neumann <a.neumann@carto.net>
2022-02-23 09:29:13 +0100

    [arp_npl_pub] next try 2

--- arp_npl_pub/arp_npl_pub_bfs_zuordnung.sql
+++ arp_npl_pub/arp_npl_pub_bfs_zuordnung.sql
@@ -3,6 +3,6 @@
 -- Grundnutzung
 UPDATE
     ${dbSchemaNPL}.nutzungsplanung_grundnutzung gr
-     SET gr.bfs_nr = gg.bfs_gemeindenummer
+     SET bfs_nr = gg.bfs_gemeindenummer
   FROM ${dbSchemaHoheitsgr}.hoheitsgrenzen_gemeindegrenze gg
     WHERE ST_Within(ST_Centroid(gr.geometrie),gg.geometrie);
