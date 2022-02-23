1f8dc2fa80aa935a0cb9d386efee21f98c697f4d
Andreas Neumann <a.neumann@carto.net>
2022-02-23 09:14:50 +0100

    [arp_npl_pub] DB Schema korrekt übergeben

--- arp_npl_pub/arp_npl_pub_bfs_zuordnung.sql
+++ arp_npl_pub/arp_npl_pub_bfs_zuordnung.sql
@@ -3,8 +3,6 @@
 -- Grundnutzung
 UPDATE
    ${dbSchemaNPL}.nutzungsplanung_grundnutzung gr
-     SET bfs_nr = gg.bfs_gemeindenummer
-  FROM ${dbSchemaHoheitsg}.hoheitsgrenzen_gemeindegrenze gg
+     SET gr.bfs_nr = gg.bfs_gemeindenummer
+  FROM ${dbSchemaHoheitsgr}.hoheitsgrenzen_gemeindegrenze gg
     WHERE ST_Within(ST_Centroid(gr.geometrie),gg.geometrie);
-
-    
