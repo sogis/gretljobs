0481bb947eac4b2708947f278012814ff8585028
Andreas Neumann <a.neumann@carto.net>
2022-02-23 08:58:13 +0100

    [arp_npl_pub] Schemaname als sqlParameter

--- arp_npl_pub/arp_npl_pub_bfs_zuordnung.sql
+++ arp_npl_pub/arp_npl_pub_bfs_zuordnung.sql
@@ -2,7 +2,9 @@
 
 -- Grundnutzung
 UPDATE
-   arp_npl_pub.nutzungsplanung_grundnutzung gr
+   ${dbSchemaNPL}.nutzungsplanung_grundnutzung gr
      SET bfs_nr = gg.bfs_gemeindenummer
-  FROM agi_hoheitsgrenzen_pub.hoheitsgrenzen_gemeindegrenze gg
+  FROM ${dbSchemaHoheitsg}.hoheitsgrenzen_gemeindegrenze gg
     WHERE ST_Within(ST_Centroid(gr.geometrie),gg.geometrie);
+
+    
