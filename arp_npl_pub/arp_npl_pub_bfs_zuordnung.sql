fdf5ddcc6245851338a1edb38fb84c7f13470c27
Andreas Neumann <a.neumann@carto.net>
2022-02-23 10:18:53 +0100

    [arp_npl_pub] BFS Zuordnung für gesamten Rest

--- arp_npl_pub/arp_npl_pub_bfs_zuordnung.sql
+++ arp_npl_pub/arp_npl_pub_bfs_zuordnung.sql
@@ -10,6 +10,13 @@ UPDATE
   FROM ${dbSchemaHoheitsgr}.hoheitsgrenzen_gemeindegrenze gg
     WHERE ST_Within(ST_Centroid(gr.geometrie),gg.geometrie);
 
+-- Beschriftung
+UPDATE
+    ${dbSchemaNPL}.nutzungsplanung_nutzungsplanung_beschriftung be
+     SET bfs_nr = gg.bfs_gemeindenummer
+  FROM ${dbSchemaHoheitsgr}.hoheitsgrenzen_gemeindegrenze gg
+    WHERE ST_Within(be.pos,gg.geometrie);
+    
 -- Erschliessungsplanung
 -- Beschriftung
 UPDATE
@@ -38,3 +45,39 @@ UPDATE
      SET bfs_nr = gg.bfs_gemeindenummer
   FROM ${dbSchemaHoheitsgr}.hoheitsgrenzen_gemeindegrenze gg
     WHERE ST_Within(epkt.geometrie,gg.geometrie);
+
+-- überlagernde Objekte
+-- Flächenobjekt
+UPDATE
+    ${dbSchemaNPL}.nutzungsplanung_ueberlagernd_flaeche uefl
+     SET bfs_nr = gg.bfs_gemeindenummer
+  FROM ${dbSchemaHoheitsgr}.hoheitsgrenzen_gemeindegrenze gg
+    WHERE ST_Within(ST_Centroid(uefl.geometrie),gg.geometrie);
+    
+-- Linienobjekt
+UPDATE
+    ${dbSchemaNPL}.nutzungsplanung_ueberlagernd_linie uelin
+     SET bfs_nr = gg.bfs_gemeindenummer
+  FROM ${dbSchemaHoheitsgr}.hoheitsgrenzen_gemeindegrenze gg
+    WHERE ST_Within(ST_LineInterpolatePoint(uelin.geometrie,0.5),gg.geometrie);
+    
+-- Punktobjekt
+UPDATE
+    ${dbSchemaNPL}.nutzungsplanung_ueberlagernd_punkt uepkt
+     SET bfs_nr = gg.bfs_gemeindenummer
+  FROM ${dbSchemaHoheitsgr}.hoheitsgrenzen_gemeindegrenze gg
+    WHERE ST_Within(uepkt.geometrie,gg.geometrie);
+
+-- VS Perimeter Beschriftung
+UPDATE
+    ${dbSchemaNPL}.nutzungsplanung_vs_perimeter_beschriftung vsbe
+     SET bfs_nr = gg.bfs_gemeindenummer
+  FROM ${dbSchemaHoheitsgr}.hoheitsgrenzen_gemeindegrenze gg
+    WHERE ST_Within(vsbe.pos,gg.geometrie);
+
+-- VS Perimeter Verfahrensstand
+UPDATE
+    ${dbSchemaNPL}.nutzungsplanung_vs_perimeter_verfahrensstand vsver
+     SET bfs_nr = gg.bfs_gemeindenummer
+  FROM ${dbSchemaHoheitsgr}.hoheitsgrenzen_gemeindegrenze gg
+    WHERE ST_Within(ST_Centroid(vsver.geometrie),gg.geometrie);
