28ff9d0ce2acdacb4b64780d8d5a5506dd860bb5
bjsvwcur <38523407+bjsvwcur@users.noreply.github.com>
2022-04-12 07:48:38 +0200

    [agi_hoehen] 2001 und 2014 abgeloest durch neue Schemas (#1116)

--- agi_hoehen_2001_pub/agi_hoehen_2001_pub_hoehenkurven_10m.sql
+++
@@ -1,8 +0,0 @@
-SELECT
-    gid AS t_id,
-    "level",
-    wkb_geometry AS geometrie,
-    kategorie 
-FROM
-    public.sogis_dtm_contour_10m
-;
--- agi_hoehen_2014_pub/agi_hoehen_2014_pub_hoehenkurven_10m_vereinfacht.sql
+++
@@ -1,8 +0,0 @@
-SELECT
-    gid AS t_id,
-    "ID" AS id,
-    elev,
-    ST_Multi(wkb_geometry) AS geometrie
-FROM
-    lidar.contour_simplify
-;
