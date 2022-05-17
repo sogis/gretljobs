610968ae8e91a24e25ff0ac87f657d5187794beb
barpasen <peter.senn@bd.so.ch>
2022-05-17 13:51:19 +0200

    [arp_richtplan_pub] allg. Dokumente anhängen

---
+++ arp_richtplan_pub/arp_richplan_pub_update_documents.sql
@@ -0,0 +1,36 @@
+UPDATE arp_richtplan_pub.richtplankarte_ueberlagernde_flaeche
+    SET dokumente = '[{"t_id":null,"t_ili_tid":null,"titel":"Richtplankapitel E-2.4 Windenergie","publiziertab":null,"bemerkung":null,"dokument":"https://geo.so.ch/docs/ch.so.arp.richtplan/E-2.4.pdf"}]'
+WHERE     
+    objekttyp = 'Windenergie'
+;
+UPDATE arp_richtplan_pub.richtplankarte_ueberlagernde_flaeche
+    SET dokumente = '[{"t_id":null,"t_ili_tid":null,"titel":"Richtplankapitel E-3.2 Kies","publiziertab":null,"bemerkung":null,"dokument":"https://geo.so.ch/docs/ch.so.arp.richtplan/E-3_2.pdf"}]'
+WHERE     
+    objekttyp = 'Abbaustelle.Kies'
+;
+UPDATE arp_richtplan_pub.richtplankarte_ueberlagernde_flaeche
+    SET dokumente = '[{"t_id":null,"t_ili_tid":null,"titel":"Richtplankapitel E-3.3 Kalkstein","publiziertab":null,"bemerkung":null,"dokument":"https://geo.so.ch/docs/ch.so.arp.richtplan/E-3_3.pdf"}]'
+WHERE     
+    objekttyp = 'Abbaustelle.Kalkstein'
+;
+UPDATE arp_richtplan_pub.richtplankarte_ueberlagernde_flaeche
+    SET dokumente = '[{"t_id":null,"t_ili_tid":null,"titel":"Richtplankapitel E-3.4 Ton","publiziertab":null,"bemerkung":null,"dokument":"https://geo.so.ch/docs/ch.so.arp.richtplan/E-3_4.pdf"}]'
+WHERE     
+    objekttyp = 'Abbaustelle.Ton'
+;
+UPDATE arp_richtplan_pub.richtplankarte_ueberlagernde_flaeche
+    SET dokumente = '[{"t_id":null,"t_ili_tid":null,"titel":"Richtplankapitel E-4.2 Deponien","publiziertab":null,"bemerkung":null,"dokument":"https://geo.so.ch/docs/ch.so.arp.richtplan/E-4_2.pdf"}]'
+WHERE     
+    objekttyp = 'Deponie'
+;
+UPDATE arp_richtplan_pub.richtplankarte_ueberlagernde_flaeche
+    SET dokumente = '[{"t_id":null,"t_ili_tid":null,"titel":"Richtplankapitel L-1.2 Fruchtfolgeflächen","publiziertab":null,"bemerkung":null,"dokument":"https://geo.so.ch/docs/ch.so.arp.richtplan/L-1_2.pdf"}]'
+WHERE     
+    objekttyp = 'Fruchtfolgeflaeche'
+;
+
+UPDATE arp_richtplan_pub.richtplankarte_ueberlagernde_flaeche
+    SET dokumente = '[{"t_id":null,"t_ili_tid":null,"titel":"Richtplankapitel L-1.2 Fruchtfolgeflächen","publiziertab":null,"bemerkung":null,"dokument":"https://geo.so.ch/docs/ch.so.arp.richtplan/L-1_2.pdf"}]'
+WHERE     
+    objekttyp = 'Fruchtfolgeflaeche'
+;
