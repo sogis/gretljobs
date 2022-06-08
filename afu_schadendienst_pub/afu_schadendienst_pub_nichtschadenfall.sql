2cb5dd46f0104050f3f466807aedc2ef9409c08c
barpasen <peter.senn@bd.so.ch>
2022-06-08 15:05:28 +0200

    [afu_schadendienst_pub] new model

--- afu_schadendienst_pub/afu_schadendienst_pub_nichtschadenfall.sql
+++ afu_schadendienst_pub/afu_schadendienst_pub_nichtschadenfall.sql
@@ -1,15 +1,12 @@
 SELECT
-    "Nichtschaden-ID" AS nichtschaden_id,
-    "Kontaktperson_AfU" AS kontaktperson_afu,
-    "Datum" AS datum,
-    "Ort" AS ort,
-    "Strasse_Nr" AS strasse_nr,
-    "x-Koordinate" AS x_koordinate,
-    "y-Koordinate" AS y_koordinate,
-    "Kurzbeschrieb" AS kurzbeschrieb,
-    "Bemerkungen" AS bemerkungen,
-    wkb_geometry AS geometrie,
-    oid AS t_id
+    nichtschaden_id,
+    kontaktperson_afu,
+    datum,
+    ort,
+    strasse_nr,
+    kurzbeschrieb,
+    bemerkungen,
+    geometrie
 FROM
-    schadendienst.nichtschadenfaelle
+    afu_schadendienst_v1.nichtschadenfall
 ;
