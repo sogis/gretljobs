7064f71b62a63b4885d6253b883901020714f724
barpasen <peter.senn@bd.so.ch>
2022-07-06 14:22:45 +0200

    [arp_richtplan_pub] new Enum-Type "Historischer_Verkehrsweg", ommiting t_ili_tid

--- arp_richtplan_pub/arp_richtplan_pub_add_documents.sql
+++ arp_richtplan_pub/arp_richtplan_pub_add_documents.sql
@@ -3,28 +3,39 @@ UPDATE arp_richtplan_pub_v1.richtplankarte_ueberlagernde_flaeche
 WHERE     
     objekttyp = 'Windenergie'
 ;
+
 UPDATE arp_richtplan_pub_v1.richtplankarte_ueberlagernde_flaeche
     SET dokumente = '[{"t_id":null,"titel":"Richtplankapitel E-3.2 Kies","publiziertab":null,"bemerkung":null,"dokument":"https://geo.so.ch/docs/ch.so.arp.richtplan/E-3_2.pdf"}]'
 WHERE     
     objekttyp = 'Abbaustelle.Kies'
 ;
+
 UPDATE arp_richtplan_pub_v1.richtplankarte_ueberlagernde_flaeche
     SET dokumente = '[{"t_id":null,"titel":"Richtplankapitel E-3.3 Kalkstein","publiziertab":null,"bemerkung":null,"dokument":"https://geo.so.ch/docs/ch.so.arp.richtplan/E-3_3.pdf"}]'
 WHERE     
     objekttyp = 'Abbaustelle.Kalkstein'
 ;
+
 UPDATE arp_richtplan_pub_v1.richtplankarte_ueberlagernde_flaeche
     SET dokumente = '[{"t_id":null,"titel":"Richtplankapitel E-3.4 Ton","publiziertab":null,"bemerkung":null,"dokument":"https://geo.so.ch/docs/ch.so.arp.richtplan/E-3_4.pdf"}]'
 WHERE     
     objekttyp = 'Abbaustelle.Ton'
 ;
+
 UPDATE arp_richtplan_pub_v1.richtplankarte_ueberlagernde_flaeche
     SET dokumente = '[{"t_id":null,"titel":"Richtplankapitel E-4.2 Deponien","publiziertab":null,"bemerkung":null,"dokument":"https://geo.so.ch/docs/ch.so.arp.richtplan/E-4_2.pdf"}]'
 WHERE     
     objekttyp = 'Deponie'
 ;
+
 UPDATE arp_richtplan_pub_v1.richtplankarte_ueberlagernde_flaeche
     SET dokumente = '[{"t_id":null,"titel":"Richtplankapitel L-1.2 Fruchtfolgeflächen","publiziertab":null,"bemerkung":null,"dokument":"https://geo.so.ch/docs/ch.so.arp.richtplan/L-1_2.pdf"}]'
 WHERE     
     objekttyp = 'Fruchtfolgeflaeche'
+;
+
+UPDATE arp_richtplan_pub_v1.richtplankarte_ueberlagernde_flaeche
+    SET dokumente = '[{"t_id":null,"titel":"Richtplankapitel S-2.3 Historische Verkehrswege","publiziertab":null,"bemerkung":null,"dokument":"https://geo.so.ch/docs/ch.so.arp.richtplan/S-2_3.pdf"}]'
+WHERE     
+    objekttyp = 'Historischer_Verkehrsweg'
 ;
