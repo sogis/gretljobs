e9e7be392af87a970e7f73588d3f7c0ed5a94c8b
barpasen <peter.senn@bd.so.ch>
2022-07-25 07:08:00 +0200

    [afu_stoerfallverordnung_pub] dissolve buffer

--- afu_stoerfallverordnung_pub/afu_stoerfallverordnung_pub_durchgangsstrassen_kb.sql
+++ afu_stoerfallverordnung_pub/afu_stoerfallverordnung_pub_durchgangsstrassen_kb.sql
@@ -1,13 +1,15 @@
 SELECT
     CASE
         WHEN konsultationsbereich = 'm_100'
-            THEN ST_Multi(ST_Buffer(geometrie, 100))
+            THEN ST_Multi(ST_Union(ST_Buffer(geometrie, 100)))
         WHEN konsultationsbereich = 'm_300'
-            THEN ST_Multi(ST_Buffer(geometrie, 300))
+            THEN ST_Multi(ST_Union(ST_Buffer(geometrie, 300)))
     END AS geometrie,
     'Durchgangsstrasse' AS typ
 FROM
     afu_stoerfallverordnung_v1.durchgangsstrasse
 WHERE
     konsultationsbereich <> 'm_0'
+GROUP BY
+    achsennummer, konsultationsbereich
 ;
