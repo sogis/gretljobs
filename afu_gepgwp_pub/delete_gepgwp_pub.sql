3db4356f8467fd7aedc72a53ba609038fb4dc4a1
bjsvwcur <38523407+bjsvwcur@users.noreply.github.com>
2022-07-25 13:07:57 +0200

    [afu_gepgwp_pub] neuer Gretljob (#1179)

---
+++ afu_gepgwp_pub/delete_gepgwp_pub.sql
@@ -0,0 +1,3 @@
+DELETE FROM
+    afu_gepgwp_pub_v1.gepgwp_gepgwp
+;
---
+++ afu_gepgwp_pub/import_gepgwp_pub.sql
@@ -0,0 +1,52 @@
+WITH json_documents AS (
+    SELECT
+    (
+    row_to_json
+    ((
+        SELECT
+            docs
+        FROM
+        (
+            SELECT
+                art AS "Planart",
+                gebiet AS "Planbezeichnung",
+                rrbnr AS "RRBNr",
+                rrbdatum AS "RRBDatum",
+                plandatum AS "PlanDatum",
+                plannr AS "PlanNr",
+                link AS "Link"
+        ) docs
+    ))
+    )::text AS json_dok,
+        t_id,
+        bfs_gemeindenummer
+    FROM
+        afu_gepgwp_v1.gepgwp_gepgwp AS gepgwp
+),
+
+bfs_gepgwp_json_dokument_agg AS (
+    SELECT
+        bfs_gemeindenummer,
+        geometrie,
+        '[' || dokumente::varchar || ']' AS dokumente
+    FROM
+    (
+        SELECT
+            gemgrenze.bfs_gemeindenummer,
+            gemgrenze.geometrie,
+            string_agg(json_dok, ',') AS dokumente
+        FROM
+            json_documents
+            LEFT JOIN afu_gepgwp_v1.gepgwp_gemgrenze AS gemgrenze
+            ON gemgrenze.bfs_gemeindenummer = json_documents.bfs_gemeindenummer
+        GROUP BY
+        gemgrenze.bfs_gemeindenummer, gemgrenze.geometrie
+    ) AS a
+)
+
+INSERT INTO afu_gepgwp_pub_v1.gepgwp_gepgwp
+SELECT 
+    bfs_gemeindenummer, 
+    geometrie, 
+    dokumente::json AS dokumente 
+FROM bfs_gepgwp_json_dokument_agg
