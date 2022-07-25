a10fc9e620944428631d187519c04cd029dddf6f
barpasen <peter.senn@bd.so.ch>
2022-07-25 09:27:06 +0200

    [afu_stoerfallverordnung_pub] dissolve buffer

--- afu_stoerfallverordnung_pub/afu_stoerfallverordnung_pub_betriebe_kb.sql
+++ afu_stoerfallverordnung_pub/afu_stoerfallverordnung_pub_betriebe_kb.sql
@@ -1,13 +1,22 @@
 SELECT
     CASE
-        WHEN konsultationsbereich = 'm_0'
-            THEN geometrie
         WHEN konsultationsbereich = 'm_100'
-            THEN ST_Buffer(geometrie, 100)
+            THEN ST_Multi(ST_Union(ST_intersection(ST_Buffer(betriebsareal.geometrie, 100), kantonsgrenze.geometrie)))
         WHEN konsultationsbereich = 'm_300'
-            THEN ST_Buffer(geometrie, 300)
+            THEN ST_Multi(ST_Union(ST_intersection(ST_Buffer(betriebsareal.geometrie, 300), kantonsgrenze.geometrie)))
     END AS geometrie,
     'Betriebsareal' AS typ
 FROM
-    afu_stoerfallverordnung_v1.betriebsareal
+    afu_stoerfallverordnung_v1.betriebsareal as betriebsareal,
+    agi_hoheitsgrenzen_pub.hoheitsgrenzen_kantonsgrenze kantonsgrenze
+WHERE
+    betriebsareal.geometrie && kantonsgrenze.geometrie
+AND
+    NOT ST_IsEmpty(ST_intersection(betriebsareal.geometrie, kantonsgrenze.geometrie))
+AND
+    NOT ST_Disjoint(ST_intersection(betriebsareal.geometrie, kantonsgrenze.geometrie), kantonsgrenze.geometrie)
+AND
+    betriebsareal.konsultationsbereich <> 'm_0'
+GROUP BY
+    konsultationsbereich
 ;
--- afu_stoerfallverordnung_pub/afu_stoerfallverordnung_pub_durchgangsstrassen_kb.sql
+++ afu_stoerfallverordnung_pub/afu_stoerfallverordnung_pub_durchgangsstrassen_kb.sql
@@ -1,15 +1,22 @@
 SELECT
     CASE
         WHEN konsultationsbereich = 'm_100'
-            THEN ST_Multi(ST_Union(ST_Buffer(geometrie, 100)))
+            THEN ST_Multi(ST_Union(ST_intersection(ST_Buffer(durchgangsstrasse.geometrie, 100), kantonsgrenze.geometrie)))
         WHEN konsultationsbereich = 'm_300'
-            THEN ST_Multi(ST_Union(ST_Buffer(geometrie, 300)))
+            THEN ST_Multi(ST_Union(ST_intersection(ST_Buffer(durchgangsstrasse.geometrie, 300), kantonsgrenze.geometrie)))
     END AS geometrie,
     'Durchgangsstrasse' AS typ
 FROM
-    afu_stoerfallverordnung_v1.durchgangsstrasse
+    afu_stoerfallverordnung_v1.durchgangsstrasse AS durchgangsstrasse,
+        agi_hoheitsgrenzen_pub.hoheitsgrenzen_kantonsgrenze kantonsgrenze
 WHERE
-    konsultationsbereich <> 'm_0'
+    durchgangsstrasse.geometrie && kantonsgrenze.geometrie
+AND
+    NOT ST_IsEmpty(ST_intersection(durchgangsstrasse.geometrie, kantonsgrenze.geometrie))
+AND
+    NOT ST_Disjoint(ST_intersection(durchgangsstrasse.geometrie, kantonsgrenze.geometrie), kantonsgrenze.geometrie)
+AND
+    durchgangsstrasse.konsultationsbereich <> 'm_0'
 GROUP BY
     konsultationsbereich
 ;
