06d9c332476a24d2998ddd6118beac88f2037907
edigonzales <edi.gonzales@gmail.com>
2022-08-03 12:06:48 +0200

    [agi_hoheitsgrenzen_pub] force multi geom

--- agi_hoheitsgrenzen_pub/fix_geometries.sql
+++ agi_hoheitsgrenzen_pub/fix_geometries.sql
@@ -1,23 +1,23 @@
 UPDATE 
-    agi_hoheitsgrenzen_pub.hoheitsgrenzen_gemeindegrenze SET geometrie = ST_ReducePrecision(geometrie, 0.001)
+    agi_hoheitsgrenzen_pub.hoheitsgrenzen_gemeindegrenze SET geometrie = ST_Multi(ST_ReducePrecision(geometrie, 0.001))
 ;
 
 UPDATE 
-    agi_hoheitsgrenzen_pub.hoheitsgrenzen_gemeindegrenze_generalisiert SET geometrie = ST_ReducePrecision(geometrie, 0.001)
+    agi_hoheitsgrenzen_pub.hoheitsgrenzen_gemeindegrenze_generalisiert SET geometrie = ST_Multi(ST_ReducePrecision(geometrie, 0.001))
 ;
 
 UPDATE 
-    agi_hoheitsgrenzen_pub.hoheitsgrenzen_bezirksgrenze SET geometrie = ST_ReducePrecision(geometrie, 0.001)
+    agi_hoheitsgrenzen_pub.hoheitsgrenzen_bezirksgrenze SET geometrie = ST_Multi(ST_ReducePrecision(geometrie, 0.001))
 ;
 
 UPDATE 
-    agi_hoheitsgrenzen_pub.hoheitsgrenzen_bezirksgrenze_generalisiert SET geometrie = ST_ReducePrecision(geometrie, 0.001)
+    agi_hoheitsgrenzen_pub.hoheitsgrenzen_bezirksgrenze_generalisiert SET geometrie = ST_Multi(ST_ReducePrecision(geometrie, 0.001))
 ;
 
 UPDATE 
-    agi_hoheitsgrenzen_pub.hoheitsgrenzen_kantonsgrenze SET geometrie = ST_ReducePrecision(geometrie, 0.001)
+    agi_hoheitsgrenzen_pub.hoheitsgrenzen_kantonsgrenze SET geometrie = ST_Multi(ST_ReducePrecision(geometrie, 0.001))
 ;
 
 UPDATE 
-    agi_hoheitsgrenzen_pub.hoheitsgrenzen_kantonsgrenze_generalisiert SET geometrie = ST_ReducePrecision(geometrie, 0.001)
+    agi_hoheitsgrenzen_pub.hoheitsgrenzen_kantonsgrenze_generalisiert SET geometrie = ST_Multi(ST_ReducePrecision(geometrie, 0.001))
 ;
