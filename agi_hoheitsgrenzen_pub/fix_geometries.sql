b0817dd0ac7d18fe0ca87af089ebe962488360cf
edigonzales <edi.gonzales@gmail.com>
2022-08-03 12:01:20 +0200

    [agi_hoheitsgrenzen_pub] fix geometrie (or at least try to)

---
+++ agi_hoheitsgrenzen_pub/fix_geometries.sql
@@ -0,0 +1,23 @@
+UPDATE 
+    agi_hoheitsgrenzen_pub.hoheitsgrenzen_gemeindegrenze SET geometrie = ST_ReducePrecision(geometrie, 0.001)
+;
+
+UPDATE 
+    agi_hoheitsgrenzen_pub.hoheitsgrenzen_gemeindegrenze_generalisiert SET geometrie = ST_ReducePrecision(geometrie, 0.001)
+;
+
+UPDATE 
+    agi_hoheitsgrenzen_pub.hoheitsgrenzen_bezirksgrenze SET geometrie = ST_ReducePrecision(geometrie, 0.001)
+;
+
+UPDATE 
+    agi_hoheitsgrenzen_pub.hoheitsgrenzen_bezirksgrenze_generalisiert SET geometrie = ST_ReducePrecision(geometrie, 0.001)
+;
+
+UPDATE 
+    agi_hoheitsgrenzen_pub.hoheitsgrenzen_kantonsgrenze SET geometrie = ST_ReducePrecision(geometrie, 0.001)
+;
+
+UPDATE 
+    agi_hoheitsgrenzen_pub.hoheitsgrenzen_kantonsgrenze_generalisiert SET geometrie = ST_ReducePrecision(geometrie, 0.001)
+;
