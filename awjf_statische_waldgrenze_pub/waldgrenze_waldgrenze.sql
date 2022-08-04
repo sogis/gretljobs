8a14b451dbf01afd9a0f9d8ba563ee90f9896aba
Lüscher Andrea <bjsvwlue@verw.rootso.org>
2022-08-04 11:13:30 +0200

    [awjf_statische_waldgrenze] JSON angepasst Ticket #1357

--- awjf_statische_waldgrenze_pub/waldgrenze_waldgrenze.sql
+++ awjf_statische_waldgrenze_pub/waldgrenze_waldgrenze.sql
@@ -1,62 +1,73 @@
-with documents as (
-    select 
-        typ as titel,
-        offiziellertitel as offizieller_titel, 
-        abkuerzung as abkuerzung,
-        offiziellenr as offizielle_nr,
-        kanton as kanton,
-        gemeinde as gemeinde,
-        publiziert_ab as publiziert_ab,
-        rechtsstatus as rechtsstatus, 
-        text_im_web as text_im_web, 
-        bemerkungen as bemerkungen,
-        datum_archivierung as datum_archivierung,
-        erfasser as erfasser,
-        datum_erfassung as datum_erfassung,
-        typ_dokument.festlegung as waldgrenze_waldgrenze_dokumente
-    from 
+with json_documents AS (
+    SELECT
+  (
+    row_to_json
+    ((
+        SELECT
+            docs
+        FROM
+        (
+            SELECT
+                typ AS "Titel",
+                offiziellertitel AS "Offizieller_Titel",
+                abkuerzung AS "Abkuerzung",
+                offiziellenr AS "Offizielle_Nr",
+                kanton AS "Kanton",
+                gemeinde AS "Gemeinde",
+                publiziert_ab AS "publiziert_ab",
+                rechtsstatus AS "Rechtsstatus",
+                text_im_web AS "Text_im_Web",
+                bemerkungen AS "Bemerkungen",
+                datum_archivierung AS "Datum_Archivierung",
+                erfasser AS "Erfasser",
+                datum_erfassung AS "Datum_Erfassung",
+                'SO_AWJF_Statische_Waldgrenzen_Publikation_20191119.Waldgrenze.Dokument' AS "@type"
+        ) docs
+    ))
+  )::text AS json_dok,
+        t_id
+    FROM
         awjf_statische_waldgrenze.dokumente_dokument
-        left join 
-            awjf_statische_waldgrenze.geobasisdaten_typ_dokument typ_dokument 
-            on 
-            typ_dokument.dokumente = dokumente_dokument.t_id
-    order by 
-        waldgrenze_waldgrenze_dokumente, typ desc 
-), 
+),
 
-documents_json AS (
-    SELECT 
-        array_to_json(array_agg(row_to_json(documents))) AS dokumente, 
-        waldgrenze_waldgrenze_dokumente
-    FROM 
-        documents
-    GROUP BY 
-        waldgrenze_waldgrenze_dokumente
+typ_waldgrenze_json_dokument_agg AS (
+    SELECT
+        festlegung_t_id,
+        '[' || dokumente::varchar || ']' AS dokumente
+    FROM
+    (
+        SELECT
+            dokument_waldgrenze.festlegung AS festlegung_t_id,
+            string_agg(json_dok, ',') AS dokumente
+        FROM
+            json_documents
+            LEFT JOIN awjf_statische_waldgrenze.geobasisdaten_typ_dokument AS dokument_waldgrenze
+            ON dokument_waldgrenze.dokumente = json_documents.t_id
+        GROUP BY
+        dokument_waldgrenze.festlegung
+    ) AS a
 )
 
-select 
-    typ.bezeichnung as typ_bezeichnung,
-    typ.abkuerzung as typ_abkuerzung,
-    typ.verbindlichkeit as typ_verbindlichkeit,
-    typ.bemerkungen as typ_bemerkungen, 
-    typ.art as typ_art,
-    waldgrenze.geometrie as geometrie, 
-    waldgrenze.nummer as nummer,
-    waldgrenze.rechtsstatus as rechtsstatus,
-    waldgrenze.publiziert_ab as publiziert_ab,
-    waldgrenze.bemerkungen as bemerkungen, 
-    waldgrenze.erfasser as erfasser,
-    waldgrenze.datum_erfassung as datum_erfassung,
-    documents_json.dokumente::jsonb,
-    waldgrenze.genehmigt_am as genehmigt_am, 
-    waldgrenze.datum_archivierung as datum_archivierung
-from 
+SELECT 
+    typ.bezeichnung AS typ_bezeichnung,
+    typ.abkuerzung AS typ_abkuerzung,
+    typ.verbindlichkeit AS typ_verbindlichkeit,
+    typ.bemerkungen AS typ_bemerkungen,
+    typ.art AS typ_art,
+    waldgrenze.geometrie AS geometrie,
+    waldgrenze.nummer AS nummer,
+    waldgrenze.rechtsstatus AS rechtsstatus,
+    waldgrenze.publiziert_ab AS publiziert_ab,
+    waldgrenze.bemerkungen AS bemerkungen,
+    waldgrenze.erfasser AS erfasser,
+    waldgrenze.datum_erfassung AS datum_erfassung,
+    typ_waldgrenze_json_dokument_agg.dokumente::json AS dokumente,
+    waldgrenze.genehmigt_am AS genehmigt_am,
+    waldgrenze.datum_archivierung AS datum_archivierung
+FROM
     awjf_statische_waldgrenze.geobasisdaten_waldgrenze_linie waldgrenze
-left join 
-    awjf_statische_waldgrenze.geobasisdaten_typ typ 
-    on 
-    waldgrenze.waldgrenze_typ = typ.t_id 
-left join 
-    documents_json 
-    on 
-    documents_json.waldgrenze_waldgrenze_dokumente = typ.t_id
+LEFT JOIN
+    awjf_statische_waldgrenze.geobasisdaten_typ typ
+    ON waldgrenze.waldgrenze_typ = typ.t_id
+LEFT JOIN typ_waldgrenze_json_dokument_agg
+    ON typ.t_id= typ_waldgrenze_json_dokument_agg.festlegung_t_id
