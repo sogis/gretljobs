d3185d689f6fdb131189bf247d907442074b1188
edigonzales <edi.gonzales@gmail.com>
2022-08-05 07:34:21 +0200

    [arp_bauzonengrenzen] use no frills st_union

--- arp_bauzonengrenzen_pub/create_bauzonengrenzen.sql
+++ arp_bauzonengrenzen_pub/create_bauzonengrenzen.sql
@@ -7,7 +7,7 @@ INSERT INTO
     )
 
 SELECT 
-    ST_Multi(ST_Union(geometrie, 0.001)),
+    ST_Multi(ST_Union(geometrie)),
     bfs_nr AS bfsnr,
     'Bauzone' AS zonentyp
 FROM 
@@ -24,7 +24,7 @@ GROUP BY
 UNION ALL
 
 SELECT 
-    ST_Multi(ST_Union(geometrie, 0.001)),
+    ST_Multi(ST_Union(geometrie)),
     bfs_nr AS bfsnr,
     'Reservezone' AS zonentyp
 FROM 
