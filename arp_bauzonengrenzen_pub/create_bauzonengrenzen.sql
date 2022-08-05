7e1754b5a7b9e76bdb187c2b40743d0c9f6ef90a
edigonzales <edi.gonzales@gmail.com>
2022-08-05 07:08:10 +0200

    [arp_bauzonengrenzen] remove snapttogrid/removerepeatedpoints

--- arp_bauzonengrenzen_pub/create_bauzonengrenzen.sql
+++ arp_bauzonengrenzen_pub/create_bauzonengrenzen.sql
@@ -1,40 +1,40 @@
 INSERT INTO 
-	arp_bauzonengrenzen_pub.bauzonengrenzen_bauzonengrenze 
-	(
-		geometrie,
-		bfsnr,
-		zonentyp
-	)
+    arp_bauzonengrenzen_pub.bauzonengrenzen_bauzonengrenze 
+    (
+        geometrie,
+        bfsnr,
+        zonentyp
+    )
 
 SELECT 
-    ST_Multi(ST_Union(ST_RemoveRepeatedPoints(ST_SnapToGrid(ST_Buffer(geometrie,0), 0.001)))),
+    ST_Multi(ST_Union(geometrie, 0.001)),
     bfs_nr AS bfsnr,
     'Bauzone' AS zonentyp
 FROM 
     arp_nutzungsplanung_pub_v1.nutzungsplanung_grundnutzung
 WHERE 
-	bfs_nr = ${bfsnr}
-	AND 
-	(
-		substring(typ_kt from 2 for 2)::int < 20  
-	)
+    bfs_nr = ${bfsnr}
+    AND 
+    (
+        substring(typ_kt from 2 for 2)::int < 20  
+    )
 GROUP BY
-	bfs_nr
+    bfs_nr
 
 UNION ALL
 
 SELECT 
-    ST_Multi(ST_Union(ST_RemoveRepeatedPoints(ST_SnapToGrid(ST_Buffer(geometrie,0), 0.001)))),
+    ST_Multi(ST_Union(geometrie, 0.001)),
     bfs_nr AS bfsnr,
     'Reservezone' AS zonentyp
 FROM 
     arp_nutzungsplanung_pub_v1.nutzungsplanung_grundnutzung
 WHERE 
-	bfs_nr = ${bfsnr}
-	AND 
-	(
-		substring(typ_kt from 2 for 2)::int = 43
-	)
+    bfs_nr = ${bfsnr}
+    AND 
+    (
+        substring(typ_kt from 2 for 2)::int = 43
+    )
 GROUP BY
-	bfs_nr
+    bfs_nr
 ;
