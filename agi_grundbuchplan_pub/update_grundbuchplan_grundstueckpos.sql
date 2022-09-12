f4d81776fc59bc7d154a988e484b8dafd70760d2
bjsvwcur <sandra.curiger@bd.so.ch>
2022-09-12 13:54:23 +0200

    [agi_grundbuchplan_pub] Schema versioniert

--- agi_grundbuchplan_pub/update_grundbuchplan_grundstueckpos.sql
+++ agi_grundbuchplan_pub/update_grundbuchplan_grundstueckpos.sql
@@ -1,5 +1,5 @@
 UPDATE
-    agi_grundbuchplan_pub.grundbuchplan_grundstueckpos
+    agi_grundbuchplan_pub_v1.grundbuchplan_grundstueckpos
 SET
     mutation = TRUE
 WHERE
@@ -12,11 +12,11 @@ WHERE
                     ST_SnapToGrid((ST_DumpPoints(ST_GeomFromText(numpos, 2056))).geom, 0.001) AS geom,
                     bfs_nr
                 FROM
-                    agi_grundbuchplan_pub.grundbuchplan_liegenschaft
+                    agi_grundbuchplan_pub_v1.grundbuchplan_liegenschaft
                 WHERE
                     mutation = true
             ) AS foo
-            INNER JOIN agi_grundbuchplan_pub.grundbuchplan_grundstueckpos AS bar
+            INNER JOIN agi_grundbuchplan_pub_v1.grundbuchplan_grundstueckpos AS bar
             ON (bar.bfs_nr = foo.bfs_nr AND ST_Equals(foo.geom, ST_SnapToGrid(bar.pos, 0.001)))
 )
 ;
