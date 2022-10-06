be6da94a70be3f3516a090a41923fa2ba2a51152
Andreas Neumann <a.neumann@carto.net>
2022-10-06 15:30:18 +0200

    [afu_onlinerisk_transfer] Test ohne Geometrie

--- afu_onlinerisk_transfer/gebaeude.sql
+++ afu_onlinerisk_transfer/gebaeude.sql
@@ -2,7 +2,7 @@ SELECT
     g.id_gebaeude AS t_id,
     g.egid,
     k.text AS name_gebaeude,
-    'Point(' || g.koordinate_x::text || ' ' || g.koordinate_y::text || ')' AS geometrie,
+    --'Point(' || g.koordinate_x::text || ' ' || g.koordinate_y::text || ')' AS geometrie,
     g.zusatz,
     g.id_untersuchungseinheit
   FROM afu_online_risk.gebaeude g
