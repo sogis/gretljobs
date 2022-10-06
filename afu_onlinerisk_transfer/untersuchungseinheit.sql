f72d51d681ae51cf6d91911a86bd1966b30614fc
Andreas Neumann <a.neumann@carto.net>
2022-10-06 15:35:09 +0200

    [afu_onlinerisk_transfer] Datentyp aktiv auf boolean korrigiert

--- afu_onlinerisk_transfer/untersuchungseinheit.sql
+++ afu_onlinerisk_transfer/untersuchungseinheit.sql
@@ -4,7 +4,7 @@ SELECT
      k.text AS name_untersuchungseinheit,
      ue.konsultations_abstand,
      ue.parzellen_nummer,
-     k.aktiv,
+     k.aktiv::boolean,
      ue.id_betrieb
   FROM afu_online_risk.untersuchungseinheit ue
     LEFT JOIN afu_online_risk.konstruktion k ON ue.id_untersuchungseinheit = k.id_konstruktion;
