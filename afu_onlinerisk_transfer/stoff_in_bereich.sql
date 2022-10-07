b318e0342c0862e367f5fca18e3beacbe8ec84a8
Andreas Neumann <a.neumann@carto.net>
2022-10-07 17:22:21 +0200

    [afu_onlinerisk_transfer] Fix more not null constraint in name_stoff

--- afu_onlinerisk_transfer/stoff_in_bereich.sql
+++ afu_onlinerisk_transfer/stoff_in_bereich.sql
@@ -4,9 +4,10 @@ SELECT
     sta.text AS name_stoff_alternativ,
     round(sto.ms_stfv::numeric,1) AS ms_stfv,
     round(bstof.menge::numeric,1) AS max_menge,
-    bstof.id_bereich,
-    bstof.bemerkung::text
+    bstof.bemerkung::text AS bemerkung,
+    bstof.id_bereich
   FROM afu_online_risk.bereichstoff bstof
     LEFT JOIN afu_online_risk.stoff sto ON bstof.id_stoff = sto.id_stoff
     LEFT JOIN afu_online_risk.stammdaten sta ON sto.id_stoff = sta.id_stammdaten
+   WHERE bstof.menge IS NOT NULL and bstof.menge > 0
 ;
