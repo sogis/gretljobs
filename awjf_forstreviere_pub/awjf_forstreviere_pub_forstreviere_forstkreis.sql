e18068f96dd3821cf39c8cb2cef491a95eae8aed
barpasen <peter.senn@bd.so.ch>
2022-10-19 12:11:09 +0200

    [awjf_forstreviere_pub] Layer awjf_forstreviere an neue Gemeindgrenzen angepasst

--- awjf_forstreviere_pub/awjf_forstreviere_pub_forstreviere_forstkreis.sql
+++ awjf_forstreviere_pub/awjf_forstreviere_pub_forstreviere_forstkreis.sql
@@ -1,12 +1,10 @@
 SELECT
-    ST_Multi(ST_ReducePrecision(ST_Buffer(ST_Buffer(ST_Union(geometrie),1),-1), 0.001)) AS geometrie,
+    ST_Multi(ST_Union(geometrie)) AS geometrie,
     forstreviere_forstkreis.aname AS forstkreis
 FROM
     awjf_forstreviere.forstreviere_forstreviergeometrie
-    JOIN awjf_forstreviere.forstreviere_forstrevier
-        ON forstreviere_forstrevier.t_id=forstreviere_forstreviergeometrie.forstrevier
-    JOIN awjf_forstreviere.forstreviere_forstkreis
-        ON forstreviere_forstkreis.t_id=forstreviere_forstreviergeometrie.forstkreis
+    LEFT JOIN awjf_forstreviere.forstreviere_forstkreis
+        ON forstreviere_forstkreis.t_id = forstreviere_forstreviergeometrie.forstkreis
 GROUP BY
     forstreviere_forstkreis.t_id
 ;
--- awjf_forstreviere_pub/awjf_forstreviere_pub_forstreviere_forstrevier.sql
+++ awjf_forstreviere_pub/awjf_forstreviere_pub_forstreviere_forstrevier.sql
@@ -1,13 +1,13 @@
 SELECT
-    ST_Multi(ST_ReducePrecision(ST_Buffer(ST_Buffer(ST_Union(geometrie),1),-1), 0.001)) AS geometrie,
+    ST_Multi(ST_Union(geometrie)) AS geometrie,
     forstreviere_forstrevier.aname AS forstrevier,
     forstreviere_forstkreis.aname AS forstkreis
 FROM
     awjf_forstreviere.forstreviere_forstreviergeometrie
     JOIN awjf_forstreviere.forstreviere_forstrevier
-        ON forstreviere_forstrevier.t_id=forstreviere_forstreviergeometrie.forstrevier
+        ON forstreviere_forstrevier.t_id = forstreviere_forstreviergeometrie.forstrevier
     JOIN awjf_forstreviere.forstreviere_forstkreis
-        ON forstreviere_forstkreis.t_id=forstreviere_forstreviergeometrie.forstkreis
+        ON forstreviere_forstkreis.t_id = forstreviere_forstreviergeometrie.forstkreis
 GROUP BY
     forstreviere_forstkreis.t_id,
     forstreviere_forstrevier.aname
