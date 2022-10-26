d01c8035489c542c4beec4c0bb8367f8fb4c0534
EchoCharlie <martin.schweizer@bd.so.ch>
2022-10-26 08:41:56 +0200

    [agi_simi_regions]

--- agi_simi_regions/copy_from/edit_db.sql
+++ agi_simi_regions/copy_from/edit_db.sql
@@ -1,15 +1,13 @@
 select 
     uuid_generate_v4 () AS id,
-    abdeckung.identifier as identifier,
-    abdeckung.identifier as title,
-    st_asbinary(st_union(apolygon)) as geom_wkb, 
+    teilgebiet.identifier as identifier,
+    teilgebiet.bezeichnung as title,
+    st_asbinary(apolygon) as geom_wkb, 
     localtimestamp AS updated,
-    'metadaten_abdeckung' AS coverage_ident
+    abdeckung.identifier AS coverage_ident
 from 
     agi_metadaten_datenabdeckung_v1.abdeckung_teilgebiet teilgebiet
 left join 
     agi_metadaten_datenabdeckung_v1.abdeckung_datenabdeckung abdeckung
     on 
     abdeckung.t_id = teilgebiet.datenabdeckung 
-group by 
-    abdeckung.identifier
