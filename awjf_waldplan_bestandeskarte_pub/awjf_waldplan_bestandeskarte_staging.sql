fc29cf42dcc4793f89c848e2fb2f3dcd29224bdf
barpasen <peter.senn@bd.so.ch>
2022-11-02 14:26:17 +0100

    [awjf_waldplan_bestandeskarte_pub] Initialisierung Job-Verzeichnis

---
+++ awjf_waldplan_bestandeskarte_pub/awjf_waldplan_bestandeskarte_staging.sql
@@ -0,0 +1,38 @@
+SELECT
+    geometrie,
+    id_wp,
+    fid_amtei,
+    fid_fk,
+    fid_fr,
+    wirt_zone,
+    gem_bfs,
+    fid_we,
+    gb_flaeche,
+    we_text,
+    fid_eigcod,
+    fid_eig,
+    fid_prod,
+    wpnr,
+    wptyp,
+    betriebsteil,
+    fid_abt,
+    bstnr,
+    bsttyp,
+    wpinfo,
+    bemerkung,
+    flae_gis,
+    zeitstand,
+    beschrift,
+    x_beschr,
+    y_beschr,
+    objnummer,
+    weidewald,
+    gb_gem_bfs,
+    astatus
+FROM
+    awjf_waldplan_bestandeskarte_v1.waldplan_bestandeskarte
+WHERE
+    astatus = 'Review'
+OR
+    id_wp = 0
+;
