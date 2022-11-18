3f6d507f76e0991cc01166be3de0e9dfd1a15d84
Andreas Neumann <a.neumann@carto.net>
2022-11-18 10:10:26 +0100

    [afu_onlinerisk_transfer] FIX empty strings in Gefahrengutsymbol

--- afu_onlinerisk_transfer/stoff.sql
+++ afu_onlinerisk_transfer/stoff.sql
@@ -3,10 +3,10 @@ SELECT
    stasto.text AS stoff_name,
    sto.display AS stoff_anzeigetext,
    staagg.text AS aggregatzustand,
-   sto.ms_stfv::numeric AS mengenschwelle_stfv,
+   sto.ms_stfv AS mengenschwelle_stfv,
    sto.cas AS cas_nummer,
    sto.cea_nummer AS cea_nummer,
-   sto.gf_symbol1 AS gf_symbol
+   CASE WHEN sto.gf_symbol1 = '' THEN NULL ELSE sto.gf_symbol1 END AS gf_symbol
  FROM
    afu_online_risk.stoff sto
    LEFT JOIN afu_online_risk.stammdaten stasto ON sto.id_stoff = stasto.id_stammdaten
