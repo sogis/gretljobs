b9a1e900dcfd55d8c8bc9625d039bbb8d59dfc26
EchoCharlie <martin.schweizer@bd.so.ch>
2022-12-01 13:37:59 +0100

    [arp_statent_import]Bugfix

--- arp_statent_import/transfer_staging_edit.sql
+++ arp_statent_import/transfer_staging_edit.sql
@@ -1,21 +1,3 @@
-WITH grundnutzung AS (
-    SELECT
-        typ.bezeichnung AS typ_bezeichnung,
-        typ.typ_kt,
-        typ.code_kommunal AS typ_code_kommunal,
-        grundnutzung.geometrie,
-        substring(typ.typ_kt,2,3)::int4 AS typ_code_kt,
-        substring(typ.typ_kt,2,2)::int4  AS typ_code_ch
-    FROM
-        arp_nutzungsplanung_v1.nutzungsplanung_grundnutzung AS grundnutzung
-        LEFT JOIN arp_nutzungsplanung_v1.nutzungsplanung_typ_grundnutzung AS typ
-        ON grundnutzung.typ_grundnutzung = typ.t_id
-    WHERE
-        grundnutzung.publiziertbis IS NULL
-        and 
-        substring(typ.typ_kt,2,3)::int4 < 200
-)
-
 SELECT 
 	st_point(meter_x, meter_y) AS geometrie,
 	empfte,
@@ -28,7 +10,7 @@ SELECT
 FROM 
     arp_statpop_statent_staging_v1.statent statent
 LEFT JOIN 
-    grundnutzung  
+    arp_statpop_statent_staging_v1.nutzungszonen grundnutzung  
     ON 
     st_dwithin(st_setsrid(st_point(meter_x, meter_y),2056), grundnutzung.geometrie,0)
 LEFT JOIN 
