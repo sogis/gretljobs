06fc6f4865df473ebeb26262880421200f482c01
EchoCharlie <martin.schweizer@bd.so.ch>
2022-12-14 15:59:36 +0100

    [alw_fruchtfolgeflaechen]Berechnungsanpassungen 2022

--- alw_fruchtfolgeflaechen/01_Datenimport/fruchtfolgeflaechen_alt.sql
+++ alw_fruchtfolgeflaechen/01_Datenimport/fruchtfolgeflaechen_alt.sql
@@ -1,25 +1,10 @@
 SELECT
-    (st_dump(geometrie)).geom AS geometrie, 
-    CASE 
-        WHEN fffart IN (1,4) 
-        THEN 'geeignet'
-        WHEN fffart IN (2,6) 
-        THEN 'bedingt_geeignet'
-        WHEN fffart = 3
-        THEN 'bedingt_geeignet'
-    END AS bezeichnung, 
-    CASE
-        WHEN fffart = 4 
-        THEN 'Kiesgrube_Abbaugebiet'
-        WHEN fffart = 6 
-        THEN 'Golfplatz'
-        WHEN fffart = 3
-        THEN 'Reservezone'
-        ELSE null 
-    END AS spezialfall, 
-    datum_erst::date AS datenstand, 
-    anrechenbar AS anrechenbar, 
-    st_area(geometrie)/100 AS area_aren, 
-    (st_area(geometrie)/100)*anrechenbar AS area_anrech
+    geometrie, 
+    bezeichnung, 
+    spezialfall, 
+    datenstand, 
+    anrechenbar, 
+    area_aren, 
+    area_anrech
 from 
-    alw_grundlagen_pub.fruchtfolgeflaechen
+    alw_fruchtfolgeflaechen_pub_v1.fruchtfolgeflaeche
