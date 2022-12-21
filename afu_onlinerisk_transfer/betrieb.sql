91154cc80edd40149e4d662a16202601258477c6
Andreas Neumann <a.neumann@carto.net>
2022-12-21 17:08:38 +0100

    [afu_onlinerisk_transfer] zusaetzliche Online-Risk Attribute, Betriebe gefiltert - nur Störfallverordnung

--- afu_onlinerisk_transfer/betrieb.sql
+++ afu_onlinerisk_transfer/betrieb.sql
@@ -1,6 +1,29 @@
 SELECT
       b.id_betrieb AS t_id,
       k.text::varchar(255) AS name_betrieb,
-      b.bur_nummer
+	  b.zusatz,
+	  b.firmenhistory,
+      b.bur_nummer,
+	  b.egid,
+	  b.eigentuemer_betrieb,
+	  b.eigentuemer_areal,
+	  b.eigentuemer_gebaeude,
+	  b.flaeche::integer,
+	  b.personal,
+	  b.letzte_kontrolle,
+	  b.naechste_kontrolle,
+	  0 AS anzahl_szenarien_brand,
+	  0 AS anzahl_szenarien_explosion,
+	  0 AS anzahl_szenarien_toxische_wolke,  
+	  'Point(' || b.koordinate_x::text || ' ' || b.koordinate_y::text || ')' AS geometrie
   FROM afu_online_risk.betrieb b
-   LEFT JOIN afu_online_risk.konstruktion k ON b.id_betrieb = k.id_konstruktion;
+   LEFT JOIN afu_online_risk.konstruktion k
+      ON b.id_betrieb = k.id_konstruktion
+   LEFT JOIN afu_online_risk.verordnung v
+      ON b.id_betrieb = v.id_betrieb AND v.aktiv = 1
+   LEFT JOIN afu_online_risk.verordnungsrelevanz vr
+      ON v.id_verordnungsrelevanz = vr.id_verordnungsrelevanz
+   LEFT JOIN afu_online_risk.stammdaten vrstd
+      ON vr.id_verordnungsrelevanz = vrstd.id_stammdaten AND vrstd.aktiv = 1
+  WHERE vrstd.text = 'Störfallverordnung'
+   ;
--- afu_onlinerisk_transfer/gebaeude.sql
+++ afu_onlinerisk_transfer/gebaeude.sql
@@ -2,8 +2,8 @@ SELECT
     g.id_gebaeude AS t_id,
     CASE WHEN g.egid = '0' THEN NULL ELSE g.egid END::integer AS egid,
     k.text AS name_gebaeude,
-    'Point(' || g.koordinate_x::text || ' ' || g.koordinate_y::text || ')' AS geometrie,
     g.zusatz,
+    'Point(' || g.koordinate_x::text || ' ' || g.koordinate_y::text || ')' AS geometrie,
     g.id_untersuchungseinheit
   FROM afu_online_risk.gebaeude g
    LEFT JOIN afu_online_risk.konstruktion k ON g.id_gebaeude = k.id_konstruktion;
