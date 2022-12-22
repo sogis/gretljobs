64dc138922c14b89c2839d9475929b4eb7f1c65b
Andreas Neumann <a.neumann@carto.net>
2022-12-22 10:55:17 +0100

    [afu_onlinerisk_transfer] Mehr Filter auf Störfallverordnung damit es keine fkey violations gibt

--- afu_onlinerisk_transfer/bereich.sql
+++ afu_onlinerisk_transfer/bereich.sql
@@ -1,7 +1,22 @@
 SELECT
-   b.id_bereich AS t_id,
+   ber.id_bereich AS t_id,
    k.text AS name_bereich,
-   b.etage,
-   b.id_gebaeude
-  FROM afu_online_risk.bereich b
-    LEFT JOIN afu_online_risk.konstruktion k ON b.id_bereich = k.id_konstruktion;
+   ber.etage,
+   ber.id_gebaeude
+  FROM afu_online_risk.bereich ber
+    LEFT JOIN afu_online_risk.konstruktion k
+      ON ber.id_bereich = k.id_konstruktion
+    LEFT JOIN afu_online_risk.gebaeude g
+      ON ber.id_gebaeude = g.id_gebaeude
+    LEFT JOIN afu_online_risk.untersuchungseinheit ue
+      ON g.id_untersuchungseinheit = ue.id_untersuchungseinheit
+    LEFT JOIN afu_online_risk.betrieb b
+       ON ue.id_betrieb = b.id_betrieb
+    LEFT JOIN afu_online_risk.verordnung v
+      ON b.id_betrieb = v.id_betrieb AND v.aktiv = 1
+    LEFT JOIN afu_online_risk.verordnungsrelevanz vr
+      ON v.id_verordnungsrelevanz = vr.id_verordnungsrelevanz
+    LEFT JOIN afu_online_risk.stammdaten vrstd
+      ON vr.id_verordnungsrelevanz = vrstd.id_stammdaten AND vrstd.aktiv = 1
+  WHERE vrstd.text = 'Störfallverordnung'
+;
--- afu_onlinerisk_transfer/stoff_in_bereich.sql
+++ afu_onlinerisk_transfer/stoff_in_bereich.sql
@@ -1,3 +1,4 @@
+
 SELECT
     bstof.id_assoziation AS t_id,
     CASE WHEN bstof.text IS NOT NULL THEN bstof.text ELSE sta.text END AS name_stoff_in_bereich,
@@ -7,7 +8,25 @@ SELECT
     bstof.id_bereich,
     bstof.id_stoff
   FROM afu_online_risk.bereichstoff bstof
-    LEFT JOIN afu_online_risk.stoff sto ON bstof.id_stoff = sto.id_stoff
-    LEFT JOIN afu_online_risk.stammdaten sta ON sto.id_stoff = sta.id_stammdaten
-   WHERE bstof.menge IS NOT NULL and bstof.menge > 0
+    LEFT JOIN afu_online_risk.stoff sto
+      ON bstof.id_stoff = sto.id_stoff
+    LEFT JOIN afu_online_risk.stammdaten sta
+      ON sto.id_stoff = sta.id_stammdaten
+    LEFT JOIN afu_online_risk.bereich ber
+      ON bstof.id_bereich = ber.id_bereich
+    LEFT JOIN afu_online_risk.gebaeude g
+      ON ber.id_gebaeude = g.id_gebaeude
+    LEFT JOIN afu_online_risk.untersuchungseinheit ue
+      ON g.id_untersuchungseinheit = ue.id_untersuchungseinheit
+    LEFT JOIN afu_online_risk.betrieb b
+       ON ue.id_betrieb = b.id_betrieb
+    LEFT JOIN afu_online_risk.verordnung v
+      ON b.id_betrieb = v.id_betrieb AND v.aktiv = 1
+    LEFT JOIN afu_online_risk.verordnungsrelevanz vr
+      ON v.id_verordnungsrelevanz = vr.id_verordnungsrelevanz
+    LEFT JOIN afu_online_risk.stammdaten vrstd
+      ON vr.id_verordnungsrelevanz = vrstd.id_stammdaten AND vrstd.aktiv = 1
+WHERE
+    bstof.menge IS NOT NULL and bstof.menge > 0
+    AND vrstd.text = 'Störfallverordnung'
 ;
