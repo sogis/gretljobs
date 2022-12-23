0deef2e42d8819a316ba75476dde1ee30109edc7
Andreas Neumann <a.neumann@carto.net>
2022-12-23 09:29:20 +0100

    [afu_onlinerisk_transfer] do not transfer and reimport domain values

--- afu_onlinerisk_transfer/delete_qrcat.sql
+++ afu_onlinerisk_transfer/delete_qrcat.sql
@@ -5,8 +5,3 @@ DELETE FROM ${DB_Schema_QRcat}.qrcat_szenario;
 DELETE FROM ${DB_Schema_QRcat}.qrcat_fwert;
 DELETE FROM ${DB_Schema_QRcat}.qrcat_detailszenario_grundhaeufigkeit;
 DELETE FROM ${DB_Schema_QRcat}.qrcat_toxreferenzszenario;
-DELETE FROM ${DB_Schema_QRcat}.qrcat_toxreferenzszenario_typstoff;
-DELETE FROM ${DB_Schema_QRcat}.qrcat_wk_grundhaeufigkeit_art;
-DELETE FROM ${DB_Schema_QRcat}.qrcat_bevoelkerung_typ;
-DELETE FROM ${DB_Schema_QRcat}.qrcat_letalitaetsradius_art;
-DELETE FROM ${DB_Schema_QRcat}.qrcat_szenario_art;
