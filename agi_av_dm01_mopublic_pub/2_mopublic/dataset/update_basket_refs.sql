32f8e886d40c36aabaddde0c7ddd5f87befe1bca
uncoyote <jeker.git@outlook.de>
2023-01-25 16:31:19 +0100

    [agi_av_dm01_mopublic_pub] created

---
+++ agi_av_dm01_mopublic_pub/2_mopublic/dataset/update_basket_refs.sql
@@ -0,0 +1,201 @@
+SET search_path TO ${dbSchema};
+
+UPDATE 
+    mopublic_bodenbedeckung t
+SET 
+    t_basket = b.t_id
+FROM 
+    t_ili2db_basket b
+WHERE 
+    t.bfs_nr = b.t_ili_tid::int
+;
+
+UPDATE 
+    mopublic_bodenbedeckung_proj t
+SET 
+    t_basket = b.t_id
+FROM 
+    t_ili2db_basket b
+WHERE 
+    t.bfs_nr = b.t_ili_tid::int
+;
+
+UPDATE 
+    mopublic_einzelobjekt_flaeche t
+SET 
+    t_basket = b.t_id
+FROM 
+    t_ili2db_basket b
+WHERE 
+    t.bfs_nr = b.t_ili_tid::int
+;
+
+UPDATE 
+    mopublic_einzelobjekt_linie t
+SET 
+    t_basket = b.t_id
+FROM 
+    t_ili2db_basket b
+WHERE 
+    t.bfs_nr = b.t_ili_tid::int
+;
+
+UPDATE 
+    mopublic_einzelobjekt_punkt t
+SET 
+    t_basket = b.t_id
+FROM 
+    t_ili2db_basket b
+WHERE 
+    t.bfs_nr = b.t_ili_tid::int
+;
+
+UPDATE 
+    mopublic_fixpunkt t
+SET 
+    t_basket = b.t_id
+FROM 
+    t_ili2db_basket b
+WHERE 
+    t.bfs_nr = b.t_ili_tid::int
+;
+
+UPDATE 
+    mopublic_flurname t
+SET 
+    t_basket = b.t_id
+FROM 
+    t_ili2db_basket b
+WHERE 
+    t.bfs_nr = b.t_ili_tid::int
+;
+
+UPDATE 
+    mopublic_gebaeudeadresse t
+SET 
+    t_basket = b.t_id
+FROM 
+    t_ili2db_basket b
+WHERE 
+    t.bfs_nr = b.t_ili_tid::int
+;
+
+UPDATE 
+    mopublic_gelaendename t
+SET 
+    t_basket = b.t_id
+FROM 
+    t_ili2db_basket b
+WHERE 
+    t.bfs_nr = b.t_ili_tid::int
+;
+
+UPDATE 
+    mopublic_gemeindegrenze t
+SET 
+    t_basket = b.t_id
+FROM 
+    t_ili2db_basket b
+WHERE 
+    t.bfs_nr = b.t_ili_tid::int
+;
+
+UPDATE 
+    mopublic_gemeindegrenze_proj t
+SET 
+    t_basket = b.t_id
+FROM 
+    t_ili2db_basket b
+WHERE 
+    t.bfs_nr = b.t_ili_tid::int
+;
+
+UPDATE 
+    mopublic_grenzpunkt t
+SET 
+    t_basket = b.t_id
+FROM 
+    t_ili2db_basket b
+WHERE 
+    t.bfs_nr = b.t_ili_tid::int
+;
+
+UPDATE 
+    mopublic_grundstueck t
+SET 
+    t_basket = b.t_id
+FROM 
+    t_ili2db_basket b
+WHERE 
+    t.bfs_nr = b.t_ili_tid::int
+;
+
+UPDATE 
+    mopublic_grundstueck_proj t
+SET 
+    t_basket = b.t_id
+FROM 
+    t_ili2db_basket b
+WHERE 
+    t.bfs_nr = b.t_ili_tid::int
+;
+
+UPDATE 
+    mopublic_hoheitsgrenzpunkt t
+SET 
+    t_basket = b.t_id
+FROM 
+    t_ili2db_basket b
+WHERE 
+    t.bfs_nr = b.t_ili_tid::int
+;
+
+UPDATE 
+    mopublic_objektname_pos t
+SET 
+    t_basket = b.t_id
+FROM 
+    t_ili2db_basket b
+WHERE 
+    t.bfs_nr = b.t_ili_tid::int
+;
+
+UPDATE 
+    mopublic_ortsname t
+SET 
+    t_basket = b.t_id
+FROM 
+    t_ili2db_basket b
+WHERE 
+    t.bfs_nr = b.t_ili_tid::int
+;
+
+UPDATE 
+    mopublic_rohrleitung t
+SET 
+    t_basket = b.t_id
+FROM 
+    t_ili2db_basket b
+WHERE 
+    t.bfs_nr = b.t_ili_tid::int
+;
+
+UPDATE 
+    mopublic_strassenachse t
+SET 
+    t_basket = b.t_id
+FROM 
+    t_ili2db_basket b
+WHERE 
+    t.bfs_nr = b.t_ili_tid::int
+;
+
+UPDATE 
+    mopublic_strassenname_pos t
+SET 
+    t_basket = b.t_id
+FROM 
+    t_ili2db_basket b
+WHERE 
+    t.bfs_nr = b.t_ili_tid::int
+;
---
+++ agi_av_dm01_mopublic_pub/readme.md
@@ -0,0 +1,21 @@
+# Publikation der AV-Daten
+
+Mit diesem Job werden die AV-Daten gemeindeweise über die Datenbanken und den Datenbezug publiziert.
+
+## Aufteilung in Sub-Projekte
+
+Der Job ist in drei Sub-Projekte aufgeteilt. Gemäss dem jeweiligen INTERLIS-Modell.
+
+## Abhängigkeits-Konfiguration der Sub-Projekte
+
+Zwecks stabiler Referenzen zwischen den Subprojekten referenzieren die Subprojekte untereinander jeweils den "Sammel-Task" "execProjPubTasks".
+
+Die zu publizierenden Gemeinden folgen:
+* Für die Publikation von DM01AVSO24LV95 aus dem Inhalt des Ordners build/dm01_so des Unterprojekts 0_dm01_so
+* Für die Modelle DM01AVCH24LV95D und SO_AGI_MOpublic_YYYYMMDD aus dem Publisher von 0_dm01_so mittels Konfiguration der Kind-Publisher: `regions = project(':0_dm01_so').tasks.publish.publishedRegions`
+
+## Abgelöste Jobs
+
+* agi_av_import_export
+* agi_mopublic_pub
+* agi_mopublic_pub_export
