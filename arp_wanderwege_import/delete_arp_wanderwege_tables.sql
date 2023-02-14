8a42949cbdc8b54a61dd5ef2e4ee7f2fc6a76baf
barpasen <peter.senn@bd.so.ch>
2023-02-14 13:40:58 +0100

    [arp_wanderwege_import] initial version

---
+++ arp_wanderwege_import/delete_arp_wanderwege_tables.sql
@@ -0,0 +1,69 @@
+DELETE FROM
+    arp_wanderwege_v2.hpm_base_base_geometry
+;
+DELETE FROM
+    arp_wanderwege_v2.hpm_base_metadata
+;
+DELETE FROM
+    arp_wanderwege_v2.hpm_base_route
+;
+DELETE FROM
+    arp_wanderwege_v2.hpm_base_route_signalisation
+;
+DELETE FROM
+    arp_wanderwege_v2.hpm_base_way_route
+;
+DELETE FROM
+    arp_wanderwege_v2.hpm_walk_lv95_hiking_way
+;
+DELETE FROM
+    arp_wanderwege_v2.hpm_walk_lv95_signalisation
+;
+DELETE FROM
+    arp_wanderwege_v2.hpm_walk_lv95_way
+;
+DELETE FROM
+    arp_wanderwege_v2.hpm_catalogues_direction_of_signalisation
+;
+DELETE FROM
+    arp_wanderwege_v2.hpm_catalogues_hiking_segment_type
+;
+DELETE FROM
+    arp_wanderwege_v2.hpm_catalogues_hpm_catalog_item
+;
+DELETE FROM
+    arp_wanderwege_v2.hpm_catalogues_hpm_route_type
+;
+DELETE FROM
+    arp_wanderwege_v2.hpm_catalogues_hpm_type
+    ;
+DELETE FROM
+    arp_wanderwege_v2.hpm_catalogues_location_category
+;
+DELETE FROM
+    arp_wanderwege_v2.hpm_catalogues_mtb_segment_type
+;
+DELETE FROM
+    arp_wanderwege_v2.hpm_catalogues_obliging_authorities
+;
+DELETE FROM
+    arp_wanderwege_v2.hpm_catalogues_obliging_land_owner
+;
+DELETE FROM
+    arp_wanderwege_v2.hpm_catalogues_public_transport_section
+;
+DELETE FROM
+    arp_wanderwege_v2.hpm_catalogues_quality_of_way
+;
+DELETE FROM
+    arp_wanderwege_v2.hpm_catalogues_route_category
+;
+DELETE FROM
+    arp_wanderwege_v2.hpm_catalogues_route_type
+;
+DELETE FROM
+    arp_wanderwege_v2.hpm_catalogues_state
+;
+DELETE FROM
+    arp_wanderwege_v2.hpm_catalogues_surface
+;
