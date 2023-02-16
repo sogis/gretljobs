e0c2791783c91543a51227bfb27b71d6b07b2eab
barpasen <peter.senn@bd.so.ch>
2023-02-16 15:11:46 +0100

    [arp_wanderwege_pub] new model

--- arp_wanderwege_pub/arp_wanderwege_pub_signalisation.sql
+++ arp_wanderwege_pub/arp_wanderwege_pub_signalisation.sql
@@ -3,7 +3,7 @@ SELECT
     field_name AS standortname,
     altitude AS hoehe,
     NULL AS standortnummer,
-    'https://geo-t.so.ch/docs/ch.so.arp.wanderwege/standorttafeln/'||field_name||'.pdf' AS dokument
+    NULL AS dokument
 FROM
     arp_wanderwege_v1.hpm_walk_lv95_signalisation
 ;
