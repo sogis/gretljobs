d2ea7f2548eced6c8f8fdc809101b8828c895fee
barpasen <peter.senn@bd.so.ch>
2023-02-28 09:39:20 +0100

    [test_waldplan_pub] Test wegen Jenkinsfile

---
+++ test_waldplan_pub/awjf_waldplan_bestandeskarte_uebersicht_pub.sql
@@ -0,0 +1,35 @@
+SELECT 
+    waldplan_bestandeskarte.t_id, 
+    ST_Multi(waldplan_bestandeskarte.geometrie) AS geometrie, 
+    'Waldplan'::text AS typ
+FROM 
+    awjf_waldplan_bestandeskarte_v1.waldplan_bestandeskarte
+WHERE 
+    waldplan_bestandeskarte.wptyp < 9
+AND 
+    waldplan_bestandeskarte.wap_vollstaendig = true
+    
+UNION 
+
+SELECT 
+    bodenbedeckung_boflaeche.t_id,
+    ST_Multi(bodenbedeckung_boflaeche.geometrie) AS geometrie,
+    'AVWald'::text AS typ
+FROM 
+    agi_dm01avso24.bodenbedeckung_boflaeche
+WHERE 
+    (
+        bodenbedeckung_boflaeche.art = 'bestockt.geschlossener_Wald' 
+        OR 
+        bodenbedeckung_boflaeche.art = 'bestockt.uebrige_bestockte.Hecke'
+    )
+AND 
+   bodenbedeckung_boflaeche.t_datasetname::int NOT IN ( 
+       SELECT 
+           waldplan_bestandeskarte.gem_bfs
+       FROM 
+           awjf_waldplan_bestandeskarte_v1.waldplan_bestandeskarte
+       WHERE 
+           waldplan_bestandeskarte.wap_vollstaendig = true
+    )
+;
