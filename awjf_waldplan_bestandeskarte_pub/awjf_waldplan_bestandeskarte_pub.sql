49d0fea9096f779df9d3d63cea8e5b2b41c49fdc
barpasen <peter.senn@bd.so.ch>
2023-03-02 11:56:41 +0100

    [awjf_waldplan_bestandeskarte_pub] exclude gem_bfs for publication

--- awjf_waldplan_bestandeskarte_pub/awjf_waldplan_bestandeskarte_pub.sql
+++ awjf_waldplan_bestandeskarte_pub/awjf_waldplan_bestandeskarte_pub.sql
@@ -29,7 +29,7 @@ SELECT
     weidewald,
     gb_gem_bfs,
     astatus,
-    hoheitsgrenzen_gemeindegrenze.gemeindename,
+    'Olten' AS gemeindename,
     CASE 
         WHEN fid_eig = 1000
             THEN '1000 - Bundeswald' 
@@ -166,11 +166,12 @@ SELECT
     END AS bsttyp_text
 FROM
     awjf_waldplan_bestandeskarte_v1.waldplan_bestandeskarte
-    LEFT JOIN agi_hoheitsgrenzen_pub.hoheitsgrenzen_gemeindegrenze
-        ON hoheitsgrenzen_gemeindegrenze.bfs_gemeindenummer = waldplan_bestandeskarte.gem_bfs
-WHERE
-    astatus = 'abgeschlossen'
-OR
-    id_wp > 0
-;
 
+WHERE
+    (
+        astatus = 'abgeschlossen'
+    OR
+        id_wp > 0
+    )
+${bfsnr_param}
+;
