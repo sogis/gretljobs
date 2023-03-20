379683f002e699c34a2f59daf0b118b92b5bd542
bjsvwcur <38523407+bjsvwcur@users.noreply.github.com>
2023-03-20 11:21:30 +0100

    Update arp_agglomerationsprogramme_pub_agglomrtnsprgrmme_massnahme_punkt.sql

--- arp_agglomerationsprogramme_pub/arp_agglomerationsprogramme_pub_agglomrtnsprgrmme_massnahme_punkt.sql
+++ arp_agglomerationsprogramme_pub/arp_agglomerationsprogramme_pub_agglomrtnsprgrmme_massnahme_punkt.sql
@@ -34,26 +34,26 @@ SELECT
     massnahme.baubeginn_geplant_lv,
     punktobjekt.geometrie AS punktgeometrie
 FROM
-    arp_agglomerationsprogramme.agglomrtnsprgrmme_massnahme massnahme
-    LEFT JOIN arp_agglomerationsprogramme.agglomrtnsprgrmme_agglomerationsprogramm aggloprogramm
+    arp_agglomerationsprogramme_v1.agglomrtnsprgrmme_massnahme massnahme
+    LEFT JOIN arp_agglomerationsprogramme_v1.agglomrtnsprgrmme_agglomerationsprogramm aggloprogramm
         ON massnahme.agglo_programm = aggloprogramm.t_id
-    LEFT JOIN arp_agglomerationsprogramme.agglomrtnsprgrmme_paket paket
+    LEFT JOIN arp_agglomerationsprogramme_v1.agglomrtnsprgrmme_paket paket
         ON massnahme.paket = paket.t_id
-    RIGHT JOIN arp_agglomerationsprogramme.agglomrtnsprgrmme_agglo_point punktobjekt
+    RIGHT JOIN arp_agglomerationsprogramme_v1.agglomrtnsprgrmme_agglo_point punktobjekt
         ON massnahme.punkte_geometrie = punktobjekt.t_id 
-    LEFT JOIN arp_agglomerationsprogramme.agglomrtnsprgrmme_federfuehrung_massnahme federfuehrung_massnahme
+    LEFT JOIN arp_agglomerationsprogramme_v1.agglomrtnsprgrmme_federfuehrung_massnahme federfuehrung_massnahme
         ON massnahme.t_id = federfuehrung_massnahme.massnahme
-    LEFT JOIN arp_agglomerationsprogramme.agglomrtnsprgrmme_federfuehrung federfuehrung
+    LEFT JOIN arp_agglomerationsprogramme_v1.agglomrtnsprgrmme_federfuehrung federfuehrung
         ON federfuehrung_massnahme.federfuehrung_name = federfuehrung.t_id
-    LEFT JOIN arp_agglomerationsprogramme.agglomrtnsprgrmme_gemeinde_massnahme gemeinde_massnahme
+    LEFT JOIN arp_agglomerationsprogramme_v1.agglomrtnsprgrmme_gemeinde_massnahme gemeinde_massnahme
         ON massnahme.t_id = gemeinde_massnahme.massnahme
-    LEFT JOIN arp_agglomerationsprogramme.agglomrtnsprgrmme_gemeinde gemeinde
+    LEFT JOIN arp_agglomerationsprogramme_v1.agglomrtnsprgrmme_gemeinde gemeinde
         ON gemeinde_massnahme.gemeinde_name = gemeinde.t_id
-    LEFT JOIN arp_agglomerationsprogramme.agglomrtnsprgrmme_projektphase projektphase 
+    LEFT JOIN arp_agglomerationsprogramme_v1.agglomrtnsprgrmme_projektphase projektphase 
 	ON projektphase.ilicode = massnahme.projektphase
-    LEFT JOIN arp_agglomerationsprogramme.agglomrtnsprgrmme_prioritaet prioritaet 
+    LEFT JOIN arp_agglomerationsprogramme_v1.agglomrtnsprgrmme_prioritaet prioritaet 
         ON prioritaet.ilicode = massnahme.prioritaet 
-    LEFT JOIN arp_agglomerationsprogramme.agglomrtnsprgrmme_umsetzungsstand umsetzungsstand 
+    LEFT JOIN arp_agglomerationsprogramme_v1.agglomrtnsprgrmme_umsetzungsstand umsetzungsstand 
 	ON umsetzungsstand.ilicode = massnahme.umsetzungsstand 	
   
   -- nur Punkte uebertragen die auch eine Massnahme dran haben
