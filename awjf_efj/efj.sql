ee9579ec7bf0dd7ce2e0ecfb47c53decc11b00bf
Martin Schweizer <martin.schweizer@bd.so.ch>
2023-03-30 09:13:29 +0200

    [awjf_efj]Added new Job

---
+++ awjf_efj/efj.sql
@@ -0,0 +1,28 @@
+select 
+    rowdef.*
+from 
+    awjf_efj_v1.efj_abgaenge s
+CROSS JOIN LATERAL
+	jsonb_to_record(replace(s.content,'""','null')::jsonb) as rowdef(
+        "Betriebsart" TEXT,
+        "fallnr" TEXT,
+        "tierart" TEXT,
+        "anzahl" INTEGER,
+        "datum" DATE,
+        "zeit" TIME,
+        "bezugsperson" TEXT,
+        "bezugsperson_id" INTEGER,
+        "reviernr" INTEGER,
+        "kulturodernutztierart" TEXT,
+        "totalschadenauszahlung" FLOAT,
+        "todesgrund" TEXT,
+        "tieralter" INTEGER,
+        "tieralter_txt" TEXT,
+        "geschlecht" TEXT,
+        "schutzart" TEXT,
+        "beobachtungsfall" TEXT,
+        "eFJ_id" TEXT,
+        "x_koordinate" INTEGER,
+        "y_koordinate" INTEGER,
+        "apolygon" TEXT
+    )
