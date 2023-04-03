8ef8b46c4cba3901691027f2c4563dc149492716
Andreas Neumann <a.neumann@carto.net>
2023-04-03 11:44:48 +0200

    [afu_onlinerisk_transfer] Unnötige SQL-Skripte entfernen

--- afu_onlinerisk_transfer/delete_qrcat.sql
+++
@@ -1,7 +0,0 @@
--- DELETE previously parked data
-DELETE FROM ${DB_Schema_QRcat}.qrcat_letalflaeche;
-DELETE FROM ${DB_Schema_QRcat}.qrcat_toxischunguenstigster_sektor;
-DELETE FROM ${DB_Schema_QRcat}.qrcat_szenario;
-DELETE FROM ${DB_Schema_QRcat}.qrcat_fwert;
-DELETE FROM ${DB_Schema_QRcat}.qrcat_detailszenario_grundhaeufigkeit;
-DELETE FROM ${DB_Schema_QRcat}.qrcat_toxreferenzszenario;
--- afu_onlinerisk_transfer/qrcat_fwert.sql
+++
@@ -1,7 +0,0 @@
-SELECT 
-  t_id, 
-  t_ili_tid, 
-  fwert, 
-  bemerkung 
-FROM 
-  ${DB_Schema_QRcat}.qrcat_fwert;
