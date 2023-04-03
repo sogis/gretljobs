5493e45fc0054b0873504cd0d5d480b6830d918d
Andreas Neumann <a.neumann@carto.net>
2023-04-03 11:54:28 +0200

    [afu_onlinerisk_transfer] Übertrag stark vereinfachen (#1422)
    * [afu_onlinerisk_transfer] Test Replace
    
    * [afu_onlinerisk_transfer] Test Replace 2
    
    * [afu_onlinerisk_transfer] Unnötige SQL-Skripte entfernen

--- afu_onlinerisk_transfer/delete_online_risk.sql
+++
@@ -1,6 +0,0 @@
-DELETE FROM ${DB_Schema_QRcat}.onlinerisk_stoff_in_bereich;
-DELETE FROM ${DB_Schema_QRcat}.onlinerisk_stoff;
-DELETE FROM ${DB_Schema_QRcat}.onlinerisk_bereich;
-DELETE FROM ${DB_Schema_QRcat}.onlinerisk_gebaeude;
-DELETE FROM ${DB_Schema_QRcat}.onlinerisk_untersuchungseinheit;
-DELETE FROM ${DB_Schema_QRcat}.onlinerisk_betrieb;
