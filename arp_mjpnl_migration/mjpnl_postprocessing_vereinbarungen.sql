2714c9566d81c1272caffe99d82d2df87db7ba15
Andreas Neumann <a.neumann@carto.net>
2023-04-05 15:27:32 +0200

    [arp_mjpnl_migration] diverse spatial joins im postprocessing

--- arp_mjpnl_migration/mjpnl_postprocessing_vereinbarungen.sql
+++ arp_mjpnl_migration/mjpnl_postprocessing_vereinbarungen.sql
@@ -1,47 +1,39 @@
---neue Vereinbarungs-Nummern setzen
---zusammengesetzter Wert aus bezirknr_vereinbarungsart_zaehler-pro-vbgart-bezirk
---wobei bei den Bezirksnr nur die letzten 2 Stellen berücksichtigt werden
---und bei der Vereinbarungsart eine Abkürzung verwendet wird (siehe CASE Statement unten)
+--diverse spatial joins:
+--Gemeinden, GS-Nummern, Flurnamen
 UPDATE
    arp_mjpnl_v1.mjpnl_vereinbarung AS vbg
-     SET vereinbarungs_nr=(
-       WITH bezvbart AS (
-            SELECT
-                 --BezirksNr (letzte 2 Ziffern)
-                 substring(bezirksnummer::TEXT,3,2) || '_' ||
-                 --Vereinbarungsart (abgekürzt)
-                 CASE
-                   WHEN vbg.vereinbarungsart = 'Wiese' THEN 'Wi'
-                   WHEN vbg.vereinbarungsart = 'Weide_LN' THEN 'WeLN'
-                   WHEN vbg.vereinbarungsart = 'Weide_SOEG' THEN 'WeSöG'
-                   WHEN vbg.vereinbarungsart = 'Hecke' THEN 'He'
-                   WHEN vbg.vereinbarungsart = 'Lebhag' THEN 'Le'
-                   WHEN vbg.vereinbarungsart = 'Hostet' THEN 'Ho'
-                   WHEN vbg.vereinbarungsart = 'OBL' THEN 'OBL'
-                   WHEN vbg.vereinbarungsart = 'WBL_Wiese' THEN 'WBLWi'
-                   WHEN vbg.vereinbarungsart = 'WBL_Weide' THEN 'WBLWe'
-                   WHEN vbg.vereinbarungsart = 'ALR_Buntbrache' THEN 'ALRB'
-                   WHEN vbg.vereinbarungsart = 'ALR_Saum' THEN 'ALRS'
-                 END AS beznr_vbart          
-            FROM
-                 agi_hoheitsgrenzen_pub.hoheitsgrenzen_bezirksgrenze bez
-            WHERE
-                ST_Within(ST_PointOnSurface(vbg.geometrie),bez.geometrie)
-       )
-       SELECT
-         --BezirksNr (letzte 2 Ziffern)
-         bez.beznr_vbart || '_' ||
-         (SELECT
-            lpad((COALESCE(MAX((SELECT (string_to_array(vereinbarungs_nr,'_'))[3])::int4),0) + 1)::TEXT,5,'0') AS maxval
-            FROM
-                arp_mjpnl_v1.mjpnl_vereinbarung
-            WHERE
-                starts_with(vereinbarungs_nr,bez.beznr_vbart)
-        )
-       FROM
-         bezvbart bez
-     ) 
-WHERE
+     SET bfs_nr=(
+        SELECT
+		 COALESCE(array_agg(gg.bfs_gemeindenummer),'{9999}') AS bfs_nr
+		FROM
+		   agi_hoheitsgrenzen_pub.hoheitsgrenzen_gemeindegrenze gg
+		 WHERE
+		   ST_Intersects(gg.geometrie,vbg.geometrie)
+		   AND
+		   (ST_MaximumInscribedCircle(ST_Intersection(gg.geometrie,vbg.geometrie))).radius > 1
+	 ),
+	 gemeinde=(
+        SELECT
+		 COALESCE(array_agg(gg.gemeindename),'{"unbekannt"}') AS gemeinde
+		FROM
+		   agi_hoheitsgrenzen_pub.hoheitsgrenzen_gemeindegrenze gg
+		 WHERE
+		   ST_Intersects(gg.geometrie,vbg.geometrie)
+		   AND
+		   (ST_MaximumInscribedCircle(ST_Intersection(gg.geometrie,vbg.geometrie))).radius > 1
+	 )/*,
+	 gb_nr=(
+        SELECT
+		 COALESCE(array_agg(gs.nummer),'{"unbekannt"}') AS gb_nr
+		FROM
+		   agi_dm01avso24.liegenschaften_liegenschaft ls, agi_dm01avso24.liegenschaften_grundstueck gs
+		 WHERE
+		   ST_Intersects(ls.geometrie,vbg.geometrie)
+		   AND
+		   (ST_MaximumInscribedCircle(ST_Intersection(ls.geometrie,vbg.geometrie))).radius > 1
+		   AND gs.t_id = ls.liegenschaft_von
+	 )*/
+ WHERE
   vbg.mjpnl_version = 'MJPNL_2020'
- --AND vbg.vereinbarungs_nr = 'bla'
+;
   
