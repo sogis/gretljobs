4837585701a50c156e87225a6110e702eeb62df8
Andreas Neumann <a.neumann@carto.net>
2023-04-05 17:55:59 +0200

    [arp_mjpnl_migration] Fix typo

--- arp_mjpnl_migration/mjpnl_vereinbarung.sql
+++ arp_mjpnl_migration/mjpnl_vereinbarung.sql
@@ -48,7 +48,7 @@ SELECT
   4::integer AS kontrollintervall,
   COALESCE(vbg.notiz,'') || E'\n§\n' ||
   '{"vbart_vereinbarung_alt":' || COALESCE(vbartvb.bez,'NULL') ||
-  '"vbart_vereinbarungsflaeche_alt":' || COALESCE(vbartf.bez,'NULL') ||
+  '"vbart_vereinbarungsflaeche_alt":' || COALESCE(vbartfl.bez,'NULL') ||
  ',"vereinbarungsflaeche_alt":' || COALESCE(mjpfl.flaeche::text,'NULL') ||
   '}'  AS bemerkung, --TODO: Zwischenparkieren weiterer alter Attribut-Werte
   18::int8 AS uzl_subregion, -- im Postprocessing zu ersetzen
