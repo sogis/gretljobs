902524ca99fdcda8b61dd53503ec41da06dd90cc
Andreas Neumann <a.neumann@carto.net>
2023-04-06 10:14:40 +0200

    [arp_mjpnl_migration] alten Infos in Bemerkung schöner formattiert

--- arp_mjpnl_migration/mjpnl_vereinbarung.sql
+++ arp_mjpnl_migration/mjpnl_vereinbarung.sql
@@ -47,10 +47,10 @@ SELECT
   'MJPNL_2020' AS mjpnl_version,
   4::integer AS kontrollintervall,
   COALESCE(vbg.notiz,'') || E'\n§\n' ||
-  '{\n"vbart_vereinbarung_alt":' || COALESCE(vbartvb.bez,'NULL') ||
-  '\n,"vbart_vereinbarungsflaeche_alt":' || COALESCE(vbartfl.bez,'NULL') ||
-  '\n,"vereinbarungsflaeche_alt":' || COALESCE(mjpfl.flaeche::text,'NULL') ||
-  '\n,"bewirtschafter_alt":' || COALESCE(pers.name || COALESCE(' '|| pers.vorname,''),'NULL') ||
+  '{\n"vbart_vereinbarung_alt":' || COALESCE(vbartvb.bez,'NULL') || ',\n' ||
+  '"vbart_vereinbarungsflaeche_alt":' || COALESCE(vbartfl.bez,'NULL') || ',\n' ||
+  '"vereinbarungsflaeche_alt":' || COALESCE(mjpfl.flaeche::text,'NULL') || ',\n' ||
+  '"bewirtschafter_alt":' || COALESCE(pers.name || COALESCE(' '|| pers.vorname,''),'NULL') ||
   '\n}'  AS bemerkung, --TODO: Zwischenparkieren weiterer alter Attribut-Werte
   18::int8 AS uzl_subregion, -- im Postprocessing zu ersetzen
   'migration' AS dateipfad_oder_url,
