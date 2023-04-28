da1fa391a55a1718b68cc7fa3c7f0d4c9e39bbea
Andreas Neumann <a.neumann@carto.net>
2023-04-28 15:18:57 +0200

    [arp_mjpnl_migration] Zuweisen Liegenschaften ohne Maximum Inscribed Circle

--- arp_mjpnl_migration/mjpnl_postprocessing_vereinbarungen_liegenschaften_zuweisen.sql
+++ arp_mjpnl_migration/mjpnl_postprocessing_vereinbarungen_liegenschaften_zuweisen.sql
@@ -1,23 +1,22 @@
--- Liegenschaften zuweisen
+--Liegenschaften zuweisen
 UPDATE
    ${DB_Schema_MJPNL}.mjpnl_vereinbarung AS vbg
-	 SET 
-	  gb_nr=(
+     SET 
+      gb_nr=(
         SELECT
-		 COALESCE(array_agg(gs.nummer),'{"unbekannt"}') AS gb_nr
-		FROM
-		   agi_dm01avso24.liegenschaften_liegenschaft ls, agi_dm01avso24.liegenschaften_grundstueck gs
-		 WHERE
-		  ls.t_datasetname::integer = ANY (vbg.bfs_nr)
-		  AND gs.t_datasetname::integer = ANY (vbg.bfs_nr)
-		  AND gs.t_id = ls.liegenschaft_von
-		  AND 
-		  (
-		    ST_Intersects(ls.geometrie,vbg.geometrie)
-		     AND
-		    (ST_MaximumInscribedCircle(ST_Intersection(ls.geometrie,vbg.geometrie))).radius > 1
-          )
-	 )
- WHERE
+         COALESCE(array_agg(gs.nummer ORDER BY gs.nummer),'{"unbekannt"}') AS gb_nr
+        FROM
+           agi_dm01avso24.liegenschaften_liegenschaft ls
+           LEFT JOIN agi_dm01avso24.liegenschaften_grundstueck gs
+                ON gs.t_datasetname::integer = ANY (vbg.bfs_nr)
+                   AND gs.t_id = ls.liegenschaft_von
+        WHERE
+          ls.t_datasetname::integer = ANY (vbg.bfs_nr)
+           AND ST_Intersects(ls.geometrie,vbg.geometrie)
+           /* doesn't work (memory problems) 
+             AND
+            (ST_MaximumInscribedCircle(ST_Intersection(ls.geometrie,vbg.geometrie))).radius > 1 */
+     )
+WHERE
   vbg.mjpnl_version = 'MJPNL_2020'
 ;
