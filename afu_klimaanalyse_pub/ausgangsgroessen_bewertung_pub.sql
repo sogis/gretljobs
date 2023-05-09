8bdd1daea94b64574e6303691a5968e16bfe725b
barpasen <peter.senn@bd.so.ch>
2023-05-09 10:49:35 +0200

    [afu_klimaanalyse_pub] zum Testen

--- afu_klimaanalyse_pub/ausgangsgroessen_bewertung_pub.sql
+++ afu_klimaanalyse_pub/ausgangsgroessen_bewertung_pub.sql
@@ -6,17 +6,22 @@ SELECT
     windgeschwindigkeit,
     pet,
     kaltluftprozess,
-    --kaltluftprozess_code.dispname AS kaltluftprozess_txt,
+    kaltluftprozess_code.dispname AS kaltluftprozess_txt,
     kaltluftentstehungsgebiet,
+    kaltluftentstehungsgebiet::text AS kaltluftentstehungsgebiet_txt,
     flaeche,
     einwohnerzahl_gesamt,
     einwohner_unter_6_jahren,
     einwohner_ueber_65_jahre,
     einwohnerdichte,
     bewertung_tag,
-    bewertung_nacht
+    bewertung_code.dispname AS bewertung_tag_txt,
+    bewertung_nacht,
+    bewertung_code.dispname AS bewertung_nacht_txt
 FROM
-    afu_klimaanalyse_v1.ausgangsgroessen_bewertung AS bewertung
-    --LEFT JOIN afu_klimaanalyse_v1.ausgangsgroessen_bewertung_kaltluftprozess AS kaltluftprozess_code
-    --ON kaltluftprozess = kaltluftprozess_code.ilicode
+    afu_klimaanalyse_v1.ausgangsgroessen_bewertung AS ausgangsgroessen_bewertung
+    LEFT JOIN afu_klimaanalyse_v1.ausgangsgroessen_bewertung_kaltluftprozess AS kaltluftprozess_code
+      ON kaltluftprozess = kaltluftprozess_code.ilicode
+    LEFT JOIN afu_klimaanalyse_v1.bewertung AS bewertung_code
+      ON bewertung_tag = bewertung_code.ilicode
 ;
--- afu_klimaanalyse_pub/entwicklungsflaechen_pub.sql
+++ afu_klimaanalyse_pub/entwicklungsflaechen_pub.sql
@@ -3,10 +3,14 @@ SELECT
     nummer,
     flaeche,
     typ,
+    typ_code.dispname AS typ_txt,
     aggloprogramm,
+    aggloprogramm::text AS aggloprogramm_txt,
     lufttemperatur_14uhr,
     pet_14uhr,
     lufttemperatur_04uhr
 FROM
     afu_klimaanalyse_v1.entwicklungsflaechen
+    LEFT JOIN afu_klimaanalyse_v1.entwicklungsflaechen_typ AS typ_code
+      ON typ = typ_code.ilicode
 ;
