7524e6aa584ef2cf8e3122d2f01781f79510b8e0
signedav <david@opengis.ch>
2023-05-26 22:09:08 +0200

    [arp_mjpnl_migration] Beachte dass UZL-Subregionen im Topic Umweltziele liegen

--- arp_mjpnl_migration/mjpnl_postprocessing_vereinbarung_uzl-region.sql
+++ arp_mjpnl_migration/mjpnl_postprocessing_vereinbarung_uzl-region.sql
@@ -2,6 +2,6 @@ UPDATE
      ${DB_Schema_MJPNL}.mjpnl_vereinbarung vb
   SET
      uzl_subregion = uzl.t_id
-  FROM ${DB_Schema_MJPNL}.mjpnl_uzl_subregion uzl
+  FROM ${DB_Schema_MJPNL}.umweltziele_uzl_subregion uzl
     WHERE ST_Within(ST_PointOnSurface(vb.geometrie),uzl.geometrie)
 ;
