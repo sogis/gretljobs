e1975750f6468fd08180165da9d490eb94c8745b
barpasen <peter.senn@bd.so.ch>
2023-06-06 13:27:23 +0200

    [avt_oevkov] initial version

--- afu_klimaanalyse_pub/ausgangsgroessen_bewertung_pub.sql
+++ afu_klimaanalyse_pub/ausgangsgroessen_bewertung_pub.sql
@@ -16,13 +16,15 @@ SELECT
     einwohner_ueber_65_jahre,
     einwohnerdichte,
     bewertung_tag,
-    bewertung_code.dispname AS bewertung_tag_txt,
+    bewertung_code_tag.dispname AS bewertung_tag_txt,
     bewertung_nacht,
-    bewertung_code.dispname AS bewertung_nacht_txt
+    bewertung_code_nacht.dispname AS bewertung_nacht_txt
 FROM
     afu_klimaanalyse_v1.ausgangsgroessen_bewertung AS ausgangsgroessen_bewertung
     LEFT JOIN afu_klimaanalyse_v1.ausgangsgroessen_bewertung_kaltluftprozess AS kaltluftprozess_code
       ON kaltluftprozess = kaltluftprozess_code.ilicode
-    LEFT JOIN afu_klimaanalyse_v1.bewertung AS bewertung_code
-      ON bewertung_tag = bewertung_code.ilicode
+    LEFT JOIN afu_klimaanalyse_v1.bewertung AS bewertung_code_tag
+      ON bewertung_tag = bewertung_code_tag.ilicode
+    LEFT JOIN afu_klimaanalyse_v1.bewertung AS bewertung_code_nacht
+      ON bewertung_nacht = bewertung_code_nacht.ilicode
 ;
