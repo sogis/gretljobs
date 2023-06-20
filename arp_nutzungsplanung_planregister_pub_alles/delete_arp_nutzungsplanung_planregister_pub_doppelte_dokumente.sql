6fc0db32e077e12f337452d582686da85ff99b90
Lüscher Andrea <bjsvwlue@verw.rootso.org>
2023-06-20 07:42:46 +0200

    [arp_nutzungsplanung_planregister_pub_alles] task ziplanregister angepasst, SQL delete doppelte Einträge ergänzt um "offiziellenr" in der WHERE Bedingung, damit die Dokumente "nicht digital-verfügbar" gelöschen werden.

--- arp_nutzungsplanung_planregister_pub_alles/delete_arp_nutzungsplanung_planregister_pub_doppelte_dokumente.sql
+++ arp_nutzungsplanung_planregister_pub_alles/delete_arp_nutzungsplanung_planregister_pub_doppelte_dokumente.sql
@@ -1,6 +1,5 @@
--- In der Nutzungsplanung müssen komunale Dokumente in mehreren Gemeidnen erfasst werden, 
--- wenn die Nutzungsplanung über die Gemeindegrenze geht. Das kann zu doppelten Dokumenten 
--- führen die wir im Planregsiter nicht wollen. Deshalb diese Query, welche die Doppelten Einträge löscht.
+-- In der Nutzungsplanung werden kantonale und kommunale Nutzungspläne separat (Schemas nutzungsplanung, nutzungsplanung_kanton, statsiche_waldgrenze, gewaesserschutz, naturreservat) erfasst. 
+---Das kann zu doppelten Dokumenten führen, die wir im Planregsiter nicht wollen. Deshalb diese Query, welche die Doppelten Einträge löscht. 
 
 DELETE
 FROM
@@ -15,8 +14,10 @@ FROM
     arp_nutzungsplanung_planregister_pub_v1.planregister_dokument b
 WHERE
     a.dokument_url = b.dokument_url
-	AND
-	a.gemeinde = b.gemeinde
+    AND
+    a.gemeinde = b.gemeinde -- Für die Pläne, die über die Gemeindegrenze gehen. Diese werden pro Gemeinde erfasst. Hier ist es richtig, dass sie doppelt sind.
+    AND
+    a.offiziellenr = b.offiziellenr --Für die Dokumente die die URL https://planregister-data.so.ch/public/kanton/Dokument-nicht-digital-verfuegbar.pdf haben
     AND
     a.t_id < b.t_id
 )
