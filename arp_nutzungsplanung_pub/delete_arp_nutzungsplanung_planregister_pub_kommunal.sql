ee626a4bb2cd543fb403c63de16edb8e5836a6a4
Luescher <andrea.luescher@bd.so.ch>
2023-06-27 15:51:21 +0200

    Update delete_arp_nutzungsplanung_planregister_pub_kommunal.sql

--- arp_nutzungsplanung_pub/delete_arp_nutzungsplanung_planregister_pub_kommunal.sql
+++ arp_nutzungsplanung_pub/delete_arp_nutzungsplanung_planregister_pub_kommunal.sql
@@ -1,6 +1,6 @@
 -- Dataset kantonal
 DELETE FROM
-    arp_nutzungsplanung_planregister_v1.planregister_dokument
+    arp_nutzungsplanung_planregister_pub_v1.planregister_dokument
 WHERE
     t_datasetname = 'nutzungsplanung_kommunal' || '_' || ${bfsnr_param}
 ;
