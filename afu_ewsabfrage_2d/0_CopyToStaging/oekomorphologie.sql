a1e3132943577e95ed3ce07d07691e4fce604c97
bjsvwcur <38523407+bjsvwcur@users.noreply.github.com>
2023-06-29 09:24:53 +0200

    Update oekomorphologie.sql

--- afu_ewsabfrage_2d/0_CopyToStaging/oekomorphologie.sql
+++ afu_ewsabfrage_2d/0_CopyToStaging/oekomorphologie.sql
@@ -1,4 +1,9 @@
 SELECT 
     'Gewaesserraum' AS abklaerung,
-    ST_Buffer(geometrie, raumbedarf, 'endcap=round join=round') AS mpoly
+    CASE
+        WHEN raumbedarf IS NULL 
+            THEN ST_MULTI(ST_Buffer(geometrie, '5', 'endcap=round join=round'))
+        ELSE
+            ST_MULTI(ST_Buffer(geometrie, '5', 'endcap=round join=round'))
+    END AS mpoly
 FROM afu_gewaesser_oekomorphologie_pub_v1.oekomorphologie;
