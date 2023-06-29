a20d5508d7b337cf6e6d1112c2befaa4dfe3874f
bjsvwcur <38523407+bjsvwcur@users.noreply.github.com>
2023-06-29 10:51:28 +0200

    [afu_ewsabfrage_2d] add copy to pub (#1478)

---
+++ afu_ewsabfrage_2d/1_CopyToPub/abklaerung.sql
@@ -0,0 +1,5 @@
+SELECT
+    t_id,
+    abklaerung,
+    mpoly
+FROM afu_ewsabfrage_2d_staging_v1.abklaerung
---
+++ afu_ewsabfrage_2d/1_CopyToPub/hinweis.sql
@@ -0,0 +1,8 @@
+SELECT
+    t_id,
+    hinweis,
+    hinweistext,
+    tiefe_hinweis,
+    hinweis_oeffentlich,
+    mpoly 
+FROM afu_ewsabfrage_2d_staging_v1.hinweis
