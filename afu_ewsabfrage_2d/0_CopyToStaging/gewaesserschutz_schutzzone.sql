4318b7280d43b3781d15be3238495c5748a6d6f6
bjsvwcur <38523407+bjsvwcur@users.noreply.github.com>
2023-06-29 09:04:01 +0200

    Update gewaesserschutz_schutzzone.sql

--- afu_ewsabfrage_2d/0_CopyToStaging/gewaesserschutz_schutzzone.sql
+++ afu_ewsabfrage_2d/0_CopyToStaging/gewaesserschutz_schutzzone.sql
@@ -1,5 +1,5 @@
 SELECT 
     0 AS max_bohrtiefe,
     'GW_Schutzzone' AS grund_beschraenkung,
-    apolygon AS mpoly
+    ST_Multi(apolygon) AS mpoly
 FROM afu_gewaesserschutz_pub_v1.gewaesserschutz_schutzzone;
