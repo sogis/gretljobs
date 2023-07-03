71845efefb8d7533037daa84111084720f69ebea
barpasen <peter.senn@bd.so.ch>
2023-07-03 10:23:29 +0200

    [afu_klimaanalyse_pub] initial version

--- afu_klimaanalyse_pub/ausgangsgroessen_bewertung_pub.sql
+++ afu_klimaanalyse_pub/ausgangsgroessen_bewertung_pub.sql
@@ -15,18 +15,118 @@ SELECT
     einwohner_unter_6_jahren,
     einwohner_ueber_65_jahre,
     einwohnerdichte,
-    bewertung_tag,
-    bewertung_code_tag.dispname AS bewertung_tag_txt,
-    bewertung_nacht,
-    bewertung_code_nacht.dispname AS bewertung_nacht_txt,
-    bewertung_tag_pub,
-    bewertung_nacht_pub
+    CASE
+        WHEN
+            nutzung IN ('Siedlung, Arbeitszone ohne Wohnen', 'Siedlung, bewohnt', 'Verkehrswege und Plätze')
+        THEN
+            CASE
+                WHEN
+                    bewertung_tag = 'Sehr_guenstige_bioklimatische_Situation'
+                THEN
+                    'keine'
+                WHEN
+                    bewertung_tag = 'Guenstige_bioklimatische_Situation'
+                THEN
+                    'schwach'
+                WHEN
+                    bewertung_tag = 'Mittlere_bioklimatische_Situation'
+                THEN
+                    'mässig'
+                WHEN
+                    bewertung_tag = 'Unguenstige_bioklimatische_Situation'
+                THEN
+                    'hoch'
+                WHEN
+                    bewertung_tag = 'Sehr_unguenstige_bioklimatische_Situation'
+                THEN
+                    'sehr hoch'
+            END
+        ELSE
+            CASE
+                WHEN
+                    nutzung = 'Grün- und Freiflächen'
+                THEN
+                    CASE
+                        WHEN
+                            bewertung_tag = 'Sehr_hohe_bioklimatische_Bedeutung'
+                        THEN
+                           'sehr gut'
+                        WHEN
+                            bewertung_tag = 'Hohe_bioklimatische_Bedeutung'
+                        THEN
+                           'gut'
+                        WHEN
+                            bewertung_tag = 'Mittlere_bioklimatische_Bedeutung'
+                        THEN
+                           'mässig'
+                        WHEN
+                            bewertung_tag = 'Geringe_bioklimatische_Bedeutung'
+                        THEN
+                           'ausbaufähig'
+                        WHEN
+                            bewertung_tag = 'Sehr_geringe_bioklimatische_Bedeutung'
+                        THEN
+                           'begrenzt'
+                    END
+            END
+    END AS bewertung_tag,
+    CASE
+        WHEN
+            nutzung IN ('Siedlung, Arbeitszone ohne Wohnen', 'Siedlung, bewohnt', 'Verkehrswege und Plätze')
+        THEN
+            CASE
+                WHEN
+                    bewertung_nacht = 'Sehr_guenstige_bioklimatische_Situation'
+                THEN
+                    'keine'
+                WHEN
+                    bewertung_nacht = 'Guenstige_bioklimatische_Situation'
+                THEN
+                    'schwach'
+                WHEN
+                    bewertung_nacht = 'Mittlere_bioklimatische_Situation'
+                THEN
+                    'mässig'
+                WHEN
+                    bewertung_nacht = 'Unguenstige_bioklimatische_Situation'
+                THEN
+                    'hoch'
+                WHEN
+                    bewertung_nacht = 'Sehr_unguenstige_bioklimatische_Situation'
+                THEN
+                    'sehr hoch'
+            END
+        ELSE
+            CASE
+                WHEN
+                    nutzung = 'Grün- und Freiflächen'
+                THEN
+                    CASE
+                        WHEN
+                            bewertung_nacht = 'Sehr_hohe_bioklimatische_Bedeutung'
+                        THEN
+                           'sehr hoch'
+                        WHEN
+                            bewertung_nacht = 'Hohe_bioklimatische_Bedeutung'
+                        THEN
+                           'hoch'
+                        WHEN
+                            bewertung_nacht = 'Mittlere_bioklimatische_Bedeutung'
+                        THEN
+                           'mässig'
+                        WHEN
+                            bewertung_nacht = 'Geringe_bioklimatische_Bedeutung'
+                        THEN
+                           'gering'
+                        WHEN
+                            bewertung_nacht = 'Sehr_geringe_bioklimatische_Bedeutung'
+                        THEN
+                           'sehr gering'
+                    END
+            END
+    END AS bewertung_nacht
 FROM
     afu_klimaanalyse_v1.ausgangsgroessen_bewertung AS ausgangsgroessen_bewertung
     LEFT JOIN afu_klimaanalyse_v1.ausgangsgroessen_bewertung_kaltluftprozess AS kaltluftprozess_code
       ON kaltluftprozess = kaltluftprozess_code.ilicode
-    LEFT JOIN afu_klimaanalyse_v1.bewertung AS bewertung_code_tag
-      ON bewertung_tag = bewertung_code_tag.ilicode
-    LEFT JOIN afu_klimaanalyse_v1.bewertung AS bewertung_code_nacht
-      ON bewertung_nacht = bewertung_code_nacht.ilicode
 ;
