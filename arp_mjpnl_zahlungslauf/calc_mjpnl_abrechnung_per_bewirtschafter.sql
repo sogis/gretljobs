3c288624822f838b5d3784b5cbeee69ac351e4e7
signedav <david@opengis.ch>
2023-07-21 13:24:26 +0200

    [arp_mjpnl_zahlungslauf] Zusammenzug in abrechnung_per_vereinbarung und abrechnung_per_bewirtschafter.

--- arp_mjpnl_zahlungslauf/calc_mjpnl_abrechnung_per_bewirtschafter.sql
+++ arp_mjpnl_zahlungslauf/calc_mjpnl_abrechnung_per_bewirtschafter.sql
@@ -1,3 +1,87 @@
 /* Zusammenzug Zahlungen per Bewirtschafter, Auszahlungsjahr und Status Zahlung */
-/* Entspricht arp_mjpnl_migration/mjpnl_postprocessing_abrechnung_per_bewirtschafter.sql und muss allerdings nur die diesjährigen Leistungen berücksichtigen */
-/* Dies soll gemacht werden, wenn Unklarheiten in mjpnl_postprocessing_abrechnung_per_bewirtschafter gelöst sind */
+/* Entspricht arp_mjpnl_migration/mjpnl_postprocessing_abrechnung_per_bewirtschafter.sql muss allerdings nur die diesjährigen Leistungen berücksichtigen */
+
+INSERT INTO ${DB_Schema_MJPNL}.mjpnl_abrechnung_per_bewirtschafter 
+(t_basket, gelan_pid_gelan, gelan_person, gelan_ortschaft, gelan_iban, betrag_total, status_abrechnung,
+ datum_abrechnung, auszahlungsjahr, dateipfad_oder_url, erstellungsdatum, operator_erstellung, migriert)
+
+WITH gelan_persons AS (
+/* zuerst alle GELAN Personen filtern die eine aktive Vereinbarung haben */
+SELECT 
+    DISTINCT
+      pers.pid_gelan,
+      pers.name_vorname,
+      pers.ortschaft,
+      pers.iban
+FROM
+    ${DB_Schema_MJPNL}.mjpnl_vereinbarung vbg
+    LEFT JOIN ${DB_Schema_MJPNL}.betrbsdttrktrdten_gelan_person pers
+      ON vbg.gelan_pid_gelan = pers.pid_gelan
+   WHERE
+     pers.pid_gelan IS NOT NULL AND pers.iban IS NOT NULL
+     AND vbg.status_vereinbarung = 'aktiv'
+   ORDER BY pers.pid_gelan ASC
+)
+/* Dann Abrechnungsdaten per Bewirtschafter und Auszahlungsjahr und Status aggregieren */
+SELECT 
+   MIN(vbg.t_basket) AS t_basket,   
+   pers.pid_gelan,
+   pers.name_vorname AS gelan_person,
+   pers.ortschaft AS gelan_ortschaft,
+   pers.iban,
+   SUM(abrg_vbg.gesamtbetrag) AS betrag_total,
+   -- wenn es eine status_abrechnung "freigegeben" gibt, dann soll der status "freigegeben" sein
+   CASE WHEN (SELECT COUNT(*) FROM ${DB_Schema_MJPNL}.mjpnl_abrechnung_per_vereinbarung v WHERE v.status_abrechnung = 'freigegeben' AND v.gelan_pid_gelan = pers.pid_gelan) > 0 
+     THEN 'freigegeben' 
+     -- ansonsten ist es für alle gleich ("ausbezahlt" oder "intern_verrechnet")
+     ELSE MAX(v.status_abrechnung) 
+   END AS status_abrechnung,
+   -- wenn es eine status_abrechnung "freigegeben" gibt, dann soll es noch kein datum_abrechnung haben
+   CASE WHEN (SELECT COUNT(*) FROM ${DB_Schema_MJPNL}.mjpnl_abrechnung_per_vereinbarung v WHERE v.status_abrechnung = 'freigegeben' AND v.gelan_pid_gelan = pers.pid_gelan) > 0 
+     THEN NULL
+     -- ansonsten kann es das späteste datum nehmen
+     ELSE MAX(v.datum_abrechnung) 
+   END AS datum_abrechnung,
+   abrg_vbg.auszahlungsjahr,
+   'Migration' AS dateipfad_oder_url,
+   COALESCE ( MAX(abrg_vbg.datum_abrechnung), now()::date ) AS erstellungsdatum,
+   'Migration' AS operator_erstellung,
+   FALSE AS migriert
+FROM
+  gelan_persons pers
+  LEFT JOIN ${DB_Schema_MJPNL}.mjpnl_vereinbarung vbg
+     ON pers.pid_gelan = vbg.gelan_pid_gelan
+  LEFT JOIN ${DB_Schema_MJPNL}.mjpnl_abrechnung_per_vereinbarung abrg_vbg
+     ON vbg.vereinbarungs_nr = abrg_vbg.vereinbarungs_nr
+  WHERE 
+    abrg_vbg.gesamtbetrag IS NOT NULL
+    -- berücksichtige nur diesjährige Leistungen
+    AND abrg_vbg.auszahlungsjahr = date_part('year', now())::integer;
+  GROUP BY pers.pid_gelan, pers.iban, pers.name_vorname, pers.ortschaft, abrg_vbg.auszahlungsjahr
+  ORDER BY pers.pid_gelan ASC
+
+/* Abrechnung per Vereinbarung aktualisieren mit Fremdschlüsseln zur Abrechnung per Bewirtschafter */
+WITH abrg_per_bewirtschafter AS (
+SELECT 
+   abrg_bew.t_id,
+   abrg_bew.auszahlungsjahr,
+   abrg_bew.status_abrechnung,
+   vbg.vereinbarungs_nr 
+FROM
+   ${DB_Schema_MJPNL}.mjpnl_abrechnung_per_bewirtschafter abrg_bew
+   LEFT JOIN ${DB_Schema_MJPNL}.mjpnl_vereinbarung vbg
+      ON abrg_bew.gelan_pid_gelan = vbg.gelan_pid_gelan
+   WHERE
+      abrg_bew.t_id != 9999999
+   ORDER BY vbg.vereinbarungs_nr ASC, abrg_bew.auszahlungsjahr ASC, abrg_bew.status_abrechnung ASC
+)
+/* Update der Abrechnung per Vereinbarung über gemeinsame Attribute */
+UPDATE
+  ${DB_Schema_MJPNL}.mjpnl_abrechnung_per_vereinbarung abrg_vbg 
+    SET abrechnungperbewirtschafter = abrg_bew.t_id
+  FROM abrg_per_bewirtschafter abrg_bew
+    WHERE
+       abrg_vbg.vereinbarungs_nr = abrg_bew.vereinbarungs_nr
+       AND abrg_vbg.auszahlungsjahr = abrg_bew.auszahlungsjahr 
+       AND abrg_vbg.status_abrechnung = abrg_bew.status_abrechnung
+;
--- arp_mjpnl_zahlungslauf/calc_mjpnl_abrechnung_per_vereinbarung.sql
+++ arp_mjpnl_zahlungslauf/calc_mjpnl_abrechnung_per_vereinbarung.sql
@@ -1,3 +1,81 @@
 /* Zusammenzug Zahlungen per Vereinbarung und Datum der Auszahlung */
-/* Entspricht arp_mjpnl_migration/mjpnl_postprocessing_abrechnung_per_vereinbarung.sql und muss allerdings nur die diesjährigen Leistungen berücksichtigen */
-/* Dies soll gemacht werden, wenn Unklarheiten in mjpnl_postprocessing_abrechnung_per_vereinbarung gelöst sind */
+/* Entspricht arp_mjpnl_migration/mjpnl_postprocessing_abrechnung_per_vereinbarung.sql muss allerdings nur die diesjährigen Leistungen berücksichtigen */
+
+INSERT INTO ${DB_Schema_MJPNL}.mjpnl_abrechnung_per_vereinbarung 
+  (t_basket, vereinbarungs_nr, gelan_pid_gelan, gelan_bewe_id, gb_nr, flurnamen, gemeinde, flaeche, anzahl_baeume, betrag_flaeche, betrag_baeume, betrag_pauschal, gesamtbetrag,
+   auszahlungsjahr, status_abrechnung, datum_abrechnung, bewirtschaftabmachung_schnittzeitpunkt_1, bewirtschaftabmachung_messerbalkenmaehgeraet, bewirtschaftabmachung_herbstweide,abrechnungperbewirtschafter, vereinbarung, migriert)
+SELECT
+  vbg.t_basket,
+  vbg.vereinbarungs_nr,
+  vbg.gelan_pid_gelan,
+  vbg.gelan_bewe_id,
+  array_to_string(vbg.gb_nr,',') AS gb_nr,
+  array_to_string(vbg.flurname,',') AS flurnamen,
+  array_to_string(vbg.gemeinde,',') AS gemeinde,
+  vbg.flaeche,
+  COALESCE(SUM(lstg_stueck.anzahl_einheiten),0) AS anzahl_baeume,
+  COALESCE(SUM(lstg_ha.betrag_total),0) AS betrag_flaeche,
+  COALESCE(SUM(lstg_stueck.betrag_total),0) AS betrag_baeume,
+  COALESCE(SUM(lstg_pauschal.betrag_total),0) AS betrag_pauschal,
+  COALESCE(SUM(lstg.betrag_total),0) AS gesamtbetrag,
+  lstg.auszahlungsjahr,
+  -- wenn es ein status_abrechnung "freigegeben" gibt, dann soll der status "freigegeben" sein
+  CASE WHEN (SELECT COUNT(*) FROM ${DB_Schema_MJPNL}.mjpnl_abrechnung_per_leistung l WHERE l.status_abrechnung = 'freigegeben' AND l.vereinbarung = vbg.t_id) > 0 
+   THEN 'freigegeben' 
+   -- ansonsten ist es für alle gleich ("ausbezahlt" oder "intern_verrechnet")
+   ELSE MAX(lstg.status_abrechnung) 
+  END AS status_abrechnung,
+  -- wenn es ein status_abrechnung "freigegeben" gibt, dann soll es noch kein datum_abrechnung haben
+  CASE WHEN (SELECT COUNT(*) FROM ${DB_Schema_MJPNL}.mjpnl_abrechnung_per_leistung l WHERE l.status_abrechnung = 'freigegeben' AND l.vereinbarung = vbg.t_id) > 0 
+   THEN NULL
+   -- ansonsten kann es das späteste datum nehmen
+   ELSE MAX(lstg.datum_abrechnung) 
+  END AS datum_abrechnung,
+  bw.bewirtschaftabmachung_schnittzeitpunkt_1,
+  COALESCE(bw.bewirtschaftabmachung_messerbalkenmaehgeraet, FALSE) as bewirtschaftabmachung_messerbalkenmaehgeraet,
+  COALESCE(bw.bewirtschaftabmachung_herbstweide, FALSE) as bewirtschaftabmachung_herbstweide,
+  9999999 AS abrechnungperbewirtschafter,
+  vbg.t_id AS vereinbarung,
+  FALSE AS migriert
+FROM
+  -- Werte der einzelnen Leistungen
+  ${DB_Schema_MJPNL}.mjpnl_abrechnung_per_leistung lstg
+  -- Werte der Vereinbarung
+  LEFT JOIN ${DB_Schema_MJPNL}.mjpnl_vereinbarung vbg
+     ON lstg.vereinbarung = vbg.t_id
+  -- Werte der Beurteilung Wiese
+  LEFT JOIN ${DB_Schema_MJPNL}.mjpnl_beurteilung_wiese bw
+     ON lstg.vereinbarung = bw.vereinbarung
+     -- berücksichtige nur die neusten (sofern mehrere existieren)
+     AND bw.beurteilungsdatum = (SELECT MAX(beurteilungsdatum) FROM ${DB_Schema_MJPNL}.mjpnl_beurteilung_wiese b WHERE b.vereinbarung = lstg.vereinbarung)
+  LEFT JOIN ${DB_Schema_MJPNL}.mjpnl_abrechnung_per_leistung lstg_stueck
+     ON lstg_stueck.t_id = lstg.t_id AND lstg_stueck.abgeltungsart = 'per_stueck'
+  LEFT JOIN ${DB_Schema_MJPNL}.mjpnl_abrechnung_per_leistung lstg_ha
+     ON lstg_ha.t_id = lstg.t_id AND lstg_ha.abgeltungsart = 'per_ha'
+  LEFT JOIN ${DB_Schema_MJPNL}.mjpnl_abrechnung_per_leistung lstg_pauschal
+     ON lstg_pauschal.t_id = lstg.t_id AND lstg_pauschal.abgeltungsart = 'pauschal'
+  WHERE
+    vbg.t_id IS NOT NULL
+    -- berücksichtige nur relevante status
+    AND lstg.status_abrechnung NOT IN ('in_bearbeitung', 'abgeltungslos')
+    -- berücksichtige nur diesjährige Leistungen
+    AND lstg.auszahlungsjahr = date_part('year', now())::integer;
+  GROUP BY vbg.t_id, vbg.vereinbarungs_nr, vbg.gelan_bewe_id, vbg.gb_nr, vbg.flurname, vbg.gemeinde, vbg.flaeche,
+           lstg.auszahlungsjahr, bw.bewirtschaftabmachung_schnittzeitpunkt_1, bw.bewirtschaftabmachung_messerbalkenmaehgeraet, bw.bewirtschaftabmachung_herbstweide
+  ORDER BY  vbg.vereinbarungs_nr ASC
+  ;
+  
+/* fkey update abrechnung_per_leistung zu abrechung_per_vereinbarung */  
+UPDATE ${DB_Schema_MJPNL}.mjpnl_abrechnung_per_leistung abr_lstg
+SET abrechnungpervereinbarung=abr_vbg.t_id
+FROM
+   ${DB_Schema_MJPNL}.mjpnl_vereinbarung vbg,
+   ${DB_Schema_MJPNL}.mjpnl_abrechnung_per_vereinbarung abr_vbg
+WHERE 
+   abr_lstg.vereinbarung != 9999999
+   AND vbg.t_id = abr_lstg.vereinbarung 
+   AND vbg.vereinbarungs_nr = abr_vbg.vereinbarungs_nr
+   AND abr_lstg.auszahlungsjahr = date_part('year', now())::integer
+   AND abr_vbg.auszahlungsjahr = date_part('year', now())::integer
+;
+
