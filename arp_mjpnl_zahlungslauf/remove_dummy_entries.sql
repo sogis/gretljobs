241d1ca6c36508e5febb18060eaa31d6af854e47
signedav <david@opengis.ch>
2023-08-11 13:27:18 +0200

    Revert "[arp_mjpnl_zahlungslauf] Dummie Entries zur sicherstellung des FK constraints"
    This reverts commit 0bb84b7186ea9fcbd921c5832ace6ffcc05f19e2.

--- arp_mjpnl_zahlungslauf/remove_dummy_entries.sql
+++
@@ -1,5 +0,0 @@
-/* Dummy-Entry Abrechnung per Vereinbarung */
-DELETE FROM ${DB_Schema_MJPNL}.mjpnl_abrechnung_per_vereinbarung WHERE t_id = 9999999;
-
-/* Dummy-Entry Abrechnung per Bewirtschafter */
-DELETE FROM ${DB_Schema_MJPNL}.mjpnl_abrechnung_per_bewirtschafter WHERE t_id = 9999999;
