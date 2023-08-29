12a0b2f8ebab666bcd4f5a44fdace3ba958cecf7
Martin Schweizer <martin.schweizer@bd.so.ch>
2023-08-29 09:23:15 +0200

    [awjf_statische_waldgrenzen_export_ai] Neues Modell Version 1.2

--- awjf_statische_waldgrenzen_export_ai/awjf_statische_waldgrenzen_mgdm.sql
+++ awjf_statische_waldgrenzen_export_ai/awjf_statische_waldgrenzen_mgdm.sql
@@ -8,12 +8,23 @@ WITH waldgrenzen AS
         waldgrenze.rechtsstatus = 'inKraft'
 )
 ,
+basket AS 
+(
+    select 
+        t_id
+    from 
+        awjf_statische_waldgrenze_mgdm_v1.t_ili2db_basket 
+    where 
+        topic = 'Waldgrenzen_V1_2.Geobasisdaten'
+)
+,
 geobasisdaten_typ AS 
 (
     INSERT INTO 
-        awjf_statische_waldgrenzen_mgdm.geobasisdaten_typ 
+        awjf_statische_waldgrenze_mgdm_v1.geobasisdaten_typ 
         (
             t_id, 
+            t_basket, 
             acode,
             bezeichnung,
             abkuerzung,
@@ -23,7 +34,8 @@ geobasisdaten_typ AS
         )
     SELECT 
         DISTINCT ON (typ.t_id)
-        typ.t_id,
+        typ.t_id as t_id,
+        basket.t_id as t_basket,
         CASE 
             WHEN typ.art = 'Nutzungsplanung_in_Bauzonen' THEN 'in_BZ'
             ELSE 'ausserh_BZ'
@@ -41,16 +53,21 @@ geobasisdaten_typ AS
         END AS art
         
     FROM 
-        waldgrenzen 
-        LEFT JOIN awjf_statische_waldgrenze.geobasisdaten_typ AS typ
-        ON typ.t_id = waldgrenzen.waldgrenze_typ
+        waldgrenzen
+        LEFT JOIN 
+            awjf_statische_waldgrenze.geobasisdaten_typ AS typ
+            ON 
+            typ.t_id = waldgrenze_typ, 
+        basket
     WHERE 
     verbindlichkeit = 'Nutzungsplanfestlegung' OR verbindlichkeit = 'orientierend'
 )
+
 INSERT INTO 
-    awjf_statische_waldgrenzen_mgdm.geobasisdaten_waldgrenze_linie 
+    awjf_statische_waldgrenze_mgdm_v1.geobasisdaten_waldgrenze_linie 
     (
         t_id,
+        t_basket,
         geometrie,
         rechtsstatus,
         publiziertab,
@@ -58,12 +75,120 @@ INSERT INTO
         wg
     )
     SELECT 
-        t_id,
+        waldgrenzen.t_id as t_id,
+        basket.t_id as t_basket,
         geometrie,
         rechtsstatus,
         publiziert_ab,
         bemerkungen,
         waldgrenze_typ
     FROM 
-        waldgrenzen 
+        waldgrenzen, 
+        basket
+;
+
+-- DOKUMENTE
+with basket AS 
+(
+    select 
+        t_id
+    from 
+        awjf_statische_waldgrenze_mgdm_v1.t_ili2db_basket 
+    where 
+        topic = 'Waldgrenzen_V1_2.Rechtsvorschriften'
+)
+
+INSERT INTO awjf_statische_waldgrenze_mgdm_v1.rechtsvorschrften_dokument 
+    ( 
+        t_id,
+        t_basket,
+        typ,
+        titel,
+        abkuerzung, 
+        offiziellenr,
+        auszugindex,
+        rechtsstatus,
+        publiziertab
+    )
+SELECT
+    dokument.t_id,
+    basket.t_id AS t_basket, 
+    'Rechtsvorschrift' AS typ, 
+    offiziellertitel AS titel,
+    abkuerzung, 
+    offiziellenr,
+    CASE WHEN typ = 'RRB' THEN 999 ELSE 998 END AS auszugindex, 
+    rechtsstatus,
+    publiziert_ab
+FROM 
+    awjf_statische_waldgrenze.dokumente_dokument dokument, 
+    basket
+;
+
+with basket AS 
+(
+    select 
+        t_id
+    from 
+        awjf_statische_waldgrenze_mgdm_v1.t_ili2db_basket 
+    where 
+        topic = 'Waldgrenzen_V1_2.Rechtsvorschriften'
+)
+,
+multilingualuri AS
+(
+    INSERT INTO
+        awjf_statische_waldgrenze_mgdm_v1.multilingualuri
+        (
+            t_id,
+            t_basket,
+            t_seq,
+            rechtsvrschrftn_dkment_textimweb
+        )
+    SELECT
+        nextval('awjf_statische_waldgrenze_mgdm_v1.t_ili2db_seq'::regclass) AS t_id,
+        basket.t_id AS t_basket,
+        0 AS t_seq,
+        rechtsvorschrften_dokument.t_id AS rechtsvrschrftn_dkment_textimweb
+    FROM
+        awjf_statische_waldgrenze.dokumente_dokument AS rechtsvorschrften_dokument,
+        basket 
+    RETURNING *
+),
+
+localiseduri AS 
+(
+    SELECT 
+        nextval('awjf_statische_waldgrenze_mgdm_v1.t_ili2db_seq'::regclass) AS t_id,
+        basket.t_id AS t_basket,       
+        0 AS t_seq,
+        'de' AS alanguage,
+        text_im_web AS atext,
+        multilingualuri.t_id AS multilingualuri_localisedtext
+    FROM
+        awjf_statische_waldgrenze.dokumente_dokument AS rechtsvorschrften_dokument
+        RIGHT JOIN multilingualuri 
+        ON multilingualuri.rechtsvrschrftn_dkment_textimweb = rechtsvorschrften_dokument.t_id,
+        basket
+)
+
+INSERT INTO
+    awjf_statische_waldgrenze_mgdm_v1.localiseduri
+    (
+        t_id,
+        t_basket,
+        t_seq,
+        alanguage,
+        atext,
+        multilingualuri_localisedtext
+    )
+    SELECT 
+        t_id,
+        t_basket,
+        t_seq,
+        alanguage,
+        atext, 
+        multilingualuri_localisedtext
+    FROM 
+        localiseduri
 ;
--- awjf_statische_waldgrenzen_export_ai/truncate_all_awjf_statische_waldgrenzen_mgdm_tables.sql
+++ awjf_statische_waldgrenzen_export_ai/truncate_all_awjf_statische_waldgrenzen_mgdm_tables.sql
@@ -1,14 +1,14 @@
-DELETE FROM awjf_statische_waldgrenzen_mgdm.geobasisdaten_typ_dokument
+DELETE FROM awjf_statische_waldgrenzen_mgdm_v1.geobasisdaten_typ_dokument
 ;
-DELETE FROM awjf_statische_waldgrenzen_mgdm.geobasisdaten_waldgrenze_linie 
+DELETE FROM awjf_statische_waldgrenzen_mgdm_v1.geobasisdaten_waldgrenze_linie 
 ;
-DELETE FROM awjf_statische_waldgrenzen_mgdm.geobasisdaten_typ
+DELETE FROM awjf_statische_waldgrenzen_mgdm_v1.geobasisdaten_typ
 ;
-DELETE FROM awjf_statische_waldgrenzen_mgdm.localiseduri
+DELETE FROM awjf_statische_waldgrenzen_mgdm_v1.localiseduri
 ;
-DELETE FROM awjf_statische_waldgrenzen_mgdm.multilingualuri
+DELETE FROM awjf_statische_waldgrenzen_mgdm_v1.multilingualuri
 ;
-DELETE FROM awjf_statische_waldgrenzen_mgdm.rechtsvorschrften_hinweiseweiteredokumente
+DELETE FROM awjf_statische_waldgrenzen_mgdm_v1.geobasisdaten_geometrie_dokument
 ;
-DELETE FROM awjf_statische_waldgrenzen_mgdm.rechtsvorschrften_dokument
+DELETE FROM awjf_statische_waldgrenzen_mgdm_v1.rechtsvorschrften_dokument
 ;
