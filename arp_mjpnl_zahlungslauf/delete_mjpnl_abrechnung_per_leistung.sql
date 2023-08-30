91dde3212506c3ff2b6b7d11e71ca47d8d2c5c1c
signedav <david@opengis.ch>
2023-08-30 13:10:16 +0200

    [arp_mjpnl_zahlungslauf] Setzen des FKs der bestehenden Leistungen auf NULL um cascade delete zu vermeiden

--- arp_mjpnl_zahlungslauf/delete_mjpnl_abrechnung_per_leistung.sql
+++ arp_mjpnl_zahlungslauf/delete_mjpnl_abrechnung_per_leistung.sql
@@ -1,5 +1,13 @@
-DELETE FROM 
-    ${DB_Schema_MJPNL}.mjpnl_abrechnung_per_leistung 
+-- wir löschen alle diesjährigen Leitungen (ausser die einmaligen)
+DELETE FROM
+    ${DB_Schema_MJPNL}.mjpnl_abrechnung_per_leistung
 WHERE 
     auszahlungsjahr = ${AUSZAHLUNGSJAHR}::integer
     AND NOT einmalig; -- denn diese wurden manuell erfasst
+    
+-- und alle andern (die einmaligen) möchten wir vom CASCADE delete bewahren, also FK auf NULL setzen
+UPDATE
+    ${DB_Schema_MJPNL}.mjpnl_abrechnung_per_leistung 
+SET abrechnungpervereinbarung = NULL
+WHERE 
+    auszahlungsjahr = ${AUSZAHLUNGSJAHR}::integer;
