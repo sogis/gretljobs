52954425a625b0f4cabd7fad7422260c0888c662
signedav <david@opengis.ch>
2023-09-25 15:43:45 +0200

    [arp_mjpnl_migration] Auch Leistungen von Feuchtgebieten müssen migriert werden.

--- arp_mjpnl_migration/mjpnl_leistung.sql
+++ arp_mjpnl_migration/mjpnl_leistung.sql
@@ -67,7 +67,6 @@ WHERE
    AND vbggeom.wkb_geometry IS NOT NULL
    AND ST_IsValid(vbggeom.wkb_geometry)
    AND Round((ST_Area(vbggeom.wkb_geometry) / 10000)::NUMERIC,2) > 0 --IGNORE small OR emptry geometries
-   AND NOT (vbartvb.bez = 'Heumatten' AND flart.bez = 'Feuchtgebiet')
    AND NOT (vbartvb.bez = 'Waldränder' AND flart.bez = 'Waldrand')
    AND NOT (vbartvb.bez = 'Waldreservate' AND flart.bez = 'Waldreservat')
    -- AND l.betrag > 0
