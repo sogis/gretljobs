832fe364cb0a3309c6f1aa6785c1bf0d8ee33ba3
barpasen <peter.senn@bd.so.ch>
2023-10-04 11:08:11 +0200

    [afu_naturereigniskataster_mgdm_import] initial version

---
+++ afu_naturereigniskataster_mgdm_import/prozessraum_rutschung_pub.sql
@@ -0,0 +1,14 @@
+SELECT
+    pr_r_prozess AS teilprozess_rutschung,
+    pr_geometrie AS geometrie,
+    bi_storme_nr AS storme_nr,
+    prozessraum.pr_evidenz AS evidenz,
+    bi_massgebender_prozess AS massgebender_hauptprozess,
+    bi_datenherr AS datenherr
+FROM
+    afu_naturereigniskataster_mgdm_v1.storme_mgdm_prozessraum_r AS prozessraum
+    INNER JOIN afu_naturereigniskataster_mgdm_v1.storme_mgdm_basisinformation AS basisinformation
+      ON prozessraum.bi = basisinformation.t_id AND substring(bi_storme_nr, 4, 4)::int >= 2019
+    LEFT JOIN afu_naturereigniskataster_mgdm_v1.storme_mgdm_detailinformation_r AS detailinformation
+      ON prozessraum.di_storme_mgdm_detailinformation_r = detailinformation.t_id
+;
