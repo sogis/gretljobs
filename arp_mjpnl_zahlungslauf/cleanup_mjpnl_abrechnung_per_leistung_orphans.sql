6787637088f1ab1d4aa6380cbf38caf4afa979a7
signedav <david@opengis.ch>
2023-10-13 10:27:49 +0200

    [arp_mjpnl_zahlungslauf] Löschen von diesjährigen Leistungen ohne Vereinbarung (die wahrscheinlich manuell gelöscht wurde). Dies ist wichtig, weil Leistungen nicht CASCADE verknüpft sind.

---
+++ arp_mjpnl_zahlungslauf/cleanup_mjpnl_abrechnung_per_leistung_orphans.sql
@@ -0,0 +1,11 @@
+-- wir löschen alle diesjährigen Leitungen ohne Vereinbarung
+-- denn die Leistungen sind nicht über CASCADE abhängig von Vereinbarungen und bleiben so auch bei manuellem Löschen einer Vereinbarung bestehen
+DELETE FROM
+    ${DB_Schema_MJPNL}.mjpnl_abrechnung_per_leistung
+WHERE 
+    auszahlungsjahr = ${AUSZAHLUNGSJAHR}::integer
+    AND (
+        -- wenn NULL oder nicht in vereinbarungen
+        vereinbarung IS NULL
+        OR vereinbarung NOT IN (SELECT t_id FROM ${DB_Schema_MJPNL}.mjpnl_vereinbarung)
+    )
