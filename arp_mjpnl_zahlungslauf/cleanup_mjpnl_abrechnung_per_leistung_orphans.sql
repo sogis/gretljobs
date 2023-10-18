37b5237fa5da3cbddf265c61d1012e7adc985aed
signedav <david@opengis.ch>
2023-10-18 14:55:35 +0200

    [arp_mjpnl_zahlungslauf] Bessere Kommentare

--- arp_mjpnl_zahlungslauf/cleanup_mjpnl_abrechnung_per_leistung_orphans.sql
+++ arp_mjpnl_zahlungslauf/cleanup_mjpnl_abrechnung_per_leistung_orphans.sql
@@ -1,5 +1,6 @@
 -- wir löschen alle diesjährigen Leitungen ohne Vereinbarung
 -- denn die Leistungen sind nicht über CASCADE abhängig von Vereinbarungen und bleiben so auch bei manuellem Löschen einer Vereinbarung bestehen
+-- eigentlich wird zwar eine Vereinbarung nicht gelöscht, sondern auf inaktiv gesetzt, doch es kann dennoch der Fall sein (e.g. Bereinigung nach Migration)
 DELETE FROM
     ${DB_Schema_MJPNL}.mjpnl_abrechnung_per_leistung
 WHERE 
