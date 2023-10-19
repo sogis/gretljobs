1695fed33f98b0c6c459558782cc123b87ed02c9
dva999 <49029487+dva999@users.noreply.github.com>
2023-10-19 15:11:02 +0200

    Update afu_wasserbewirtschaftung_quellen_pub.sql
    Anpassung an neue Datenmodelle GWO und WVO

--- afu_wasserbewirtschaftung_pub/afu_wasserbewirtschaftung_quellen_pub.sql
+++ afu_wasserbewirtschaftung_pub/afu_wasserbewirtschaftung_quellen_pub.sql
@@ -1,84 +1,37 @@
 (SELECT 
-    TRUE AS gefasst,
-    b.besitzer AS eigentuemer, 
-    a.min_schuettung, 
-    a.max_schuettung,
-    coalesce(a.schutzzone,FALSE) AS schutzzone, 
-    CASE 
-        WHEN a.nutzungsart_schutzzone = 3 
-        THEN 'privat'
-        WHEN a.nutzungsart_schutzzone = 2 
-        THEN 'privat_oeffentliches_Interesse'
-        WHEN a.nutzungsart_schutzzone = 1 
-        THEN 'oeffentlich'
-    END AS nutzungstyp, 
-    CASE 
-        WHEN a.verwendung = 1 
-        THEN 'Trinkwasser' 
-        WHEN a.verwendung = 2 
-        THEN 'Brauchwasser'
-        WHEN a.verwendung = 3
-        THEN 'Notbrunnen'
-    END AS verwendungszweck,
-    CASE 
-        WHEN a.nutzungsart_schutzzone = 3
-        THEN 'Gefasste Quelle mit privater Nutzung'
-        WHEN a.nutzungsart_schutzzone = 2
-        THEN 'Gefasste Quelle mit privater Nutzung von öffentlichem Interesse'
-        WHEN a.nutzungsart_schutzzone = 1
-        THEN 'Gefasste Quelle für die öffentliche Wasserversorgung'
-        WHEN a.nutzungsart_schutzzone IS NULL OR a.nutzungsart_schutzzone > 3 
-        THEN 'Gefasste Quelle'
-    END AS objekttyp_anzeige,
-    a.bezeichnung AS objektname, 
-    a.mobj_id AS objektnummer,
-    a.beschreibung AS technische_angabe,
-    a.bemerkung AS bemerkung,
-    array_to_json(c.dokumente) AS dokumente, 
-    a.wkb_geometry AS geometrie
-FROM 
-    vegas.obj_quelle_gefasst a 
+	TRUE AS gefasst,
+	NULL AS eigentuemer, 
+	qg.minimale_schuettung AS min_schuettung, 
+	qg.maximale_schuettung AS max_schuettung,
+	qg.schutzzone,
+	CASE 
+		WHEN qg.nutzungsart = 'Private_Fassung' THEN 'privat'
+		WHEN qg.nutzungsart = 'Private_Fasung_von_oeffentlichem_Interesse' THEN 'privat_oeffentliches_Interesse'
+		WHEN qg.nutzungsart = 'Private_Fassung_von_oeffentlichem_Interesse' THEN 'privat_oeffentliches_Interesse'
+		WHEN qg.nutzungsart = 'Oeffentliche_Fassung' THEN 'oeffentlich'
+	END AS nutzungstyp,
+	verwendung AS verwendungszweck,
+	CASE
+		WHEN qg.nutzungsart = 'Private_Fassung' THEN 'Gefasste Quelle mit privater Nutzung'
+		WHEN qg.nutzungsart = 'Private_Fassung_von_oeffentlichem_Interesse' THEN 'Gefasste Quelle mit privater Nutzung von öffentlichem Interesse'
+		WHEN qg.nutzungsart = 'Private_Fasung_von_oeffentlichem_Interesse' THEN 'Gefasste Quelle mit privater Nutzung von öffentlichem Interesse'
+		WHEN qg.nutzungsart = 'Oeffentliche_Fassung' THEN 'Gefasste Quelle für die öffentliche Wasserversorgung' 
+		WHEN qg.nutzungsart = 'keine Angabe' THEN 'Gefasste Quelle'
+	END AS objekttyp_anzeige,
+	qg.bezeichnung AS objektname, 
+	qg.objekt_id AS objektnummer,
+	qg.beschreibung AS technische_angabe,
+	qg.bemerkung,
+	array_to_json(dokumente.dokumente) AS dokumente, 
+	qg.geometrie
+FROM afu_wasserversorg_obj_v1.quelle_gefasst qg
 LEFT JOIN 
-    gaso.gso_mobj b ON a.mobj_id = b.mobj_id
-LEFT JOIN 
-    (SELECT 
-         array_agg('https://geo.so.ch/docs/ch.so.afu.grundwasserbewirtschaftung/'||y.vegas_id||'_'||x.dokument_id||'.'||x.dateiendung) AS dokumente, 
-         y.vegas_id
-     FROM 
-         vegas.adm_dokument x, 
-         vegas.adm_objekt_dokument y 
-     WHERE x.dokument_id = y.dokument_id
-     GROUP BY y.vegas_id) c ON a.vegas_id = c.vegas_id
-WHERE a.archive = 0
-)
-UNION ALL 
-(SELECT 
-    FALSE AS gefasst, 
-    b.besitzer AS eigentuemer, 
-    a.min_schuettung, 
-    a.max_schuettung, 
-    coalesce(a.schutzzone, FALSE) AS schutzzone,
-    NULL AS nutzungstyp, --Die nicht gefassten Quellen werden logischerweise auch nicht genutzt. 
-    NULL AS verwendungszweck, --Nicht gefasste Quellen werden nicht verwendet. 
-    'ungefasste Quelle' AS objekttyp_anzeige,
-    a.bezeichnung AS objektname, 
-    a.mobj_id AS objektnummer, 
-    a.beschreibung AS technische_angabe,
-    a.bemerkung AS bemerkung, 
-    array_to_json(c.dokumente) AS dokumente, 
-    a.wkb_geometry AS geometrie
- FROM 
-    vegas.obj_quelle a
- LEFT JOIN 
-    gaso.gso_mobj b ON a.mobj_id = b.mobj_id
-LEFT JOIN 
-    (SELECT 
-         array_agg('https://geo.so.ch/docs/ch.so.afu.grundwasserbewirtschaftung/'||y.vegas_id||'_'||x.dokument_id||'.'||x.dateiendung) AS dokumente, 
-         y.vegas_id
-     FROM 
-         vegas.adm_dokument x, 
-         vegas.adm_objekt_dokument y 
-     WHERE x.dokument_id = y.dokument_id
-     GROUP BY y.vegas_id) c ON a.vegas_id = c.vegas_id
-WHERE a.archive = 0
-);
+	(SELECT
+		qgd.quelle_gefasst_r,
+		array_agg(REPLACE(d.dateiname,'G:\documents\ch.so.afu.wasserversorgung\','https://geo.so.ch/docs/ch.so.afu.wasserversorgung/')) AS dokumente
+	FROM 
+		afu_wasserversorg_obj_v1.quelle_gefasst__dokument qgd, 
+		afu_wasserversorg_obj_v1.dokument d 
+	WHERE qgd.dokument_r = d.t_id
+	GROUP BY qgd.quelle_gefasst_r) dokumente ON qg.t_id = dokumente.quelle_gefasst_r)
+;
