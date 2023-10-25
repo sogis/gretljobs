3a8ec5e5039d4f7f6c991e476f4e7369243845fb
barpasen <peter.senn@bd.so.ch>
2023-10-25 11:30:18 +0200

    [afu_stehende_gewaesser_pub] initial version

---
+++ afu_stehende_gewaesser_pub/afu_av_gewaesser.sql
@@ -0,0 +1,15 @@
+SELECT 
+    aname,
+    bodenbedeckung.geometrie,
+    typ,
+    gemeindename,
+    erhebung_abgeschlossen,
+    av_geometrie,
+    av_link
+FROM 
+    afu_stehende_gewaesser_v1.stehendes_gewaesser AS stehendes_gewaesser
+    LEFT JOIN agi_dm01avso24.bodenbedeckung_boflaeche AS bodenbedeckung
+    ON art = 'Gewaesser.stehendes' AND ST_Within(ST_PointOnSurface(stehendes_gewaesser.geometrie), bodenbedeckung.geometrie)
+WHERE
+    av_geometrie IS TRUE
+;
