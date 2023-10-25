f7b8f1bd2ce29ceca814362dce35c66b1d5481b0
barpasen <peter.senn@bd.so.ch>
2023-10-25 14:05:41 +0200

    [afu_stehende_gewaesser_pub] initial version

--- afu_stehende_gewaesser_pub/afu_stehendes_gewaesser_pub.sql
+++ afu_stehende_gewaesser_pub/afu_stehendes_gewaesser_pub.sql
@@ -10,4 +10,20 @@ FROM
     afu_stehende_gewaesser_v1.stehendes_gewaesser
 WHERE
     av_geometrie IS FALSE
+AND
+    av_link IS FALSE
+
+UNION
+    
+SELECT 
+    aname,
+    geometrie,
+    typ,
+    gemeindename,
+    erhebung_abgeschlossen,
+    av_geometrie,
+    av_link
+FROM 
+    afu_stehende_gewaesser_v1.av_gewaesser
 ;
+
