eacfa274e7e0460c77d340c67beb49e4eeac673f
barpasen <peter.senn@bd.so.ch>
2023-10-25 14:55:28 +0200

    [afu_stehende_gewaesser_abgleich] initial version

---
+++ afu_stehende_gewaesser_abgleich/update_gemeindename.sql
@@ -0,0 +1,13 @@
+UPDATE
+    afu_stehende_gewaesser_v1.stehendes_gewaesser
+SET
+    gemeindename = gemeindegrenze.gemeindename
+        FROM
+            agi_hoheitsgrenzen_pub.hoheitsgrenzen_gemeindegrenze AS gemeindegrenze
+        WHERE
+            ST_Contains(gemeindegrenze.geometrie, stehendes_gewaesser.geometrie)
+        AND
+            av_geometrie IS TRUE
+        AND
+            av_link IS TRUE
+;
