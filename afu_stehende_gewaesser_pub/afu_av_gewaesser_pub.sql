2020b234839d1091bba0257d2e279389eabb0c0a
barpasen <peter.senn@bd.so.ch>
2023-10-25 11:30:18 +0200

    [afu_stehende_gewaesser_pub] initial version

--- afu_stehende_gewaesser_pub/afu_av_gewaesser_pub.sql
+++ afu_stehende_gewaesser_pub/afu_av_gewaesser_pub.sql
@@ -1,11 +1,11 @@
 SELECT 
     aname,
-    bodenbedeckung_boflaeche.geometrie,
+    geometrie,
     typ,
     gemeindename,
-    erhebung_abgeschlossen
+    erhebung_abgeschlossen,
+    av_geometrie,
+    av_link
 FROM 
     afu_stehende_gewaesser_v1.av_gewaesser
-    INNER JOIN agi_dm01avso24.bodenbedeckung_boflaeche
-    ON art = 'Gewaesser.stehendes' AND ST_Within(av_gewaesser.geometrie, bodenbedeckung_boflaeche.geometrie)
 ;
--- afu_stehende_gewaesser_pub/afu_stehendes_gewaesser_pub.sql
+++ afu_stehende_gewaesser_pub/afu_stehendes_gewaesser_pub.sql
@@ -3,7 +3,11 @@ SELECT
     geometrie,
     typ,
     gemeindename,
-    erhebung_abgeschlossen
+    erhebung_abgeschlossen,
+    av_geometrie,
+    av_link
 FROM 
     afu_stehende_gewaesser_v1.stehendes_gewaesser
+WHERE
+    av_geometrie IS FALSE
 ;
