1cbc30fff4b29fbcc8dd81400f05095f91dec23f
barpasen <peter.senn@bd.so.ch>
2023-11-20 14:26:36 +0100

    [afu_stehende_gewaesser_abgleich] initial version

--- afu_stehende_gewaesser_abgleich/unlink_av_gewaesser.sql
+++ afu_stehende_gewaesser_abgleich/unlink_av_gewaesser.sql
@@ -1,34 +1,15 @@
-WITH av_gewaesser AS (
-    SELECT
-        ST_Buffer(ST_PointOnSurface(geometrie), 5) AS geometrie_av
-    FROM
-        agi_dm01avso24.bodenbedeckung_boflaeche   
-    WHERE
-       art = 'Gewaesser.stehendes'
-),
-not_av_gewaesser AS (
-    SELECT
-        t_id
-    FROM
-        afu_stehende_gewaesser_v1.stehendes_gewaesser
-        LEFT JOIN av_gewaesser
-          ON ST_Contains(av_gewaesser.geometrie_av, stehendes_gewaesser.geometrie)
-    WHERE
-        av_gewaesser.geometrie_av IS NULL
-    AND
-        av_geometrie IS TRUE
-)
 DELETE FROM
-    afu_stehende_gewaesser_v1.stehendes_gewaesser AS stehendes_gewaesser
+    afu_stehende_gewaesser_v1.stehendes_gewaesser
 WHERE
-    stehendes_gewaesser.t_id IN (
+    stehendes_gewaesser.t_id NOT IN (
         SELECT
-            t_id 
+            stehendes_gewaesser.t_id
         FROM
-            not_av_gewaesser
-    )  
+            afu_stehende_gewaesser_v1.stehendes_gewaesser AS stehendes_gewaesser,
+            agi_dm01avso24.bodenbedeckung_boflaeche AS bodenflaeche
+        WHERE
+            ST_Within(ST_PointOnSurface(stehendes_gewaesser.geometrie), bodenflaeche.geometrie)
+    )
 AND
-    stehendes_gewaesser.av_geometrie IS TRUE
-AND
-    stehendes_gewaesser.av_link IS FALSE
+    av_geometrie IS TRUE
 ;
