e11b719325bd97bfb90c0c5b08593f2b6c75bc2d
Martin Schweizer <martin.schweizer@bd.so.ch>
2023-12-08 15:57:29 +0100

    [afu_naturgefahren_freigeben]Neuer Job afu_naturgefahren_freigeben

---
+++ afu_naturgefahren_freigeben/freigeben.sql
@@ -0,0 +1,71 @@
+-- Naturgefahren
+with dataset_id as (
+    select
+        t_id 
+    from 
+        afu_naturgefahren_v1.t_ili2db_dataset 
+    where 
+        datasetname = ${kennung}
+),
+
+main_dataset as (
+    select 
+        t_id 
+    from 
+        afu_naturgefahren_v1.t_ili2db_dataset
+    where 
+        datasetname = 'main'
+)
+
+update 
+    afu_naturgefahren_v1.t_ili2db_basket 
+set 
+    dataset = main_dataset.t_id 
+from 
+    dataset_id, 
+    main_dataset 
+where 
+    dataset = dataset_id.t_id
+;
+
+delete from 
+    afu_naturgefahren_v1.t_ili2db_dataset
+where 
+    datasetname = ${kennung}
+;
+
+--Beurteilungsgebiet
+with dataset_id as (
+    select
+        t_id 
+    from 
+        afu_naturgefahren_beurteilungsgebiet_v1.t_ili2db_dataset 
+    where 
+        datasetname = ${kennung}
+),
+
+main_dataset as (
+    select 
+        t_id 
+    from 
+        afu_naturgefahren_beurteilungsgebiet_v1.t_ili2db_dataset
+    where 
+        datasetname = 'main'
+)
+
+update 
+    afu_naturgefahren_beurteilungsgebiet_v1.t_ili2db_basket 
+set 
+    dataset = main_dataset.t_id 
+from 
+    dataset_id, 
+    main_dataset 
+where 
+    dataset = dataset_id.t_id
+;
+
+delete from 
+    afu_naturgefahren_beurteilungsgebiet_v1.t_ili2db_dataset
+where 
+    datasetname = ${kennung}
+;
