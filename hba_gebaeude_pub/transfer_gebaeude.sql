b10ad20a3413dad429a0085d5871746580241949
Stefan Ziegler <edi.gonzales@gmail.com>
2024-01-11 15:28:51 +0100

    [HBA_SO_Gebaeude] update model and data

--- hba_gebaeude_pub/transfer_gebaeude.sql
+++ hba_gebaeude_pub/transfer_gebaeude.sql
@@ -27,59 +27,35 @@ WITH av_gebaeude AS
 )
 SELECT 
     egid, 
-    CASE 
-        WHEN energietraeger = 'Pellet' THEN 'Pellet'
-        WHEN energietraeger = 'Grundwasser' THEN 'Grundwasser'
-        WHEN energietraeger = 'Erdgas' THEN 'Erdgas'
-        WHEN energietraeger = 'Holzschnitzel' THEN 'Holzschnitzel'
-        WHEN energietraeger = 'Heizöl' THEN 'Heizoel'
-        WHEN energietraeger = 'Luft' THEN 'Luft'
-        WHEN energietraeger = 'Fernwärme' THEN 'Fernwaerme'
-        WHEN energietraeger = 'Erdwärme' THEN 'Erdwaerme'
-        WHEN energietraeger = 'Elektro' THEN 'Elektro'
-        WHEN energietraeger = '-' THEN 'nicht_beheizt'
-        WHEN energietraeger IS NULL THEN 'unbekannt'
-    END AS energietraeger,
-    CASE 
-        WHEN energietraeger = 'Pellet' THEN 'Pellet'
-        WHEN energietraeger = 'Grundwasser' THEN 'Grundwasser'
-        WHEN energietraeger = 'Erdgas' THEN 'Erdgas'
-        WHEN energietraeger = 'Holzschnitzel' THEN 'Holzschnitzel'
-        WHEN energietraeger = 'Heizöl' THEN 'Heizöl'
-        WHEN energietraeger = 'Luft' THEN 'Luft'
-        WHEN energietraeger = 'Fernwärme' THEN 'Fernwärme'
-        WHEN energietraeger = 'Erdwärme' THEN 'Erdwärme'
-        WHEN energietraeger = 'Elektro' THEN 'Elektro'
-        WHEN energietraeger = '-' THEN 'nicht beheizt'
-        WHEN energietraeger IS NULL THEN 'unbekannt'
-    END AS energietraeger_txt,
-    CASE 
-        WHEN nutzungsart = 'Finanzvermögen' THEN 'Finanzvermoegen'
-        WHEN nutzungsart = 'Anmietobjekte' THEN 'Anmietobjekte'
-        WHEN nutzungsart = 'Verwaltungsvermögen' THEN 'Verwaltungsvermoegen'
-        WHEN nutzungsart = 'Stiftungsvermögen' THEN 'Stiftungsvermoegen'
-    END AS nutzungsart,
-    CASE 
-        WHEN nutzungsart = 'Finanzvermögen' THEN 'Finanzvermögen'
-        WHEN nutzungsart = 'Anmietobjekte' THEN 'Anmietobjekte'
-        WHEN nutzungsart = 'Verwaltungsvermögen' THEN 'Verwaltungsvermögen'
-        WHEN nutzungsart = 'Stiftungsvermögen' THEN 'Stiftungsvermögen'
-    END AS nutzungsart_txt,
     wirtschaftseinheit,
     geometrie,
-    fahrzeugladestation
+    vermoegensart,
+    CASE 
+        WHEN vermoegensart = 'VV' THEN 'Verwaltungsvermögen'
+        WHEN vermoegensart = 'ALLM' THEN 'Allmend'
+        WHEN vermoegensart = 'SV' THEN 'Stiftungsvermögen'
+        WHEN vermoegensart = 'FV' THEN 'Finanzvermögen'
+        WHEN vermoegensart = 'AM' THEN 'Anmietobjekt'
+    END AS vermoegensart_txt,
+    prio AS prioritaet, 
+    CASE 
+        WHEN prio = 'A' THEN 'betriebsnotwendig'
+        WHEN prio = 'B' THEN 'Nicht betriebsnotwendig, halten, periodische Überprüfung'
+        WHEN prio = 'C' THEN 'Nicht betriebsnotwendig, verwertbar'
+    END AS prioritaet_txt,
+    aktuelle_nutzung
 FROM 
 (
     SELECT DISTINCT ON (gebaeude.t_id)
         gebaeude.t_id,
         gebaeude.egid,
-        gebaeude.energietraeger,
-        gebaeude.nutzungsart,
-        gebaeude.wirtschaftseinheit, 
+        gebaeude.vermoegensart,
+        gebaeude.wirtschaftseinheit,
+        gebaeude.aktuelle_nutzung, 
         av_gebaeude.geometrie,
-        gebaeude.fahrzeugladestation
+        gebaeude.prio
     FROM 
-        hba_gebaeude_v1.gebaeude_gebaeude AS gebaeude
+        hba_gebaeude_v2.gebaeude_gebaeude AS gebaeude
         INNER JOIN av_gebaeude
         ON ST_Intersects(ST_SetSRID(ST_MakePoint(xkoordinaten, ykoordinaten),2056), av_gebaeude.geometrie)
     WHERE 
@@ -94,17 +70,17 @@ FROM
     SELECT DISTINCT ON (gebaeude.t_id)
         gebaeude.t_id,
         gebaeude.egid,
-        gebaeude.energietraeger,
-        gebaeude.nutzungsart,
-        gebaeude.wirtschaftseinheit, 
+        gebaeude.vermoegensart,
+        gebaeude.wirtschaftseinheit,
+        gebaeude.aktuelle_nutzung, 
         av_gebaeude.geometrie,
-        gebaeude.fahrzeugladestation
+        gebaeude.prio
     FROM 
-        hba_gebaeude_v1.gebaeude_gebaeude AS gebaeude
+        hba_gebaeude_v2.gebaeude_gebaeude AS gebaeude
         INNER JOIN av_gebaeude 
         ON CAST(av_gebaeude.gwr_egid AS TEXT) = gebaeude.egid 
     WHERE 
         gebaeude.egid IS NOT NULL 
 )
 AS foo
-    
+;
