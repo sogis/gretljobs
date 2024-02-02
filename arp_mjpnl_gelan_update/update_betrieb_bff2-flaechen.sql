87153a907d04e4ace81a4dfb2c9505696a88a1c1
signedav <david@opengis.ch>
2024-02-02 09:11:01 +0100

    [arp_mjpnl_gelan_update] Fix falsch gesetze Klammer und bff Geometriecheck

--- arp_mjpnl_gelan_update/update_betrieb_bff2-flaechen.sql
+++ arp_mjpnl_gelan_update/update_betrieb_bff2-flaechen.sql
@@ -5,7 +5,10 @@ SET
 FROM 
   ${DB_Schema_MJPNL}.betrbsdttrktrdten_bewirtschaftungseinheit AS bewe 
 WHERE 
+  ST_IsValid(bff.geometrie) = TRUE 
+  AND
   ST_WITHIN(
     ST_PointOnSurface(bff.geometrie), 
     bewe.geometrie
-  );
+  )
+;
