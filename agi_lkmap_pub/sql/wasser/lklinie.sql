2896125533baa42f21be42eef507746ea410f671
Stefan Ziegler <edi.gonzales@gmail.com>
2024-02-29 08:25:51 +0100

    [agi_lkmap_pub] add labels query / fix dataset delete and replace

--- agi_lkmap_pub/sql/wasser/lklinie.sql
+++ agi_lkmap_pub/sql/wasser/lklinie.sql
@@ -1,3 +1,21 @@
+WITH beschriftung AS 
+(
+    SELECT
+        l.t_id,
+        string_agg(DISTINCT t.textinhalt, '|') AS textinhalt
+    FROM 
+        ${DB_SCHEMA_EDIT}.sia405_lkmap_lkobjekt_text AS t
+        LEFT JOIN ${DB_SCHEMA_EDIT}.sia405_lkmap_lklinie AS l 
+        ON t.lkobjektref_sia405_lkmap_lklinie = l.t_id 
+    WHERE 
+        t.lkobjektref_sia405_lkmap_lklinie IS NOT NULL
+        AND 
+        t.t_datasetname = ${DATASET}
+        AND 
+        l.t_datasetname = ${DATASET}
+    GROUP BY 
+        l.t_id 
+)
 SELECT 
     t_ili_tid AS t_ili_tid,
     l.linie AS geometrie,
@@ -12,9 +30,16 @@ SELECT
     l.eigentuemer AS eigentuemer,
     m.datenherr AS datenherr,
     m.datenlieferant AS datenlieferant,
-    m.letzte_aenderung AS letzte_aenderung
+    m.letzte_aenderung AS letzte_aenderung,
+    beschriftung.textinhalt
 FROM 
     ${DB_SCHEMA_EDIT}.sia405_lkmap_lklinie AS l
     LEFT JOIN ${DB_SCHEMA_EDIT}.metaattribute AS m
     ON l.t_id = m.sia405_lkmap_lklinie_metaattribute  
+    LEFT JOIN beschriftung 
+    ON l.t_id = beschriftung.t_id
+WHERE 
+    l.t_datasetname = ${DATASET}
+    AND 
+    m.t_datasetname = ${DATASET}
 ;
