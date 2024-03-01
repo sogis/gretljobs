fc221d4e12ba6f9b32905346314b5be75f3ede41
signedav <david@opengis.ch>
2024-03-01 15:02:37 +0100

    [arp_mjpnl_gelan_update] check kulturfläche für circle Kalkulation

--- arp_mjpnl_gelan_update/mjpnl_update_vereinbarungen_gelan_kulturflaechen.sql
+++ arp_mjpnl_gelan_update/mjpnl_update_vereinbarungen_gelan_kulturflaechen.sql
@@ -4,6 +4,8 @@ SET kultur_id=(
     SELECT array_agg(kf.kultur_id) AS kultur_id
     FROM ${DB_Schema_MJPNL}.betrbsdttrktrdten_kultur_flaechen kf
     WHERE
+        ST_IsValid(kf.geometrie) = TRUE 
+        AND
         ST_Intersects(kf.geometrie,vbg.geometrie)
         AND
         (ST_MaximumInscribedCircle(ST_Intersection(kf.geometrie,vbg.geometrie))).radius > 1
