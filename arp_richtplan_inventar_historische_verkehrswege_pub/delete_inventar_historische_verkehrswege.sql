0207ebaf9e99013f31b28499b69a349211eb2f2b
Patrick Bur <patrick.bur@bd.so.ch>
2024-03-11 10:40:11 +0100

    [arp_richtplan] Anpassung gretljob für IVS

--- arp_richtplan_inventar_historische_verkehrswege_pub/delete_inventar_historische_verkehrswege.sql
+++ arp_richtplan_inventar_historische_verkehrswege_pub/delete_inventar_historische_verkehrswege.sql
@@ -2,9 +2,3 @@ DELETE FROM
     arp_richtplan_pub_v2.richtplankarte_ueberlagernde_linie
 WHERE
     datenquelle = 'inventar_historische_verkehrswege'
-;
-
-DELETE FROM 
-    arp_richtplan_pub_v2.richtplankarte_ueberlagernde_linie
-WHERE
-    datenquelle = 'inventar_historischer_verkehrsweg'
