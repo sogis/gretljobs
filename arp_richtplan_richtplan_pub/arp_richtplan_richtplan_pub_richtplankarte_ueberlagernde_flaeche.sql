d40a092ffa0cc4830002cc0a703e2a381451bf65
Patrick Bur <156295083+bjsvwbur@users.noreply.github.com>
2024-03-26 15:44:40 +0100

    [arp_richtplan] Anpassung gretljob & Loeschung Datenmigrations-Job (#1676)
    * [arp_richtplan] gretljob for point data-migration
    
    * [arp_richtplan] patch for build.gradle
    
    * [arp_richtplan] Anpassung users
    
    * [arp_richtplan] Anpassung gretljob für regionale ivs
    
    * [arp_richtplan] Anpassung gretljob für IVS
    
    * [arp_richtplan] gretl-job datamigration
    
    * [arp_richtplan] Anpassung Datenmigration
    
    * [arp_richtplan] Anpassung sequenz
    
    * [arp_richtplan] Anpassung Datenmigrtion & Gretljob edit_pub
    
    * [arp_richtplan] Korrektur gemeindegrenze
    
    * [arp_richtplan] Anpassung delete_data.sql
    
    * [arp_richtplan] Update fehlerhaftes Attribut in Tabelle
    
    * [arp_richtplan] Anpassung gretljob & Loeschung Datenmigrations-Job

--- arp_richtplan_richtplan_pub/arp_richtplan_richtplan_pub_richtplankarte_ueberlagernde_flaeche.sql
+++ arp_richtplan_richtplan_pub/arp_richtplan_richtplan_pub_richtplankarte_ueberlagernde_flaeche.sql
@@ -1,32 +1,47 @@
 WITH
-    ueberlagernde_flaeche AS (
-        SELECT
-            f.objektnummer,
-            f.objekttyp,
-            f.weitere_informationen,
-            f.geometrie,
-            string_agg(g.gemeindename, ', ') AS gemeindenamen,
-            f.objektname,
-            f.abstimmungskategorie,
-            f.bedeutung,
-            'rechtsgueltig' AS planungsstand,
-            NULL AS dokumente,
-            f.astatus,
-            f.anpassung AS richtplananpassung
-        FROM
+
+dokumente AS (
+    SELECT
+        f.ueberlagernde_flaeche,
+        string_agg(d.dateipfad, ', ') AS dokumente
+    FROM
+        arp_richtplan_v2.richtplankarte_dokument AS d
+    JOIN
+        arp_richtplan_v2.richtplankarte_ueberlagernde_flaeche_dokument AS f ON d.t_id = f.dokument
+    GROUP BY 
+        f.ueberlagernde_flaeche
+),
+
+ueberlagernde_flaeche AS (
+    SELECT
+        f.t_id,
+        f.objektnummer,
+        f.objekttyp,
+        f.weitere_informationen,
+        f.geometrie,
+        string_agg(g.gemeindename, ', ') AS gemeindenamen,
+        f.objektname,
+        f.abstimmungskategorie,
+        f.bedeutung,
+        'rechtsgueltig' AS planungsstand,
+        NULL AS dokumente,
+        f.astatus,
+        f.anpassung AS richtplananpassung
+    FROM
             arp_richtplan_v2.richtplankarte_ueberlagernde_flaeche AS f           
-        JOIN
+    JOIN
             agi_hoheitsgrenzen_pub.hoheitsgrenzen_gemeindegrenze AS g ON ST_Intersects(f.geometrie, g.geometrie)
-        GROUP BY 
-            f.objektnummer,
-            f.objekttyp,
-            f.weitere_informationen,
-            f.geometrie,
-            f.objektname,
-            f.abstimmungskategorie,
-            f.bedeutung,
-            f.astatus,
-            f.anpassung
+    GROUP BY
+        f.t_id,
+        f.objektnummer,
+        f.objekttyp,
+        f.weitere_informationen,
+        f.geometrie,
+        f.objektname,
+        f.abstimmungskategorie,
+        f.bedeutung,
+        f.astatus,
+        f.anpassung
 )
 
 SELECT
@@ -39,7 +54,7 @@ SELECT
     f.abstimmungskategorie,
     f.bedeutung,
     f.planungsstand,
-    f.dokumente,
+    d.dokumente,
     f.astatus,
     f.richtplananpassung,
     a.rrb_nr,
@@ -54,3 +69,5 @@ FROM
     ueberlagernde_flaeche AS f  
 LEFT JOIN
     arp_richtplan_v2.richtplankarte_anpassung AS a ON f.richtplananpassung = a.t_id
+LEFT JOIN 
+    dokumente AS d ON f.t_id = d.ueberlagernde_flaeche
