fc04a5e32a395bfef113b3a53308ac588d61defb
webrian <adrian.weber@bd.so.ch>
2024-04-18 08:15:01 +0200

    [afu_stehende_gewaesser_abgleich] Add comments in SQL query

--- afu_stehende_gewaesser_abgleich/unlink_av_gewaesser.sql
+++ afu_stehende_gewaesser_abgleich/unlink_av_gewaesser.sql
@@ -1,3 +1,13 @@
+-- Setze das Attribut av_link auf FALSE für alle Objekte, welche
+-- 1. innerhalb einer Bodenbedeckungsfläche mit der Art 'Gewaesser.stehendes' liegen
+-- und
+-- 2. das Attribut av_geometrie TRUE haben.
+--
+-- Dies betrifft alle Objekte welche ursprünglich aus der AV übernommen worden
+-- sind, aber nicht mehr länger in der AV vorliegen.
+--
+-- Siehe auch Roter Faden:
+-- "Flächen mit "av_geometrie" = 'true' und "av_link" = 'false' werden nicht publiziert."
 UPDATE 
     afu_stehende_gewaesser_v1.stehendes_gewaesser
 SET
