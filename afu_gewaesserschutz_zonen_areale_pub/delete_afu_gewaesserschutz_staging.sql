811c23194826162d4ae407459738f4a36de46bc4
webrian <adrian.weber@bd.so.ch>
2024-04-30 11:09:34 +0200

    [afu_gewaesserschutz_zonen_areale_pub] Update to new data model

--- afu_gewaesserschutz_zonen_areale_pub/delete_afu_gewaesserschutz_staging.sql
+++ afu_gewaesserschutz_zonen_areale_pub/delete_afu_gewaesserschutz_staging.sql
@@ -1,41 +1,41 @@
 --Dokument
 DELETE FROM
-    afu_gewaesserschutz_staging_v1.gewaesserschutz_dokument
+    afu_gewaesserschutz_staging_v2.gewaesserschutz_dokument
 ;
 
 DELETE FROM
-    afu_gewaesserschutz_staging_v1.gewaesserschutz_gewaesserschutzbereich
+    afu_gewaesserschutz_staging_v2.gewaesserschutz_gewaesserschutzbereich
 ;
 
 DELETE FROM
-    afu_gewaesserschutz_staging_v1.gewaesserschutz_bereich_typ
+    afu_gewaesserschutz_staging_v2.gewaesserschutz_bereich_typ
 ;
 
 DELETE FROM
-    afu_gewaesserschutz_staging_v1.gewaesserschutz_rechtsstatusart
+    afu_gewaesserschutz_staging_v2.gewaesserschutz_rechtsstatusart
 ;
 
 DELETE FROM
-    afu_gewaesserschutz_staging_v1.gewaesserschutz_schutzareal
+    afu_gewaesserschutz_staging_v2.gewaesserschutz_schutzareal
 ;
 
 DELETE FROM
-    afu_gewaesserschutz_staging_v1.gewaesserschutz_schutzzone
+    afu_gewaesserschutz_staging_v2.gewaesserschutz_schutzzone
 ;
 
 DELETE FROM
-    afu_gewaesserschutz_staging_v1.gewaesserschutz_zoneundareal_typ
+    afu_gewaesserschutz_staging_v2.gewaesserschutz_zoneundareal_typ
 ;
 
 DELETE FROM
-    afu_gewaesserschutz_staging_v1.gewaesserschutz_zustroembereich
+    afu_gewaesserschutz_staging_v2.gewaesserschutz_zustroembereich
 ;
 
 -- Basket / Dataset 
 DELETE FROM
-    afu_gewaesserschutz_staging_v1.t_ili2db_basket
+    afu_gewaesserschutz_staging_v2.t_ili2db_basket
 ;
 
 DELETE FROM
-    afu_gewaesserschutz_staging_v1.t_ili2db_dataset
+    afu_gewaesserschutz_staging_v2.t_ili2db_dataset
 ;
