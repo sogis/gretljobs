d9e299833e3a693eef3777aeb06679181cbf6b39
Sandra Curiger <38523407+bjsvwcur@users.noreply.github.com>
2024-05-22 13:19:34 +0200

    Update bodenbedeckung_gebaeude.sql

--- agi_gebaeudeflaechen_pub/bodenbedeckung_gebaeude.sql
+++ agi_gebaeudeflaechen_pub/bodenbedeckung_gebaeude.sql
@@ -1,6 +1,17 @@
+WITH gebaeudename AS (
+SELECT 
+    objektname.objektname,
+    bodenbedeckung.t_id
+FROM agi_mopublic_pub.mopublic_objektname_pos AS objektname
+    JOIN agi_mopublic_pub.mopublic_bodenbedeckung AS bodenbedeckung
+    ON ST_CONTAINS(bodenbedeckung.geometrie , objektname.pos)
+WHERE objektname.art_txt = 'Gebaeude' AND objektname.herkunft = 'BB'
+)
+
 SELECT
     bodenbedeckung.bfs_nr,
     bodenbedeckung.geometrie,
+    gebaeudename.objektname,
     adresse.strassenname,
     adresse.hausnummer,
     adresse.egid,
@@ -20,8 +31,10 @@ SELECT
         WHEN adresse.egid IS NOT NULL
             THEN concat('https://www.housing-stat.ch/de/query/egid.html?egid=',adresse.egid::TEXT)
         ELSE 'https://www.housing-stat.ch/'
-    END AS link_gwr
+    END AS ling_gwr
 FROM agi_mopublic_pub.mopublic_gebaeudeadresse adresse
-    JOIN agi_mopublic_pub.mopublic_bodenbedeckung bodenbedeckung 
-        ON ST_CONTAINS(bodenbedeckung.geometrie , adresse.lage)
+    JOIN agi_mopublic_pub.mopublic_bodenbedeckung AS bodenbedeckung
+    ON ST_CONTAINS(bodenbedeckung.geometrie , adresse.lage)
+    LEFT JOIN gebaeudename AS gebaeudename 
+    ON gebaeudename.t_id = bodenbedeckung.t_id 
 WHERE adresse.ist_offizielle_bezeichnung IS TRUE AND bodenbedeckung.art_txt = 'Gebaeude'
