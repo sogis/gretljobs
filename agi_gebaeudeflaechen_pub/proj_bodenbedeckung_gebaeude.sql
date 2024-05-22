cc884869370f56a450e7f201f38ca0fb5e9aacc4
bjsvwcur <sandra.curiger@bd.so.ch>
2024-05-22 09:12:59 +0200

    [agi_gebaeudeflaechen_pub] new Gretljob

---
+++ agi_gebaeudeflaechen_pub/proj_bodenbedeckung_gebaeude.sql
@@ -0,0 +1,26 @@
+SELECT
+    bodenbedeckung_proj.bfs_nr,
+    bodenbedeckung_proj.geometrie,
+    adresse.strassenname,
+    adresse.hausnummer,
+    adresse.egid,
+    adresse.edid,
+    adresse.plz,
+    adresse.ortschaft,
+    adresse.astatus,
+    adresse.hoehenlage,
+    adresse.gebaeudename,
+    adresse.orientierung,
+    adresse.hali,
+    adresse.vali,
+    adresse.posx,
+    adresse.posy,
+    CASE
+        WHEN adresse.egid IS NOT NULL
+            THEN concat('https://www.housing-stat.ch/de/query/egid.html?egid=',adresse.egid::TEXT)
+        ELSE 'https://www.housing-stat.ch/'
+    END AS ling_gwr
+FROM agi_mopublic_pub.mopublic_gebaeudeadresse adresse
+    JOIN agi_mopublic_pub.mopublic_bodenbedeckung_proj AS bodenbedeckung_proj
+        ON ST_CONTAINS(bodenbedeckung_proj.geometrie , adresse.lage)
+WHERE adresse.ist_offizielle_bezeichnung IS TRUE
