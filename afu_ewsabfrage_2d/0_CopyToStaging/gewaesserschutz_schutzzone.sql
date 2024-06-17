33ba938c32164eb90cf9cf6ac75658185bf86520
webrian <adrian.weber@bd.so.ch>
2024-06-17 09:17:48 +0200

    Update to Gewaesserschutz pub v3 schema

--- afu_ewsabfrage_2d/0_CopyToStaging/gewaesserschutz_schutzzone.sql
+++ afu_ewsabfrage_2d/0_CopyToStaging/gewaesserschutz_schutzzone.sql
@@ -2,4 +2,4 @@ SELECT
     0 AS max_bohrtiefe,
     'GW_Schutzzone' AS grund_beschraenkung,
     ST_Multi(apolygon) AS mpoly
-FROM afu_gewaesserschutz_pub_v2.gewaesserschutz_schutzzone;
+FROM afu_gewaesserschutz_pub_v3.gewaesserschutz_schutzzone;
