bfa2956d772a746e0ee8fc69c576c2ceab9766a7
Martin Schweizer <martin.schweizer@bd.so.ch>
2024-06-25 14:19:35 +0200

    [afu_naturgefahren_produkte] Bugfix copy to edit erhebungsgebiete und abklaerungsgebiete

--- afu_naturgefahren_produkte/copy_to_edit/publish_erhebungsgebiet.sql
+++ afu_naturgefahren_produkte/copy_to_edit/publish_erhebungsgebiet.sql
@@ -1,8 +1,17 @@
 SELECT 
-    teilprozess, 
-    erhebungsstand, 
-    geometrie, 
-    datenherkunft, 
-    auftrag_neudaten
+    flaeche, 
+    datenherr, 
+    status_ueberschwemmung, 
+    status_uebermurung, 
+    status_ufererosion, 
+    status_permanente_rutschung, 
+    status_spontane_rutschung, 
+    status_hangmure, 
+    status_stein_block_schlag, 
+    status_fels_berg_sturz, 
+    status_einsturz, 
+    status_absenkung, 
+    kommentar
 FROM 
-    afu_naturgefahren_staging_v1.erhebungsgebiet;
+    afu_naturgefahren_staging_v1.erhebungsgebiet
+;
