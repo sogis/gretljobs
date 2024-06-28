874303d93ae107e84beafd7f0343fa42854bca13
Stefan Ziegler <edi.gonzales@gmail.com>
2024-06-28 11:47:41 +0200

    [dsbjd_pikettregionen_militaerjustiz] add new job

---
+++ dsbjd_pikettregionen_militaerjustiz/dsbjd_pikettregionen_militaerjustiz_pub.sql
@@ -0,0 +1,10 @@
+SELECT 
+    nummer,
+    bemerkung,
+    geometrie,
+    pikettdienst,
+    pikettdienst_flugunfaelle,
+    kommunikation_militaerjustiz
+FROM 
+    dsbjd_pikettregionen_militaerjustiz_v1.pikettregionen_pikettregion 
+;
