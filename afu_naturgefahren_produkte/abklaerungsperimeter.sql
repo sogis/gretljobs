c99e38140911bb70429e26ef2d999de69c20c1e2
Martin Schweizer <martin.schweizer@bd.so.ch>
2024-07-01 15:29:21 +0200

    [afu_naturgefahren_produkte] Bugfix abklaerungsperimeter

--- afu_naturgefahren_produkte/abklaerungsperimeter.sql
+++ afu_naturgefahren_produkte/abklaerungsperimeter.sql
@@ -20,40 +20,132 @@ INSERT INTO afu_naturgefahren_staging_v1.abklaerungsperimeter (
 
 SELECT 
     basket.t_id as t_basket,
-    teilprozess, 
+    'absenkung' AS teilprozess, 
     erhebungsstand,
     geometrie, 
     'Neudaten' as datenherkunft, 
     NULL as auftrag_neudaten
 from 
-    afu_naturgefahren_beurteilungsgebiet_v1.erhebungsgebiet_rutschung
+    afu_naturgefahren_beurteilungsgebiet_v1.erhebungsgebiet_absenkung
     ,basket
 
     union all 
     
 SELECT 
     basket.t_id as t_basket,
-    teilprozess, 
+    'einsturz' as teilprozess, 
     erhebungsstand,
     geometrie, 
     'Neudaten' as datenherkunft, 
     NULL as auftrag_neudaten
 from 
-    afu_naturgefahren_beurteilungsgebiet_v1.erhebungsgebiet_sturz
+    afu_naturgefahren_beurteilungsgebiet_v1.erhebungsgebiet_einsturz
     ,basket
     
     union all 
     
 SELECT 
     basket.t_id as t_basket,
-    teilprozess, 
+    'fels_bergsturz' AS teilprozess, 
     erhebungsstand,
     geometrie, 
     'Neudaten' as datenherkunft, 
     NULL as auftrag_neudaten
 from 
-    afu_naturgefahren_beurteilungsgebiet_v1.erhebungsgebiet_wasser
+    afu_naturgefahren_beurteilungsgebiet_v1.erhebungsgebiet_fels_berg_sturz
+    ,basket
+    
+    union all 
+    
+SELECT 
+    basket.t_id as t_basket,
+    'hangmure' AS teilprozess, 
+    erhebungsstand,
+    geometrie, 
+    'Neudaten' as datenherkunft, 
+    NULL as auftrag_neudaten
+from 
+    afu_naturgefahren_beurteilungsgebiet_v1.erhebungsgebiet_hangmure
+    ,basket
+    
+    union all 
+    
+SELECT 
+    basket.t_id as t_basket,
+    'permanente_rutschung' AS teilprozess, 
+    erhebungsstand,
+    geometrie, 
+    'Neudaten' as datenherkunft, 
+    NULL as auftrag_neudaten
+from 
+    afu_naturgefahren_beurteilungsgebiet_v1.erhebungsgebiet_permanente_rutschung
+    ,basket
+    
+    union all 
+    
+SELECT 
+    basket.t_id as t_basket,
+    'spontane_rutschung' AS teilprozess, 
+    erhebungsstand,
+    geometrie, 
+    'Neudaten' as datenherkunft, 
+    NULL as auftrag_neudaten
+from 
+    afu_naturgefahren_beurteilungsgebiet_v1.erhebungsgebiet_spontane_rutschung
+    ,basket
+    
+    union all 
+    
+SELECT 
+    basket.t_id as t_basket,
+    'stein_blockschlag' AS teilprozess, 
+    erhebungsstand,
+    geometrie, 
+    'Neudaten' as datenherkunft, 
+    NULL as auftrag_neudaten
+from 
+    afu_naturgefahren_beurteilungsgebiet_v1.erhebungsgebiet_stein_block_schlag
+    ,basket
+    
+    union all 
+    
+SELECT 
+    basket.t_id as t_basket,
+    'uebermurung' AS teilprozess, 
+    erhebungsstand,
+    geometrie, 
+    'Neudaten' as datenherkunft, 
+    NULL as auftrag_neudaten
+from 
+    afu_naturgefahren_beurteilungsgebiet_v1.erhebungsgebiet_uebermurung
+    ,basket
+    
+    union all 
+    
+SELECT 
+    basket.t_id as t_basket,
+    'ueberschwemmung' AS teilprozess, 
+    erhebungsstand,
+    geometrie, 
+    'Neudaten' as datenherkunft, 
+    NULL as auftrag_neudaten
+from 
+    afu_naturgefahren_beurteilungsgebiet_v1.erhebungsgebiet_ueberschwemmung
+    ,basket
+    
+    union all 
+    
+SELECT 
+    basket.t_id as t_basket,
+    'ufererosion' AS teilprozess, 
+    erhebungsstand,
+    geometrie, 
+    'Neudaten' as datenherkunft, 
+    NULL as auftrag_neudaten
+from 
+    afu_naturgefahren_beurteilungsgebiet_v1.erhebungsgebiet_ufererosion
     ,basket
 ;
+
 
 
