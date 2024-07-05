fe780ba05c97b191db0a551ea37e4555d1219431
signedav <david@opengis.ch>
2024-07-05 15:40:59 +0200

    [arp_mjpnl_v2_auswertung] Kalkulation rrb_bewirtschafterwechsel

---
+++ arp_mjpnl_v2_auswertung/calc_rrb_bewirtschafterwechsel.sql
@@ -0,0 +1,22 @@
+INSERT INTO ${DB_Schema_MJPNL}.rrb_bewirtschafterwechsel
+    t_basket,
+    jahr,
+    gemeinde,
+    vereinbarungs_nr,
+    vereinbarungs_nr_alt
+    gelan_pid_gelan,
+    gelan_person
+SELECT 
+    (SELECT t_id FROM arp_mjpnl_v1.t_ili2db_basket WHERE topic = 'SO_ARP_MJPNL_20240606.Auswertung' LIMIT 1) as t_basket,
+    vbg_hist.jahr_bewirtschafterwechsel as jahr,
+    array_to_string(vbg.gemeinde, ',') as gemeinde, 
+    vbg.vereinbarungs_nr as vereinbarungs_nr,
+    vbg.vereinbarungs_nr_alt as vereinbarungs_nr_alt,
+    vbg.gelan_pid_gelan as gelan_pid_gelan,
+    gp.name_vorname as gelan_person
+FROM ${DB_Schema_MJPNL}.mjpnl_vereinbarung vbg
+JOIN ${DB_Schema_MJPNL}.mjpnl_vereinbarung_bewirtschafter_historie vbg_hist
+ON vbg_hist.vereinbarung = vbg.t_id
+JOIN ${DB_Schema_MJPNL}.betrbsdttrktrdten_gelan_person gp
+ON gp.pid_gelan = vbg.gelan_pid_gelan 
+WHERE vbg_hist.jahr_bewirtschafterwechsel = ${AUSZAHLUNGSJAHR}::integer;
