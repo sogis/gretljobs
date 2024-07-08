75ae1f9389ac7a31f236e184330fdac7222cbce1
Christian Baumann <christian.baumann@bd.so.ch>
2024-07-08 11:44:58 +0200

    [afu_asiatische_hornisse] wgs84 coordinates to pub

--- afu_asiatische_hornisse_pub/afu_asiatische_hornisse_pub.sql
+++ afu_asiatische_hornisse_pub/afu_asiatische_hornisse_pub.sql
@@ -4,6 +4,8 @@ SELECT
     ort,
     round(st_x(geometrie)) AS x_koordinate,
     round(st_y(geometrie)) AS y_koordinate,
+    st_y(st_transform(geometrie, 4326)) AS koordinate_lat ,
+    st_x(st_transform(geometrie, 4326)) AS  koordinate_lon,
     geometrie,
     kanton,
     massnahmenstatus
