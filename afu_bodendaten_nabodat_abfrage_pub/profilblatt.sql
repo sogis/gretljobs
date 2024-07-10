c20828e999ee9d4184204b3c82c3fe70bec6f633
Patrick Bur <patrick.bur@bd.so.ch>
2024-07-10 14:55:20 +0200

    [afu_nabodat] update profilblatt

---
+++ afu_bodendaten_nabodat_abfrage_pub/profilblatt.sql
@@ -0,0 +1,19 @@
+WITH
+
+pubschema AS (
+    SELECT
+        profilnummer,
+        'https://geo.so.ch/api/v1/document/afu_bodenprofilstandorte.pdf?feature=' || t_id || '&x=' || st_x(geometrie) || '&y=' || st_y(geometrie) || '&crs=EPSG%3A2056' AS profilblatt
+    FROM
+        afu_bodendaten_nabodat_pub.bodenproflstndrte_bodenprofilstandort
+)
+
+UPDATE 
+    afu_bodendaten_nabodat_abfrage_pub_v1.bodenprofil AS abfrageschema
+SET 
+    profilblatt = pubschema.profilblatt
+FROM 
+    pubschema
+WHERE 
+    abfrageschema.profilnummer = pubschema.profilnummer
+;
