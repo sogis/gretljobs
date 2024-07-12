6f084e6acacaef57268ae55e26f868d02d99320e
Sandra Curiger <38523407+bjsvwcur@users.noreply.github.com>
2024-07-12 08:49:40 +0200

    Update agi_mopublic_pub_mopublic_grundstueck_linie.sql

--- agi_av_dm01_mopublic_pub/2_mopublic/db2db/agi_mopublic_pub_mopublic_grundstueck_linie.sql
+++ agi_av_dm01_mopublic_pub/2_mopublic/db2db/agi_mopublic_pub_mopublic_grundstueck_linie.sql
@@ -6,5 +6,7 @@ FROM
     agi_dm01avso24.liegenschaften_grundstueckpos AS grundstueckpos
     JOIN agi_dm01avso24.liegenschaften_grundstueck AS grundstueck
         ON grundstueck.t_id  = grundstueckpos.grundstueckpos_von
+    LEFT JOIN agi_dm01avso24.t_ili2db_basket AS basket
+        ON grundstueckpos.t_basket = basket.t_id    
 WHERE grundstueckpos.hilfslinie IS NOT NULL     
 ;
