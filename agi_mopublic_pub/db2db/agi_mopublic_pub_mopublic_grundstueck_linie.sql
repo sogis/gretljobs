f7455d722c283719c8790222b31f3c2a0bc25053
Sandra Curiger <38523407+bjsvwcur@users.noreply.github.com>
2024-07-12 11:02:05 +0200

    Update agi_mopublic_pub_mopublic_grundstueck_linie.sql

--- agi_mopublic_pub/db2db/agi_mopublic_pub_mopublic_grundstueck_linie.sql
+++ agi_mopublic_pub/db2db/agi_mopublic_pub_mopublic_grundstueck_linie.sql
@@ -1,9 +1,12 @@
-SELECT
+SSELECT
+    ${basket_tid} AS t_basket,
     grundstueck.nummer,
     grundstueckpos.hilfslinie
 FROM
     agi_dm01avso24.liegenschaften_grundstueckpos AS grundstueckpos
     JOIN agi_dm01avso24.liegenschaften_grundstueck AS grundstueck
         ON grundstueck.t_id  = grundstueckpos.grundstueckpos_von
+    LEFT JOIN agi_dm01avso24.t_ili2db_basket AS basket
+        ON grundstueckpos.t_basket = basket.t_id    
 WHERE grundstueckpos.hilfslinie IS NOT NULL     
 ;
