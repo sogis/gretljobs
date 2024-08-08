fafb9e691b3a3f5bedbaad1d15971e275a0eb521
Stefan Ziegler <edi.gonzales@gmail.com>
2024-08-08 11:43:08 +0200

    [dsbjd_ebauso_rahmenmodell] initial commit

---
+++ dsbjd_ebauso_rahmenmodell_pub/naturgefahren.sql
@@ -0,0 +1,20 @@
+INSERT INTO 
+    dsbjd_ebauso_rahmenmodell_stage_v1.fachthemen_fachthema_polygon
+    (
+        geometrie,
+        artcode,
+        beschreibung,
+        thema,
+        rechtsstatus,
+        rechtsstatus_txt
+    )    
+SELECT
+    geometrie,
+    gefahrenstufe_txt AS artcode,
+    charakterisierung AS beschreibung,
+    'ch.SO.naturgefahren' AS thema,
+    NULL::TEXT AS rechtsstatus,
+    NULL::TEXT AS rechtsstatus_txt
+FROM 
+    afu_naturgefahren_pub_v1.synoptisches_gefahrengebiet
+;
---
+++ dsbjd_ebauso_rahmenmodell_pub/oereb_to_stage_polygon.sql
@@ -0,0 +1,24 @@
+SELECT 
+    ST_Multi(g.flaeche) AS geometrie,
+    l.artcode,
+    l.legendetext_de AS beschreibung,
+    'ch.BelasteteStandorte' AS thema,
+    e.rechtsstatus,
+    CASE 
+        WHEN e.rechtsstatus = 'inKraft' THEN 'in Kraft'
+        WHEN e.rechtsstatus = 'AenderungMitVorwirkung' THEN 'Änderung mit Vorwirkung'
+        WHEN e.rechtsstatus = 'AenderungOhneVorwirkung' THEN 'Änderung ohne Vorwirkung'
+    END AS rechtsstatus_txt
+FROM 
+    live.oerbkrmfr_v2_0transferstruktur_eigentumsbeschraenkung AS e
+    LEFT JOIN live.oerbkrmfr_v2_0transferstruktur_legendeeintrag AS l 
+    ON e.legende = l.t_id
+    LEFT JOIN live.oerbkrmfr_v2_0transferstruktur_geometrie AS g 
+    ON g.eigentumsbeschraenkung = e.t_id 
+WHERE 
+    l.thema = 'ch.BelasteteStandorte'
+    OR 
+    l.thema = 'ch.Grundwasserschutzzonen'
+    OR 
+    l.thema = 'ch.Grundwasserschutzareale'
+;
