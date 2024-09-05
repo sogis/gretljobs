53c2f7fe8f679b81cb18229293d56c2b8372df47
webrian <adrian.weber@bd.so.ch>
2024-09-05 16:18:00 +0200

    [avt_mehrjahresplanung] Initial commit

---
+++ avt_mehrjahresplanung_projekte_pub/delete_projekte.sql
@@ -0,0 +1,1 @@
+DELETE FROM avt_mehrjahresplanung_v1.projekte_projekt;
---
+++ avt_mehrjahresplanung_projekte_pub/select_bezugspunkte.sql
@@ -0,0 +1,6 @@
+SELECT
+    bp.bezeichnun AS bezeichnung,
+    bp.achsenumme AS achsenummer,
+    bp.geometrie
+FROM
+    avt_kantonsstrassen_pub.bezugspunkt bp;
