1430acc56ffd74e7237f9d94e4f4f53417221524
Sandra Curiger <38523407+bjsvwcur@users.noreply.github.com>
2024-09-17 14:03:39 +0200

    [arp_wanderwege_pub] Zeilenumbrüche

--- arp_wanderwege_pub/arp_wanderwege_pub_route.sql
+++ arp_wanderwege_pub/arp_wanderwege_pub_route.sql
@@ -38,7 +38,6 @@ routes AS (
         route.starting_point AS astart,
         route.destination AS ziel,
         catalogues_direction.aname_de AS signalisationsrichtung,
-        regexp_replace(atalogues_direction.aname_de, E'[\\n\\r]+', ' ', 'g' ) AS signalisationsrichtung_sandra,
         regexp_replace(route.route_description, E'[\\n\\r]+', ' ', 'g' ) AS beschreibung,
         substr(metadata.last_updated, 1, 4)||'-'||substr(metadata.last_updated, 5, 2)||'-'||substr(metadata.last_updated, 7, 2) AS letzte_aktualisierung,
         '<a href="https://www.solothurner-wanderwege.ch/de/kontakt" target="_blank">Solothurner Wanderwege</a>' AS kontakt
