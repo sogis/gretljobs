e92cc2becde41eabf4103e2c229b71efd4f5d28a
Martin Schweizer <martin.schweizer@bd.so.ch>
2024-09-25 10:57:06 +0200

    [arp_mjpnatur_pub] Neue Attributte hinzugefügt

--- arp_mjpnatur_pub/arp_mjpnatur_pub_flaechen.sql
+++ arp_mjpnatur_pub/arp_mjpnatur_pub_flaechen.sql
@@ -33,6 +33,86 @@ beurteilungs_metainfo_wiesen AS (
          -- berücksichtige nur besprochene Beurteilungen
     WHERE 
         mit_bewirtschafter_besprochen IS TRUE
+),
+
+beurteilungen_datum AS (
+    SELECT 
+        to_char(beurteilung.beurteilungsdatum, 'DD.MM.YYYY') AS beurteilungsdatum,
+        NULL AS letzter_unterhalt,
+        vereinbarung
+    FROM 
+        arp_mjpnl_v2.mjpnl_beurteilung_alr_buntbrache beurteilung 
+    UNION ALL 
+    SELECT 
+        to_char(beurteilung.beurteilungsdatum, 'DD.MM.YYYY') AS beurteilungsdatum,
+        NULL AS letzter_unterhalt,
+        vereinbarung
+    FROM 
+        arp_mjpnl_v2.mjpnl_beurteilung_alr_saum beurteilung 
+    UNION ALL 
+    SELECT 
+        to_char(beurteilung.beurteilungsdatum, 'DD.MM.YYYY') AS beurteilungsdatum,
+        bewirtschaftung_hecke_letzterunterhalt::text AS letzter_unterhalt,
+        vereinbarung
+    FROM 
+        arp_mjpnl_v2.mjpnl_beurteilung_hecke beurteilung 
+    UNION ALL 
+    SELECT 
+        to_char(beurteilung.beurteilungsdatum, 'DD.MM.YYYY') AS beurteilungsdatum,
+        NULL AS letzter_unterhalt,
+        vereinbarung
+    FROM 
+        arp_mjpnl_v2.mjpnl_beurteilung_hostet beurteilung 
+    UNION ALL 
+    SELECT 
+        to_char(beurteilung.beurteilungsdatum, 'DD.MM.YYYY') AS beurteilungsdatum,
+        NULL AS letzter_unterhalt,
+        vereinbarung
+    FROM 
+        arp_mjpnl_v2.mjpnl_beurteilung_obl beurteilung 
+    UNION ALL 
+    SELECT 
+        to_char(beurteilung.beurteilungsdatum, 'DD.MM.YYYY') AS beurteilungsdatum,
+        NULL AS letzter_unterhalt,
+        vereinbarung
+    FROM 
+        arp_mjpnl_v2.mjpnl_beurteilung_wbl_weide beurteilung 
+    UNION ALL 
+    SELECT 
+        to_char(beurteilung.beurteilungsdatum, 'DD.MM.YYYY') AS beurteilungsdatum,
+        NULL AS letzter_unterhalt,
+        vereinbarung
+    FROM 
+        arp_mjpnl_v2.mjpnl_beurteilung_wbl_wiese beurteilung 
+    UNION ALL 
+    SELECT 
+        to_char(beurteilung.beurteilungsdatum, 'DD.MM.YYYY') AS beurteilungsdatum,
+        NULL AS letzter_unterhalt,
+        vereinbarung
+    FROM 
+        arp_mjpnl_v2.mjpnl_beurteilung_weide_ln beurteilung 
+    UNION ALL 
+    SELECT 
+        to_char(beurteilung.beurteilungsdatum, 'DD.MM.YYYY') AS beurteilungsdatum,
+        NULL AS letzter_unterhalt,
+        vereinbarung
+    FROM 
+        arp_mjpnl_v2.mjpnl_beurteilung_weide_soeg beurteilung 
+    UNION ALL 
+    SELECT 
+        to_char(beurteilung.beurteilungsdatum, 'DD.MM.YYYY') AS beurteilungsdatum,
+        NULL AS letzter_unterhalt,
+        vereinbarung
+    FROM 
+        arp_mjpnl_v2.mjpnl_beurteilung_wiese beurteilung 
+),
+
+laufmeter AS (
+    SELECT 
+        bewirtschaftung_lebhag_laufmeter AS laufmeter,
+        vereinbarung
+    FROM 
+        arp_mjpnl_v2.mjpnl_beurteilung_hecke 
 )
 
 SELECT 
@@ -42,7 +122,7 @@ SELECT
     vereinbarungsart,
     person.name_vorname AS bewirtschafter,
     flaeche,
-    NULL AS laufmeter,
+    laufmeter.laufmeter AS laufmeter,
     flurname,
     bw.einstufungbeurteilungistzustand_wiesenkategorie AS wiesenkategorie,
     bewirtschaftabmachung_emdenbodenheu AS emden,
@@ -50,24 +130,46 @@ SELECT
     bewirtschaftabmachung_messerbalkenmaehgeraet AS balkenmaeher,
     bewirtschaftabmachung_herbstweide AS herbstweide,
     bewirtschaftabmachung_rueckzugstreifen AS rueckzugsstreifen,
-    NULL AS letzter_unterhalt,
-    NULL AS datum_beurteilung,
+    beurteilungen_datum.letzter_unterhalt AS letzter_unterhalt,
+    beurteilungen_datum.beurteilungsdatum AS datum_beurteilung,
     vereinbarung.status_vereinbarung AS vereinbarung_status
 FROM 
     arp_mjpnl_v2.mjpnl_vereinbarung vereinbarung
-LEFT JOIN arp_mjpnl_v2.betrbsdttrktrdten_gelan_person person 
-    ON vereinbarung.gelan_pid_gelan = person.pid_gelan 
-LEFT JOIN beurteilungs_metainfo_wiesen bw
-     ON vereinbarung.t_id = bw.vereinbarung
+LEFT JOIN 
+    arp_mjpnl_v2.betrbsdttrktrdten_gelan_person person 
+    ON 
+    vereinbarung.gelan_pid_gelan = person.pid_gelan 
+LEFT JOIN 
+    beurteilungs_metainfo_wiesen bw
+    ON 
+    vereinbarung.t_id = bw.vereinbarung
      -- berücksichtige nur die neusten (sofern mehrere existieren)
-     AND bw.beurteilungsdatum = (
-                                 SELECT 
-                                     MAX(beurteilungsdatum) 
-                                 FROM 
-                                     beurteilungs_metainfo_wiesen b 
-                                 WHERE 
-                                     b.vereinbarung = vereinbarung.t_id
-                                )
+    AND bw.beurteilungsdatum = (
+                                SELECT 
+                                    MAX(beurteilungsdatum) 
+                                FROM 
+                                    beurteilungs_metainfo_wiesen b 
+                                WHERE 
+                                    b.vereinbarung = vereinbarung.t_id
+                               )
+LEFT JOIN 
+    beurteilungen_datum  
+    ON 
+    beurteilungen_datum.vereinbarung = vereinbarung.t_id
+     -- berücksichtige nur die neusten (sofern mehrere existieren)
+    AND beurteilungen_datum.beurteilungsdatum = (
+                                SELECT 
+                                    MAX(beurteilungsdatum) 
+                                FROM 
+                                    beurteilungen_datum b 
+                                WHERE 
+                                    b.vereinbarung = vereinbarung.t_id
+                               )
+LEFT JOIN 
+    laufmeter 
+    ON 
+    laufmeter.vereinbarung = vereinbarung.t_id 
+        
 WHERE 
     vereinbarung.bewe_id_geprueft IS TRUE 
     AND 
