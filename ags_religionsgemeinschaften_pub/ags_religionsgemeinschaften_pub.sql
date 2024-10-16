8c6a594fa1fcff0d05036299e7891dd42468080e
Sandra Curiger <38523407+bjsvwcur@users.noreply.github.com>
2024-10-16 14:56:23 +0200

    [ags_religionsgemeinschaften_pub] neue Schemaversion

--- ags_religionsgemeinschaften_pub/ags_religionsgemeinschaften_pub.sql
+++ ags_religionsgemeinschaften_pub/ags_religionsgemeinschaften_pub.sql
@@ -3,10 +3,10 @@ SELECT
     CONCAT(strasse || ' ', hausnummer || ', ', plz || ' ', ortschaft) AS adresse,
     ansaessig_seit,
     geometrie,
-    bemerkungen,
+    CONCAT('Kontaktname: ', kontaktperson ,E'\n', 'eMail: ', kontaktmail ,E'\n', 'Homepage: ', homepage, E'\n', bemerkungen) AS bemerkungen,
     tradition.aname AS religioese_tradition
 FROM
-    ags_religionsgemeinschaften_v1.religionsgemeinschaft AS gemeinschaft
-    LEFT JOIN ags_religionsgemeinschaften_v1.religioese_tradition AS tradition
+    ags_religionsgemeinschaften_v2.religionsgemeinschaft AS gemeinschaft
+    LEFT JOIN ags_religionsgemeinschaften_v2.religioese_tradition AS tradition
     ON gemeinschaft.r_tradition = tradition.t_id
 ;
