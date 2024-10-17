77fe39ebbccf5bd1144cd22a482ac049b37a5397
Sandra Curiger <38523407+bjsvwcur@users.noreply.github.com>
2024-10-17 08:42:25 +0200

    [ags_religionsgemeinschaften_pub] neues Version

--- ags_religionsgemeinschaften_pub/ags_religionsgemeinschaften_pub.sql
+++ ags_religionsgemeinschaften_pub/ags_religionsgemeinschaften_pub.sql
@@ -3,7 +3,21 @@ SELECT
     CONCAT(strasse || ' ', hausnummer || ', ', plz || ' ', ortschaft) AS adresse,
     ansaessig_seit,
     geometrie,
-    CONCAT('Kontaktname: ', kontaktperson ,E'\n', 'eMail: ', kontaktmail ,E'\n', 'Homepage: ', homepage, E'\n', bemerkungen) AS bemerkungen,
+    CASE
+        WHEN kontaktperson IS NOT NULL AND kontaktmail IS NULL AND homepage IS NULL
+            THEN CONCAT('Kontaktname: ' ,kontaktperson, ' | Bemerkungen: ', bemerkungen)
+        WHEN kontaktperson IS NULL AND kontaktmail IS NOT NULL AND homepage IS NULL
+            THEN CONCAT('Mailadresse: ' ,kontaktmail, ' | Bemerkungen: ', bemerkungen)
+        WHEN kontaktperson IS NULL AND kontaktmail IS NULL AND homepage IS NOT NULL
+            THEN CONCAT('Homepage: ' ,homepage, ' | Bemerkungen: ', bemerkungen)
+        WHEN kontaktperson IS NOT NULL AND kontaktmail IS NOT NULL AND homepage IS NULL
+            THEN CONCAT('Kontaktname: ' ,kontaktperson, ' | Mailadresse: ' ,kontaktmail,' | Bemerkungen: ', bemerkungen)
+        WHEN kontaktperson IS NOT NULL AND kontaktmail IS NULL AND homepage IS NOT NULL
+            THEN CONCAT('Kontaktname: ' ,kontaktperson, ' | Homepage: ' ,homepage, ' | Bemerkungen: ', bemerkungen)
+        WHEN kontaktperson IS NULL AND kontaktmail IS NOT NULL AND homepage IS NOT NULL
+            THEN CONCAT('Mailadresse: ' ,kontaktmail, ' | Homepage: ' ,homepage, ' | Bemerkungen: ', bemerkungen)
+       ELSE bemerkungen
+    END AS bemerkungen,
     tradition.aname AS religioese_tradition
 FROM
     ags_religionsgemeinschaften_v2.religionsgemeinschaft AS gemeinschaft
