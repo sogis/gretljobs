d1bf90a99e54cd591c1a65f12244da242fe824cc
Sandra Curiger <38523407+bjsvwcur@users.noreply.github.com>
2024-10-18 11:20:19 +0200

    Update ags_religionsgemeinschaften_pub.sql

--- ags_religionsgemeinschaften_pub/ags_religionsgemeinschaften_pub.sql
+++ ags_religionsgemeinschaften_pub/ags_religionsgemeinschaften_pub.sql
@@ -7,15 +7,17 @@ SELECT
         WHEN kontaktperson IS NOT NULL AND kontaktmail IS NULL AND homepage IS NULL
             THEN CONCAT('Kontaktname: ' ,kontaktperson)
         WHEN kontaktperson IS NULL AND kontaktmail IS NOT NULL AND homepage IS NULL
-            THEN CONCAT('Mailadresse: ' ,kontaktmail)
+            THEN CONCAT('Mailadresse: <p><a href="mailto:' ,kontaktmail, '">', kontaktmail, '</a></p> ')
         WHEN kontaktperson IS NULL AND kontaktmail IS NULL AND homepage IS NOT NULL
             THEN CONCAT('Homepage: ' ,homepage)
         WHEN kontaktperson IS NOT NULL AND kontaktmail IS NOT NULL AND homepage IS NULL
-            THEN CONCAT('Kontaktname: ' ,kontaktperson, ' | Mailadresse: ' ,kontaktmail)
+            THEN CONCAT('Kontaktname: ' ,kontaktperson, ' | Mailadresse: <p><a href="mailto:' ,kontaktmail, '">', kontaktmail, '</a></p> ')
         WHEN kontaktperson IS NOT NULL AND kontaktmail IS NULL AND homepage IS NOT NULL
             THEN CONCAT('Kontaktname: ' ,kontaktperson, ' | Homepage: ' ,homepage)
         WHEN kontaktperson IS NULL AND kontaktmail IS NOT NULL AND homepage IS NOT NULL
-            THEN CONCAT('Mailadresse: ' ,kontaktmail, ' | Homepage: ' ,homepage)
+            THEN CONCAT('Mailadresse: <p><a href="mailto:' ,kontaktmail, '">', kontaktmail, '</a></p> | Homepage: ' ,homepage)
+        WHEN kontaktperson IS NOT NULL AND kontaktmail IS NOT NULL AND homepage IS NOT NULL
+            THEN CONCAT('Kontaktname: ' ,kontaktperson, ' | Mailadresse: <p><a href="mailto:' ,kontaktmail, '">', kontaktmail, '</a></p> | Homepage: ' ,homepage)
        ELSE ''
     END AS bemerkungen,
     tradition.aname AS religioese_tradition
