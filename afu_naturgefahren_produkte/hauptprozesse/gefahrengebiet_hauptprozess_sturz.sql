854fec47b1613ac8508c98f820ab8eae98f4e4fe
Martin Schweizer <martin.schweizer@bd.so.ch>
2024-10-30 13:14:57 +0100

    [ afu_naturgefahren_produkte] Diverse Änderungen gemäss Ticket 2640

--- afu_naturgefahren_produkte/hauptprozesse/gefahrengebiet_hauptprozess_sturz.sql
+++ afu_naturgefahren_produkte/hauptprozesse/gefahrengebiet_hauptprozess_sturz.sql
@@ -1,138 +1,26 @@
-WITH 
-basket AS (
-     SELECT 
-         t_id,
-         attachmentkey
-     FROM 
-         afu_naturgefahren_staging_v1.t_ili2db_basket
-), 
+DROP TABLE IF EXISTS gk_poly;
 
-hauptprozess_sturz AS ( 
-    SELECT
-        gefahrenstufe, 
-        charakterisierung, 
-        teilprozess,
-        (ST_dump(geometrie)).geom AS geometrie	
-    FROM 
-        afu_naturgefahren_staging_v1.gefahrengebiet_teilprozess_stein_blockschlag  
-    WHERE 
-        datenherkunft = 'Neudaten'
-    UNION ALL 
-    SELECT
-        gefahrenstufe, 
-        charakterisierung, 
-        teilprozess,
-        (ST_dump(geometrie)).geom AS geometrie	
-    FROM 
-        afu_naturgefahren_staging_v1.gefahrengebiet_teilprozess_fels_bergsturz 
-    WHERE 
-        datenherkunft = 'Neudaten'
-)
+CREATE TABLE gk_poly (
+    prio int4 NOT NULL,
+    gefahrenstufe varchar(255) NOT NULL,
+    geometrie geometry(polygon, 2056) NOT NULL,
+    charakterisierung varchar(10) NOT NULL,
+    hauptprozess varchar(30) NOT NULL
+);
 
-,hauptprozess_sturz_clean AS (
-    SELECT 
-        gefahrenstufe, 
-        charakterisierung, 
-        teilprozess,
-        geometrie 
-    FROM 
-        hauptprozess_sturz 
-    WHERE 
-        ST_area(geometrie) > 0.001
-)
+DROP INDEX IF EXISTS gk_poly_geometrie;
+CREATE INDEX gk_poly_geometrie ON gk_poly USING GIST (geometrie);
 
--- Die Priorisierung wird hier gleich von der Charakterisierung übernommen. Alle Sturz-Prozesse weisen die Charakterisierung SX auf und gemäss
--- Prozessmatrix ist die Zahl auch gleichbedeutend mit "Höherrangig". Es gibt keine geteilten Kästchen.  
--- Das heisst: Bei einer allfälligen Überlagerung werden die Charakterisierungen eh nicht aggregiert, sondern die Charakterisierung 
--- mit dem höchsten Wert gewinnt. Deshalb reicht ein einfaches Clip aus.  
+DROP TABLE IF EXISTS splited;
 
-,hauptprozess_sturz_clean_prio AS (
-    SELECT 
-        gefahrenstufe, 
-        charakterisierung, 
-        teilprozess,
-        geometrie,
-        CASE 
-            WHEN charakterisierung = 'S10' THEN 0::integer
-            ELSE substring(charakterisierung FROM 2 for 1)::integer 
-    END AS prio  
-    FROM 
-        hauptprozess_sturz_clean
-)
+CREATE TABLE splited (
+    id int4 NOT NULL,
+    poly geometry(polygon, 2056) NOT NULL,
+    point geometry(point, 2056) NOT NULL,
+    gef_max int4 NULL,
+    charakterisierung varchar(255) NULL,
+    hauptprozess varchar(255) NULL
+);
 
-,hauptprozess_sturz_clean_prio_clip AS (
-    SELECT 
-        a.gefahrenstufe, 
-        a.charakterisierung, 
-        teilprozess,
-        ST_Multi(COALESCE(
-            ST_Difference(a.geometrie, blade.geometrie),
-            a.geometrie
-        )) AS geometrie
-    FROM  
-        hauptprozess_sturz_clean_prio AS a
-    CROSS JOIN LATERAL (
-        SELECT 
-            ST_Union(geometrie) AS geometrie
-        FROM   
-            hauptprozess_sturz_clean_prio AS b
-        WHERE 
-            a.geometrie && b.geometrie 
-            AND 
-            a.prio < b.prio              
-    ) AS blade		
-)
-	
--- Flächen mit gleicher Gefahrenstufe und gleicher Charakterisierung können zusammengefasst werden
-,hauptprozess_sturz_union AS (
-    SELECT 
-        gefahrenstufe,
-        charakterisierung,
-        teilprozess,
-        st_union(geometrie) AS geometrie
-    FROM 
-        hauptprozess_sturz_clean_prio_clip
-    GROUP BY 
-        gefahrenstufe,
-        charakterisierung,
-        teilprozess
-)
-
-,hauptprozess_sturz_dump AS (
-    SELECT 
-        gefahrenstufe,
-        charakterisierung,
-        teilprozess,
-        (st_dump(geometrie)).geom AS geometrie
-    FROM 
-        hauptprozess_sturz_union
-)
-
-
-INSERT INTO afu_naturgefahren_staging_v1.gefahrengebiet_hauptprozess_sturz (
-    t_basket, 
-    hauptprozess, 
-    gefahrenstufe, 
-    charakterisierung, 
-    teilprozess,
-    geometrie, 
-    datenherkunft, 
-    auftrag_neudaten
-)
-
-SELECT 
-    basket.t_id AS t_basket,
-    'sturz' AS hauptprozess,
-    gefahrenstufe,
-    charakterisierung,
-    teilprozess,
-    st_multi(geometrie) AS geometrie,
-    'Neudaten' AS datenherkunft,
-    basket.attachmentkey AS auftrag_neudaten   
-FROM 
-    basket,
-    hauptprozess_sturz_dump
-WHERE 
-    ST_area(geometrie) > 0.001 
-    and 
-    charakterisierung is not null 
+DROP INDEX IF EXISTS splited_point;
+CREATE INDEX splited_point ON splited USING GIST (point);
