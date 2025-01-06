9a40f161388192b8194af5c279f6d4929b99ca03
Martin Schweizer <martin.schweizer@bd.so.ch>
2025-01-06 09:12:33 +0100

    [ada_denkmalschutz_pub] Neuer Pub-Job für Denkmalschutz

--- ada_denkmalschutz_pub/denkmal_polygon.sql
+++ ada_denkmalschutz_pub/denkmal_polygon.sql
@@ -1,35 +1,67 @@
 WITH 
 
 mpoly AS (
-	SELECT 
+SELECT 
 		denkmal_id,
-		ST_RemoveRepeatedPoints(st_multi(st_buffer(st_buffer(st_buffer(st_buffer(st_union(apolygon),0.01),-0.01),-0.01),0.01)),0.01) AS mpoly
--- TODO: Das Buffern und Re-Buffern ist definitiv nicht optimal. Allerdings ist es der einzige funktionierende Weg, die kleinen Digitalisierungsfehler zu eliminieren. 
---       Evtl. findet man hier aber in Zukunft eine bessere Lösung! 
-	FROM 
+		ST_RemoveRepeatedPoints(st_multi(st_buffer(st_buffer(st_buffer(st_buffer(st_union(apolygon),
+	0.01),
+	-0.01),
+	-0.01),
+	0.01)),
+	0.01) AS mpoly
+	-- TODO: Das Buffern und Re-Buffern ist definitiv nicht optimal. Allerdings ist es der einzige funktionierende Weg, die kleinen Digitalisierungsfehler zu eliminieren. 
+	--       Evtl. findet man hier aber in Zukunft eine bessere Lösung! 
+FROM 
 		ada_denkmalschutz.gis_geometrie
-	WHERE 
-		apolygon IS NOT NULL 
-	GROUP BY 
+WHERE 
+		apolygon IS NOT NULL
+GROUP BY 
 		denkmal_id
 )
+
+,rechtsvorschriften AS (
+SELECT
+	denkmal_id AS denkmal_id,
+	json_agg(json_build_object('Titel', titel, 'Link', 'http://irgendwas.so.ch/dokumente/' || multimedia_id || '.pdf', 'Datum', datum, 'Nummer', nummer)) AS dokumente
+FROM
+	ada_denkmalschutz.fachapplikation_rechtsvorschrift_link
+WHERE 
+    multimedia_id IS NOT NULL 
+GROUP BY
+	denkmal_id
+)
+
 
 SELECT 
 	id, 
 	objektname, 
 	gemeindename, 
 	gemeindeteil, 
-	adr_strasse, 
+	nullif(trim(adr_strasse),'') AS adr_strasse,
 	adr_hausnummer, 
 	objektart_code, 
 	objektart_text, 
 	schutzstufe_code, 
 	schutzstufe_text, 
 	schutzdurchgemeinde,
-	mpoly 
+	CASE 
+		WHEN schutzdurchgemeinde = TRUE 
+		THEN 'Ja'
+		ELSE 'Nein'
+	END AS schutzdurchgemeinde_txt,
+	mpoly, 
+	'https://irgendwas.so.ch/dokument/' || id || '.pdf' AS objektblatt, 
+	rechtsvorschriften.dokumente AS rechtsvorschriften
 FROM 
 	ada_denkmalschutz.fachapplikation_denkmal
 JOIN
 	mpoly 
-		ON fachapplikation_denkmal.id = mpoly.denkmal_id
+	ON 
+	fachapplikation_denkmal.id = mpoly.denkmal_id
+LEFT JOIN 
+    rechtsvorschriften
+    ON 
+    fachapplikation_denkmal.id = rechtsvorschriften.denkmal_id
 ;
+
+
--- ada_denkmalschutz_pub/denkmal_punkt.sql
+++ ada_denkmalschutz_pub/denkmal_punkt.sql
@@ -12,22 +12,49 @@ mpoint AS (
 		denkmal_id
 )
 
+,rechtsvorschriften AS (
+SELECT
+	denkmal_id AS denkmal_id,
+	json_agg(json_build_object('Titel', titel, 'Link', 'http://irgendwas.so.ch/dokumente/' || multimedia_id || '.pdf', 'Datum', datum, 'Nummer', nummer)) AS dokumente
+FROM
+	ada_denkmalschutz.fachapplikation_rechtsvorschrift_link
+WHERE 
+    multimedia_id IS NOT NULL 
+GROUP BY
+	denkmal_id
+)
+
 SELECT 
 	id, 
 	objektname, 
 	gemeindename, 
 	gemeindeteil, 
-	adr_strasse, 
+	nullif(trim(adr_strasse),'') AS adr_strasse, 
 	adr_hausnummer, 
 	objektart_code, 
 	objektart_text, 
 	schutzstufe_code, 
 	schutzstufe_text, 
 	schutzdurchgemeinde,
-	mpunkt
+	CASE 
+		WHEN schutzdurchgemeinde IS TRUE 
+		THEN 'Ja'
+		ELSE 'Nein'
+	END AS schutzdurchgemeinde_txt,
+	mpunkt,
+	'https://irgendwas.so.ch/dokument/' || id || '.pdf' AS objektblatt, 
+	rechtsvorschriften.dokumente AS rechtsvorschriften
 FROM 
 	ada_denkmalschutz.fachapplikation_denkmal
 JOIN
 	mpoint 
-		ON fachapplikation_denkmal.id = mpoint.denkmal_id
+	ON 
+	fachapplikation_denkmal.id = mpoint.denkmal_id
+LEFT JOIN 
+    rechtsvorschriften
+    ON 
+    fachapplikation_denkmal.id = rechtsvorschriften.denkmal_id
 ;
+
+
+
