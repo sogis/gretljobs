1ffd589965d2d399e9227c818b29615dbdc11582
Stefan Ziegler <edi.gonzales@gmail.com>
2025-02-24 10:56:44 +0100

    [agi_qm_betrieb] clean code

--- agi_qm_betrieb/simi_raster_ohne_datasetview.sql
+++ agi_qm_betrieb/simi_raster_ohne_datasetview.sql
@@ -5,7 +5,7 @@ ATTACH ${connectionStringSimi} AS simidb (TYPE POSTGRES, READ_ONLY);
 CREATE TEMP TABLE simiRasterOhneDatasetView AS
 SELECT 
     srd."path" AS raster_path,
-    srv.raster_ds_id::text 
+    srv.raster_ds_id::text AS raster_ds_id
 FROM 
     simidb.simi.simidata_raster_ds AS srd 
     LEFT JOIN simidb.simi.simidata_raster_view AS srv
--- agi_qm_betrieb/simi_schema_ohne_tabelle.sql
+++ agi_qm_betrieb/simi_schema_ohne_tabelle.sql
@@ -5,14 +5,17 @@ ATTACH ${connectionStringSimi} AS simidb (TYPE POSTGRES, READ_ONLY);
 CREATE TEMP TABLE simiSchemaOhneTabelle AS
 SELECT 
     sds.schema_name,
-    spt.table_name 
+    db.identifier
 FROM 
     simidb.simi.simidata_db_schema AS sds 
     LEFT JOIN simidb.simi.simidata_postgres_table AS spt 
     ON spt.db_schema_id = sds.id
+    LEFT JOIN simidb.simi.simidata_postgres_db AS db 
+    ON db.id = sds.postgres_db_id 
 WHERE 
     spt.table_name IS NULL 
 ;
+
 
 -- Funktioniert ab V1.2 ohne GDAL.
 COPY simiSchemaOhneTabelle TO '/tmp/qmbetrieb/simi_schema_ohne_tabelle.xlsx' WITH (FORMAT GDAL, DRIVER 'xlsx');
