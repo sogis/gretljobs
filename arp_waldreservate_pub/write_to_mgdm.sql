9370f4cc92d57ac049ccf6feed25ac7fc3beb224
Martin Schweizer <martin.schweizer@bd.so.ch>
2025-02-25 08:08:10 +0100

    [arp_waldreservate_pub] Inserted MGDM and ai upload

---
+++ arp_waldreservate_pub/write_to_mgdm.sql
@@ -0,0 +1,46 @@
+DELETE FROM arp_waldreservate_mgdm_v1.waldreservate_waldreservat_teilobjekt 
+;
+DELETE FROM arp_waldreservate_mgdm_v1.waldreservate_waldreservat 
+;
+
+INSERT INTO arp_waldreservate_mgdm_v1.waldreservate_waldreservat  (
+    t_id,
+    objnummer,
+    aname, 
+    obj_gesflaeche, 
+    obj_gisflaeche
+)
+SELECT 
+    t_id,
+    objnummer, 
+    aname, 
+    obj_gesflaeche, 
+    obj_gisflaeche
+FROM 
+    arp_waldreservate_v1.geobasisdaten_waldreservat
+;
+
+
+INSERT INTO arp_waldreservate_mgdm_v1.waldreservate_waldreservat_teilobjekt (
+    teilobj_nr, 
+    mcpfe_class, 
+    obj_gisteilobjekt, 
+    geo_obj, 
+    wr
+)
+SELECT 
+    teilobj_nr, 
+    code.t_id  AS mcpfe_code, 
+    obj_gisteilobjekt, 
+    geo_obj, 
+    wr
+FROM 
+    arp_waldreservate_v1.geobasisdaten_waldreservat_teilobjekt teilobjekt
+LEFT JOIN 
+    arp_waldreservate_mgdm_v1.codelisten_mcpfe_class_catalogue code 
+    ON 
+    replace(teilobjekt.mcpfe_code, '_', '.') = code.acode  
+;
+
+
+
