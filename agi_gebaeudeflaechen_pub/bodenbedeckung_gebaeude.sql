d2a96560ebd2533e1a4043e56809ffd2398f8a85
Stefan Ziegler <edi.gonzales@gmail.com>
2025-02-25 07:24:34 +0100

    [SO_AGI_Gebaeudeflaechen] adjust query to new model version

--- agi_gebaeudeflaechen_pub/bodenbedeckung_gebaeude.sql
+++ agi_gebaeudeflaechen_pub/bodenbedeckung_gebaeude.sql
@@ -23,7 +23,16 @@ INSERT INTO
         gebaeudename,
         gebaeudeeingang,
         link_gwr,
-        astatus
+        astatus,
+        gebaeudeklasse,
+        gebaeudeflaeche_gwr,
+        gebaeudeflaeche_av,
+        anzahl_geschosse,
+        energiebezugsflaeche,
+        waermeerzeuger_heizung_1,
+        energie_waermequelle_heizung_1,
+        waermeerzeuger_warmwasser_1,
+        energie_waermequelle_warmwasser_1
     )
 (
     SELECT
@@ -47,17 +56,29 @@ INSERT INTO
                 THEN concat('https://www.housing-stat.ch/de/query/egid.html?egid=',bodenbedeckung.egid::TEXT)
             ELSE 'https://www.housing-stat.ch/'
         END AS link_gwr,
-        'real' AS astatus
+        'real' AS astatus,
+        gg.gklas_txt AS gebaeudeklasse,
+        gg.garea AS gebaeudeflaeche_gwr,
+        CAST(ST_Area(bodenbedeckung.geometrie) AS int) AS gebaeudeflaeche_av,
+        gg.gastw AS anzahl_geschosse,
+        gg.gebf AS energiebezugsflaeche,
+        gg.gwaerzh1_txt AS waermeerzeuger_heizung_1,
+        gg.genh1_txt AS energie_waermequelle_heizung_1,
+        gg.gwaerzw1_txt AS waermeerzeuger_warmwasser_1,
+        gg.genw1_txt AS energie_waermequelle_warmwasser_1
     FROM 
         agi_mopublic_pub.mopublic_bodenbedeckung AS bodenbedeckung
         LEFT JOIN agi_mopublic_pub.mopublic_gebaeudeadresse AS adresse 
         ON ST_Contains(bodenbedeckung.geometrie, adresse.lage)
         LEFT JOIN gebaeudename
         ON gebaeudename.t_id = bodenbedeckung.t_id
+        LEFT JOIN agi_gwr_pub_v1.gwr_gebaeude AS gg 
+        ON gg.egid = bodenbedeckung.egid
     WHERE 
         bodenbedeckung.art_txt = 'Gebaeude'
     GROUP BY 
-        bodenbedeckung.egid, bodenbedeckung.bfs_nr, bodenbedeckung.geometrie
+        bodenbedeckung.egid, bodenbedeckung.bfs_nr, bodenbedeckung.geometrie, gebaeudeflaeche_av,
+        gg.gklas_txt, gg.garea, gg.gastw, gg.gebf, gg.gwaerzh1_txt, gg.genh1_txt, gg.gwaerzw1_txt, gg.genw1_txt  
 )
 ;
 
