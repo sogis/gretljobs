0d20749cf9bd502a5a63a9e8e2422ff3d19f4a7a
Sandra Curiger <38523407+bjsvwcur@users.noreply.github.com>
2025-04-07 14:52:29 +0200

    [alw_bund_hanglagen_allgemein_pub] add case

--- alw_bund_hanglagen_allgemein_pub/alw_bund_hanglagen_allgemein_pub_hl_allgemein.sql
+++ alw_bund_hanglagen_allgemein_pub/alw_bund_hanglagen_allgemein_pub_hl_allgemein.sql
@@ -1,2 +1,15 @@
-SELECT * FROM alw_bund_hanglagen_allgemein_v1.hl_allgemein
+SELECT 
+    lknr,
+    CASE 
+        WHEN hl_klasse = 'hang_18'
+            THEN 'A1'
+        WHEN hl_klasse = 'hang_18_35'
+            THEN 'A2'
+        WHEN hl_klasse = 'hang_35_50'
+            THEN 'A3'
+        WHEN hl_klasse = 'hang_50'
+            THEN 'A4'
+    END AS hl_klasse,
+    hl_neigung        
+FROM alw_bund_hanglagen_allgemein_v1.hl_allgemein
 ;
