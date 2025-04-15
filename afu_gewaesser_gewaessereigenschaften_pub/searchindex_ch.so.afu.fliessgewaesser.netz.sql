4a35c6d38c50e07f2506c9c0cce3260a4effa836
Sandra Curiger <38523407+bjsvwcur@users.noreply.github.com>
2025-04-15 12:21:33 +0200

    selete search_path

--- afu_gewaesser_gewaessereigenschaften_pub/searchindex_ch.so.afu.fliessgewaesser.netz.sql
+++ afu_gewaesser_gewaessereigenschaften_pub/searchindex_ch.so.afu.fliessgewaesser.netz.sql
@@ -1,5 +1,3 @@
-SET search_path to afu_gewaesser_gewaessereigenschaften_pub_v1;
-
 INSERT INTO ${db_schema}.feature (
     anzeige,            -- Anzeigetext
     suchbegriffe,       -- Suchbegriffe für den Index
@@ -20,7 +18,7 @@ index_base AS (
         'Fliessgewässer Name Nr'::text AS part_3,
         (st_asgeojson(st_envelope(st_collect(geometrie)), 0, 1)::json -> 'bbox'::text)::text AS bbox
     FROM
-        gewaessereigenschaften
+        afu_gewaesser_gewaessereigenschaften_pub_v1.gewaessereigenschaften
     WHERE
         aname IS NOT NULL
     GROUP BY
