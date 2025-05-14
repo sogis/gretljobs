cade22f350246a1baa4a72047d1bcc20bc9b892f
Christian Baumann <christian.baumann@bd.so.ch>
2025-05-14 15:10:43 +0200

    job migration

---
+++ afu_wasserbewirtschaftung_pub/searchindex_ch.so.afu.wasserbewirtschaftung.weitere_einbauten_geschuetzt.sql
@@ -0,0 +1,34 @@
+SET search_path to afu_wasserbewirtschaftung_pub_v2, public;
+
+INSERT INTO ${db_schema}.feature (
+    anzeige,            -- Anzeigetext
+    suchbegriffe,       -- Suchbegriffe für den Index
+    layer_ident,        -- Layer-Identifikation
+    ausdehnung,         -- Geometrische Ausdehnung als Text
+    id_feature,         -- ID des Features
+    id_spalten_name,    -- Spaltenname, z. B. 't_id'
+    id_in_hochkomma     -- Wahrheitswert für ID-In-Hochkomma
+)
+WITH
+index_base AS (
+    SELECT
+        'ch.so.afu.wasserbewirtschaftung.weitere_einbauten_geschuetzt'::text AS subclass,
+        t_id AS id_in_class,
+        concat('Nr: ', objektnummer, ' (VEGAS Einbauten)')  AS displaytext,
+        objektnummer AS part_1,
+        'Objekt Nr Einbauten'::text AS part_3,
+        (st_asgeojson(st_envelope(geometrie), 0, 1)::json -> 'bbox'::text)::text AS bbox
+    FROM
+        wassrbwrtschftung_grundwassereinbau_weitere_einbauten_v
+)
+SELECT
+    displaytext AS anzeige,
+    lower((part_1 || ' '::text) || index_base.part_3) AS suchbegriffe,
+    subclass AS layer_ident,
+    bbox as ausdehnung,
+    id_in_class::text AS id_feature,
+    't_id'::text as id_spalten_name,
+    false as id_in_hochkomma
+FROM
+    index_base
+;
