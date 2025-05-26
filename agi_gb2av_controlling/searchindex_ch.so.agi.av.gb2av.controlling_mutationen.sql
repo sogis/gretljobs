626f0bbb519f36938d48ace01fd69eafbe9be8a2
Christian Baumann <christian.baumann@bd.so.ch>
2025-05-26 11:04:40 +0200

    agi_gb2av_controlling

---
+++ agi_gb2av_controlling/searchindex_ch.so.agi.av.gb2av.controlling_mutationen.sql
@@ -0,0 +1,34 @@
+SET search_path to agi_gb2av_controlling_pub, public;
+
+INSERT INTO ${db_schema}.feature (
+    anzeige,            -- Anzeigetext
+    suchbegriffe,       -- Suchbegriffe für den Index
+    layer_ident,        -- Layer-Identifikation
+    ausdehnung,         -- Geometrische Ausdehnung als Text
+    id_feature,         -- ID des Features
+    id_spalten_name,    -- Spaltenname, z. B. 't_id'
+    id_in_hochkomma     -- Wahrheitswert für ID-In-Hochkomma
+)
+WITH
+index_base AS (
+    SELECT
+        'ch.so.agi.av.gb2av.controlling_mutationen'::text AS subclass,
+        t_id AS id_in_class,
+        concat(mutationsnummer, ' / ', nbident, ' (Mut.-Nr. AV-Mutation)') AS displaytext,
+        concat(mutationsnummer, ' ', nbident) AS part_1,
+        beschrieb::text AS part_3,
+        (st_asgeojson(st_envelope(perimeter), 0, 1)::json -> 'bbox'::text)::text AS bbox
+    FROM
+        controlling_av2gb_mutationen_v
+)
+SELECT
+    displaytext AS anzeige,
+    lower((part_1 || ' '::text) || index_base.part_3) AS suchbegriffe,
+    subclass AS layer_ident,
+    bbox as ausdehnung,
+    id_in_class::text AS id_feature,
+    't_id'::text as id_spalten_name,
+    false as id_in_hochkomma
+FROM
+    index_base
+;
---
+++ agi_gb2av_controlling/searchindex_ch.so.agi.av.gb2av.controlling_vollzugsmeldungen.sql
@@ -0,0 +1,34 @@
+SET search_path to agi_gb2av_controlling_pub, public;
+
+INSERT INTO ${db_schema}.feature (
+    anzeige,            -- Anzeigetext
+    suchbegriffe,       -- Suchbegriffe für den Index
+    layer_ident,        -- Layer-Identifikation
+    ausdehnung,         -- Geometrische Ausdehnung als Text
+    id_feature,         -- ID des Features
+    id_spalten_name,    -- Spaltenname, z. B. 't_id'
+    id_in_hochkomma     -- Wahrheitswert für ID-In-Hochkomma
+)
+WITH
+index_base AS (
+    SELECT
+        'ch.so.agi.av.gb2av.controlling_vollzugsmeldungen'::text AS subclass,
+        t_id AS id_in_class,
+        concat(mutationsnummer, ' / ', nbident, ' (Mut.-Nr. Vollzugsmeldung)') AS displaytext,
+        concat(mutationsnummer, ' ', nbident) AS part_1,
+        av_beschreibung::text AS part_3,
+        (st_asgeojson(st_envelope(perimeter), 0, 1)::json -> 'bbox'::text)::text AS bbox
+    FROM
+        controlling_gb2av_vollzugsmeldung_delta
+)
+SELECT
+    displaytext AS anzeige,
+    lower((part_1 || ' '::text) || index_base.part_3) AS suchbegriffe,
+    subclass AS layer_ident,
+    bbox as ausdehnung,
+    id_in_class::text AS id_feature,
+    't_id'::text as id_spalten_name,
+    false as id_in_hochkomma
+FROM
+    index_base
+;
