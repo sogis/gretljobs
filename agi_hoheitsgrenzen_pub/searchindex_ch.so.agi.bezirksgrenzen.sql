e2b5f1ec3443b11222f3343b7baef067a30873b3
Christian Baumann <christian.baumann@bd.so.ch>
2025-05-26 10:50:57 +0200

    agi_hoheitsgrenzen_pub

---
+++ agi_hoheitsgrenzen_pub/searchindex_ch.so.agi.bezirksgrenzen.sql
@@ -0,0 +1,34 @@
+SET search_path to agi_hoheitsgrenzen_pub, public;
+
+INSERT INTO ${db_schema}.feature (
+    anzeige,            -- Anzeigetext
+    suchbegriffe,       -- Suchbegriffe für den Index
+    layer_ident,        -- Layer-Identifikation
+    ausdehnung,         -- Geometrische Ausdehnung als Text
+    id_feature,         -- ID des Features
+    id_spalten_name,    -- Spaltenname, z. B. 't_id'
+    id_in_hochkomma     -- Wahrheitswert für ID-In-Hochkomma
+)
+WITH
+index_base AS (
+    SELECT
+        'ch.so.agi.bezirksgrenzen'::text AS subclass,
+        t_id AS id_in_class,
+        concat('Name: ', bezirksname, ' (Bezirk)') AS displaytext,
+        bezirksname AS part_1,
+        'Bezirk Name'::text AS part_3,
+        (st_asgeojson(st_envelope(geometrie), 0, 1)::json -> 'bbox'::text)::text AS bbox
+    FROM
+        hoheitsgrenzen_bezirksgrenze
+)
+SELECT
+    displaytext AS anzeige,
+    lower((part_1 || ' '::text) || index_base.part_3) AS suchbegriffe,
+    subclass AS layer_ident,
+    bbox as ausdehnung,
+    id_in_class::text AS id_feature,
+    't_id'::text as id_spalten_name,
+    false as id_in_hochkomma
+FROM
+    index_base
+;
