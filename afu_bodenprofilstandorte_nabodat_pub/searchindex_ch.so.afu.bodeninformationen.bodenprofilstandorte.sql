d60eb9d3296650c3043bfb868897dc31df9d300b
Christian Baumann <christian.baumann@bd.so.ch>
2025-06-24 15:36:25 +0200

    [afu_bodenprofilstandorte_nabodat_pub] fixed searchindex 2

--- afu_bodenprofilstandorte_nabodat_pub/searchindex_ch.so.afu.bodeninformationen.bodenprofilstandorte.sql
+++ afu_bodenprofilstandorte_nabodat_pub/searchindex_ch.so.afu.bodeninformationen.bodenprofilstandorte.sql
@@ -14,7 +14,7 @@ index_base AS (
     SELECT
         ${layername}::text AS subclass,
         t_id AS id_in_class,
-        concat('Nr: ', (profilnummer::text || ' '::text) || flurname::text, ' (Bodenprofil)') AS displaytext,
+        concat('Nr: ', profilnummer::text, ' '::text, flurname::text, ' (Bodenprofil)') AS displaytext,
         (COALESCE(profilnummer, '')::text || ' '::text) || COALESCE(flurname, '')::text AS part_1,
         'Boden Profil Nr'::text AS part_3,
         (st_asgeojson(st_envelope(geometrie), 0, 1)::json -> 'bbox'::text)::text AS bbox
