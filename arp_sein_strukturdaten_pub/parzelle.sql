6e7679d30746204eff8cd78ddd74049bd59b9819
Christa Rohrbach <thunwal@gmail.com>
2025-07-15 16:39:18 +0200

    [ARP_SEin_Strukturdaten] fehlerhafte Berechnungen gefixt; explizite Datentypen; beautify

--- arp_sein_strukturdaten_pub/parzelle.sql
+++ arp_sein_strukturdaten_pub/parzelle.sql
@@ -4,24 +4,43 @@
 -- GRUNDLAGEN -------------------------------------------------------------------------
 -- Mapping von GWR-Gebäudeklassen-Codes und -Beschreibungen
 drop table if exists export.gklas_10_map cascade;
-CREATE TABLE export.gklas_10_map AS
-SELECT * FROM (
-    VALUES 
-    (111, 'Wohnbauten'),
-    (112, 'Gebäude mit zwei oder mehr Wohnungen'),
-    (113, 'Wohngebäude für Gemeinschaften'),
-    (121, 'Hotels und ähnliche Gebäude'),
-    (122, 'Bürogebäude'),
-    (123, 'Gross- und Einzelhandelsgebäude'),
-    (124, 'Gebäude des Verkehrs- und Nachrichtenwesens'),
-    (125, 'Industrie- und Lagergebäude'),
-    (126, 'Gebäude für Kultur- und Freizeitzwecke sowie das Bildungs- und Gesundheitswesen'),
-    (127, 'Sonstige Nichtwohngebäude')
-) AS tbl(gklas_10, gklas_10_txt);
+
+create table export.gklas_10_map (
+    gklas_10		integer,
+    gklas_10_txt	text
+);
+
+insert into export.gklas_10_map values
+	(111, 'Wohnbauten'),
+	(112, 'Gebäude mit zwei oder mehr Wohnungen'),
+	(113, 'Wohngebäude für Gemeinschaften'),
+	(121, 'Hotels und ähnliche Gebäude'),
+	(122, 'Bürogebäude'),
+	(123, 'Gross- und Einzelhandelsgebäude'),
+	(124, 'Gebäude des Verkehrs- und Nachrichtenwesens'),
+	(125, 'Industrie- und Lagergebäude'),
+	(126, 'Gebäude für Kultur- und Freizeitzwecke sowie das Bildungs- und Gesundheitswesen'),
+	(127, 'Sonstige Nichtwohngebäude')
+;
 
 -- Alle Ausgangspolygone aus der Bauzonenstatistik (= Grundstück x Zone abzüglich Strassen etc.) plus Reservezonen
 drop table if exists export.parzellen_basis cascade;
-create table export.parzellen_basis as
+
+create table export.parzellen_basis (
+	t_ili_tid 					uuid,
+	geometrie 					public.geometry(MultiPolygon, 2056),
+	bfs_nr						integer,
+	typ_kt						text,
+	typ_bund					text,
+	bebauungsstand				text,
+	flaeche						numeric,
+	flaeche_bebaut				numeric,
+	flaeche_unbebaut			numeric,
+	flaeche_teilweise_bebaut	numeric,
+	pip							public.geometry(MultiPoint, 2056)
+);
+
+insert into export.parzellen_basis
 	with baustat as (
 		select 
 			t_ili_tid,
@@ -67,7 +86,7 @@ create table export.parzellen_basis as
 			ST_ReducePrecision(geometrie, 0.001) as geometrie, 
 			typ_kt, 
 			null::text as bebauungsstand, 
-			st_area(geometrie) as flaeche, 
+			ST_Area(geometrie) as flaeche, 
 			ST_PointOnSurface(geometrie) as pip
 	    from import.nutzungsplanung_grundnutzung
 	    where (ST_MaximumInscribedCircle(geometrie)).radius > 0.2
@@ -86,7 +105,7 @@ create table export.parzellen_basis as
 	    case when bebauungsstand = 'teilweise_bebaut' then flaeche else 0 end as flaeche_teilweise_bebaut,
 		pip
 	from polys
-	;
+;
 
 create index on export.parzellen_basis using gist (geometrie);
 create index on export.parzellen_basis using gist (pip);
@@ -96,17 +115,26 @@ create index on export.parzellen_basis (typ_kt);
 
 -- Verschnitt Parzelle x Bodenbedeckung
 drop table if exists export.parzellen_bodenbedeckung cascade;
-create table export.parzellen_bodenbedeckung as
+
+create table export.parzellen_bodenbedeckung (
+    t_ili_tid		uuid,
+	bfs_nr			integer,
+	typ_kt			text,
+    kategorie_text	text,
+    flaeche_agg		numeric
+);
+
+insert into export.parzellen_bodenbedeckung
 	select
 	    p.t_ili_tid,
 	    p.bfs_nr,
 	    p.typ_kt,
 	    mo.art_txt as kategorie_text,
-	    sum(st_area(st_intersection(p.geometrie, mo.geometrie, 0.001))) as flaeche_agg
+	    sum(ST_Area(ST_Intersection(p.geometrie, mo.geometrie, 0.001))) as flaeche_agg
 	from export.parzellen_basis p
-	join import.mopublic_bodenbedeckung mo on st_intersects(p.geometrie, mo.geometrie)
-	group by p.t_ili_tid, p.bfs_nr, p.typ_kt, mo.art_txt;
-	;
+	join import.mopublic_bodenbedeckung mo on ST_Intersects(p.geometrie, mo.geometrie)
+	group by p.t_ili_tid, p.bfs_nr, p.typ_kt, mo.art_txt
+;
 
 create index on export.parzellen_bodenbedeckung (t_ili_tid);
 create index on export.parzellen_bodenbedeckung (bfs_nr);
@@ -115,7 +143,24 @@ create index on export.parzellen_bodenbedeckung (kategorie_text);
 
 -- Join Parzelle x GWR Gebäude
 drop table if exists export.gebaeude cascade;
-create table export.gebaeude as
+
+create table export.gebaeude (
+    t_ili_tid		uuid,
+    typ_kt			text,
+    bfs_nr			integer,
+    egid			integer,
+    gkat			integer,
+    gkat_txt		text,
+    gklas_10		integer,
+    gklas_10_txt	text,
+    gbaup			integer,
+    gbaup_txt		text,
+    garea			numeric,
+    gastw			integer,
+	geometrie		public.geometry(Point, 2056)
+);
+
+insert into export.gebaeude
 	select
 	    p.t_ili_tid,
 	    p.typ_kt,
@@ -131,19 +176,31 @@ create table export.gebaeude as
 	    geb.gastw,       -- Anzahl Geschosse
 		geb.lage as geometrie
 	from export.parzellen_basis p
-	join import.gwr_gebaeude geb on st_within(geb.lage, p.geometrie)
+	join import.gwr_gebaeude geb on ST_Within(geb.lage, p.geometrie)
 	left join export.gklas_10_map g on left(geb.gklas::text,3)::int = g.gklas_10
 	where geb.gstat = 1004  -- nur existierende
-	;
+;
 
-CREATE INDEX ON export.gebaeude using gist (geometrie);
-CREATE INDEX ON export.gebaeude(t_ili_tid);
-CREATE INDEX ON export.gebaeude(typ_kt);
-CREATE INDEX ON export.gebaeude(bfs_nr);
+create index on export.gebaeude using gist (geometrie);
+create index on export.gebaeude(t_ili_tid);
+create index on export.gebaeude(typ_kt);
+create index on export.gebaeude(bfs_nr);
 
 -- Join Parzelle x GWR Wohnung
 drop table if exists export.wohnung cascade;
-create table export.wohnung as
+
+create table export.wohnung (
+    t_ili_tid		uuid,
+    typ_kt			text,
+    bfs_nr			integer,
+    egid			integer,
+	geometrie		public.geometry(Point, 2056),
+	ewid			integer,
+	warea			numeric,
+	wazim			integer
+);
+
+insert into export.wohnung
 	select
 	    g.t_ili_tid,
 	    g.typ_kt,
@@ -155,16 +212,25 @@ create table export.wohnung as
 	    w.wazim        -- Anzahl Zimmer
 	from export.gebaeude g
 	left join import.gwr_wohnung w using (egid)
-	;
+;
 
-CREATE INDEX ON export.wohnung using gist (geometrie);
-CREATE INDEX ON export.wohnung(t_ili_tid);
-CREATE INDEX ON export.wohnung(typ_kt);
-CREATE INDEX ON export.wohnung(bfs_nr);
+create index on export.wohnung using gist (geometrie);
+create index on export.wohnung(t_ili_tid);
+create index on export.wohnung(typ_kt);
+create index on export.wohnung(bfs_nr);
 
 -- Join Parzelle x STATPOP
 drop table if exists export.statpop cascade;
-create table export.statpop as
+
+create table export.statpop (
+    t_ili_tid			uuid,
+    typ_kt				text,
+    bfs_nr				integer,
+	geometrie			public.geometry(MultiPolygon, 2056),
+	classagefiveyears	integer
+);
+
+insert into export.statpop
 	select
 	    p.t_ili_tid,
 	    p.typ_kt,
@@ -172,16 +238,25 @@ create table export.statpop as
 		p.geometrie,
 	    pop.classagefiveyears
 	from export.parzellen_basis p
-	join import.statpop_statent_statpop pop on st_within(pop.geometrie, p.geometrie)
-	;
+	join import.statpop_statent_statpop pop on ST_Within(pop.geometrie, p.geometrie)
+;
 
-CREATE INDEX ON export.statpop(t_ili_tid);
-CREATE INDEX ON export.statpop(typ_kt);
-CREATE INDEX ON export.statpop(bfs_nr);
+create index on export.statpop(t_ili_tid);
+create index on export.statpop(typ_kt);
+create index on export.statpop(bfs_nr);
 
 -- Join Parzelle x STATENT
 drop table if exists export.statent cascade;
-create table export.statent as
+
+create table export.statent (
+    t_ili_tid	uuid,
+    typ_kt		text,
+    bfs_nr		integer,
+	geometrie	public.geometry(MultiPolygon, 2056),
+	empfte		numeric
+);
+
+insert into export.statent
 	select
 	    p.t_ili_tid,
 	    p.typ_kt,
@@ -189,12 +264,12 @@ create table export.statent as
 		p.geometrie,
 	    ent.empfte
 	from export.parzellen_basis p
-	join import.statpop_statent_statent ent on st_within(ent.geometrie, p.geometrie)
-	;
+	join import.statpop_statent_statent ent on ST_Within(ent.geometrie, p.geometrie)
+;
 
-CREATE INDEX ON export.statent(t_ili_tid);
-CREATE INDEX ON export.statent(typ_kt);
-CREATE INDEX ON export.statent(bfs_nr);
+create index on export.statent(t_ili_tid);
+create index on export.statent(typ_kt);
+create index on export.statent(bfs_nr);
 
 -- AGGREGATIONEN -------------------------------------------------------------------------
 -- GRUNDNUTZUNG: Aggregiert auf Parzellen-Ebene (Array) (faktisch keine Aggregation nötig)
@@ -213,7 +288,7 @@ create table export.parzellen_grundnutzungen_array as
 	        'Flaeche', round(flaeche::numeric, 2)
 	    )) as grundnutzungen_bund
 	FROM export.parzellen_basis
-	;
+;
 
 -- BODENBEDECKUNG: Aggregiert auf Parzellen-Ebene (Array)
 drop table if exists export.parzellen_bodenbedeckungen_array cascade;
@@ -228,7 +303,7 @@ create table export.parzellen_bodenbedeckungen_array as
 	from export.parzellen_bodenbedeckung
 	where round(flaeche_agg::numeric, 2) > 0
 	group by t_ili_tid
-	;
+;
 
 -- GWR GEBÄUDE: Aggregiert auf Parzellen-Ebene (Arrays)
 drop table if exists export.parzellen_gwr_array cascade;
@@ -236,95 +311,117 @@ create table export.parzellen_gwr_array as
 	select
 	    t_ili_tid,
 	    (
-	        SELECT jsonb_agg(jsonb_build_object(
+	        select jsonb_agg(jsonb_build_object(
 	            '@type', 'SO_ARP_SEin_Strukturdaten_Publikation_20250407.Strukturdaten.Gebaeudekategorie',
 	            'Kategorie_Id', gkat_group.gkat,
 	            'Kategorie_Text', gkat_group.gkat_txt,
 	            'Anzahl', gkat_group.anzahl
 	        ))
-	        FROM (
-	            SELECT gkat, gkat_txt, COUNT(*) as anzahl
-	            FROM export.gebaeude g2
-	            WHERE g2.t_ili_tid = g1.t_ili_tid
+	        from (
+	            select gkat, gkat_txt, count(*) as anzahl
+	            from export.gebaeude g2
+	            where g2.t_ili_tid = g1.t_ili_tid
 	            and gkat is not null
-	            GROUP BY gkat, gkat_txt
+	            group by gkat, gkat_txt
 	        ) gkat_group
 	    ) gebaeudekategorien,
 	    (
-	        SELECT jsonb_agg(jsonb_build_object(
+	        select jsonb_agg(jsonb_build_object(
 	            '@type', 'SO_ARP_SEin_Strukturdaten_Publikation_20250407.Strukturdaten.Gebaeudeklasse_10',
 	            'Kategorie_Id', gklas_group.gklas_10,
 	            'Kategorie_Text', gklas_group.gklas_10_txt,
 	            'Anzahl', gklas_group.anzahl
 	        ))
-	        FROM (
-	            SELECT gklas_10, gklas_10_txt, COUNT(*) as anzahl
-	            FROM export.gebaeude g2
-	            WHERE g2.t_ili_tid = g1.t_ili_tid
+	        from (
+	            select gklas_10, gklas_10_txt, count(*) as anzahl
+	            from export.gebaeude g2
+	            where g2.t_ili_tid = g1.t_ili_tid
 	            and gklas_10 is not null
-	            GROUP BY gklas_10, gklas_10_txt
+	            group by gklas_10, gklas_10_txt
 	        ) gklas_group
 	    ) gebaeudeklassen_10,
 	    (
-	        SELECT jsonb_agg(jsonb_build_object(
+	        select jsonb_agg(jsonb_build_object(
 	            '@type', 'SO_ARP_SEin_Strukturdaten_Publikation_20250407.Strukturdaten.Gebaeudebauperiode',
 	            'Kategorie_Id', gbaup_group.gbaup,
 	            'Kategorie_Text', gbaup_group.gbaup_txt,
 	            'Anzahl', gbaup_group.anzahl
 	        ))
-	        FROM (
-	            SELECT gbaup, gbaup_txt, COUNT(*) as anzahl
-	            FROM export.gebaeude g2
-	            WHERE g2.t_ili_tid = g1.t_ili_tid
+	        from (
+	            select gbaup, gbaup_txt, count(*) as anzahl
+	            from export.gebaeude g2
+	            where g2.t_ili_tid = g1.t_ili_tid
 	            and gbaup is not null
-	            GROUP BY gbaup, gbaup_txt
+	            group by gbaup, gbaup_txt
 	        ) gbaup_group
 	    ) gebaeudebauperioden,
 	    (
-	        SELECT jsonb_agg(jsonb_build_object(
+	        select jsonb_agg(jsonb_build_object(
 	            '@type', 'SO_ARP_SEin_Strukturdaten_Publikation_20250407.Strukturdaten.Anzahl_Zimmer',
 	            'Kategorie_Id', wazim_group.wazim_cat,
 	            'Kategorie_Text', case when wazim_cat >= 6 then '6+ Zimmer' else concat(wazim_cat, ' Zimmer') end,
 	            'Anzahl', wazim_group.anzahl
 	        ))
-	        FROM (
-	            SELECT 
+	        from (
+	            select 
 	            	case when wazim >= 6 then 6 else wazim end as wazim_cat,
-	            	COUNT(*) as anzahl
-	            FROM export.wohnung w
-	            WHERE w.t_ili_tid = g1.t_ili_tid
+	            	count(*) as anzahl
+	            from export.wohnung w
+	            where w.t_ili_tid = g1.t_ili_tid
 	            and wazim is not null
-	            GROUP by case when wazim >= 6 then 6 else wazim end
+	            group by case when wazim >= 6 then 6 else wazim end
 	        ) wazim_group
 	    ) verteilung_anzahl_zimmer
-    FROM export.parzellen_basis g1
-	;
+    from export.parzellen_basis g1
+;
 
 -- GWR GEBÄUDE: Aggregiert auf Parzellen-Ebene (Summen und Anzahlen)
 drop table if exists export.parzellen_gwr_geb_agg cascade;
-create table export.parzellen_gwr_geb_agg as
-    SELECT
+
+create table export.parzellen_gwr_geb_agg (
+    t_ili_tid					uuid,
+    total_gebaeude				integer,
+    flaeche_gebaeude			numeric,
+    flaeche_gebaeude_anz_null	integer,
+    total_geschosse				integer,
+    anzahl_geschosse_avg		numeric,
+    anzahl_geschosse_anz_null	integer   
+);
+
+insert into export.parzellen_gwr_geb_agg
+    select
         t_ili_tid,
-        COUNT(egid) AS total_gebaeude,
+        count(egid) as total_gebaeude,
         sum(garea) as flaeche_gebaeude,
-        COUNT(*) FILTER (WHERE garea IS NULL) AS flaeche_gebaeude_anz_null,
-        SUM(gastw) AS total_geschosse,
-        round(AVG(gastw),2) AS anzahl_geschosse_avg,
-        COUNT(*) FILTER (WHERE gastw IS NULL) AS anzahl_geschosse_anz_null
-    FROM export.gebaeude
-    GROUP BY t_ili_tid
-    ;
+        count(*) filter (where garea is null) as flaeche_gebaeude_anz_null,
+        sum(gastw) as total_geschosse,
+        round(avg(gastw),2) as anzahl_geschosse_avg,
+        count(*) filter (where gastw is null) as anzahl_geschosse_anz_null
+    from export.gebaeude
+    group by t_ili_tid
+;
 
  -- GWR WOHNUNG: Aggregiert auf Parzellen-Ebene (Summen und Anzahlen)
 drop table if exists export.parzellen_gwr_wohn_agg cascade;
-create table export.parzellen_gwr_wohn_agg as
+
+create table export.parzellen_gwr_wohn_agg (
+    t_ili_tid					uuid,
+	total_wohnungen				integer,
+    anzahl_wohnungen_avg		numeric,
+	flaeche_wohnungen			numeric,
+	flaeche_wohnung_avg			numeric,
+	flaeche_wohnung_anz_null	integer,
+	total_zimmer				integer,
+	anzahl_zimmer_avg			numeric,
+	anzahl_zimmer_anz_null		integer
+);
+
+insert into export.parzellen_gwr_wohn_agg
 	select
 		t_ili_tid,
-		COUNT(ewid) as total_wohnungen,
-        CASE 
-            WHEN COUNT(DISTINCT egid) = 0 THEN 0 
-            ELSE round((COUNT(DISTINCT ewid) / COUNT(DISTINCT egid)),2)
-        END AS anzahl_wohnungen_avg,
+		count(ewid) as total_wohnungen,
+        -- mind. 1 Wert muss nach numeric gecastet werden, damit keine integer division durchgeführt wird
+    	round((count(ewid) / count(distinct egid)::numeric), 2) as anzahl_wohnungen_avg,
 		sum(warea) as flaeche_wohnungen,
 		round(avg(warea),2) as flaeche_wohnung_avg,
 		count(*) filter (where warea is null) flaeche_wohnung_anz_null,
@@ -333,14 +430,14 @@ create table export.parzellen_gwr_wohn_agg as
 		count(*) filter (where wazim is null) anzahl_zimmer_anz_null
 	from export.wohnung
 	group by t_ili_tid
-	;
+;
 
 -- STATPOP: Aggregiert auf Parzellen-Ebene
 drop table if exists export.parzellen_statpop_array cascade;
 create table export.parzellen_statpop_array as
-    SELECT 
+    select 
     	t_ili_tid,
-    	sum(anzahl) as popcount,
+    	sum(anzahl)::integer as popcount,
     	jsonb_agg(jsonb_build_object(
 	        '@type', 'SO_ARP_SEin_Strukturdaten_Publikation_20250407.Strukturdaten.Altersklasse_5j',
 	        'Kategorie_Id', classagefiveyears,
@@ -352,11 +449,11 @@ create table export.parzellen_statpop_array as
 			,
 	        'Anzahl', anzahl
 		)) altersklassen_5j
-    FROM (
-        SELECT t_ili_tid, classagefiveyears, COUNT(*) as anzahl
-        FROM export.statpop
+    from (
+        select t_ili_tid, classagefiveyears, count(*) as anzahl
+        from export.statpop
         where classagefiveyears is not null
-        GROUP BY t_ili_tid, classagefiveyears
+        group by t_ili_tid, classagefiveyears
     ) agg
    group by t_ili_tid;
 
@@ -368,7 +465,7 @@ create table export.parzellen_statent_agg as
 		sum(empfte) as beschaeftigte_fte
 	from export.statent
 	group by t_ili_tid
-	;
+;
 
 -- FINALE TABELLE BEFÜLLEN (wird in nachgelagertem Task in die pub-db kopiert)
 delete from export.strukturdaten_parzelle;
@@ -397,8 +494,10 @@ select
 	a.bfs_nr as gemeindenummer,
     h.altersklassen_5j,
 	coalesce(i.beschaeftigte_fte, 0) as beschaeftigte_fte,
-	coalesce(a.flaeche / (h.popcount + i.beschaeftigte_fte), 0) as raumnutzendendichte,
-	coalesce((h.popcount + i.beschaeftigte_fte) / a.flaeche * 10000, 0) as flaechendichte,
+	-- Raumnutzendendichte: division by zero vermeiden mit NULLIF
+	a.flaeche / nullif(coalesce(h.popcount, 0) + coalesce(i.beschaeftigte_fte, 0), 0) as raumnutzendendichte,
+	-- Flächendichte: Umrechnung von m2 auf ha = Faktor 10'000
+	(coalesce(h.popcount, 0) + coalesce(i.beschaeftigte_fte, 0)) / a.flaeche * 10000 as flaechendichte,
 	grundnutzungen_kanton,
 	grundnutzungen_bund,
 	c.gebaeudekategorien,
