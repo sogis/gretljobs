0a8cd28dd4ccfccfbcc732cf96cacd8b7d7aff48
Christa Rohrbach <thunwal@gmail.com>
2025-07-15 16:58:23 +0200

    [ARP_SEin_Strukturdaten] fehlerhafte Berechnungen gefixt; explizite Datentypen; beautify

--- arp_sein_strukturdaten_pub/zonentyp.sql
+++ arp_sein_strukturdaten_pub/zonentyp.sql
@@ -3,13 +3,26 @@
 
 -- Zonentyp
 drop table if exists export.zonentyp_basis cascade;
-create table export.zonentyp_basis as
+
+create table export.zonentyp_basis (
+	geometrie 					public.geometry(MultiPolygon, 2056),
+	bfs_nr						integer,
+	typ_kt						text,
+	typ_bund					text,
+	bebauungsstand				text,
+	flaeche						numeric,
+	flaeche_bebaut				numeric,
+	flaeche_unbebaut			numeric,
+	flaeche_teilweise_bebaut	numeric
+);
+
+insert into export.zonentyp_basis
 	select
-		st_union(
-			st_makevalid(
+		ST_Union(
+			ST_MakeValid(
 				ST_ReducePrecision(
-		        	st_buffer(
-		        		st_buffer(
+		        	ST_Buffer(
+		        		ST_Buffer(
 		        			geometrie, 0.25, 'join=mitre'
 		        		), -0.25, 'join=mitre'
 		        	), 0.001
@@ -19,14 +32,14 @@ create table export.zonentyp_basis as
 	    bfs_nr,
 	    typ_kt,
 	    typ_bund,
-	    null::text as bebauungsstand,  -- alternativ: Array -> Modelländerung
-	    st_area(st_union(geometrie)) as flaeche,
-	    coalesce(sum(st_area(geometrie)) filter (where bebauungsstand = 'bebaut'), 0) as flaeche_bebaut,
-	    coalesce(sum(st_area(geometrie)) filter (where bebauungsstand = 'unbebaut'), 0) as flaeche_unbebaut,
-	    coalesce(sum(st_area(geometrie)) filter (where bebauungsstand = 'teilweise_bebaut'), 0) as flaeche_teilweise_bebaut
+	    null as bebauungsstand,
+	    ST_Area(ST_Union(geometrie)) as flaeche,
+	    coalesce(sum(ST_Area(geometrie)) filter (where bebauungsstand = 'bebaut'), 0) as flaeche_bebaut,
+	    coalesce(sum(ST_Area(geometrie)) filter (where bebauungsstand = 'unbebaut'), 0) as flaeche_unbebaut,
+	    coalesce(sum(ST_Area(geometrie)) filter (where bebauungsstand = 'teilweise_bebaut'), 0) as flaeche_teilweise_bebaut
 	from export.parzellen_basis
 	group by typ_kt, typ_bund, bfs_nr
-	;
+;
 
 create index on export.zonentyp_basis using gist (geometrie);
 create index on export.zonentyp_basis (typ_kt);
@@ -34,7 +47,15 @@ create index on export.zonentyp_basis (bfs_nr);
 
 -- Zonentyp x Bodenbedeckung
 drop table if exists export.zonentyp_bodenbedeckung cascade;
-create table export.zonentyp_bodenbedeckung as
+
+create table export.zonentyp_bodenbedeckung (
+    bfs_nr			integer,
+    typ_kt			text,
+    kategorie_text	text,
+    flaeche_agg		numeric
+);
+
+insert into export.zonentyp_bodenbedeckung
 	select
 	    bfs_nr,
 	    typ_kt,
@@ -42,7 +63,7 @@ create table export.zonentyp_bodenbedeckung as
 	    sum(flaeche_agg) as flaeche_agg
 	from export.parzellen_bodenbedeckung
 	group by typ_kt, bfs_nr, kategorie_text
-	;
+;
 
 create index on export.zonentyp_bodenbedeckung (typ_kt);
 create index on export.zonentyp_bodenbedeckung (bfs_nr);
@@ -54,18 +75,18 @@ create table export.zonentyp_grundnutzungen_array as
 	select
 		typ_kt,
 		bfs_nr,
-	    jsonb_build_array(jsonb_BUILD_OBJECT(
+	    jsonb_build_array(jsonb_build_object(
 	    	'@type', 'SO_ARP_SEin_Strukturdaten_Publikation_20250407.Strukturdaten.Grundnutzung_Kanton',
 	    	'Kategorie_Text', typ_kt,
 	    	'Flaeche', round(flaeche::numeric, 2)
 	    )) as grundnutzungen_kanton,
-	    jsonb_build_array(jsonb_BUILD_OBJECT(
+	    jsonb_build_array(jsonb_build_object(
 	        '@type', 'SO_ARP_SEin_Strukturdaten_Publikation_20250407.Strukturdaten.Grundnutzung_Bund',
 	        'Kategorie_Text', typ_bund,
 	        'Flaeche', round(flaeche::numeric, 2)
 	    )) as grundnutzungen_bund
 	FROM export.zonentyp_basis
-	;
+;
 
 -- BODENBEDECKUNG: Aggregiert auf Zonentyp-Ebene (Array)
 drop table if exists export.zonentyp_bodenbedeckungen_array cascade;
@@ -79,9 +100,9 @@ create table export.zonentyp_bodenbedeckungen_array as
 			'Flaeche', round(flaeche_agg::numeric, 2)
 		)) as bodenbedeckungen
 	from export.zonentyp_bodenbedeckung
-	where round(flaeche_agg::numeric, 2) > 0
+	where round(flaeche_agg, 2) > 0
 	group by typ_kt, bfs_nr
-	;
+;
 
 -- GWR: Gebäudedaten aggregiert auf Zonentyp-Ebene (Arrays)
 drop table if exists export.zonentyp_gwr_array cascade;
@@ -90,97 +111,121 @@ create table export.zonentyp_gwr_array as
 	    typ_kt,
 		bfs_nr,
 	    (
-	        SELECT jsonb_agg(jsonb_build_object(
+	        select jsonb_agg(jsonb_build_object(
 	            '@type', 'SO_ARP_SEin_Strukturdaten_Publikation_20250407.Strukturdaten.Gebaeudekategorie',
 	            'Kategorie_Id', gkat_group.gkat,
 	            'Kategorie_Text', gkat_group.gkat_txt,
 	            'Anzahl', gkat_group.anzahl
 	        ))
-	        FROM (
-	            SELECT gkat, gkat_txt, COUNT(*) as anzahl
-	            FROM export.gebaeude g2
-	            WHERE g2.typ_kt = g1.typ_kt
+	        from (
+	            select gkat, gkat_txt, count(*) as anzahl
+	            from export.gebaeude g2
+	            where g2.typ_kt = g1.typ_kt
 	            and gkat is not null
-	            GROUP BY gkat, gkat_txt
+	            group by gkat, gkat_txt
 	        ) gkat_group
 	    ) gebaeudekategorien,
 	    (
-	        SELECT jsonb_agg(jsonb_build_object(
+	        select jsonb_agg(jsonb_build_object(
 	            '@type', 'SO_ARP_SEin_Strukturdaten_Publikation_20250407.Strukturdaten.Gebaeudeklasse_10',
 	            'Kategorie_Id', gklas_group.gklas_10,
 	            'Kategorie_Text', gklas_group.gklas_10_txt,
 	            'Anzahl', gklas_group.anzahl
 	        ))
-	        FROM (
-	            SELECT gklas_10, gklas_10_txt, COUNT(*) as anzahl
-	            FROM export.gebaeude g2
-	            WHERE g2.typ_kt = g1.typ_kt
+	        from (
+	            select gklas_10, gklas_10_txt, count(*) as anzahl
+	            from export.gebaeude g2
+	            where g2.typ_kt = g1.typ_kt
 	            and gklas_10 is not null
-	            GROUP BY gklas_10, gklas_10_txt
+	            group by gklas_10, gklas_10_txt
 	        ) gklas_group
 	    ) gebaeudeklassen_10,
 	    (
-	        SELECT jsonb_agg(jsonb_build_object(
+	        select jsonb_agg(jsonb_build_object(
 	            '@type', 'SO_ARP_SEin_Strukturdaten_Publikation_20250407.Strukturdaten.Gebaeudebauperiode',
 	            'Kategorie_Id', gbaup_group.gbaup,
 	            'Kategorie_Text', gbaup_group.gbaup_txt,
 	            'Anzahl', gbaup_group.anzahl
 	        ))
-	        FROM (
-	            SELECT gbaup, gbaup_txt, COUNT(*) as anzahl
-	            FROM export.gebaeude g2
-	            WHERE g2.typ_kt = g1.typ_kt
+	        from (
+	            select gbaup, gbaup_txt, count(*) as anzahl
+	            from export.gebaeude g2
+	            where g2.typ_kt = g1.typ_kt
 	            and gbaup is not null
-	            GROUP BY gbaup, gbaup_txt
+	            group by gbaup, gbaup_txt
 	        ) gbaup_group
 	    ) gebaeudebauperioden,
 	    (
-	        SELECT jsonb_agg(jsonb_build_object(
+	        select jsonb_agg(jsonb_build_object(
 	            '@type', 'SO_ARP_SEin_Strukturdaten_Publikation_20250407.Strukturdaten.Anzahl_Zimmer',
 	            'Kategorie_Id', wazim_group.wazim_cat,
 	            'Kategorie_Text', case when wazim_cat >= 6 then '6+ Zimmer' else concat(wazim_cat, ' Zimmer') end,
 	            'Anzahl', wazim_group.anzahl
 	        ))
-	        FROM (
-	            SELECT 
+	        from (
+	            select 
 	            	case when wazim >= 6 then 6 else wazim end as wazim_cat,
-	            	COUNT(*) as anzahl
-	            FROM export.wohnung w
-	            WHERE w.typ_kt = g1.typ_kt
+	            	count(*) as anzahl
+	            from export.wohnung w
+	            where w.typ_kt = g1.typ_kt
 	            and wazim is not null
-	            GROUP by case when wazim >= 6 then 6 else wazim end
+	            group by case when wazim >= 6 then 6 else wazim end
 	        ) wazim_group
 	    ) verteilung_anzahl_zimmer
-    FROM export.zonentyp_basis g1
-	;
+    from export.zonentyp_basis g1
+;
 
 -- GWR: Gebäudedaten aggregiert auf Zonentyp-Ebene (Summen und Anzahlen)
 drop table if exists export.zonentyp_gwr_geb_agg cascade;
-create table export.zonentyp_gwr_geb_agg as
-    SELECT
+
+create table export.zonentyp_gwr_geb_agg (
+    typ_kt						text,
+    bfs_nr						integer,
+    total_gebaeude				integer,
+    flaeche_gebaeude			numeric,
+    flaeche_gebaeude_anz_null	integer,
+    total_geschosse				integer,
+    anzahl_geschosse_avg		numeric,
+    anzahl_geschosse_anz_null	integer   
+);
+
+insert into export.zonentyp_gwr_geb_agg
+    select
         typ_kt,
 		bfs_nr,
-        COUNT(egid) AS total_gebaeude,
+        count(egid) as total_gebaeude,
         sum(garea) as flaeche_gebaeude,
-        COUNT(*) FILTER (WHERE garea IS NULL) AS flaeche_gebaeude_anz_null,
-        SUM(gastw) AS total_geschosse,
-        round(AVG(gastw),2) AS anzahl_geschosse_avg,
-        COUNT(*) FILTER (WHERE gastw IS NULL) AS anzahl_geschosse_anz_null
-    FROM export.gebaeude
-    GROUP BY typ_kt, bfs_nr
-    ;
+        count(*) filter (where garea is null) as flaeche_gebaeude_anz_null,
+        sum(gastw) as total_geschosse,
+        round(avg(gastw),2) as anzahl_geschosse_avg,
+        count(*) filter (where gastw is null) as anzahl_geschosse_anz_null
+    from export.gebaeude
+    group by typ_kt, bfs_nr
+;
 
  -- GWR: Wohnungsdaten aggregiert auf Zonentyp-Ebene (Summen und Anzahlen)
 drop table if exists export.zonentyp_gwr_wohn_agg cascade;
-create table export.zonentyp_gwr_wohn_agg as
+
+create table export.zonentyp_gwr_wohn_agg (
+    typ_kt						text,
+    bfs_nr						integer,
+	total_wohnungen				integer,
+    anzahl_wohnungen_avg		numeric,
+	flaeche_wohnungen			numeric,
+	flaeche_wohnung_avg			numeric,
+	flaeche_wohnung_anz_null	integer,
+	total_zimmer				integer,
+	anzahl_zimmer_avg			numeric,
+	anzahl_zimmer_anz_null		integer
+);
+
+insert into export.zonentyp_gwr_wohn_agg
 	select
 		typ_kt,
 		bfs_nr,
-		COUNT(ewid) as total_wohnungen,
-        CASE 
-            WHEN COUNT(DISTINCT egid) = 0 THEN 0 
-            ELSE round((COUNT(DISTINCT ewid) / COUNT(DISTINCT egid)),2)
-        END AS anzahl_wohnungen_avg,
+		count(ewid) as total_wohnungen,
+        -- mind. 1 Wert muss nach numeric gecastet werden, damit keine integer division durchgeführt wird
+    	round((count(ewid) / count(distinct egid)::numeric), 2) as anzahl_wohnungen_avg,
 		sum(warea) as flaeche_wohnungen,
 		round(avg(warea),2) as flaeche_wohnung_avg,
 		count(*) filter (where warea is null) flaeche_wohnung_anz_null,
@@ -189,12 +234,12 @@ create table export.zonentyp_gwr_wohn_agg as
 		count(*) filter (where wazim is null) anzahl_zimmer_anz_null
 	from export.wohnung
 	group by typ_kt, bfs_nr
-	;
+;
 
 -- STATPOP: Aggregiert auf Zonentyp-Ebene
 drop table if exists export.zonentyp_statpop_array cascade;
 create table export.zonentyp_statpop_array as
-    SELECT 
+    select 
 		typ_kt,
 		bfs_nr,
     	sum(anzahl) as popcount,
@@ -209,14 +254,14 @@ create table export.zonentyp_statpop_array as
 			,
 	        'Anzahl', anzahl
 		)) altersklassen_5j
-    FROM (
-        SELECT typ_kt, bfs_nr, classagefiveyears, COUNT(*) as anzahl
-        FROM export.statpop
+    from (
+        select typ_kt, bfs_nr, classagefiveyears, count(*) as anzahl
+        from export.statpop
         where classagefiveyears is not null
-        GROUP BY typ_kt, bfs_nr, classagefiveyears
+        group by typ_kt, bfs_nr, classagefiveyears
     ) agg
-    GROUP BY typ_kt, bfs_nr
-    ;
+    group by typ_kt, bfs_nr
+;
 
 -- STATENT: Aggregiert auf Zonentyp-Ebene
 drop table if exists export.zonentyp_statent_agg cascade;
@@ -226,8 +271,8 @@ create table export.zonentyp_statent_agg as
 		bfs_nr,
 		sum(empfte) as beschaeftigte_fte
 	from export.statent
-	GROUP BY typ_kt, bfs_nr
-	;
+	group by typ_kt, bfs_nr
+;
 
 delete from export.strukturdaten_zonentyp;
 insert into export.strukturdaten_zonentyp (geometrie, 
@@ -255,8 +300,10 @@ select
 	a.bfs_nr as gemeindenummer,
     h.altersklassen_5j,
 	coalesce(i.beschaeftigte_fte, 0) as beschaeftigte_fte,
-	coalesce(a.flaeche / (h.popcount + i.beschaeftigte_fte), 0) as raumnutzendendichte,
-	coalesce((h.popcount + i.beschaeftigte_fte) / a.flaeche * 10000, 0) as flaechendichte,
+	-- Raumnutzendendichte: division by zero vermeiden mit NULLIF
+	a.flaeche / nullif(coalesce(h.popcount, 0) + coalesce(i.beschaeftigte_fte, 0), 0) as raumnutzendendichte,
+	-- Flächendichte: Umrechnung von m2 auf ha = Faktor 10'000
+	(coalesce(h.popcount, 0) + coalesce(i.beschaeftigte_fte, 0)) / a.flaeche * 10000 as flaechendichte,
 	grundnutzungen_kanton,
 	grundnutzungen_bund,
 	c.gebaeudekategorien,
