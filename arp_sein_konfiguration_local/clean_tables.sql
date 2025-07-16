242c9c948cc05ccfd1cce681d5a5139f66b9d284
Patrick Bur <156295083+bjsvwbur@users.noreply.github.com>
2025-07-16 14:56:17 +0200

    Delete arp_sein_konfiguration_local/clean_tables.sql

--- arp_sein_konfiguration_local/clean_tables.sql
+++
@@ -1,7 +0,0 @@
--- delete everything that isn't necessary for the xtf-export --
-
-DELETE FROM sein.arp_sein_konfiguration_grundlagen_v2.gemeinde_objektinfo;
-DELETE FROM sein.arp_sein_konfiguration_grundlagen_v2.so_rp_s0250115grundlagen_gemeinde;
-DELETE FROM sein.arp_sein_konfiguration_grundlagen_v2.so_rp_s0250115grundlagen_objektinfo;
-DELETE FROM sein.arp_sein_konfiguration_grundlagen_v2.so_rp_s0250115grundlagen_thema;
-DELETE FROM sein.arp_sein_konfiguration_grundlagen_v2.so_rp_s0250115grundlagen_gruppe;
