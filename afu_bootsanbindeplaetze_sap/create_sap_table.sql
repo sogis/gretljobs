cb54d69ebfe648d222e857650464376c5c5e46a3
bjsvwbur <patrick.bur@bd.so.ch>
2025-09-16 11:51:52 +0200

    [afu_bootsanbindeplaetze] first version sap-job

---
+++ afu_bootsanbindeplaetze_sap/create_sap_table.sql
@@ -0,0 +1,38 @@
+DROP TABLE IF EXISTS sap_structure;
+
+--- Create Table for intersecting objects --- 
+CREATE TABLE sap_structure (
+	AuftrArt VARCHAR,
+	VerkOrg VARCHAR,
+	VertrWeg VARCHAR,
+	Sparte VARCHAR,
+	Verkaufsbüro VARCHAR,
+	KundenNr INTEGER,
+	"Bestellnummer des Kunden" VARCHAR,
+	BestellDatum VARCHAR,
+	Wunschlieferdatum VARCHAR,
+	Preisdatum VARCHAR,
+	"MaterialNr." VARCHAR,
+	Materialtext VARCHAR,
+	"Betrag 2Komma" DOUBLE,
+	"Menge Ganzahlg" INTEGER,
+	Auftragsnummer VARCHAR,
+	Profitcenter VARCHAR,
+	"MaterialVerkaufstext Zeile 1 Position" VARCHAR,
+	"MaterialVerkaufstext Zeile 2 Position" VARCHAR,
+	"MaterialVerkaufstext Zeile 3 Position" VARCHAR,
+	"MaterialVerkaufstext Zeile 4 Position" VARCHAR,
+	"MaterialVerkaufstext Zeile 5 Position" VARCHAR,
+	"PositionsNotiz Zeile 1" VARCHAR,
+	"PositionsNotiz Zeile 2" VARCHAR,
+	"PositionsNotiz Zeile 3" VARCHAR,
+	"PositionsNotiz Zeile 4" VARCHAR,
+	"PositionsNotiz Zeile 5" VARCHAR,
+	"Email Kunde" VARCHAR,
+	"Name1 Debitor(Info)" VARCHAR,
+	"Name2 Debitor(Info)" VARCHAR,
+	"Strasse Debitor(Info)" VARCHAR,
+	"PostLz(Info)" VARCHAR,
+	"Ortschaft(Info)" VARCHAR
+)
+;
