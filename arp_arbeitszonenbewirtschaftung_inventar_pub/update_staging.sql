6457b0612d00cb20b6d796ac2a7d2e41dc7d00ab
Lüscher Andrea <bjsvwlue@verw.rootso.org>
2025-10-20 10:56:08 +0200

    [arp_arbeitszonenbewirtschaftung_inventar_pub] Baurecht ergänzt

--- arp_arbeitszonenbewirtschaftung_inventar_pub/update_staging.sql
+++ arp_arbeitszonenbewirtschaftung_inventar_pub/update_staging.sql
@@ -1,4 +1,4 @@
-UPDATE arp_arbeitszonenbewirtschaftung_staging_v1.arbtsznnng_nvntar_arbeitszonenbewirtschaftung_inventar
+UPDATE arp_arbeitszonenbewirtschaftung_staging_v2.arbtsznnng_nvntar_arbeitszonenbewirtschaftung_inventar
 SET
     gestaltungsplanpflicht=(
         CASE 
@@ -8,10 +8,11 @@ SET
         END)   
 ;
 
-UPDATE arp_arbeitszonenbewirtschaftung_staging_v1.arbtsznnng_nvntar_arbeitszonenbewirtschaftung_inventar
+UPDATE arp_arbeitszonenbewirtschaftung_staging_v2.arbtsznnng_nvntar_arbeitszonenbewirtschaftung_inventar
 SET
     nutzungsgrad_txt=n.dispname,
     bebauungsstand_txt=b.dispname,
+    grundstuecksart_txt=g.dispname,
     gestaltungsplanpflicht_txt=(
         CASE 
             WHEN gestaltungsplanpflicht = TRUE 
@@ -19,10 +20,13 @@ SET
             ELSE 'nein' 
         END) 
 FROM
-    arp_arbeitszonenbewirtschaftung_staging_v1.arbtszng_nvntar_arbeitszonenbewirtschaftung_inventar_bebtand AS b,
-    arp_arbeitszonenbewirtschaftung_staging_v1.arbtszng_nvntar_arbeitszonenbewirtschaftung_inventar_nutgrad AS n    
+    arp_arbeitszonenbewirtschaftung_staging_v2.arbtszng_nvntar_arbeitszonenbewirtschaftung_inventar_bebtand AS b,
+    arp_arbeitszonenbewirtschaftung_staging_v2.arbtszng_nvntar_arbeitszonenbewirtschaftung_inventar_nutgrad AS n,
+    arp_arbeitszonenbewirtschaftung_staging_v2.arbtszng_nvntar_arbeitszonenbewirtschaftung_inventar_grusart AS g
 WHERE
     nutzungsgrad=n.ilicode
     AND
-    bebauungsstand=b .ilicode;
+    bebauungsstand=b.ilicode
+    AND
+    grundstuecksart=g.ilicode
 ; 
