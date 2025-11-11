9c0cc4c247eb09844adc33daf431406f68d43c75
Patrick Bur <156295083+bjsvwbur@users.noreply.github.com>
2025-11-11 12:39:03 +0100

    [afu_bootsanbindeplaetze] new gretljob (#2060)
    * [afu_bootsanbindeplaetze] Pub-Job v1
    
    * [afu_bootsanbindeplaetze] first version sap-job
    
    * [afu_bootsanbindeplaetze] Anpassung Gretljobs
    
    * [afu_bootsanbindeplaetze] Test Jenkinsfile
    
    * [afu_bootsanbindeplaetze] Anpassung Gretljob
    
    * [afu_bootsanbindeplaetze] update jobs
    
    * [afu_botsanbindeplaetze] changed export to csv
    
    * [afu_bootsanbindeplaetze] new gretl-job
    
    * [afu_bootsanbindeplaetze] update job
    
    * [afu_bootsanbindeplaetze] update mfk-job
    
    * [afu_bootsanbindeplaetze] update mfk-job
    
    * [afu_bootsanbindeplaetze] deleted test-folder

---
+++ afu_bootsanbindeplaetze_mfk/import_data_mfk.sql
@@ -0,0 +1,44 @@
+DROP TABLE IF EXISTS data_mfk;
+
+--- Create Table for intersecting objects --- 
+CREATE TABLE data_mfk (
+	Stammnummer VARCHAR,
+	Schild INTEGER,
+	Schilderart VARCHAR,
+	Standort VARCHAR,
+	Schiffstyp VARCHAR,
+	Motorisierung VARCHAR,
+	"Anzahl eingelöster Motoren" INTEGER,
+	"Motorenstärke Hauptantrieb [kw]" DOUBLE,
+	"Länge [cm]" INTEGER,
+	"Breite [cm]" INTEGER,
+	Name VARCHAR,
+	Vorname VARCHAR,
+	Adresse VARCHAR,
+	PLZ INTEGER,
+	Ort VARCHAR,
+	Land VARCHAR
+)
+;
+
+INSERT INTO data_mfk
+	SELECT
+		Stammnummer,
+		Schild,
+		Schilderart,
+		Standort,
+		Schiffstyp,
+		Motorisierung,
+		"Anzahl eingelöster Motoren",
+		"Motorenstärke Hauptantrieb [kw]",
+		"Länge [cm]",
+		"Breite [cm]",
+		Name,
+		Vorname,
+		Adresse,
+		PLZ,
+		Ort,
+		Land
+	FROM
+		ST_READ(${mfkPath})
+;
