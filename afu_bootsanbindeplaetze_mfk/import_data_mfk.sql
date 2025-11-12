8b56f853c8c0eae0b524f9227c98f66776a18129
bjsvwbur <patrick.bur@bd.so.ch>
2025-11-12 15:28:38 +0100

    [afu_bootsanbindeplaetze] update job

--- afu_bootsanbindeplaetze_mfk/import_data_mfk.sql
+++ afu_bootsanbindeplaetze_mfk/import_data_mfk.sql
@@ -23,7 +23,22 @@ CREATE TABLE data_mfk (
 
 INSERT INTO data_mfk
 	SELECT
-		*
+		CAST(Stammnummer AS VARCHAR),
+		TRY_CAST(Schild AS INTEGER),
+		CAST(Schilderart AS VARCHAR),
+		CAST(Standort AS VARCHAR),
+		CAST(Schiffstyp AS VARCHAR),
+		CAST(Motorisierung AS VARCHAR),
+		TRY_CAST("Anzahl eingelöster Motoren" AS INTEGER),
+		TRY_CAST("Motorenstärke Hauptantrieb [kw]" AS DOUBLE),
+		TRY_CAST("Länge [cm]" AS INTEGER),
+		TRY_CAST("Breite [cm]" AS INTEGER),
+		CAST("Name" AS VARCHAR),
+		CAST(Vorname AS VARCHAR),
+		CAST(Adresse AS VARCHAR),
+		TRY_CAST(PLZ AS INTEGER),
+		CAST(Ort AS VARCHAR),
+		CAST(Land AS VARCHAR)
 	FROM
 		ST_READ(${mfkPath})
 ;
