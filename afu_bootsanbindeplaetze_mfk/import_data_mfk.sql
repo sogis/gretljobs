a0e18037564131d21b0c4a63f605117a4e6b81d0
bjsvwbur <patrick.bur@bd.so.ch>
2025-11-12 15:52:08 +0100

    [afu_bootsanbindeplaetze] update job

--- afu_bootsanbindeplaetze_mfk/import_data_mfk.sql
+++ afu_bootsanbindeplaetze_mfk/import_data_mfk.sql
@@ -1,8 +1,44 @@
 DROP TABLE IF EXISTS data_mfk;
 
 --- Create Table for intersecting objects --- 
-CREATE TABLE data_mfk AS 
+CREATE TABLE data_mfk (
+	Stammnummer VARCHAR,
+	Schild INTEGER,
+	Schilderart VARCHAR,
+	Standort VARCHAR,
+	Schiffstyp VARCHAR,
+	Motorisierung VARCHAR,
+	"Anzahl eingelöster Motoren" INTEGER,
+	"Motorenstärke Hauptantrieb [kw]" DOUBLE,
+	"Länge [cm]" INTEGER,
+	"Breite [cm]" INTEGER,
+	"Name" VARCHAR,
+	Vorname VARCHAR,
+	Adresse VARCHAR,
+	PLZ INTEGER,
+	Ort VARCHAR,
+	Land VARCHAR
+)
+;
+
+INSERT INTO data_mfk
 	SELECT
-		*
+		CAST(Stammnummer AS VARCHAR),
+		TRY_CAST(Schild AS INTEGER),
+		CAST(Schilderart AS VARCHAR),
+		CAST(Standort AS VARCHAR),
+		CAST(Schiffstyp AS VARCHAR),
+		CAST(Motorisierung AS VARCHAR),
+		TRY_CAST("Anzahl eingelöster Motoren" AS INTEGER),
+		TRY_CAST("Motorenstärke Hauptantrieb [kw]" AS DOUBLE),
+		TRY_CAST("Länge [cm]" AS INTEGER),
+		TRY_CAST("Breite [cm]" AS INTEGER),
+		CAST("Name" AS VARCHAR),
+		CAST(Vorname AS VARCHAR),
+		CAST(Adresse AS VARCHAR),
+		TRY_CAST(PLZ AS INTEGER),
+		CAST(Ort AS VARCHAR),
+		CAST(columns[15] Land AS VARCHAR)
 	FROM
-		ST_READ(${mfkPath}, open_options=['EMPTY_STRING_AS_NULL=YES']);
+		ST_READ(${mfkPath})
+;
