f48a69c7af85e87c6cc34f2272440dc0fc4550d5
bjsvwbur <patrick.bur@bd.so.ch>
2025-11-12 15:45:23 +0100

    [afu_bootsanbindeplaetze] update job

--- afu_bootsanbindeplaetze_mfk/import_data_mfk.sql
+++ afu_bootsanbindeplaetze_mfk/import_data_mfk.sql
@@ -1,31 +1,8 @@
 DROP TABLE IF EXISTS data_mfk;
 
 --- Create Table for intersecting objects --- 
-CREATE TABLE data_mfk AS
+CREATE TABLE data_mfk AS 
 	SELECT
 		*
-	FROM 
-		ST_READ(${mfkPath})
-;
-
-INSERT INTO data_mfk
-	SELECT
-		Stammnummer AS VARCHAR,
-		TRY_CAST(Schild AS INTEGER),
-		Schilderart AS VARCHAR,
-		Standort AS VARCHAR,
-		Schiffstyp AS VARCHAR,
-		Motorisierung AS VARCHAR,
-		TRY_CAST("Anzahl eingelöster Motoren" AS INTEGER),
-		TRY_CAST("Motorenstärke Hauptantrieb [kw]" AS DOUBLE),
-		TRY_CAST("Länge [cm]" AS INTEGER),
-		TRY_CAST("Breite [cm]" AS INTEGER),
-		"Name" AS VARCHAR,
-		Vorname AS VARCHAR,
-		Adresse AS VARCHAR,
-		TRY_CAST(PLZ AS INTEGER),
-		Ort AS VARCHAR,
-		Land AS VARCHAR
 	FROM
-		ST_READ(${mfkPath})
-;
+		ST_READ(${mfkPath}, open_options=['EMPTY_STRING_AS_NULL=YES']);
