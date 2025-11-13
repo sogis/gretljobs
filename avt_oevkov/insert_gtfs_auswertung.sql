7c1ef3c636586e83703ab8d53fa976c0ab4238f5
Sandra Curiger <38523407+bjsvwcur@users.noreply.github.com>
2025-11-13 13:16:48 +0100

    Update insert_gtfs_auswertung.sql

--- avt_oevkov/insert_gtfs_auswertung.sql
+++ avt_oevkov/insert_gtfs_auswertung.sql
@@ -354,9 +354,11 @@ INSERT INTO
     WHERE
         stop_name = 'Olten'
     AND
-        linienname = 'L650 Olten - Brugg AG (S29)'
-    AND
        trip_headsign IN ('Aarau', 'Rupperswil', 'Wildegg', 'Holderbank AG', 'Schinznach Bad', 'Brugg AG')
+    AND
+        substring(linienname from 1 for 4) = 'L650'
+    AND
+        linienname <> 'L650 Olten - Brugg AG (S29)'
     GROUP BY
         stop_name,
         route_id,
