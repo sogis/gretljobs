7263226753e187a7f4e737cc45455836aa2b9809
bjsvwbur <patrick.bur@bd.so.ch>
2025-11-20 07:45:42 +0100

    [awjf_waldplan] update job

--- awjf_waldplan_pub/awjf_update_hiebsatzrelevante_flaechen - Kopie.sql
+++ awjf_waldplan_pub/awjf_update_hiebsatzrelevante_flaechen - Kopie.sql
@@ -12,60 +12,83 @@ WITH
 -------------------------------------------------------------------------
 -------------- Berechnung hiebsatzrelevante Waldflächen -----------------
 -------------------------------------------------------------------------
+produktive_waldflaechen AS (
+	SELECT 
+		t_id,
+		nutzungskategorie,
+		geometrie
+	FROM
+		awjf_waldplan_pub_v2.waldplan_waldnutzung
+	WHERE 
+		nutzungskategorie IN ('Wald_bestockt', 'Nachteilige_Nutzung')
+),
+
+hiebsatzrelevante_waldfunktionsflaechen AS (
+	SELECT
+		funktion,
+		geometrie
+	FROM 
+		awjf_waldplan_pub_v2.waldplan_waldfunktion
+	WHERE
+		funktion IN ('Wirtschaftswald', 'Schutzwald', 'Erholungswald', 'Biodiversitaet', 'Schutzwald_Biodiversitaet')
+	AND
+		(
+			funktion <> 'Biodiversitaet'
+			OR biodiversitaet_objekt NOT IN ('Waldreservat', 'Altholzinsel')
+		)
+),
+
 hiebsatzrelevante_waldflaechen AS (
-    SELECT
-        ST_Intersection(hwff.geometrie, pwf.geometrie) AS geometrie
-    FROM 
-        awjf_waldplan_pub_v2.waldplan_waldfunktion hwff
-    JOIN 
-        awjf_waldplan_pub_v2.waldplan_waldnutzung pwf
-        ON hwff.geometrie && pwf.geometrie
-       AND ST_Intersects(hwff.geometrie, pwf.geometrie)
-    WHERE
-        hwff.funktion IN ('Wirtschaftswald', 'Schutzwald', 'Erholungswald', 'Biodiversitaet', 'Schutzwald_Biodiversitaet')
-        AND (hwff.funktion <> 'Biodiversitaet' OR hwff.biodiversitaet_objekt NOT IN ('Waldreservat','Altholzinsel'))
-        AND pwf.nutzungskategorie IN ('Wald_bestockt', 'Nachteilige_Nutzung')
+	SELECT
+		ST_Intersection(hwff.geometrie,pwf.geometrie) AS geometrie
+	FROM
+		hiebsatzrelevante_waldfunktionsflaechen AS hwff
+	LEFT JOIN produktive_waldflaechen AS pwf 
+		ON ST_Intersects(hwff.geometrie, pwf.geometrie)
 ),
 
--- SQL-Optimierung: Geometrien in Tiles zerlegen
-hrwf_tiled AS (
-    SELECT (ST_Dump(ST_Subdivide(geometrie, 256))).geom AS geometrie
-    FROM hiebsatzrelevante_waldflaechen
-),
-
--- 3) Jetzt Grundstücke schneiden – aber nur gegen die Kacheln
 hiebsatzrelevante_waldflaechen_grundstueck AS (
 	SELECT 
 		gs.egrid,
 		gs.waldflaeche,
-		SUM(ST_Area(ST_Intersection(gs.geometrie, hsw.geometrie))) AS relevant
+		CASE
+			WHEN ROUND(SUM(ST_Area(ST_Intersection(gs.geometrie, hrwf.geometrie)))::NUMERIC) > 0
+				THEN ROUND(SUM(ST_Area(ST_Intersection(gs.geometrie, hrwf.geometrie)))::NUMERIC)
+			ELSE 0
+		END AS relevant,
+		CASE
+			WHEN ROUND(SUM(ST_Area(ST_Intersection(gs.geometrie, hrwf.geometrie)))::NUMERIC) > 0
+				THEN gs.waldflaeche - ROUND(SUM(ST_Area(ST_Intersection(gs.geometrie, hrwf.geometrie)))::NUMERIC)
+			ELSE gs.waldflaeche
+		END AS irrelevant
 	FROM
 		awjf_waldplan_pub_v2.waldplan_waldplan_grundstueck AS gs
-	LEFT JOIN hrwf_tiled hsw
-		ON gs.geometrie && hsw.geometrie
-	   AND ST_Intersects(gs.geometrie, hsw.geometrie)
+	LEFT JOIN hiebsatzrelevante_waldflaechen AS hrwf
+		ON ST_INTERSECTS(gs.geometrie, hrwf.geometrie)
 	GROUP BY 
 		gs.egrid,
 		gs.waldflaeche
-),
+)
+
+SELECT * FROM hiebsatzrelevante_waldflaechen
 
 -------------------------------------------------------------------------
 ---------- Erstellung JSON-Attribute für berechnete Waldflächen ---------
 -------------------------------------------------------------------------
 hiebsatzrelevante_waldflaechen_grundstueck_json AS (
     SELECT
-    	egrid,
+    	hwg.egrid,
         json_agg(
             json_build_object(
-                'relevant', COALESCE(ROUND(relevant::numeric),0),
-                'irrelevant', COALESCE(ROUND(waldflaeche - ROUND(relevant::numeric)), 0),
+                'relevant', hwg.relevant,
+                'irrelevant', hwg.irrelevant,
                 '@type', 'SO_AWJF_Waldplan_Publikation_20250312.Waldplan.flaechen_hiebsatzrelevant'
             )
         ) AS flaechen_hiebsatzrelevant
     FROM 
-        hiebsatzrelevante_waldflaechen_grundstueck
+        hiebsatzrelevante_waldflaechen_grundstueck AS hwg
     GROUP BY 
-        egrid
+        hwg.egrid
 )
 
 -------------------------------------------------------------------------
