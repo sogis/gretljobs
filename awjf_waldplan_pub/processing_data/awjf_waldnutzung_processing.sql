17288cb2759765c62a486ab746b0175395c054b6
Patrick Bur <156295083+bjsvwbur@users.noreply.github.com>
2026-01-15 10:12:20 +0100

    Awjf waldplan (#2123)
    * [awjf_waldplan] update processing sql
    
    * [awjf_waldplan] update processing waldnutzung

--- awjf_waldplan_pub/processing_data/awjf_waldnutzung_processing.sql
+++ awjf_waldplan_pub/processing_data/awjf_waldnutzung_processing.sql
@@ -2,15 +2,9 @@ DELETE FROM awjf_waldplan_pub_v2.waldplan_waldnutzung;
 
 WITH
 
-waldnutzung_union AS (
-	SELECT
-		ST_UNION(geometrie) AS geometrie 
-	FROM 
-		awjf_waldplan_v2.waldplan_waldnutzung
-	WHERE 
-		t_datasetname::int4 = ${bfsnr_param}
-),
-
+-- =========================================================
+-- 1) Waldnutzung unverändert für Publikation übernehmen
+-- =========================================================
 waldnutzung_edit AS (
 	SELECT
 		basket.t_id AS t_basket,
@@ -31,41 +25,75 @@ waldnutzung_edit AS (
 		wnz.t_datasetname::int4 = ${bfsnr_param}
 ),
 
+-- =========================================================
+-- 2) Zusammenfassung Waldfunktionsflaeche
+-- =========================================================
+waldfunktion_union AS (
+	SELECT
+		ST_Union(geometrie) AS geometrie
+	FROM
+		awjf_waldplan_v2.waldplan_waldfunktion
+	WHERE
+		t_datasetname::int4 = ${bfsnr_param}
+),
+
+-- =========================================================
+-- 3) Zusammenfassung Waldnutzungsflaeche
+-- =========================================================
+waldnutzung_union AS (
+	SELECT
+		ST_Union(geometrie) AS geometrie
+	FROM
+		awjf_waldplan_v2.waldplan_waldnutzung
+	WHERE
+		t_datasetname::int4 = ${bfsnr_param}
+),
+
+-- =========================================================
+-- 4) Differenz Waldfunktion und Waldnutzung = "Wald_bestockt"
+-- =========================================================
+wald_bestockt_roh AS (
+	SELECT
+		ST_Difference(
+			(SELECT geometrie FROM waldfunktion_union),
+			(SELECT geometrie FROM waldnutzung_union)
+		) AS geometrie
+),
+
+-- =========================================================
+-- 5) CLIP: Neue Flächen in ursprüngliche Waldfunktionen einpassen
+-- =========================================================
 wald_bestockt AS (
 	SELECT
 		basket.t_id AS t_basket,
 		wf.t_datasetname,
 		'Wald_bestockt' AS nutzungskategorie,
 		'Mit Wald bestockt' AS nutzungskategorie_txt,
-		(ST_Dump(
-			CASE 
-				WHEN wnu.geometrie IS NULL
-					THEN wf.geometrie  -- Keine Überschneidung → ganze Fläche
-				WHEN ST_Intersects(wf.geometrie, wnu.geometrie)
-					THEN ST_Difference(wf.geometrie, wnu.geometrie)  -- Differenz
-				ELSE wf.geometrie  -- Sicherheitsfall
-			END
-		)).geom AS geometrie,
+		dg.geom AS geometrie,
 		NULL AS bemerkung
 	FROM
 		awjf_waldplan_v2.waldplan_waldfunktion AS wf
-    LEFT JOIN waldnutzung_union AS wnu
-    	ON ST_Intersects(wf.geometrie, wnu.geometrie)
 	LEFT JOIN awjf_waldplan_pub_v2.t_ili2db_dataset AS dataset
 		ON wf.t_datasetname = dataset.datasetname
 	LEFT JOIN awjf_waldplan_pub_v2.t_ili2db_basket AS basket
 		ON dataset.t_id = basket.dataset
-    WHERE
-    	ST_IsValid(wf.geometrie)
-    AND 
-    	wf.t_datasetname::int4 = ${bfsnr_param}
-    AND (
-    	wnu.geometrie IS NULL  -- Keine Waldnutzung → ganze Fläche
-   	OR NOT
-   		ST_IsEmpty(ST_Difference(wf.geometrie, wnu.geometrie))  -- Differenz vorhanden
-    )
+	CROSS JOIN LATERAL (
+		SELECT (
+			ST_Dump(ST_Intersection(wf.geometrie,wbr.geometrie
+		))).geom
+		FROM
+			wald_bestockt_roh AS wbr
+	) AS dg
+	WHERE
+		wf.t_datasetname::int4 = ${bfsnr_param}
+	AND
+		ST_Area(dg.geom) > 0
 ),
 
+
+-- =========================================================
+-- 6) Zusammenführen für Publikation
+-- =========================================================
 waldnutzung_pub AS (
 	SELECT
 		t_basket,
@@ -76,7 +104,9 @@ waldnutzung_pub AS (
 		bemerkung
 	FROM
 		waldnutzung_edit
+
 	UNION ALL 
+
 	SELECT
 		t_basket,
 		t_datasetname,
@@ -88,6 +118,9 @@ waldnutzung_pub AS (
 		wald_bestockt
 )
 
+-- =========================================================
+-- 7) Einfügen in Publikationstabelle
+-- =========================================================
 INSERT INTO awjf_waldplan_pub_v2.waldplan_waldnutzung(
 	t_basket,
 	t_datasetname,
@@ -96,7 +129,6 @@ INSERT INTO awjf_waldplan_pub_v2.waldplan_waldnutzung(
 	geometrie,
 	bemerkung
 )
-
 SELECT
 	t_basket,
 	t_datasetname,
@@ -105,4 +137,4 @@ SELECT
 	geometrie,
 	bemerkung
 FROM
-	waldnutzung_pub
+	waldnutzung_pub;
