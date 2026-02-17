296c337e7bccaefeaf74a27b7f72dee1d5f5fc7f
Andreas Schmid <andreas.schmid@bd.so.ch>
2026-02-17 10:46:23 +0100

    [agi_schema_dump] Pass DB name as param to pg_dump command

--- agi_schema_dump/create_dump.sh
+++ agi_schema_dump/create_dump.sh
@@ -2,14 +2,13 @@
 
 PROPERTYFILE=/home/gradle/.gradle/gradle.properties
 
-PGDATABASE=$1
+DBNAME=$1
 DBSCHEMAS=$2 # Example for providing multiple DB schema names: 'schema1 schema2 schema3 ...'
 
 export PGHOST=`echo $ORG_GRADLE_PROJECT_dbUriEdit | awk -F '/' '{ print $3 }'`
-export PGDATABASE
 export PGUSER=`awk -F ' *[=|:] *' '$1 == "dbUserEditDdl" { print $2 }' ${PROPERTYFILE}`
 export PGPASSWORD=`awk -F ' *[=|:] *' '$1 == "dbPwdEditDdl" { print $2 }' ${PROPERTYFILE}`
 
 for DBSCHEMA in $DBSCHEMAS; do
-    pg_dump -Fc -n $DBSCHEMA -f $DBSCHEMA.dump
+    pg_dump -Fc -n $DBSCHEMA -f $DBSCHEMA.dump $DBNAME
 done
