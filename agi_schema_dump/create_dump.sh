8b23e64ba6042110dfc0e2408d2592754e868d44
Andreas Schmid <andreas.schmid@bd.so.ch>
2026-02-17 09:14:36 +0100

    [agi_schema_dump] Allow dumping of multiple DB schemas (#2156)

--- agi_schema_dump/create_dump.sh
+++ agi_schema_dump/create_dump.sh
@@ -4,11 +4,13 @@ PROPERTYFILE=/home/gradle/.gradle/gradle.properties
 
 PGHOST=$1
 PGDATABASE=$2
-DBSCHEMANAME=$3
+DBSCHEMAS=$3 # Example for providing multiple DB schema names: 'schema1 schema2 schema3 ...'
 
 export PGHOST
 export PGDATABASE
 export PGUSER=`awk -F ' *[=|:] *' '$1 == "dbUserEditDdl" { print $2 }' ${PROPERTYFILE}`
 export PGPASSWORD=`awk -F ' *[=|:] *' '$1 == "dbPwdEditDdl" { print $2 }' ${PROPERTYFILE}`
 
-pg_dump -Fc -n ${DBSCHEMANAME} -f schema.dump
+for DBSCHEMA in $DBSCHEMAS; do
+    pg_dump -Fc -n $DBSCHEMA -f $DBSCHEMA.dump
+done
