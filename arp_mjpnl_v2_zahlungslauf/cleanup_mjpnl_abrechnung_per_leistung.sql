4943c83ca6c61279856cb12ec35a723a46236724
signedav <david@opengis.ch>
2026-02-17 10:04:55 +0100

    [arp_mjpnl_v2_zahlungslauf] Prüft bei den SQL scripts, die Leistungen löschen und neu-kalkulieren, ob schon der Status ausbezahlt gesetzt wurde auf diesjährige (nicht-einmalige) Leistungen. Wenn ja, dann sollen die Leistungen nicht angetastet und nicht neu kalkuliert werden.

--- arp_mjpnl_v2_zahlungslauf/cleanup_mjpnl_abrechnung_per_leistung.sql
+++ arp_mjpnl_v2_zahlungslauf/cleanup_mjpnl_abrechnung_per_leistung.sql
@@ -5,3 +5,12 @@ WHERE
     auszahlungsjahr = ${AUSZAHLUNGSJAHR}::integer
     AND (einmalig = FALSE OR einmalig IS NULL)  -- denn diese wurden manuell erfasst (also wir nehmen false und null)
     AND (migriert = FALSE OR migriert IS NULL); -- sind gültig und nicht kalkuliert (Berücksichtigung Migrationsjahr)
+    AND (
+        -- prüft, ob es bereits ausbezahlte nicht-einmalige und nicht-migrierte Leistungen gibt - wenn ja, dann wurde die Auszahlung bereits gemacht und es soll nicht erneut kalkulieren
+        SELECT COUNT(*) 
+        FROM ${DB_Schema_MJPNL}.mjpnl_abrechnung_per_leistung 
+        WHERE auszahlungsjahr = ${AUSZAHLUNGSJAHR}::integer 
+        AND status_abrechnung = 'ausbezahlt' 
+        AND (einmalig = FALSE OR einmalig IS NULL)
+        AND (migriert = FALSE OR migriert IS NULL)
+        ) < 5; --Tolleranz, falls fälschlicherweise nicht-einmalige ausbezahlt wurden
