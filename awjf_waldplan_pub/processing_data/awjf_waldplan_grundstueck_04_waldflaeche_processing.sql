5b200cdf41e6445f308b3158ee3d7fb03a5908bf
bjsvwbur <patrick.bur@bd.so.ch>
2026-02-18 07:48:48 +0100

    [awjf_waldplan] update job

--- awjf_waldplan_pub/processing_data/awjf_waldplan_grundstueck_04_waldflaeche_processing.sql
+++ awjf_waldplan_pub/processing_data/awjf_waldplan_grundstueck_04_waldflaeche_processing.sql
@@ -69,3 +69,47 @@ CREATE INDEX
 	ON waldflaeche_grundstueck_final
 	USING gist (geometrie)
 ;
+
+-- Berechnung Waldfläche pro Grundstück --
+DELETE FROM waldflaeche_berechnet;
+INSERT INTO waldflaeche_berechnet
+	SELECT
+		gs.egrid,
+		gs.flaechenmass AS flaechenmass_grundstueck,
+		ROUND(SUM(ST_Area(ST_Intersection(gs.geometrie, wf.geometrie)))::INTEGER) AS waldflaeche,
+		gs.flaechenmass -ROUND(SUM(ST_Area(ST_Intersection(gs.geometrie, wf.geometrie)))::NUMERIC) AS flaeche_differenz
+	FROM
+		grundstueck AS gs
+	LEFT JOIN waldfunktion AS wf 
+		ON ST_INTERSECTS(gs.geometrie, wf.geometrie)
+		AND gs.t_datasetname = wf.t_datasetname
+	GROUP BY 
+		gs.egrid,
+		gs.flaechenmass
+;
+
+CREATE INDEX 
+	ON waldflaeche_berechnet(egrid)
+;
+
+DELETE FROM waldflaeche_berechnet_plausibilisiert;
+INSERT INTO	waldflaeche_berechnet_plausibilisiert
+	SELECT
+		egrid,
+		CASE
+			WHEN flaeche_differenz BETWEEN -1 AND 1 
+				THEN flaechenmass_grundstueck 
+			ELSE waldflaeche
+		END AS flaeche,
+		CASE
+			WHEN flaeche_differenz BETWEEN -1 AND 1 
+				THEN TRUE
+			ELSE FALSE
+		END AS angepasst
+	FROM 
+		waldflaeche_berechnet
+;
+
+CREATE INDEX 
+	ON waldflaeche_berechnet_plausibilisiert (egrid)
+;
