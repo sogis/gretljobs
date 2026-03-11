c47ab34c511a61a4b4e4eca2e3178c85859ab549
Lüscher Andrea <bjsvwlue@verw.rootso.org>
2026-03-11 07:23:08 +0100

    [alw_landwirtschaft_tierhaltung_pub] Anpassung ans neue Datenmodell V2

--- alw_landwirtschaft_tierhaltung_pub/ln_soemmerung.sql
+++ alw_landwirtschaft_tierhaltung_pub/ln_soemmerung.sql
@@ -1,9 +1,11 @@
     -- Noch keine Geometrie optimiert
     SELECT
+        bb.t_id,
         bb.t_basket,
-        bb.t_datasetname,   
-        bb.geometrie AS geometrie,
-        bb.bezugjahr
+        bb.t_datasetname,           
+        bb.bezugjahr,
+        bb.mandant AS kanton,
+        bb.geometrie AS geometrie
     FROM
         alw_landwirtschaft_tierhaltung_v1.gelan_bodnbdckung_bodenbedeckung AS bb    
         JOIN agi_hoheitsgrenzen_pub.hoheitsgrenzen_kantonsgrenze AS ke ON ST_Within(bb.geometrie, ke.geometrie) --nur Objekte im Kanton SO
