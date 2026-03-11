a901cee89e014d19287f728a38c46da7fcadfcc4
Sandra Curiger <38523407+bjsvwcur@users.noreply.github.com>
2026-03-11 11:45:08 +0100

    Update isos_objekte.sql

--- arp_isos_inventar_pub/isos_objekte.sql
+++ arp_isos_inventar_pub/isos_objekte.sql
@@ -8,9 +8,11 @@ SELECT
     architeckturhistorische_qualitaet, 
     bedeutung, 
     erhaltungsziel, 
+    erhaltungsziel as erhaltungsziel_txt,
     hinweis, 
     stoerend, 
     bild_nummer, 
     link_objektblatt
 
 FROM arp_isos_inventar_v2.isos_inventar;
+
