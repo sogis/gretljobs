be4dcc39dedbbbdebba552690a4d3ba6201f9434
bjsvwcur <sandra.curiger@bd.so.ch>
2026-03-12 09:39:40 +0100

    [afu_gewaesserschutz_zonen_areale_pub] add MGDM

---
+++ afu_gewaesserschutz_zonen_areale_pub/gwszonen_gwsareal_mgdm.sql
@@ -0,0 +1,9 @@
+SELECT 
+    identifikator, 
+    geometrie, 
+    bemerkungen, 
+    bemerkungen_lang, 
+    typ, 
+    istaltrechtlich, 
+    astatus
+FROM afu_gewaesserschutz_zonen_areale_v1.gwszonen_gwsareal;
---
+++ afu_gewaesserschutz_zonen_areale_pub/gwszonen_gwszone_mgdm.sql
@@ -0,0 +1,11 @@
+SELECT 
+    identifikator, 
+    geometrie, 
+    bemerkungen, 
+    bemerkungen_lang, 
+    typ, 
+    kantonaletypbezeichnung, 
+    kantonaletypbezeichnung_lang, 
+    istaltrechtlich, 
+    astatus
+FROM afu_gewaesserschutz_zonen_areale_v1.gwszonen_gwszone;
