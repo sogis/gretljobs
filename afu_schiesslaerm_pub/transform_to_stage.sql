3879bf3054518821a6573acb4e43815893c643d0
Oliver Jeker <24891004+ojeker@users.noreply.github.com>
2026-03-16 16:51:37 +0100

    afu_schiesslaerm: created (#2192)
    * raw import finished
    
    * finished pubjob
    
    * finish transfer to pub
    
    * rem dummy sql
    
    * upd premiss
    
    * sep logging
    
    * isZipFile Method
    
    * added validation on "id"
    
    * added publisher task
    
    * corr
    
    * disable publisher

---
+++ afu_schiesslaerm_pub/transform_to_stage.sql
@@ -0,0 +1,68 @@
+WITH classified AS (
+    SELECT
+        CASE
+            WHEN decibel < 55 THEN 'eins'
+            WHEN decibel >= 55 AND decibel < 60 THEN 'zwei'
+            WHEN decibel >= 60 AND decibel < 65 THEN 'drei'
+            WHEN decibel >= 65 THEN 'vier'
+        END AS db_class,
+        pixel_quadrat
+    FROM 
+        afu_schiesslaerm_v1.schiesslaerm_import
+	WHERE
+		decibel >= 50
+),
+dissolved AS (
+    SELECT
+        db_class,
+        ST_UnaryUnion(
+            ST_Collect(pixel_quadrat)
+        ) AS geom
+    FROM 
+        classified
+    GROUP BY 
+        db_class
+),
+anlage AS (
+    SELECT 
+        a.id,
+        bezeichnung
+    FROM 
+        afu_schiesslaerm_v1.anlage_attribute a
+    JOIN
+        afu_schiesslaerm_v1.anlage_kandidat k ON a.id = k.id
+),
+joined AS (
+    SELECT
+        db_class,
+        id,
+        bezeichnung,
+        geom
+    FROM 
+        dissolved, anlage
+),
+single_parts AS (
+    SELECT
+        db_class,
+        id,
+        bezeichnung,
+        (ST_Dump(geom)).geom::geometry(Polygon, 2056) AS geom
+    FROM 
+        joined
+)
+
+INSERT INTO 
+    afu_schiesslaerm_stage_v1.schiesslaerm (
+        empfindlichkeitsstufe,
+        anlage_id,
+        anlage_bezeichnung,
+        flaeche
+    )
+SELECT
+    db_class,
+    id,
+    bezeichnung,
+    geom
+FROM 
+    single_parts
+;
