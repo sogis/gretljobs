b89a431ad34341fb82aeb0c8a6069fa1da38c9a1
Lüscher Andrea <bjsvwlue@verw.rootso.org>
2026-04-01 08:34:42 +0200

    [alw_landwirtschaft_tierhaltung_pub] Anpassung SuchIndex

--- alw_landwirtschaft_tierhaltung_pub/ln_soemmerung.sql
+++ alw_landwirtschaft_tierhaltung_pub/ln_soemmerung.sql
@@ -1,4 +1,10 @@
     -- Noch keine Geometrie optimiert
+    WITH kanton AS (
+    SELECT 
+        ST_Buffer(geometrie, 1.0) AS geometrie -- für leicht überlappende Flächen
+        FROM agi_hoheitsgrenzen_pub.hoheitsgrenzen_kantonsgrenze
+)
+    
     SELECT
         bb.t_id,
         bb.t_basket,
@@ -8,7 +14,7 @@
         bb.geometrie AS geometrie
     FROM
         alw_landwirtschaft_tierhaltung_v1.gelan_bodnbdckung_bodenbedeckung AS bb    
-        JOIN agi_hoheitsgrenzen_pub.hoheitsgrenzen_kantonsgrenze AS ke ON ST_Within(bb.geometrie, ke.geometrie) --nur Objekte im Kanton SO
+        JOIN kanton AS ke ON ST_Within(bb.geometrie, ke.geometrie) --nur Objekte im Kanton SO
     WHERE 
         bb.bodenbedeckung_text = 'LAND' 
             AND
