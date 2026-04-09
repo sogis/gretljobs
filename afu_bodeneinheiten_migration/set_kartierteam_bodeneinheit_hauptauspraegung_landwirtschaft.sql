5022d17dd7f093f2920ab01ba3a3790c3cddbd26
Martin Schweizer <martin.schweizer@bd.so.ch>
2026-04-09 12:58:46 +0200

    [afu_bodeneinheiten_migration]Bugfix

---
+++ afu_bodeneinheiten_migration/set_kartierteam_bodeneinheit_hauptauspraegung_landwirtschaft.sql
@@ -0,0 +1,29 @@
+UPDATE 
+    afu_bodeneinheiten_v1.bodeneinheithauptauspraegung_landwirtschaft 
+SET 
+    kartierer_r = kartierteam_id 
+FROM (
+  SELECT distinct 
+      k.t_id as kartierteam_id, 
+      imp.gemnr, 
+      imp.objnr 
+  FROM 
+      afu_bodeneinheiten_v1.bodeneinheithauptauspraegung_landwirtschaft 
+  LEFT JOIN 
+      afu_bodeneinheiten_v1.import_table imp 
+      ON 
+      gemeinde_nr = imp.gemnr 
+      and 
+      bodeneinheit_nummer = imp.objnr
+  LEFT JOIN 
+      afu_bodeneinheiten_v1.kartierteam k 
+      ON 
+      imp.kartierteam = k.teamkuerzel
+    ) 
+WHERE 
+    gemeinde_nr = gemnr 
+    and 
+    bodeneinheit_nummer = objnr 
+    and 
+    import_oid > 0
+;
