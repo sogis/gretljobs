c8f593f206e62e3b6e924ba6ea9ecf49c218b5f4
Sandra Curiger <38523407+bjsvwcur@users.noreply.github.com>
2026-05-18 11:00:11 +0200

    Update gwszonen_gwsareal_gwszone_mgdm.sql

--- afu_gewaesserschutz_zonen_areale_pub/gwszonen_gwsareal_gwszone_mgdm.sql
+++ afu_gewaesserschutz_zonen_areale_pub/gwszonen_gwsareal_gwszone_mgdm.sql
@@ -1,19 +1,3 @@
-INSERT INTO afu_gewaesserschutz_zonen_areale_mgdm_v1.multilingualuri (
-            t_id ,
-            t_seq,
-            gwszonen_dokument_textimweb,
-            transfermetadaten_amt_amtimweb,
-            transfrmtdtstllngsdnst_verweiswms     
-)
-SELECT
-    nextval('afu_gewaesserschutz_zonen_areale_mgdm_v1.t_ili2db_seq'::regclass) AS t_id,
-    0 AS t_seq,
-    dokumente_dokument.t_id AS gwszonen_dokument_textimweb,
-    0 AS transfermetadaten_amt_amtimweb,
-    0 AS transfrmtdtstllngsdnst_verweiswms
-FROM afu_gewaesserschutz_zonen_areale_v1.gwszonen_dokument AS dokumente_dokument
-;
-
 INSERT INTO afu_gewaesserschutz_zonen_areale_mgdm_v1.gwszonen_gwszone  (
     t_id,
     identifikator, 
