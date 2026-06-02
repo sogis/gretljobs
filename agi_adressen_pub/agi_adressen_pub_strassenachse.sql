79b2cced8a7dc6b1e1689486e01906e0330b709f
Sandra Curiger <38523407+bjsvwcur@users.noreply.github.com>
2026-06-02 10:34:15 +0200

    Update agi_adressen_pub_strassenachse.sql

--- agi_adressen_pub/agi_adressen_pub_strassenachse.sql
+++ agi_adressen_pub/agi_adressen_pub_strassenachse.sql
@@ -5,7 +5,7 @@ SELECT
     ms.bfs_nr AS gem_bfs,
     ms.strassenname,
     st_linemerge(st_union(ms.geometrie)) AS geometrie,
-    '@' AS ESID
+    '0' AS ESID
 FROM agi_mopublic_pub.mopublic_strassenachse ms
      JOIN agi_plz_ortschaften_pub.plzortschaften_postleitzahl plz ON st_coveredby(ms.geometrie, plz.geometrie)
      JOIN agi_plz_ortschaften_pub.plzortschaften_ortschaft ort ON st_coveredby(ms.geometrie, ort.geometrie)
