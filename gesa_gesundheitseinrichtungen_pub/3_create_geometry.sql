e2c651a0bf3724e35f15446868098bdab238e091
Christa Rohrbach <thunwal@gmail.com>
2026-06-09 09:29:27 +0200

    [GESA_Gesundheitseinrichtungen] kosmetische Anpassung

--- gesa_gesundheitseinrichtungen_pub/3_create_geometry.sql
+++ gesa_gesundheitseinrichtungen_pub/3_create_geometry.sql
@@ -1,2 +1,2 @@
 UPDATE gesa_gesundheitseinrichtungen_staging_v1.gesundheitseinrichtung
-SET geometrie = ST_SetSRID(ST_MakePoint(x_koordinate, y_koordinate), 2056);
+    SET geometrie = ST_SetSRID(ST_MakePoint(x_koordinate, y_koordinate), 2056);
