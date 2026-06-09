16f664b6e172d84c2ec945b13cc9d3b63de9f0c9
Rudin Daniel <bjsvwrud@verw.rootso.org>
2026-06-09 12:48:30 +0200

    [agi_av_gb_abgleich_pub] add agi_av_gb_abgleich_import_egrid_kontrolle.sql

---
+++ agi_av_gb_abgleich_pub/agi_av_gb_abgleich_import_egrid_kontrolle.sql
@@ -0,0 +1,105 @@
+WITH 
+
+av_grundstuecke AS (
+        SELECT 
+            g.egris_egrid,
+            g.nbident,
+            l.geometrie,
+            g.nummer
+   
+        FROM 
+            agi_dm01avso24.liegenschaften_grundstueck AS g
+            LEFT JOIN 
+                agi_dm01avso24.liegenschaften_liegenschaft AS l
+                    ON l.liegenschaft_von = g.t_id
+        WHERE 
+            g.art = 'Liegenschaft'
+                AND 
+                     g.egris_egrid IS NOT NULL
+                    
+        UNION
+        
+        SELECT 
+            g.egris_egrid,
+            g.nbident,
+            sdr.geometrie,
+            g.nummer
+   
+        FROM 
+            agi_dm01avso24.liegenschaften_grundstueck AS g
+            LEFT JOIN 
+                agi_dm01avso24.liegenschaften_selbstrecht AS sdr
+                    ON sdr.selbstrecht_von = g.t_id
+        WHERE 
+            g.art != 'Liegenschaft'
+              AND 
+                     g.egris_egrid IS NOT NULL
+),
+
+gb_grundstuecke AS (
+        SELECT
+            gb.egrid,
+            gb.grundstueck_nr
+        FROM
+            agi_av_gb_abgleich_import.gb_daten AS gb
+),
+
+-- EGRID in AV und GB vorhanden
+identisch AS (
+    SELECT
+        av.egris_egrid,    
+        av.nummer
+FROM 
+    av_grundstuecke AS av,
+    gb_grundstuecke AS gb
+WHERE 
+    av.egris_egrid = gb.egrid 
+        AND
+           av.nummer=gb.grundstueck_nr
+),
+
+-- EGRID nicht identisch mit GB
+nur_gb AS (
+    SELECT 
+        egris_egrid,
+        nummer
+    FROM
+        av_grundstuecke
+    EXCEPT
+
+    SELECT 
+        egris_egrid,
+        nummer
+    FROM
+        identisch)
+
+-- Grundstücke mit falschem Egrid
+
+SELECT 
+    ST_CurveToLine(geometrie, 0.002, 1, 1) AS geometrie,
+    NULL AS av_gem_bfs,
+    NULL AS av_nbident,
+    NULL AS av_gemeinde,
+    NULL AS av_nummer,
+    NULL AS av_art,
+    NULL AS av_art_txt,
+    NULL AS av_flaeche,
+    NULL AS av_lieferdatum,
+    NULL AS av_mutation_id,
+    NULL AS av_mut_beschreibung,
+    NULL AS gb_gem_bfs,
+    NULL AS gb_kreis_nr,
+    NULL AS gb_gemeinde,
+    av.nummer as gb_nummer,
+    'SelbstaendigesDauerndesRecht' AS gb_art,
+    NULL AS gb_flaeche,
+    NULL AS gb_fuehrungsart,
+    av.nbident as gb_nbident, 
+    NULL AS flaechen_differenz, 
+    5::integer AS fehlerart --'EGRID stimmen nicht überein'
+
+FROM    
+    nur_gb AS gb
+    LEFT JOIN 
+        av_grundstuecke AS av ON av.egris_egrid = gb.egris_egrid AND  av.nummer=gb.nummer
+;
