347e75a0f1283cd4ddf8391ebe99c6843b299446
Martin Schweizer <martin.schweizer@bd.so.ch>
2026-07-13 14:28:15 +0200

    [afu_bodeneinheiten_pub]Bugfix

--- afu_bodeneinheiten_pub/transfer_landwirtschaft.sql
+++ afu_bodeneinheiten_pub/transfer_landwirtschaft.sql
@@ -118,7 +118,8 @@ INSERT INTO
     alte_daten_Vorhanden, 
     alte_daten_vorhanden_txt, 
     aufarbeitung,
-    geometrie
+    geometrie, 
+    gewichtung
 )
 SELECT 
     CASE 
@@ -449,11 +450,12 @@ SELECT
         WHEN 
             src.alte_daten_vorhanden = 'nein' 
         THEN 
-            NULL
+            'Nein'
         ELSE
-            'Diese Kartierung stellt eine Aufarbeitung vorhandener Daten dar. Die genaue Methodik kann unter LINK eingesehen werden.' 
+            'Diese Kartierung stellt eine Aufarbeitung vorhandener Daten dar. Die genaue Methodik kann im AfU (Abteilung Boden) angefragt werden.' 
     END AS aufarbeitung,            
-    src.geometrie
+    src.geometrie, 
+    src.gewichtung
 FROM 
     afu_bodeneinheiten_v1.bodeneinheithauptauspraegung_landwirtschaft src
 LEFT JOIN 
@@ -536,6 +538,8 @@ LEFT JOIN
     afu_bodeneinheiten_v1.t_ili2db_dataset dataset 
     ON 
     los.t_datasetname = dataset.datasetname 
+LEFT JOIN 
+    
 WHERE 
     los.publizieren IS TRUE
 ;
