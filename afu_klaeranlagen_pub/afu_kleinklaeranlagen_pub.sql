c2d247f64601ca473cdf44a6fc0b4dd98f1608dd
Patrick Bur <156295083+bjsvwbur@users.noreply.github.com>
2026-08-03 14:13:41 +0200

    Afu klaeranlagen (#2277)
    * [afu_klaeranlagen] new job
    
    * [afu_klaeranlagen] update gretljob

---
+++ afu_klaeranlagen_pub/afu_kleinklaeranlagen_pub.sql
@@ -0,0 +1,79 @@
+SELECT
+	anlagename AS nachname_anlagebesitzer,
+	anlagevorname AS vorname_anlagebesitzer,
+	betreuername AS name_betreuer,
+	betreuervorname AS vorname_betreuer,
+	gewaesser AS vorfluter,
+	CASE
+		WHEN anlagetyp = 'MBR'
+			THEN 'Membranbioreaktor'
+		WHEN anlagetyp = 'ONE SBR'
+			THEN 'Einkammer SBR'
+		WHEN anlagetyp = 'SBR'
+			THEN 'Mehrkammer SBR'
+		WHEN anlagetyp = 'Tropfkörper'
+			THEN 'Tropfkörper berieselt'
+		WHEN anlagetyp = 'UTB HKA'
+			THEN 'System UTW'
+		ELSE anlagetyp
+	END AS anlagentyp,
+	groesseeg AS groesse_eg,
+	angeinwohner AS anz_angeschlossene_einwohner,
+	CASE
+		WHEN aufgehoben = 'WAHR'
+			THEN TRUE
+		WHEN aufgehoben = 'FALSCH'
+			THEN FALSE
+		ELSE NULL 
+	END AS aufgehoben,
+	CASE
+		WHEN aufgehoben = 'WAHR'
+			THEN 'Ja'
+		WHEN aufgehoben = 'FALSCH'
+			THEN 'Nein'
+		ELSE NULL
+	END AS aufgehoben_txt,
+	CASE
+		WHEN gewaesser_drainagen = 'WAHR'
+			THEN TRUE
+		WHEN gewaesser_drainagen = 'FALSCH'
+			THEN FALSE
+		ELSE null
+	END AS ableitung_drainage,
+	CASE
+		WHEN gewaesser_drainagen = 'WAHR'
+			THEN 'Ja'
+		WHEN gewaesser_drainagen = 'FALSCH'
+			THEN 'Nein'
+		ELSE NULL
+	END AS ableitung_drainage_txt,
+	CASE
+		WHEN gewaesser_versickerung = 'WAHR'
+			THEN TRUE
+		WHEN gewaesser_versickerung = 'FALSCH'
+			THEN FALSE
+		ELSE null
+	END AS versicherkung_gewaesser,
+	CASE
+		WHEN gewaesser_versickerung = 'WAHR'
+			THEN TRUE
+		WHEN gewaesser_versickerung = 'FALSCH'
+			THEN FALSE
+		ELSE null
+	END AS versicherkung_gewaesser_txt,
+	CASE
+		WHEN gewaesser IS NOT NULL
+			THEN TRUE
+		ELSE FALSE 
+	END AS ableitung_gewaesser,
+	CASE
+		WHEN gewaesser IS NOT NULL
+			THEN 'Ja'
+		ELSE 'Nein' 
+	END AS ableitung_gewaesser_txt,
+	anlagenummer AS nummer,
+	anlagestandort AS gemeindename,
+	ST_SetSRID(ST_MakePoint(x_koordinate, y_koordinate), 2056) AS geometrie
+FROM
+	afu_klaeranlagen_v1.klaeranlagen_kleinklaeranlage_import
+;
