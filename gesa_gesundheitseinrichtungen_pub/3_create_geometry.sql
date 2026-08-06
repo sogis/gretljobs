adba922b091b0d87fa23fb9681ad410ae1bce0c9
Christa Rohrbach <thunwal@gmail.com>
2026-08-06 16:48:15 +0200

    [GESA_Gesundheitseinrichtungen] Anpassungen an Modellanpassung

--- gesa_gesundheitseinrichtungen_pub/3_create_geometry.sql
+++
@@ -1,2 +0,0 @@
-UPDATE gesa_gesundheitseinrichtungen_staging_v1.gesundheitseinrichtung
-    SET geometrie = ST_SetSRID(ST_MakePoint(x_koordinate, y_koordinate), 2056);
--- gesa_gesundheitseinrichtungen_pub/4_select_gesundheitseinrichtungen.sql
+++ gesa_gesundheitseinrichtungen_pub/4_select_gesundheitseinrichtungen.sql
@@ -3,16 +3,20 @@ SELECT
     x_koordinate,
     y_koordinate,
     aname,
-    telefon,
+    typ,
+    telefon_allgemein,
+    email_allgemein,
+    email_leitung,
     adresse_ortschaft,
     adresse_plz,
     adresse_strassenname,
     adresse_hausnummer,
+    adresse_adresszusatz,
     kognitiv_schwer_beeintraechtigt,
     kognitiv_und_physisch_schwer_beeintraechtigt,
     physisch_schwer_beeintraechtigt,
     nicht_schwer_beeintraechtigt,
-    typ,
+    total,
     kommentare
 FROM 
     gesa_gesundheitseinrichtungen_staging_v1.gesundheitseinrichtung;
