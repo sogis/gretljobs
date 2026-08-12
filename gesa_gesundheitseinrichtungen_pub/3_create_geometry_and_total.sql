1228cf4803593c1420af5eb4f6c22a8f224b1899
Christa Rohrbach <thunwal@gmail.com>
2026-08-12 07:41:17 +0200

    [GESA_Gesundheitseinrichtungen] Total nur überschreiben wenn alle Summanden nicht NULL

--- gesa_gesundheitseinrichtungen_pub/3_create_geometry_and_total.sql
+++ gesa_gesundheitseinrichtungen_pub/3_create_geometry_and_total.sql
@@ -1,8 +1,11 @@
 UPDATE gesa_gesundheitseinrichtungen_staging_v1.gesundheitseinrichtung
     SET
         geometrie = ST_SetSRID(ST_MakePoint(x_koordinate, y_koordinate), 2056),
-        total = COALESCE(kognitiv_schwer_beeintraechtigt, 0)
-            + COALESCE(kognitiv_und_physisch_schwer_beeintraechtigt, 0)
-            + COALESCE(physisch_schwer_beeintraechtigt, 0)
-            + COALESCE(nicht_schwer_beeintraechtigt, 0)
+        total = COALESCE(
+                kognitiv_schwer_beeintraechtigt
+              + kognitiv_und_physisch_schwer_beeintraechtigt
+              + physisch_schwer_beeintraechtigt
+              + nicht_schwer_beeintraechtigt,
+                total
+        )
 ;
