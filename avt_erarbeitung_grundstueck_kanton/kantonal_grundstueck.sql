dc86584d6a6673f13b5ac9fb891872f6f07b1600
Rudin Daniel <bjsvwrud@verw.rootso.org>
2026-09-02 11:40:57 +0200

    [avt_erarbeitung_grundstueck_kanton] SQL anpassen, alle 90000er

--- avt_erarbeitung_grundstueck_kanton/kantonal_grundstueck.sql
+++ avt_erarbeitung_grundstueck_kanton/kantonal_grundstueck.sql
@@ -10,7 +10,7 @@ WITH grundstuecke AS (
         art_txt,
         geometrie
     FROM agi_mopublic_pub.mopublic_grundstueck
-    WHERE nummer::integer > 90000
+    WHERE nummer::integer >= 90000
 
     UNION ALL
 
@@ -25,12 +25,12 @@ WITH grundstuecke AS (
         art_txt,
         geometrie
     FROM agi_mopublic_pub.mopublic_grundstueck_proj
-    WHERE nummer::integer > 90000
+    WHERE nummer::integer >= 90000
 ),
 
 gewaesser AS (
     SELECT DISTINCT
-        g.*
+        g.egrid
     FROM grundstuecke g
     JOIN agi_mopublic_pub.mopublic_bodenbedeckung bb
       ON ST_Intersects(g.geometrie, bb.geometrie)
@@ -40,65 +40,50 @@ gewaesser AS (
 
 kantonsstrassen AS (
     SELECT DISTINCT
-        g.*
+        g.egrid
     FROM grundstuecke g
     JOIN avt_kantonsstrassen_pub_v1.achse a
-        ON g.geometrie && a.geometrie
-            AND ST_Intersects(g.geometrie, a.geometrie)
+      ON g.geometrie && a.geometrie
+     AND ST_Intersects(g.geometrie, a.geometrie)
     WHERE ST_Length(
-          ST_Intersection(g.geometrie, a.geometrie)
+              ST_Intersection(g.geometrie, a.geometrie)
           ) >= 15
-    AND ST_Length(
-          ST_Intersection(g.geometrie, a.geometrie)
+      AND ST_Length(
+              ST_Intersection(g.geometrie, a.geometrie)
           ) / ST_Perimeter(g.geometrie) >= 0.16
 )
-SELECT DISTINCT
-    egrid,
-    bfs_nr,
-    gemeinde,
-    grundbuch,
-    nummer,
-    nbident,
-    flaechenmass,
-    art_txt,
-    eigentum_kanton,
-    anpassung,
-    kontrolliert,
-    geometrie
-FROM (
-    SELECT
-        egrid,
-        bfs_nr,
-        gemeinde,
-        grundbuch,
-        nummer,
-        nbident,
-        flaechenmass,
-        art_txt,
-        TRUE  AS eigentum_kanton,
-        FALSE AS anpassung,
-        FALSE AS kontrolliert,
-        geometrie
-    FROM gewaesser
 
-    UNION
+SELECT
+    g.egrid,
+    g.bfs_nr,
+    g.gemeinde,
+    g.grundbuch,
+    g.nummer,
+    g.nbident,
+    g.flaechenmass,
+    g.art_txt,
 
-    SELECT
-        egrid,
-        bfs_nr,
-        gemeinde,
-        grundbuch,
-        nummer,
-        nbident,
-        flaechenmass,
-        art_txt,
-        TRUE  AS eigentum_kanton,
-        FALSE AS anpassung,
-        FALSE AS kontrolliert,
-        geometrie
-    FROM kantonsstrassen
-) t
+    (
+        EXISTS (
+            SELECT 1
+            FROM gewaesser gw
+            WHERE gw.egrid = g.egrid
+        )
+        OR
+        EXISTS (
+            SELECT 1
+            FROM kantonsstrassen ks
+            WHERE ks.egrid = g.egrid
+        )
+    ) AS eigentum_kanton,
+
+    FALSE AS anpassung,
+    FALSE AS kontrolliert,
+
+    g.geometrie
+
+FROM grundstuecke g
 ORDER BY
-    bfs_nr,
-    grundbuch,
-    nummer;
+    g.bfs_nr,
+    g.grundbuch,
+    g.nummer;
